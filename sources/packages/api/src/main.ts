dacda2144d7e95052e341dd31531c0b61c3d2752
fwpushan <pushan@freshworks.io>
2020-12-16 02:20:15 +0530

    1. Create API build/deployment and 2. Update API route

--- sources/packages/api/src/main.ts
+++ sources/packages/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api')
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is listing on port ${port}`);
