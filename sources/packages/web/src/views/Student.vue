9c65a170637ecb6d5ac19d1ee5211b26fabf7585
Andrew Signori <andrew.signori@cgi.com>
2021-01-26 18:12:45 -0800

    SIN Number and Contact Information fields add to Student Profile.

--- sources/packages/web/src/views/Student.vue
+++ sources/packages/web/src/views/Student.vue
@@ -5,8 +5,7 @@
     </template>
     <template #content>
       <!--The information from Services Card includes First Name, Last Name, Middle Name, Date of Birth, 
-     verified email, and Gender as read-only-->
-
+      verified email, and Gender as read-only-->
       <div class="p-fluid p-formgrid p-grid">
         <div class="p-field p-col">
           <label for="firstName">Given Names</label>
@@ -44,20 +43,88 @@
           <label for="gender">Gender</label>
           <InputText id="gender" v-model="studentProfile.gender" readonly />
         </div>
+        <div class="p-field p-col-6">
+          <label for="sinNumber">Social Insurance number</label>
+          <InputText id="sinNumber" v-model="contactInfo.sinNumber" />
+        </div>
       </div>
+      <h3 class="p-mb-5">Contact Information</h3>
+      <div class="p-fluid p-formgrid p-grid">
+        <div class="p-grid p-col-12">
+          <div class="p-field p-col-6">
+            <label for="phone">Phone Number</label>
+            <InputText id="phone" v-model="contactInfo.phone" />
+          </div>
+        </div>
+        <div class="p-field p-col-6">
+          <label for="addressLine1">Address Line 1</label>
+          <InputText id="addressLine1" v-model="contactInfo.addressLine1" />
+        </div>
+        <div class="p-field p-col-6">
+          <label for="addressLine2">Address Line 2</label>
+          <InputText id="addressLine2" v-model="contactInfo.addressLine2" />
+        </div>
+        <div class="p-field p-col-6">
+          <label for="city">City</label>
+          <InputText id="city" v-model="contactInfo.city" />
+        </div>
+        <div class="p-field p-col-6">
+          <label for="provinceState">Province/State</label>
+          <InputText id="provinceState" v-model="contactInfo.provinceState" />
+        </div>
+        <div class="p-field p-col-6">
+          <label for="country">Country</label>
+          <InputText id="country" v-model="contactInfo.country" />
+        </div>
+        <div class="p-field p-col-6">
+          <label for="postalCode">Postal Code</label>
+          <InputText id="postalCode" v-model="contactInfo.postalCode" />
+        </div>
+      </div>
+      
+    </template>
+    <template #footer>
+      <Button
+        label="Save Profile"
+        icon="pi pi-save"
+        @click="save"
+      ></Button>
     </template>
   </Card>
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { computed, reactive } from "vue";
 import { useStore } from "vuex";
+
+class ProfileState{
+  phone = "";
+  sinNumber = "";
+  addressLine1 = "";
+  addressLine2 = "";
+  city = "";
+  provinceState = "";
+  country = "";
+  postalCode = "";
+}
 
 export default {
   setup() {
     const store = useStore();
     const studentProfile = computed(() => store.state.student.profile);
-    return { studentProfile };
+    const contactInfo = reactive(new ProfileState());
+
+    const save = () => {
+      // To be removed once the API call is ready.
+      alert(JSON.stringify(contactInfo));
+    }
+
+    return { studentProfile, contactInfo, save };
   }
 };
 </script>
+<style lang="scss" scoped>
+input:read-only {
+  background-color: #ddd;
+}
+</style>
