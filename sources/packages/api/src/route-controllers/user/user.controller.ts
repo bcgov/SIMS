6235e01587141ec28beaf9f41ceab9ed9ab054c4
sandeepraj <sandeepraj.tandon@gov.bc.ca>
2021-02-04 17:27:03 -0800

    intial commit

--- sources/packages/api/src/route-controllers/user/user.controller.ts
+++ sources/packages/api/src/route-controllers/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from "@nestjs/common";
+import { Controller, Get, Patch, Req } from "@nestjs/common";
 import { Request } from "express";
 import { AuthService, UserService } from "../../services";
 
@@ -14,7 +14,25 @@ export class UserController {
     const userInfo = this.authService.parseAuthorizationHeader(
       request.headers.authorization,
     );
+    console.log('User Info in controller')
+    console.dir(userInfo) 
+    const userInSABC  = this.service.getUser(userInfo.userName);
+    console.log('returning checkuser controller')
+    console.dir(userInSABC)
+    //TO:DO Give a 404 if the user doesnt exit?
+    return userInSABC
+  }
 
-    return this.service.getUser(userInfo.userName);
+  //@Res response: Response
+  @Patch("/sync-user")
+  synchronizeUserInfo(@Req() request: Request) {
+    const userInfoBCServiceCard = this.authService.parseAuthorizationHeader(
+      request.headers.authorization
+    )
+    console.log(`Controller endpoint to sync user info called`)    
+    const syncedUser = this.service.synchronizeUserInfo(userInfoBCServiceCard);
+    console.log('Returning syncedUser from Controller')
+    console.dir(syncedUser);
+    return syncedUser
   }
 }
--- sources/packages/api/src/services/user/user.service.ts
+++ sources/packages/api/src/services/user/user.service.ts
@@ -2,9 +2,11 @@ import { Injectable, Inject } from "@nestjs/common";
 import { Connection } from "typeorm";
 import { DataModelService } from "../../database/data.model.service";
 import { User } from "../../database/entities";
+import { UserInfo } from "../../types";
 
 @Injectable()
 export class UserService extends DataModelService<User> {
+
   constructor(@Inject("Connection") connection: Connection) {
     super(connection.getRepository(User));
   }
@@ -13,5 +15,13 @@ export class UserService extends DataModelService<User> {
     return this.repo.findOne({
       userName,
     });
+  }
+
+  async synchronizeUserInfo(userInfoBCServiceCard: UserInfo): Promise<User> {
+    const userToSync = await this.getUser(userInfoBCServiceCard.userName)
+    if(userInfoBCServiceCard.email!=userToSync.email){
+      userToSync.email = userInfoBCServiceCard.email;
+    }
+    return this.save(userToSync);
   }
 }
