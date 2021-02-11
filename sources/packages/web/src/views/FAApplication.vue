1a43d17cd4d1181af0c24f1a25d1951308c215e4
fwpushan <pushan@freshworks.io>
2021-02-12 02:46:02 +0530

    [web][SIM-126] Citizen and Residency Questionnaire 1. Questionnire page 2. Add Section + Question component

--- sources/packages/web/src/views/FAApplication.vue
+++ sources/packages/web/src/views/FAApplication.vue
@@ -1,15 +1,17 @@
 <template>
   <WelcomePage v-if="!isReady" @user-ready="userReady" />
   <!-- Application Main View-->
-  <div v-else>USER IS READY FOR APPLICATION</div>
+  <PersonalInfoQuestionnaire v-else></PersonalInfoQuestionnaire>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 import WelcomePage from "../components/fa-application/WelcomePage.vue";
+import PersonalInfoQuestionnaire from "./PersonalInfoQuestionnaire.vue";
 export default {
   components: {
     WelcomePage,
+    PersonalInfoQuestionnaire,
   },
   setup() {
     const isReady = ref(false);
---
+++ sources/packages/web/src/views/PersonalInfoQuestionnaire.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="p-component">
+    <div class="p-grid">
+      <Section
+        v-bind:title="questionnaire.sections.residency.title"
+        v-bind:sub-title="questionnaire.sections.residency.subTitle"
+      >
+        <Question
+          v-bind:text="
+            questionnaire.sections.residency.questions.residencyQuestion
+          "
+        ></Question>
+        <Question
+          v-bind:text="
+            questionnaire.sections.residency.questions.bcResidencyQuestion
+          "
+        ></Question>
+      </Section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Section from "../components/fa-application/Section";
+import Question from "../components/fa-application/Question";
+
+const questionnaire = {
+  sections: {
+    residency: {
+      title: "Citizen and Residency",
+      subTitle:
+        "You must meet B.C. residency requirements to receive funding through StudentAid BC",
+      questions: {
+        residencyQuestion: "On my first day of class, I'll be a:",
+        bcResidencyQuestion:
+          "I have lived in B.C. for at least 12 consecutive months without being a full-time post-secondary student",
+      },
+    },
+  },
+};
+
+export default {
+  components: {
+    Section,
+    Question,
+  },
+  setup() {
+    return {
+      questionnaire,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
