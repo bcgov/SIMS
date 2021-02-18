d7bcc2edde953d942e022f4ad29fa133c3dd1c89
Andrew Signori <andrew.signori@cgi.com>
2021-02-17 16:30:15 -0800

    Super Test

---
+++ sources/packages/api/test/auth.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
+
+describe("Authentication (e2e)", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it("Get Config (Public)", () => {
+    return request(app.getHttpServer()).get("/config").expect(200);
+  });
+});
