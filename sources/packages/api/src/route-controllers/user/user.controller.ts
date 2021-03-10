08b6353af6723390dfccf923e332bfd10c739f45
Andrew Signori <andrew.signori@cgi.com>
2021-03-09 17:50:46 -0800

    Adjustment to   @Get(bceid-account)

--- sources/packages/api/src/route-controllers/user/user.controller.ts
+++ sources/packages/api/src/route-controllers/user/user.controller.ts
@@ -4,6 +4,7 @@ import BaseController from "../BaseController";
 import { UserToken } from "../../auth/decorators/userToken.decorator";
 import { IUserToken } from "../../auth/userToken.interface";
 import { extractRawUserName } from "../../utilities/auth-utils";
+import { BCeIDDetails } from "./models/bceid-account.model";
 
 @Controller("users")
 export class UserController extends BaseController {
@@ -30,8 +31,21 @@ export class UserController extends BaseController {
   }
 
   @Get("bceid-account")
-  async getBCeID(@UserToken() userToken: IUserToken) {
+  async getBCeID(@UserToken() userToken: IUserToken): Promise<BCeIDDetails> {
     const userName = extractRawUserName(userToken.userName);
-    return await this.bceidService.getAccountDetails(userName);
+    const account = await this.bceidService.getAccountDetails(userName);
+    return {
+      user: {
+        guid: account.user.guid,
+        displayName: account.user.displayName,
+        firstname: account.user.firstname,
+        surname: account.user.surname,
+        email: account.user.email,
+      },
+      institution: {
+        guid: account.institution.guid,
+        legalName: account.institution.legalName,
+      },
+    };
   }
 }
