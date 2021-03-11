39f7e7750e8a64dac58a9e0b5f5f3f6dd0abc69d
sandeepraj <sandeepraj.tandon@gov.bc.ca>
2021-03-10 16:29:24 -0800

    Initial Commit

---
+++ sources/packages/web/src/views/institution/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <Card class="p-m-4">
+    <template #title>
+      Login
+    </template>
+    <template #content>
+      <div>
+        <h1>Welcome to Institution Portal</h1>
+        <h4>
+          We are using BCeid for authentication. Please click on Login/Register
+          buttons below to start your sign in/sign up.
+        </h4>
+      </div>
+    </template>
+    <template #footer>
+      <Button
+        label="Login"
+        icon="pi pi-check"
+        class="p-mr-2"
+        @click="login"
+      ></Button>
+      <Button
+        label="Register"
+        icon="pi pi-user"
+        class="p-button-info"
+        @click="login"
+      ></Button>
+    </template>
+  </Card>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { AppConfigService } from "../../services/AppConfigService";
+
+@Options({
+  components: {},
+})
+export default class Login extends Vue {
+  login() {
+    AppConfigService.shared.authService?.login({
+      idpHint: "bcsc",
+    });
+  }
+}
+</script>
