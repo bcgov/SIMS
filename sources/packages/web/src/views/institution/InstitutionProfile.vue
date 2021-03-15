63920a48af61eba9d43520fe079ccab8b046d4c9
Andrew Signori <andrew.signori@cgi.com>
2021-03-15 14:33:55 -0700

    Adding validation

--- sources/packages/web/src/views/institution/InstitutionProfile.vue
+++ sources/packages/web/src/views/institution/InstitutionProfile.vue
@@ -2,7 +2,7 @@
   <Card class="p-m-4">
     <template #title>Institution Profile Setup</template>
     <template #content>
-      <form>
+      <form @submit="onSubmit" novalidate>
         <Message severity="info">
           Please notice that the read-only information below is retrieved from
           your BCeID account and it is not possible to change it here. If any
@@ -30,7 +30,14 @@
           </div>
           <div class="p-field p-col-6">
             <label for="studentEmail">Email</label>
-            <InputText id="studentEmail" v-model="profileState.studentEmail" />
+            <ValidatedInput property-name="studentEmail">
+              <Field
+                name="studentEmail"
+                label="Email"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
         </div>
         <h4>Institution Information</h4>
@@ -55,42 +62,69 @@
             <label for="operatingName"
               >Institution Operating Name (Optional)</label
             >
-            <InputText
-              id="operatingName"
-              v-model="profileState.operatingName"
-            />
+            <ValidatedInput property-name="operatingName">
+              <Field
+                name="operatingName"
+                label="Institution Operating Name"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
           <div class="p-field p-col-6">
             <label for="primaryPhoneNumber">Primary Phone Number</label>
-            <InputText
-              id="primaryPhoneNumber"
-              v-model="profileState.primaryPhoneNumber"
-            />
+            <ValidatedInput property-name="primaryPhoneNumber">
+              <Field
+                name="primaryPhoneNumber"
+                label="Primary Phone Number"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
           <div class="p-field p-col-6">
             <label for="primaryEmail">Primary Email</label>
-            <InputText id="primaryEmail" v-model="profileState.primaryEmail" />
+            <ValidatedInput property-name="primaryEmail">
+              <Field
+                name="primaryEmail"
+                label="Primary Email"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
           <div class="p-field p-col-12">
             <label for="institutionWebsite">Institution Website</label>
-            <InputText
-              id="institutionWebsite"
-              v-model="profileState.institutionWebsite"
-            />
+            <ValidatedInput property-name="institutionWebsite">
+              <Field
+                name="institutionWebsite"
+                label="Institution Website"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
           <div class="p-field p-col-12">
             <label for="regulatingBody">Institution Regulating body</label>
-            <InputText
-              id="regulatingBody"
-              v-model="profileState.regulatingBody"
-            />
+            <ValidatedInput property-name="regulatingBody">
+              <Field
+                name="regulatingBody"
+                label="Institution Regulating body"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
           <div class="p-field p-col-6">
             <label for="estabilishdDate">Established Date</label>
-            <InputText
-              id="estabilishdDate"
-              v-model="profileState.estabilishdDate"
-            />
+            <ValidatedInput property-name="estabilishdDate">
+              <Field
+                name="estabilishdDate"
+                label="Established Date"
+                rules="required"
+                as="InputText"
+              />
+            </ValidatedInput>
           </div>
         </div>
         <HorizontalSeparator />
@@ -102,31 +136,47 @@
           <div class="p-fluid p-formgrid p-grid">
             <div class="p-field p-col-6">
               <label for="primaryContactfirstName">First Name</label>
-              <InputText
-                id="primaryContactfirstName"
-                v-model="profileState.primaryContact.firstName"
-              />
+              <ValidatedInput property-name="primaryContact.firstName">
+                <Field
+                  name="primaryContact.firstName"
+                  label="First Name"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryContactLastName">Last Name</label>
-              <InputText
-                id="primaryContactLastName"
-                v-model="profileState.primaryContact.lastName"
-              />
+              <ValidatedInput property-name="primaryContact.lastName">
+                <Field
+                  name="primaryContact.lastName"
+                  label="Last Name"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryContactEmail">Email</label>
-              <InputText
-                id="primaryContactEmail"
-                v-model="profileState.primaryContact.email"
-              />
+              <ValidatedInput property-name="primaryContact.email">
+                <Field
+                  name="primaryContact.email"
+                  label="Email"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryContactPhoneNumber">Phone Number</label>
-              <InputText
-                id="primaryContactPhoneNumber"
-                v-model="profileState.primaryContact.phoneNumber"
-              />
+              <ValidatedInput property-name="primaryContact.phoneNumber">
+                <Field
+                  name="primaryContact.phoneNumber"
+                  label="Phone Number"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
           </div>
         </ContentGroup>
@@ -136,31 +186,47 @@
           <div class="p-fluid p-formgrid p-grid">
             <div class="p-field p-col-6">
               <label for="primaryContactfirstName">First Name</label>
-              <InputText
-                id="primaryContactfirstName"
-                v-model="profileState.legalContact.firstName"
-              />
+              <ValidatedInput property-name="legalContact.firstName">
+                <Field
+                  name="legalContact.firstName"
+                  label="First Name"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryContactLastName">Last Name</label>
-              <InputText
-                id="primaryContactLastName"
-                v-model="profileState.legalContact.lastName"
-              />
+              <ValidatedInput property-name="legalContact.lastName">
+                <Field
+                  name="legalContact.lastName"
+                  label="Last Name"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryContactEmail">Email</label>
-              <InputText
-                id="primaryContactEmail"
-                v-model="profileState.legalContact.email"
-              />
+              <ValidatedInput property-name="legalContact.email">
+                <Field
+                  name="legalContact.email"
+                  label="Email"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryContactPhoneNumber">Phone Number</label>
-              <InputText
-                id="primaryContactPhoneNumber"
-                v-model="profileState.legalContact.phoneNumber"
-              />
+              <ValidatedInput property-name="legalContact.phoneNumber">
+                <Field
+                  name="legalContact.phoneNumber"
+                  label="Phone Number"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
           </div>
         </ContentGroup>
@@ -173,45 +239,69 @@
           <div class="p-fluid p-formgrid p-grid">
             <div class="p-field p-col-12">
               <label for="primaryAddressAddress1">Address 1</label>
-              <InputText
-                id="primaryAddressAddress1"
-                v-model="profileState.primaryAddress.address1"
-              />
+              <ValidatedInput property-name="primaryAddress.address1">
+                <Field
+                  name="primaryAddress.address1"
+                  label="Address 1"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-12">
               <label for="primaryAddressAddress2">Address 2</label>
-              <InputText
-                id="primaryAddressAddress2"
-                v-model="profileState.primaryAddress.address2"
-              />
+              <ValidatedInput property-name="primaryAddress.address2">
+                <Field
+                  name="primaryAddress.address2"
+                  label="Address 2"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryAddressCity">City</label>
-              <InputText
-                id="primaryAddressCity"
-                v-model="profileState.primaryAddress.city"
-              />
+              <ValidatedInput property-name="primaryAddress.city">
+                <Field
+                  name="primaryAddress.city"
+                  label="City"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryAddressPostalCode">Postal/ZIP code</label>
-              <InputText
-                id="primaryAddressPostalCode"
-                v-model="profileState.primaryAddress.postalCode"
-              />
+              <ValidatedInput property-name="primaryAddress.postalCode">
+                <Field
+                  name="primaryAddress.postalCode"
+                  label="Postal/ZIP code"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryAddressProvinceState">Province/State</label>
-              <InputText
-                id="primaryAddressProvinceState"
-                v-model="profileState.primaryAddress.provinceState"
-              />
+              <ValidatedInput property-name="primaryAddress.provinceState">
+                <Field
+                  name="primaryAddress.provinceState"
+                  label="Province/State"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
             <div class="p-field p-col-6">
               <label for="primaryAddressCountry">Country</label>
-              <InputText
-                id="primaryAddressCountry"
-                v-model="profileState.primaryAddress.coutry"
-              />
+              <ValidatedInput property-name="primaryAddress.coutry">
+                <Field
+                  name="primaryAddress.coutry"
+                  label="Country"
+                  rules="required"
+                  as="InputText"
+                />
+              </ValidatedInput>
             </div>
           </div>
         </ContentGroup>
@@ -222,7 +312,13 @@
           StudentAid BC’s Privacy policy.
         </p>
         <HorizontalSeparator />
-        <Button class="p-mt-4">Next</Button>
+        <Button
+          type="submit"
+          label="Next"
+          icon="pi pi-save"
+          class="p-mt-4"
+          :disabled="isSubmitting"
+        ></Button>
       </form>
     </template>
   </Card>
@@ -233,6 +329,8 @@ import { onMounted, ref, reactive } from "vue";
 import { UserService } from "../../services/UserService";
 import HorizontalSeparator from "../../components/generic/HorizontalSeparator.vue";
 import ContentGroup from "../../components/generic/ContentGroup.vue";
+import { useForm, Field } from "vee-validate";
+import ValidatedInput from "../../components/generic/ValidatedInput.vue";
 
 interface ReadonlyProfileState {
   userfirstName: string;
@@ -273,6 +371,8 @@ export default {
   components: {
     HorizontalSeparator,
     ContentGroup,
+    ValidatedInput,
+    Field,
   },
   setup() {
     const readonlyProfileState = ref({} as ReadonlyProfileState);
@@ -281,6 +381,9 @@ export default {
       legalContact: {} as ContactInfo,
       primaryAddress: {} as Address,
     } as ProfileState);
+
+    const { handleSubmit, isSubmitting, setValues } = useForm<ProfileState>();
+
     onMounted(async () => {
       const bceidAccount = await UserService.shared.getBCeIDAccountDetails();
       readonlyProfileState.value = {
@@ -289,26 +392,27 @@ export default {
         institutionLegalName: bceidAccount.institution.legalName,
       };
 
-      profileState.studentEmail = "Student email@test.com";
-      profileState.operatingName = "Operating Name";
-      profileState.primaryPhoneNumber = "123 456 7891";
-      profileState.primaryEmail = "Primary Email@test.com";
-      profileState.institutionWebsite = "Institution Website";
-      profileState.regulatingBody = "Regulating Body";
-      profileState.estabilishdDate = new Date();
-      profileState.primaryContact = {
+      const state = {} as ProfileState;
+      state.studentEmail = "Student email@test.com";
+      state.operatingName = "Operating Name";
+      state.primaryPhoneNumber = "123 456 7891";
+      state.primaryEmail = "Primary Email@test.com";
+      state.institutionWebsite = "Institution Website";
+      state.regulatingBody = "Regulating Body";
+      state.estabilishdDate = new Date();
+      state.primaryContact = {
         firstName: "Primary First Name",
         lastName: "Primary Last Name",
         email: "Primary Email",
         phoneNumber: "Primary Phone Number",
       };
-      profileState.legalContact = {
+      state.legalContact = {
         firstName: "Legal First Name",
         lastName: "Legal Last Name",
         email: "Legal Email",
         phoneNumber: "Legal Phone Number",
       };
-      profileState.primaryAddress = {
+      state.primaryAddress = {
         address1: "Primary Address Address 1",
         address2: "Primary Address Address 2",
         city: "Primary Address City",
@@ -316,10 +420,17 @@ export default {
         provinceState: "Primary Address Province State",
         coutry: "Primary Address Country",
       };
+      setValues(state);
+    });
+
+    const onSubmit = handleSubmit(async formValues => {
+      console.dir(formValues);
     });
 
     return {
       readonlyProfileState,
+      onSubmit,
+      isSubmitting,
       profileState,
     };
   },
