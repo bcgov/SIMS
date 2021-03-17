90fd5f719328b3fe2c58db9caa2c7e4c3b31d51d
Pushan Mitra <46456190+fwpushan@users.noreply.github.com>
2021-03-17 11:28:41 +0530

    [api][SIMS #231] Enable Tests on Github Action CI/CD (#313)
    * [api][SIMS #231] Enable Tests on Github Action CI/CD
    -----
    1. Create new make rule to run api test on docker env
    2. Update tests accordingly
    3. Add new github test workflow
    ----
    
    * Update workflow
    
    * Fix workflow issue
    
    * Fix workflow issue-1
    
    * Workflow file: Fix path issue
    
    * Workflow file: Add all missing env
    
    * Workflow file: Inactive docker compose CLI printing
    
    * Make: Change docker exec option
    
    * Docker: Fix Env name issue
    
    * Docker: Fix Env name issue-1
    
    * Update workflow
    
    * Change lable in workflow file
    
    * [.github] Enable web unit test on CI/CD
    
    * Update node action version
    
    * Add npm ci on action
    
    * Web unit test: Change node v
    
    * Remove logs

--- sources/packages/api/src/services/user/user.service.spec.ts
+++ sources/packages/api/src/services/user/user.service.spec.ts
@@ -1,17 +1,27 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { DatabaseService } from "../../database/database.service";
 import { DatabaseModule } from "../../database/database.module";
+import { BCeIDService } from "../bceid/bceid.service";
+import { ConfigService } from "../config/config.service";
 import { UserService } from "./user.service";
 
 describe("UserService", () => {
   let service: UserService;
+  let dbService: DatabaseService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [DatabaseModule],
-      providers: [UserService],
+      providers: [UserService, BCeIDService, ConfigService],
     }).compile();
 
+    await module.init();
     service = module.get<UserService>(UserService);
+    dbService = module.get<DatabaseService>(DatabaseService);
+  });
+
+  afterAll(async () => {
+    await dbService.connection.close();
   });
 
   it("should be defined", () => {
