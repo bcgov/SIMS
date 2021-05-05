94324a89c4322a3d21efb7e3693a7508c0e43766
Andrew Signori <andrew.signori@cgi.com>
2021-05-05 11:24:08 -0700

    Removing test code

--- sources/packages/api/src/services/institution-location/institution-location.service.ts
+++ sources/packages/api/src/services/institution-location/institution-location.service.ts
@@ -2,38 +2,20 @@ import { Injectable, Inject } from "@nestjs/common";
 import { RecordDataModelService } from "../../database/data.model.service";
 import { InstitutionLocation } from "../../database/entities/institution-location.model";
 import { Connection } from "typeorm";
-import { Institution } from "src/database/entities/institution.model";
-
 @Injectable()
 export class InstitutionLocationService extends RecordDataModelService<InstitutionLocation> {
   constructor(@Inject("Connection") private readonly connection: Connection) {
     super(connection.getRepository(InstitutionLocation));
   }
 
+  /**
+   * Get the institution location by ID.
+   * TODO: Add restriction to the database query to ensure that the
+   * the user requesting the information has access to it.
+   * @param id Location id.
+   * @returns Location retrieved, if found, otherwise returns null.
+   */
   async getById(id: number): Promise<InstitutionLocation> {
     return await this.repo.findOne(id);
-  }
-
-  // TODO: Create the final method to add a location.
-  // This method was just created to add a dummy location
-  // to the DB while testing the GET location method.
-  async createtLocation(): Promise<InstitutionLocation> {
-    const institution = { id: 1 };
-    const newLocation = {
-      name: "New Dummy Location",
-      data: {
-        address: {
-          addressLine1: "Address Line 1",
-          addressLine2: "Address Line 2",
-          province: "BC",
-          country: "Canada",
-          city: "Victoria",
-          postalCode: "V9A 3AA",
-        },
-      },
-      institution: institution,
-    };
-
-    return await this.repo.save(newLocation);
   }
 }
