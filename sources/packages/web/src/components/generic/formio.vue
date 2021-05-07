17f0db988b0661a41d312d2c8d96c43ebdcce8aa
Ann <ann.jacob@aot-technologies.com>
2021-05-07 13:27:07 -0400

    [api][web][sims-120] Feedback addressed- line breaks reverted

--- sources/packages/web/src/components/generic/formio.vue
+++ sources/packages/web/src/components/generic/formio.vue
@@ -29,7 +29,7 @@ export default {
       const formDefinition = await ApiClient.DynamicForms.getFormDefinition(
         props.formName,
       );
-      
+
       const form = await Formio.createForm(
         formioContainerRef.value,
         formDefinition.data,
