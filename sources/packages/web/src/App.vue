be82b14fe145e22b76413a79a5066660bacbb6d2
fwpushan <pushan@freshworks.io>
2021-05-14 19:04:37 +0530

    [web][SIMS-#90] Remove commented code + enable Toast

--- sources/packages/web/src/App.vue
+++ sources/packages/web/src/App.vue
@@ -1,24 +1,15 @@
 <template>
   <v-app>
+    <!-- TODO: Find a proper solution of position styling-->
     <v-main style="position: auto">
+      <Toast />
       <router-view></router-view>
     </v-main>
   </v-app>
-  <!-- <router-view></router-view> -->
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "App",
-
-  data() {
-    return {
-      //
-    };
-  },
-});
+export default {};
 </script>
 
 <style lang="scss" scoped>
