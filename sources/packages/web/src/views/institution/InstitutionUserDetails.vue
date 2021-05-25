a9fa70c79ead84719e4bf885f1750415461e67ab
fwpushan <pushan@freshworks.io>
2021-05-26 03:50:11 +0530

    [web][SIMS-#227] Remove delete option from user table UI

--- sources/packages/web/src/views/institution/InstitutionUserDetails.vue
+++ sources/packages/web/src/views/institution/InstitutionUserDetails.vue
@@ -8,16 +8,6 @@
       <Column field="role" header="Role"></Column>
       <Column field="location" header="Location"></Column>
       <Column field="status" header="Status"></Column>
-      <Column>
-        <template #body="slotProps">
-          <Button
-            :disabled="slotProps.data.disableRemove"
-            label="Remove"
-            icon="pi pi-trash"
-            @click="removeUser(slotProps.data.id)"
-          />
-        </template>
-      </Column>
     </DataTable>
   </v-container>
 </template>
@@ -26,7 +16,6 @@
 import { ref, onMounted } from "vue";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
-import { useToast } from "primevue/usetoast";
 import { InstitutionService } from "../../services/InstitutionService";
 import { InstitutionUserViewModel } from "../../types";
 export default {
@@ -35,27 +24,7 @@ export default {
     Column,
   },
   setup() {
-    const toast = useToast();
     const users = ref([] as InstitutionUserViewModel[]);
-    const removeUser = async (userId: number) => {
-      console.log(`${userId}`);
-      try {
-        await InstitutionService.shared.removeUser(userId);
-        toast.add({
-          severity: "success",
-          summary: "Removed!",
-          detail: "Institution user successfully removed!",
-          life: 5000,
-        });
-      } catch (error) {
-        toast.add({
-          severity: "error",
-          summary: "Unexpected error",
-          detail: "An error happened during the remove process.",
-          life: 5000,
-        });
-      }
-    };
     onMounted(async () => {
       // Call Service
       const respUsers = await InstitutionService.shared.users();
@@ -63,7 +32,6 @@ export default {
     });
     return {
       users,
-      removeUser,
     };
   },
 };
