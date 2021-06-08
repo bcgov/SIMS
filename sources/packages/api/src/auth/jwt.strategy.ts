ba083160142d45f8e4debcca7e2fcfcd543e7f21
Andrew Signori <andrew.signori@cgi.com>
2021-06-08 15:50:35 -0700

    Sonar Cloud fixes

--- sources/packages/api/src/auth/jwt.strategy.ts
+++ sources/packages/api/src/auth/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { PassportStrategy } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { KeycloakConfig } from "./keycloakConfig";
 import { IInstitutionUserToken, IUserToken } from "./userToken.interface";
 import { InstitutionUserAuthService, UserService } from "../services";
