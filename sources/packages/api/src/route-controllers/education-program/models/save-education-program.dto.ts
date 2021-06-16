f298dd8cbb886c595a5752510b09365a42c2a015
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2021-06-16 14:08:42 -0700

    [web][api][SIMS #470]Institution User Views and edits Education Program Information details
    - Enabled the editing program page.
    - Fix some columns on DB that were declared as BIT and should be BOOLEAN.
    - Manually fixed OpenShift DEV.

--- sources/packages/api/src/route-controllers/education-program/models/save-education-program.dto.ts
+++ sources/packages/api/src/route-controllers/education-program/models/save-education-program.dto.ts
@@ -1,4 +1,4 @@
-export interface CreateEducationProgramDto {
+export interface EducationProgramDto {
   name: string;
   description: string;
   credentialType: string;
--- sources/packages/web/src/services/EducationProgramService.ts
+++ sources/packages/web/src/services/EducationProgramService.ts
@@ -5,12 +5,20 @@ export class EducationProgramService {
   // Share Instance
   private static instance: EducationProgramService;
 
-  public static get shared(): EducationProgramService {
+  public static get shared(): any {
     return this.instance || (this.instance = new this());
+  }
+
+  public async getProgram(programId: number): Promise<any> {
+    return ApiClient.EducationProgram.getProgram(programId);
   }
 
   public async createProgram(data: any): Promise<void> {
     await ApiClient.EducationProgram.createProgram(data);
+  }
+
+  public async updateProgram(programId: number, data: any): Promise<void> {
+    await ApiClient.EducationProgram.updateProgram(programId, data);
   }
 
   public async getLocationProgramsSummary(
