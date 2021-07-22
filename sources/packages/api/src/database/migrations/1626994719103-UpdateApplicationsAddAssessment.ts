88f597b5496163515f7287dfdd80036ed28c15b1
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-07-22 16:05:32 -0700

    Initial Commit

---
+++ sources/packages/api/src/database/migrations/1626994719103-UpdateApplicationsAddAssessment.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+import { getSQLFileData } from "../../utilities";
+
+export class UpdateApplicationsAddAssessment1626994719103 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            getSQLFileData("Add-col-assessment", "Applications"),
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            getSQLFileData("Drop-col-assessment", "Applications"),
+          );
+    }
+
+}
