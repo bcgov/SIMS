23d482a7a7f1a0215c02c3771d4008dfe3dee416
Andrew Signori <andrew.signori@cgi.com>
2021-08-05 19:21:23 -0700

    Added toast messages and redirect after save

--- sources/packages/web/src/composables/useFormatters.ts
+++ sources/packages/web/src/composables/useFormatters.ts
@@ -1,4 +1,12 @@
+/**
+ * Helpers to adjust how values are shown in the UI.
+ */
 export function useFormatters() {
+  /**
+   * Convert a string or date to a string format like "Thu Aug 05 2021".
+   * @param date string or date to be converted.
+   * @returns string representation (e.g. Thu Aug 05 2021).
+   */
   const dateString = (date: string | Date): string => {
     if (date) {
       if (date instanceof Date) {
--- sources/packages/web/src/views/institution/locations/program-info-request/LocationEditProgramInfoRequest.vue
+++ sources/packages/web/src/views/institution/locations/program-info-request/LocationEditProgramInfoRequest.vue
@@ -16,8 +16,8 @@
 </template>
 
 <script lang="ts">
-//import { useRouter } from "vue-router";
-//import { useToast } from "primevue/usetoast";
+import { useRouter } from "vue-router";
+import { useToast } from "primevue/usetoast";
 import { ProgramInfoRequestService } from "../../../../services/ProgramInfoRequestService";
 import formio from "../../../../components/generic/formio.vue";
 import { ref } from "vue";
@@ -26,6 +26,7 @@ import {
   useFormioDropdownLoader,
   useFormatters,
 } from "@/composables";
+import { InstitutionRoutesConst } from "@/constants/routes/RouteConstants";
 
 export default {
   components: { formio },
@@ -40,21 +41,12 @@ export default {
     },
   },
   setup(props: any) {
-    //const toast = useToast();
-    //const router = useRouter();
-    // Data-bind
+    const toast = useToast();
+    const router = useRouter();
     const { dateString } = useFormatters();
     const initialData = ref({});
     const formioUtils = useFormioUtils();
     const formioDataLoader = useFormioDropdownLoader();
-
-    const submitted = async (data: any) => {
-      ProgramInfoRequestService.shared.completeProgramInfoRequest(
-        props.locationId,
-        props.applicationId,
-        data,
-      );
-    };
 
     // Components names on Form.IO definition that will be manipulated.
     const PROGRAMS_DROPDOWN_KEY = "selectedProgram";
@@ -114,6 +106,33 @@ export default {
     const formChanged = async (form: any, event: any) => {
       if (event.changed?.component?.key === PROGRAMS_DROPDOWN_KEY) {
         await loadOfferingsForProgram(form);
+      }
+    };
+
+    const submitted = async (data: any) => {
+      try {
+        ProgramInfoRequestService.shared.completeProgramInfoRequest(
+          props.locationId,
+          props.applicationId,
+          data,
+        );
+        toast.add({
+          severity: "success",
+          summary: "Completed!",
+          detail: "Program Information Request completed successfully!",
+          life: 5000,
+        });
+        router.push({
+          name: InstitutionRoutesConst.PROGRAM_INFO_REQUEST_SUMMARY,
+        });
+      } catch (error) {
+        toast.add({
+          severity: "error",
+          summary: "Unexpected error",
+          detail:
+            "An error happened while saving the Program Information Request.",
+          life: 5000,
+        });
       }
     };
 
