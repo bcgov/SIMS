fed3f322f6bbd1131bd49b6744dc7b75f3f5ecbd
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-08-11 11:11:57 -0700

    Updating review comments

--- sources/packages/api/src/database/migrations/1628282476832-CoeStatusType.ts
+++ sources/packages/api/src/database/migrations/1628282476832-CoeStatusType.ts
@@ -3,10 +3,10 @@ import { getSQLFileData } from "../../utilities";
 
 export class CoeStatusType1628282476832 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(getSQLFileData("Create-CoeStatus.sql", "Types"));
+    await queryRunner.query(getSQLFileData("Create-coe-status.sql", "Types"));
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(getSQLFileData("Drop-CoeStatus.sql", "Types"));
+    await queryRunner.query(getSQLFileData("Drop-coe-status.sql", "Types"));
   }
 }
--- sources/packages/api/src/database/migrations/1628282484750-AssessmentStatusType.ts
+++ sources/packages/api/src/database/migrations/1628282484750-AssessmentStatusType.ts
@@ -4,13 +4,13 @@ import { getSQLFileData } from "../../utilities";
 export class AssessmentStatusType1628282484750 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      getSQLFileData("Create-AssessmentStatus.sql", "Types"),
+      getSQLFileData("Create-assessment-status.sql", "Types"),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      getSQLFileData("Drop-AssessmentStatus.sql", "Types"),
+      getSQLFileData("Drop-assessment-status.sql", "Types"),
     );
   }
 }
