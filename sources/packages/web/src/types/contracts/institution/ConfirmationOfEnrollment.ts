e16797ab1fde246ff90c75033e128745ae3fafa1
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-08-12 10:22:50 -0700

    UI Updated

---
+++ sources/packages/web/src/types/contracts/institution/ConfirmationOfEnrollment.ts
@@ -0,0 +1,9 @@
+export interface COESummaryDTO {
+  applicationNumber: string;
+  studyStartPeriod: string;
+  studyEndPeriod: string;
+  applicationNumberId: number;
+  coeStatus: string;
+  firstName: string;
+  lastName: string;
+}
