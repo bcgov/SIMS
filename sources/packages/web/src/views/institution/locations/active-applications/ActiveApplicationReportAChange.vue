2f2c2b746f6f92bcac4ebeda21d53d963f9fffad
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-09-13 14:22:03 -0700

    Updated application Id

--- sources/packages/web/src/views/institution/locations/active-applications/ActiveApplicationReportAChange.vue
+++ sources/packages/web/src/views/institution/locations/active-applications/ActiveApplicationReportAChange.vue
@@ -1,15 +1,24 @@
 <template>
   <Card class="p-m-4">
     <template #content>
-      <formio :formName="reportachange" :data="initialData"></formio>
       <h1 class="d-flex justify-content-center">
-        Application # {{ applicationId }} - Work in Progress
+        Report A change - Work in Progress
       </h1>
+      <formio
+        formName="reportachange"
+        :data="initialData"
+        @submitted="submitted"
+      ></formio>
     </template>
   </Card>
 </template>
 <script lang="ts">
+import { ref, onMounted } from "vue";
+import formio from "@/components/generic/formio.vue";
 export default {
+  components: {
+    formio,
+  },
   props: {
     applicationId: {
       type: Number,
@@ -19,6 +28,15 @@ export default {
       type: Number,
       required: true,
     },
+  },
+  setup(props: any) {
+    const initialData = ref({});
+    onMounted(async () => {
+      initialData.value = { appId: props.applicationId };
+    });
+    return {
+      initialData,
+    };
   },
 };
 </script>
