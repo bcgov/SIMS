e0089718884ee4ad251f317a4c05e26e3c7bca38
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-11-01 10:25:43 -0700

    updated test case

--- sources/packages/api/src/testHelpers/fake-entities/msfaa-number-fake.ts
+++ sources/packages/api/src/testHelpers/fake-entities/msfaa-number-fake.ts
@@ -1,10 +1,12 @@
 import { MSFAANumber, Student } from "../../database/entities";
 import { createFakeStudent } from "./student-fake";
+import { createFakeApplication } from "./application-fake";
 import * as faker from "faker";
 
 export function createFakeMSFAANumber(student?: Student): MSFAANumber {
   const msfaaNumber = new MSFAANumber();
   msfaaNumber.student = student ?? createFakeStudent();
+  msfaaNumber.referenceApplication = createFakeApplication();
   msfaaNumber.msfaaNumber = faker.random
     .number({
       min: 1111111111,
