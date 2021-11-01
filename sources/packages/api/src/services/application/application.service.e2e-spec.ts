a705611884816ee896478a569a4f886e1af17da6
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-11-01 13:12:28 -0700

    updated failed test case

--- sources/packages/api/src/services/application/application.service.e2e-spec.ts
+++ sources/packages/api/src/services/application/application.service.e2e-spec.ts
@@ -180,7 +180,6 @@ describe("ApplicationService", () => {
         expect(savedApplication.msfaaNumber.id).not.toBe(testMSFAANumber.id);
         expect(msfaaNumberService.createMSFAANumber).toHaveBeenCalled();
       } finally {
-        await applicationRepository.remove(testApplication);
         await applicationRepository.remove(testCompletedApplication);
         await msfaaNumberRepository.remove(testMSFAANumber);
         await studentRepository.remove(testStudent);
