a6644bc159e010bdff0f042cdbbab40a48f0c3e5
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2021-11-01 11:11:13 -0700

    Update

--- sources/packages/api/src/services/application/application.service.e2e-spec.ts
+++ sources/packages/api/src/services/application/application.service.e2e-spec.ts
@@ -180,8 +180,8 @@ describe("ApplicationService", () => {
         expect(savedApplication.msfaaNumber.id).not.toBe(testMSFAANumber.id);
         expect(msfaaNumberService.createMSFAANumber).toHaveBeenCalled();
       } finally {
-        await applicationRepository.remove(testCompletedApplication);
         await msfaaNumberRepository.remove(testMSFAANumber);
+        await applicationRepository.remove(testCompletedApplication);
         await applicationRepository.remove(testApplication);
         await studentRepository.remove(testStudent);
       }
