fa73c50df035544223172e2f286d756aaef8c6c5
dheepak-aot <dheepak.rr@aot-technologies.com>
2021-11-05 17:23:59 -0700

    Added previous and next buttons

--- sources/packages/web/src/views/aest/ApplicationDetails.vue
+++ sources/packages/web/src/views/aest/ApplicationDetails.vue
@@ -17,14 +17,31 @@
       :formName="selectedForm"
       :data="initialData"
       :readOnly="true"
+      @loaded="formLoaded"
     ></formio>
+    <v-row>
+      <v-col md="6">
+        <v-btn
+          color="primary"
+          v-show="!isFirstPage"
+          outlined
+          @click="wizardGoPrevious()"
+          >Previous section</v-btn
+        >
+      </v-col>
+      <v-col md="6" class="ml-auto text-right">
+        <v-btn color="primary" v-show="!isLastPage" @click="wizardGoNext()"
+          >Next section</v-btn
+        >
+      </v-col>
+    </v-row>
   </full-page-container>
 </template>
 <script lang="ts">
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import { AESTRoutesConst } from "@/constants/routes/RouteConstants";
-import { GetApplicationDataDto } from "@/types";
+import { GetApplicationDataDto, WizardNavigationEvent } from "@/types";
 import { AestService } from "@/services/AestService";
 import FullPageContainer from "@/components/layouts/FullPageContainer.vue";
 import formio from "@/components/generic/formio.vue";
@@ -45,6 +62,9 @@ export default {
     const applicationDetail = ref({} as GetApplicationDataDto);
     const initialData = ref({});
     const selectedForm = ref();
+    const isFirstPage = ref(true);
+    const isLastPage = ref(false);
+    let applicationWizard: any;
     onMounted(async () => {
       applicationDetail.value = await AestService.shared.getApplicationDetail(
         props.applicationId,
@@ -62,11 +82,40 @@ export default {
         },
       });
     };
+
+    const formLoaded = async (form: any) => {
+      applicationWizard = form;
+
+      applicationWizard.on("wizardPageSelected", (page: any, index: number) => {
+        isFirstPage.value = index === 0;
+        isLastPage.value = applicationWizard.isLastPage();
+      });
+      // Handle the navigation using next/prev buttons.
+      const prevNextNavigation = (navigation: WizardNavigationEvent) => {
+        isFirstPage.value = navigation.page === 0;
+        isLastPage.value = applicationWizard.isLastPage();
+      };
+      applicationWizard.on("prevPage", prevNextNavigation);
+      applicationWizard.on("nextPage", prevNextNavigation);
+    };
+
+    const wizardGoPrevious = () => {
+      applicationWizard.prevPage();
+    };
+
+    const wizardGoNext = () => {
+      applicationWizard.nextPage();
+    };
     return {
       applicationDetail,
       initialData,
       selectedForm,
       goBack,
+      isFirstPage,
+      isLastPage,
+      formLoaded,
+      wizardGoPrevious,
+      wizardGoNext,
     };
   },
 };
