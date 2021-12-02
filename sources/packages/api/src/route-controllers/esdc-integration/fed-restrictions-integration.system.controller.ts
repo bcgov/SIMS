bfe9629219a3f019bf2de09f25a70ff4d69a371f
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2021-12-02 15:56:16 -0800

    Post merge adjustments

--- sources/packages/api/src/route-controllers/esdc-integration/fed-restrictions-integration.system.controller.ts
+++ sources/packages/api/src/route-controllers/esdc-integration/fed-restrictions-integration.system.controller.ts
@@ -4,7 +4,7 @@ import { LoggerService } from "../../logger/logger.service";
 import { AllowAuthorizedParty } from "../../auth/decorators";
 import { AuthorizedParties } from "../../auth/authorized-parties.enum";
 import { FedRestrictionProcessingService } from "../../esdc-integration/fed-restriction-integration/fed-restriction-processing.service";
-import { ProcessSFTPResponseDTO } from "./models/esdc-model";
+import { ESDCFileResponseDTO } from "./models/esdc-model";
 
 @AllowAuthorizedParty(AuthorizedParties.formsFlowBPM)
 @Controller("system-access/fed-restrictions")
@@ -14,14 +14,14 @@ export class FedRestrictionsIntegrationController {
   ) {}
 
   @Post("process")
-  async processFedRestrictionsImport(): Promise<ProcessSFTPResponseDTO> {
+  async processFedRestrictionsImport(): Promise<ESDCFileResponseDTO> {
     this.logger.log("Starting federal restrictions import...");
     const uploadResult = await this.processingService.process();
     this.logger.log("Federal restrictions import process finished.");
     return {
       processSummary: uploadResult.processSummary,
       errorsSummary: uploadResult.errorsSummary,
-    } as ProcessSFTPResponseDTO;
+    } as ESDCFileResponseDTO;
   }
 
   @InjectLogger()
