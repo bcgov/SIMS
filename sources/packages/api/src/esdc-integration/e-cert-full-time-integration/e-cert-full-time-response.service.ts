1af05b58f0406267026163a3d0280c02ae6c17b2
Ann <ann.jacob@aot-technologies.com>
2021-12-02 17:05:58 -0500

    updated code to slient proceed if its is unquine constrain query failer

--- sources/packages/api/src/esdc-integration/e-cert-full-time-integration/e-cert-full-time-response.service.ts
+++ sources/packages/api/src/esdc-integration/e-cert-full-time-integration/e-cert-full-time-response.service.ts
@@ -10,7 +10,8 @@ import { ECertFullTimeIntegrationService } from "./e-cert-full-time-integration.
 import { ECertResponseRecord } from "./e-cert-files/e-cert-response-record";
 import { getUTCNow } from "../../utilities";
 import { ESDCIntegrationConfig } from "../../types";
-
+// postgresql unique constraint code, ref: https://www.postgresql.org/docs/9.6/errcodes-appendix.html
+const PG_UNIQUE_CONSTRAINT_VIOLATION = "23505";
 @Injectable()
 export class ECertFullTimeResponseService {
   private readonly esdcConfig: ESDCIntegrationConfig;
@@ -111,41 +112,49 @@ export class ECertFullTimeResponseService {
     feedbackRecord: ECertResponseRecord,
   ): Promise<void> {
     const now = getUTCNow();
-    const disbursementSchedule =
-      await this.disbursementScheduleService.getDisbursementScheduleByDocumentNumber(
-        feedbackRecord.documentNumber,
-      );
-    const errorList = [];
-    if (disbursementSchedule) {
-      const updateResult =
-        await this.disbursementScheduleErrorsService.createECertErrorRecord(
-          disbursementSchedule,
-          [
-            feedbackRecord.errorCode1,
-            feedbackRecord.errorCode2,
-            feedbackRecord.errorCode3,
-            feedbackRecord.errorCode4,
-            feedbackRecord.errorCode5,
-          ].filter((error) => error),
-          now,
+    try {
+      const disbursementSchedule =
+        await this.disbursementScheduleService.getDisbursementScheduleByDocumentNumber(
+          feedbackRecord.documentNumber,
         );
-      // Expected to update 1 and only 1 record.
-      if (updateResult.length === 0) {
-        errorList.push(
-          `Error while saving Error Codes for document number:${feedbackRecord.documentNumber}.`,
+      const errorList = [];
+      if (disbursementSchedule) {
+        const updateResult =
+          await this.disbursementScheduleErrorsService.createECertErrorRecord(
+            disbursementSchedule,
+            [
+              feedbackRecord.errorCode1,
+              feedbackRecord.errorCode2,
+              feedbackRecord.errorCode3,
+              feedbackRecord.errorCode4,
+              feedbackRecord.errorCode5,
+            ].filter((error) => error),
+            now,
+          );
+
+        // Expected to update 1 and only 1 record.
+        if (updateResult.length === 0) {
+          errorList.push(
+            `Error while saving Error Codes for document number:${feedbackRecord.documentNumber}.`,
+          );
+        }
+      } else {
+        throw new Error(
+          `${feedbackRecord.documentNumber} document number not found in disbursement_schedule table.`,
         );
       }
-    } else {
-      throw new Error(
-        `${feedbackRecord.documentNumber} document number not found in disbursement_schedule table.`,
-      );
-    }
 
-    // Expected to update 1 and only 1 record.
-    if (errorList.length > 0) {
-      throw new Error(
-        `Error while saving Error codes to the table ${errorList}.`,
-      );
+      // Expected to update 1 and only 1 record.
+      if (errorList.length > 0) {
+        throw new Error(
+          `Error while saving Error codes to the table ${errorList}.`,
+        );
+      }
+    } catch (error) {
+      // silently proceed if its a unique constraint error
+      if (error.code !== PG_UNIQUE_CONSTRAINT_VIOLATION) {
+        throw new Error(`Database error ${error}.`);
+      }
     }
   }
 
