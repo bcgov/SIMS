7d2ed5e64976230d031f38e5e23a7824a85cd7bf
dheepak-aot <dheepak.rr@aot-technologies.com>
2021-12-29 17:58:06 -0800

    formatting removed

--- sources/packages/api/src-sql/Restrictions/Insert-provincial-restrictions.sql
+++ sources/packages/api/src-sql/Restrictions/Insert-provincial-restrictions.sql
@@ -1,15 +1,3 @@
 -- Insert provincial restrictions
-INSERT INTO
-    sims.restrictions(
-        restriction_type,
-        restriction_category,
-        restriction_code,
-        description
-    )
-VALUES
-    (
-        'Provincial',
-        'Academic',
-        '12',
-        'Denial-Poor Scholastic Standing'
-    );
+INSERT INTO sims.restrictions(restriction_type, restriction_category, restriction_code, description)
+VALUES ('Provincial', 'Academic', '12','Denial-Poor Scholastic Standing');
