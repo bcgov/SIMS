d990a9758a02634abf2da5ad0634eb57b990c1ea
dheepak-aot <dheepak.rr@aot-technologies.com>
2021-12-29 22:44:50 -0800

    TEST

--- sources/packages/api/src-sql/Restrictions/Insert-provincial-restrictions.sql
+++ sources/packages/api/src-sql/Restrictions/Insert-provincial-restrictions.sql
@@ -1,8 +1,29 @@
--- Insert provincial restrictions
-INSERT INTO sims.restrictions(id,restriction_type,restriction_code,description)
+INSERT INTO
+    sims.restrictions(
+        id,
+        restriction_type,
+        restriction_code,
+        description
+    )
 VALUES
-(1,'Federal','C4','# of weeks >= 468, input by CSLP');
+    (
+        1,
+        'Federal',
+        'C4',
+        '# of weeks >= 468, input by CSLP'
+    );
 
-INSERT INTO sims.restrictions(id,restriction_type,restriction_code,description)
+INSERT INTO
+    sims.restrictions(
+        id,
+        restriction_type,
+        restriction_code,
+        description
+    )
 VALUES
-(2,'Federal','C5','# of weeks >= 520, input by CSLP');
+    (
+        2,
+        'Federal',
+        'C5',
+        '# of weeks >= 520, input by CSLP'
+    );
