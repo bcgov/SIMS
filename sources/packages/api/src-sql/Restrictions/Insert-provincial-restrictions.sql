bef0cffe3f88861cb05c9b697253040ac8f486b3
dheepak-aot <dheepak.rr@aot-technologies.com>
2021-12-29 17:14:49 -0800

    Adding single entry

--- sources/packages/api/src-sql/Restrictions/Insert-provincial-restrictions.sql
+++ sources/packages/api/src-sql/Restrictions/Insert-provincial-restrictions.sql
@@ -1,1 +1,15 @@
 -- Insert provincial restrictions
+INSERT INTO
+    sims.restrictions(
+        restriction_type,
+        restriction_category,
+        restriction_code,
+        description
+    )
+VALUES
+    (
+        'Provincial',
+        'Academic',
+        '12',
+        'Denial-Poor Scholastic Standing'
+    );
