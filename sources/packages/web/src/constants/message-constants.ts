f76ae049650106e5a02acff0021f90e2216c2f79
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-01-13 09:14:14 -0800

    693-student-sin-validation fix sonar issues by adding new lines

--- sources/packages/web/src/constants/message-constants.ts
+++ sources/packages/web/src/constants/message-constants.ts
@@ -2,10 +2,10 @@
  * SIN Validation Message
  */
 export const PENDING_SIN_MESSAGE =
-    "StudentAid BC is currently validating your account information with CRA. This process normally takes up to 3 hours. If you have not heard from us please contact us.";
+  "StudentAid BC is currently validating your account information with CRA. This process normally takes up to 3 hours. If you have not heard from us please contact us.";
 
 /**
  * SIN Validation Message
  */
- export const INVALID_SIN_MESSAGE =
- "StudentAid BC has found an issue with your account information. Please ensure your account information matches CRA and log back in. If you have further issues please contact us.";
+export const INVALID_SIN_MESSAGE =
+  "StudentAid BC has found an issue with your account information. Please ensure your account information matches CRA and log back in. If you have further issues please contact us.";
--- sources/packages/web/src/views/student/SINStatusBanner.vue
+++ sources/packages/web/src/views/student/SINStatusBanner.vue
@@ -10,8 +10,8 @@ export default {
       type: String,
     },
     severity: {
-        type: String
-    }
+      type: String,
+    },
   },
 };
 </script>
