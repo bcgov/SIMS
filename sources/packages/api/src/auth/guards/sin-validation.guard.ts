ab15be8f74fcb38ca7fde3c8e03e4527960ce75f
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-01-13 15:13:00 -0800

    add await

--- sources/packages/api/src/auth/guards/sin-validation.guard.ts
+++ sources/packages/api/src/auth/guards/sin-validation.guard.ts
@@ -29,6 +29,6 @@ export class SinValidationGuard implements CanActivate {
     }
     const { user } = context.switchToHttp().getRequest();
     const userToken = user as IUserToken;
-    return this.studentService.getStudentSinStatus(userToken.userId);
+    return await this.studentService.getStudentSinStatus(userToken.userId);
   }
 }
