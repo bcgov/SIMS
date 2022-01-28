ff5845e625ac97e02a3540c4acc600679155ef51
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2022-01-27 16:19:07 -0800

    Updated type for partTime and FullTime

--- sources/packages/api/src/database/entities/application.model.ts
+++ sources/packages/api/src/database/entities/application.model.ts
@@ -261,16 +261,9 @@ export class Application extends RecordDataModel {
 }
 
 /**
- * Interface for assessment payload.
- *
- * This interface is to provide contract for the assessment payload
- * which is stored to database by workflow.
- * It is possible that more properties can be added to the assessment payload
- * without updating this interface and displayed in NOA form.
- * Whenever there is a source code update, please ensure that properties in this interface are in sync with
- * assessment payload created by camunda workflow.
+ * Interface for FullTime assessment payload.
  */
-export interface Assessment {
+export interface FullTimeAssessment {
   weeks: number;
   totalFederalAward: number;
   totalProvincialAward: number;
@@ -294,3 +287,29 @@ export interface Assessment {
   totalProvincialContribution: number;
   otherAllowableCost: number;
 }
+
+/**
+ * Interface for PartTime assessment payload.
+ */
+export interface PartTimeAssessment {
+  weeks: number;
+  schoolFees: number;
+  tuitionCost: number;
+  childcareCost: number;
+  miscellaneousCost: number;
+  totalFamilyIncome: number;
+  totalFederalAward: number;
+  transportationCost: number;
+  totalAssessmentNeed: number;
+  booksAndSuppliesCost: number;
+  totalProvincialAward: number;
+}
+/**
+ * This is a type which provides the contract for FullTime and PartTime assessment payload
+ * which is stored to database by workflow.
+ * It is possible that more properties can be added to the assessment payload
+ * without updating this interface and displayed in NOA form.
+ * Whenever there is a source code update, please ensure that properties in this interface are in sync with
+ * assessment payload created by camunda workflow.
+ */
+export type Assessment = FullTimeAssessment | PartTimeAssessment;
