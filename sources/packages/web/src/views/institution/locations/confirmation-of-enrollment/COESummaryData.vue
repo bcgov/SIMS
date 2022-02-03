6c51e199d947f058b73b63e2329c4ee667910c31
dheepak-aot <dheepak.rr@aot-technologies.com>
2022-02-02 20:01:45 -0800

    CSS and date format change.

--- sources/packages/web/src/views/institution/locations/confirmation-of-enrollment/COESummaryData.vue
+++ sources/packages/web/src/views/institution/locations/confirmation-of-enrollment/COESummaryData.vue
@@ -31,7 +31,7 @@
         <Column field="disbursementDate" header="Disbursement Date">
           <template #body="slotProps">
             <span>
-              {{ dateString(slotProps.data.disbursementDate) }}
+              {{ dateOnlyLongString(slotProps.data.disbursementDate) }}
             </span>
           </template></Column
         >
@@ -84,7 +84,7 @@ export default {
   },
   setup(props: any) {
     const router = useRouter();
-    const { dateString } = useFormatters();
+    const { dateString, dateOnlyLongString } = useFormatters();
     const applications = ref([] as COESummaryDTO[]);
 
     const goToViewApplication = (applicationId: number) => {
@@ -116,6 +116,7 @@ export default {
     return {
       applications,
       dateString,
+      dateOnlyLongString,
       goToViewApplication,
     };
   },
