dda6fa511e3544824d6b291ed173eeb6de99925d
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-02-16 11:42:46 -0800

    move css to formio-shared

--- sources/packages/web/src/views/institution/InstitutionDashboard.vue
+++ sources/packages/web/src/views/institution/InstitutionDashboard.vue
@@ -11,22 +11,4 @@ export default {
 };
 </script>
 
-<style>
-.img-welcome-background {
-  background-image: url("../../assets/images/image_institution_welcome.svg");
-  background-repeat: no-repeat;
-  background-size: contain;
-  width: 100%;
-  height: 100%;
-  min-height: 250px;
-}
-
-.img-active-support-background {
-  background-image: url("../../assets/images/image_active_support.svg");
-  background-repeat: no-repeat;
-  background-size: contain;
-  width: 100%;
-  height: 100%;
-  min-height: 150px;
-}
-</style>
+<style></style>
