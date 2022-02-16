3d54a063be30f1b04c599732e7fd25fe84323204
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-02-16 11:33:07 -0800

    update formio definitions

--- sources/packages/web/src/views/institution/InstitutionDashboard.vue
+++ sources/packages/web/src/views/institution/InstitutionDashboard.vue
@@ -11,4 +11,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.img-welcome-background {
+  background-image: url("../../assets/images/image_institution_welcome.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 100%;
+  height: 100%;
+  min-height: 250px;
+}
+
+.img-active-support-background {
+  background-image: url("../../assets/images/image_active_support.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 100%;
+  height: 100%;
+  min-height: 150px;
+}
+</style>
