a0855f6a4496af6aa1b3099745d65ee304f641bb
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-02-18 06:56:22 -0800

    implement student welcome page

--- sources/packages/web/src/views/student/StudentDashboard.vue
+++ sources/packages/web/src/views/student/StudentDashboard.vue
@@ -5,7 +5,10 @@
       :restrictionMessage="restrictionMessage"
     />
     <CheckValidSINBanner />
-    <formio formName="studentwelcomepage"></formio>
+    <formio
+      formName="studentwelcomepage"
+      @submitted="goToStudentApplication"
+    ></formio>
   </v-container>
 </template>
 <script lang="ts">
@@ -17,18 +20,29 @@ import { StudentService } from "@/services/StudentService";
 import formio from "@/components/generic/formio.vue";
 import RestrictionBanner from "@/views/student/RestrictionBanner.vue";
 import CheckValidSINBanner from "@/views/student/CheckValidSINBanner.vue";
+import { useRouter } from "vue-router";
+import { useToastMessage } from "@/composables";
 
 export default {
+  emits: ["goToStudentApplication"],
   components: {
     formio,
     RestrictionBanner,
     CheckValidSINBanner,
   },
   setup() {
+    const toast = useToastMessage();
     const store = useStore();
+    const router = useRouter();
     const user = computed(() => store.state.student.profile);
     const hasRestriction = ref(false);
     const restrictionMessage = ref("");
+
+    const goToStudentApplication = async () => {
+      await router.push({
+        name: StudentRoutesConst.STUDENT_APPLICATION_FORM,
+      });
+    };
 
     onMounted(async () => {
       const studentRestriction = await StudentService.shared.getStudentRestriction();
@@ -40,6 +54,7 @@ export default {
       user,
       hasRestriction,
       restrictionMessage,
+      goToStudentApplication,
     };
   },
 };
