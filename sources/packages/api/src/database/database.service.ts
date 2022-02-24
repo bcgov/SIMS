9e04cef73d3e0349cc74dd376fbacb2d44fd471b
dheepak-aot <dheepak.rr@aot-technologies.com>
2022-02-24 11:05:23 -0800

    removing logger as it is not required.

--- sources/packages/api/src/database/database.service.ts
+++ sources/packages/api/src/database/database.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from "@nestjs/common";
-import { LoggerService } from "../logger/logger.service";
 import { Connection } from "typeorm";
-import { InjectLogger } from "../common";
 
 @Injectable()
 export class DatabaseService {
-  @InjectLogger()
-  logger: LoggerService;
-
   constructor(public connection: Connection) {}
 }
