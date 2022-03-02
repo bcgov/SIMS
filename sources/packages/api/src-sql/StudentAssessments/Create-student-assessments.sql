82cd46e2e4999ec64b63bf5ceb36f10106f5abf5
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2022-03-01 17:09:39 -0800

    Comments

--- sources/packages/api/src-sql/StudentAssessments/Create-student-assessments.sql
+++ sources/packages/api/src-sql/StudentAssessments/Create-student-assessments.sql
@@ -6,13 +6,17 @@
 CREATE TABLE IF NOT EXISTS sims.student_assessments (
     id SERIAL PRIMARY KEY,
     application_id INT NOT NULL REFERENCES sims.applications(id) ON DELETE CASCADE,
+    -- Will be moved from sims.applications.
     assessment_workflow_id UUID,
+    -- Will be moved from sims.applications.
     assessment_data jsonb NOT NULL,
     trigger_type sims.assessment_trigger_types NOT NULL,
-    -- PIR will be adding the id here. Moved from sims.applications
+    -- Will be moved from sims.applications.
     offering_id INT REFERENCES sims.education_programs_offerings(id) ON DELETE CASCADE,
     student_appeal_id INT REFERENCES sims.student_appeals(id) ON DELETE CASCADE,
     student_scholastic_standing_id INT REFERENCES sims.student_scholastic_standings(id) ON DELETE CASCADE,
+    -- Will be moved from sims.applications.
+    assessment_status sims.assessment_status NOT NULL,
     -- Audit columns
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
