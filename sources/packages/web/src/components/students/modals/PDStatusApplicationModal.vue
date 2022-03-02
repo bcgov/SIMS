d058fb63e90660a32306058ccfc3efa27f26c488
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-03-01 17:06:11 -0800

    fix PR comment

--- sources/packages/web/src/components/students/modals/PDStatusApplicationModal.vue
+++ sources/packages/web/src/components/students/modals/PDStatusApplicationModal.vue
@@ -1,40 +1,32 @@
 <template>
-  <Dialog
-    v-if="showModal"
-    :visible="showModal"
-    :modal="true"
-    :style="{ width: '50vw' }"
-    :closable="false"
+  <ModalDialogBase
+    title="Apply for a permanent disability status"
+    :showDialog="showModal"
   >
-    <template #header
-      ><p class="font-weight-bold h5">
-        Apply for a permanent disability status
-      </p></template
-    >
-    <p>
-      If you’re eligible, you may receive funding for provincial and federal
-      grants/bursaries. Your information will be sent to Assistive Technology BC
-      (ATBC).
-    </p>
-    <p>
-      Would you like to continue to apply for Permanent Disability Status with
-      StudentAid BC?
-    </p>
-    <template #footer>
+    <template v-slot:content>
+      <v-container>
+        <p>
+          If you’re eligible, you may receive funding for provincial and federal
+          grants/bursaries. Your information will be sent to Assistive
+          Technology BC (ATBC).
+        </p>
+      </v-container>
+    </template>
+    <template v-slot:footer>
       <v-btn outlined color="primary" @click="hidePDApplicationModal()"
         >No</v-btn
       >
       <v-btn color="primary" @click="applyPDStatus()">Yes</v-btn>
     </template>
-  </Dialog>
+  </ModalDialogBase>
 </template>
 
 <script lang="ts">
-import Dialog from "primevue/dialog";
+import ModalDialogBase from "@/components/generic/ModalDialogBase.vue";
 
 export default {
   components: {
-    Dialog,
+    ModalDialogBase,
   },
   props: {
     showModal: {
