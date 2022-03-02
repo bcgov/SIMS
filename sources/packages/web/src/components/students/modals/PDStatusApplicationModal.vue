199f159a0aae8615fd404fb4b4fe74e24deacb12
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-03-02 12:29:52 -0800

    remove v-container, add dialogType attribute

--- sources/packages/web/src/components/students/modals/PDStatusApplicationModal.vue
+++ sources/packages/web/src/components/students/modals/PDStatusApplicationModal.vue
@@ -1,16 +1,15 @@
 <template>
   <ModalDialogBase
+    dialogType="question"
     title="Apply for a permanent disability status"
     :showDialog="showDialog"
   >
     <template v-slot:content>
-      <v-container>
-        <p>
-          If you’re eligible, you may receive funding for provincial and federal
-          grants/bursaries. Your information will be sent to Assistive
-          Technology BC (ATBC).
-        </p>
-      </v-container>
+      <p>
+        If you’re eligible, you may receive funding for provincial and federal
+        grants/bursaries. Your information will be sent to Assistive Technology
+        BC (ATBC).
+      </p>
     </template>
     <template v-slot:footer>
       <v-btn outlined color="primary" @click="requestPD(false)">No</v-btn>
@@ -29,7 +28,7 @@ export default {
   },
   setup() {
     const { showDialog, showModal, resolvePromise } = useModalDialog<boolean>();
-    const requestPD = async (payload: boolean) => {
+    const requestPD = (payload: boolean) => {
       showDialog.value = false;
       resolvePromise(payload);
     };
