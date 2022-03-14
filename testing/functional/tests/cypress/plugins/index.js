843d63cca0930ffd1f09a85c5834f3b71e474c02
bfresh13 <98920571+bfresh13@users.noreply.github.com>
2022-03-14 16:08:01 -0400

    Removed mochawesome plugins.

--- testing/functional/tests/cypress/plugins/index.js
+++ testing/functional/tests/cypress/plugins/index.js
@@ -18,9 +18,4 @@
  module.exports = (on, config) => {
 
 }
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  require('cypress-mochawesome-reporter/plugin')(on);
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-};
+
