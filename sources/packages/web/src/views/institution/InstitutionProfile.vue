8981d4d5ed792b349c4dee1df08db8f345936b94
dheepak-aot <dheepak.rr@aot-technologies.com>
2022-03-14 12:38:22 -0700

    Sonar security fix

--- sources/packages/web/src/views/institution/InstitutionProfile.vue
+++ sources/packages/web/src/views/institution/InstitutionProfile.vue
@@ -3,7 +3,7 @@
     Please notice that the read-only information below is retrieved from your
     BCeID account and it is not possible to change it here. If any read-only
     information needs to be changed please visit
-    <a href="https://www.bceid.ca/" target="_blank">bceid.ca</a>.
+    <a href="https://www.bceid.ca/" target="_blank" rel="noopener">bceid.ca</a>.
   </Message>
   <full-page-container>
     <institution-profile-form
