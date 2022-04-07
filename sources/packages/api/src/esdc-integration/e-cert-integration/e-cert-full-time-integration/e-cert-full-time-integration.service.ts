bd64f77eac82a899653c48b8a19aca17b033787c
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2022-04-07 13:58:08 -0700

    Updated imports

--- sources/packages/api/src/esdc-integration/e-cert-integration/e-cert-full-time-integration/e-cert-full-time-integration.service.ts
+++ sources/packages/api/src/esdc-integration/e-cert-integration/e-cert-full-time-integration/e-cert-full-time-integration.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService, SshService } from "../../../services";
-import { ESDCIntegrationConfig } from "../../../types";
 import {
   combineDecimalPlaces,
   getDisbursementValuesByType,
