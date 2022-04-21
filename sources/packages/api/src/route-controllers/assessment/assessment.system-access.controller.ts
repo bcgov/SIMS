4a437acea45009d0e134474580c2985d3e9522a1
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2022-04-21 14:35:19 -0700

    Extracted flatten methods to a service

--- sources/packages/api/src/route-controllers/assessment/assessment.system-access.controller.ts
+++ sources/packages/api/src/route-controllers/assessment/assessment.system-access.controller.ts
@@ -26,8 +26,6 @@ import {
 import {
   ApplicationAssessmentAPIOutDTO,
   CreateDisbursementsDTO,
-  StudentAppealRequestAPIOutDTO,
-  SupportingUserAPIOutDTO,
   UpdateAssessmentDataDTO,
   UpdateAssessmentStatusDTO,
   UpdateAssessmentWorkflowIdDTO,
@@ -35,12 +33,7 @@ import {
 } from "./models/assessment.system-access.dto";
 import { AllowAuthorizedParty } from "../../auth/decorators";
 import { ClientTypeBaseRoute } from "../../types";
-import {
-  StudentAppealRequest,
-  SupportingUser,
-  SupportingUserType,
-} from "../../database/entities";
-import { DynamicAPIOutDTO } from "../models/common.dto";
+import { AssessmentControllerService } from "..";
 
 @AllowAuthorizedParty(AuthorizedParties.formsFlowBPM)
 @Controller("assessment")
@@ -50,6 +43,7 @@ export class AssessmentSystemAccessController extends BaseController {
     private readonly assessmentService: StudentAssessmentService,
     private readonly offeringService: EducationProgramOfferingService,
     private readonly disbursementScheduleService: DisbursementScheduleService,
+    private readonly assessmentControllerService: AssessmentControllerService,
   ) {
     super();
   }
@@ -111,77 +105,14 @@ export class AssessmentSystemAccessController extends BaseController {
       student: {
         studentPDStatus: application.student.studentPDVerified,
       },
-      supportingUsers: this.flattenSupportingUsersArray(
-        application.supportingUsers,
-      ),
-      appeals: this.flattenStudentAppeals(
+      supportingUsers:
+        this.assessmentControllerService.flattenSupportingUsersArray(
+          application.supportingUsers,
+        ),
+      appeals: this.assessmentControllerService.flattenStudentAppeals(
         assessment.studentAppeal?.appealRequests,
       ),
     };
-  }
-
-  /**
-   * Converts an arrays with supporting users to an object where every user
-   * will be a property. This will keep the supporting users dynamic (it can be
-   * extended to have a Parent3, Partner2 or even more types) and make easier to
-   * read and process these users in the workflow.
-   * @param supportingUsers supporting users to be converted.
-   * @returns object where every user is a property.
-   */
-  private flattenSupportingUsersArray(
-    supportingUsers: SupportingUser[],
-  ): DynamicAPIOutDTO<SupportingUserAPIOutDTO> {
-    if (!supportingUsers?.length) {
-      return null;
-    }
-    // Ensures that the users will be always ordered in the same way
-    supportingUsers.sort((userA, userB) => (userA.id > userB.id ? 1 : -1));
-    // Object to be returned.
-    const flattenedSupportingUsers =
-      {} as DynamicAPIOutDTO<SupportingUserAPIOutDTO>;
-    // Filter and process by type to have the items ordered also by the type (Parent1, Parent2, Partner1).
-    Object.keys(SupportingUserType).forEach((supportingUserType) => {
-      supportingUsers
-        .filter(
-          (supportingUser) =>
-            supportingUser.supportingUserType === supportingUserType,
-        )
-        .forEach((supportingUser, index) => {
-          const [craIncome] = supportingUser.craIncomeVerifications;
-          flattenedSupportingUsers[`${supportingUserType}${index + 1}`] = {
-            id: supportingUser.id,
-            supportingUserType: supportingUser.supportingUserType,
-            supportingData: supportingUser.supportingData,
-            reportedIncome: craIncome?.reportedIncome,
-            craReportedIncome: craIncome?.craReportedIncome,
-          };
-        });
-    });
-    return flattenedSupportingUsers;
-  }
-
-  /**
-   * Converts an arrays with student appeal requests to an object where every
-   * appeal request will be a property named by the form.io definition name used
-   * to execute the student appeal request submission.
-   * @param appealRequests approved student appeal requests.
-   * @returns object where every student appeal request is a property.
-   */
-  private flattenStudentAppeals(
-    appealRequests: StudentAppealRequest[],
-  ): DynamicAPIOutDTO<StudentAppealRequestAPIOutDTO> {
-    if (!appealRequests?.length) {
-      return null;
-    }
-    // Object to be returned.
-    const flattenedAppealRequests =
-      {} as DynamicAPIOutDTO<StudentAppealRequestAPIOutDTO>;
-    appealRequests.forEach((appealRequest) => {
-      flattenedAppealRequests[appealRequest.submittedFormName] = {
-        submittedData: appealRequest.submittedData,
-      };
-    });
-    return flattenedAppealRequests;
   }
 
   /**
