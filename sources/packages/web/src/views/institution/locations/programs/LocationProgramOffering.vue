e54f8d3089324bdb09d069cf64d5fb81815cafd2
dheepak-aot <dheepak.rr@aot-technologies.com>
2022-05-10 13:22:49 -0700

    Code review comments

--- sources/packages/web/src/views/institution/locations/programs/LocationProgramOffering.vue
+++ sources/packages/web/src/views/institution/locations/programs/LocationProgramOffering.vue
@@ -154,17 +154,6 @@ export default {
             ...programDetails,
           };
         }
-        /**
-         * The property clientType is populated for institution because
-         * the form.io for education program offering has a logic at it's root level panel
-         * to disable all the form inputs when clientType is not institution.
-         * The above mentioned logic is added to the panel of the form to display the
-         * form as read-only for ministry(AEST) user and also allow the hidden component values
-         * to be calculated.
-         *! If a form.io is loaded with readOnly attribute set to true, then the restricts
-         *! hidden components to calculate it's value by design.
-         */
-        initialData.value.clientType = ClientIdType.Institution;
       }
       if (isAESTUser.value) {
         if (props.offeringId) {
@@ -185,6 +174,17 @@ export default {
           );
         }
       }
+      /**
+       * The property clientType is populated for institution because
+       * the form.io for education program offering has a logic at it's root level panel
+       * to disable all the form inputs when clientType is not institution.
+       * The above mentioned logic is added to the panel of the form to display the
+       * form as read-only for ministry(AEST) user and also allow the hidden component values
+       * to be calculated.
+       *! If a form.io is loaded with readOnly attribute set to true, then the restricts
+       *! hidden components to calculate it's value by design.
+       */
+      initialData.value.clientType = AuthService.shared.authClientType;
     };
     onMounted(async () => {
       await loadFormData();
