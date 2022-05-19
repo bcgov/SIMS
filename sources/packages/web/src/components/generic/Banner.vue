f33aeeefc3aee3c04d5cc1d85a2b807f08fc0f23
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-05-19 06:31:52 -0700

    remove custom css class, apply vuetify css class

--- sources/packages/web/src/components/generic/Banner.vue
+++ sources/packages/web/src/components/generic/Banner.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-alert :class="getBackgroundClass" :type="color" variant="outlined">
+  <v-alert
+    class="v-alert--variant-contained-text"
+    :type="color"
+    variant="outlined"
+  >
     <template v-slot:title>
       <div class="label-bold-normal">{{ header }}</div>
     </template>
@@ -10,9 +14,6 @@
   </v-alert>
 </template>
 <script lang="ts">
-import { StatusChipTypes } from "@/components/generic/StatusChip.models";
-import { computed } from "vue";
-
 export default {
   props: {
     color: {
@@ -24,22 +25,6 @@ export default {
     summary: {
       type: String,
     },
-  },
-  setup(props: any) {
-    const getBackgroundClass = computed(() => {
-      switch (props.color) {
-        case StatusChipTypes.Success:
-          return "v-alert-background-success";
-        case StatusChipTypes.Warning:
-          return "v-alert-background-warning";
-        case StatusChipTypes.Error:
-          return "v-alert-background-error";
-        default:
-          return "";
-      }
-    });
-
-    return { getBackgroundClass };
   },
 };
 </script>
