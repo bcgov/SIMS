624ddf0d9940a5ec3bc092fb59bf473dd73f56c2
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-05-19 05:26:59 -0700

    change dto

--- sources/packages/api/src/route-controllers/education-program/models/save-education-program.dto.ts
+++ sources/packages/api/src/route-controllers/education-program/models/save-education-program.dto.ts
@@ -12,8 +12,37 @@ import {
 /**
  * Dto that represents education program with offering form object.
  */
-export interface EducationProgramWithOfferingsAPIOutDTO
-  extends EducationProgramDto {
+export class EducationProgramWithOfferingsAPIOutDTO {
+  name: string;
+  description?: string;
+  credentialType: string;
+  cipCode: string;
+  nocCode: string;
+  sabcCode: string;
+  regulatoryBody: string;
+  programDeliveryTypes: ProgramDeliveryTypes;
+  deliveredOnlineAlsoOnsite?: string;
+  sameOnlineCreditsEarned?: string;
+  earnAcademicCreditsOtherInstitution?: string;
+  courseLoadCalculation: string;
+  completionYears: string;
+  eslEligibility: string;
+  hasJointInstitution: string;
+  hasJointDesignatedInstitution: string;
+  programIntensity: ProgramIntensity;
+  institutionProgramCode?: string;
+  minHoursWeek?: string;
+  isAviationProgram?: string;
+  minHoursWeekAvi?: string;
+  entranceRequirements: EntranceRequirements;
+  hasWILComponent: string;
+  isWILApproved?: string;
+  wilProgramEligibility?: string;
+  hasTravel: string;
+  travelProgramEligibility?: string;
+  hasIntlExchange?: string;
+  intlExchangeProgramEligibility?: string;
+  programDeclaration: boolean;
   hasOfferings: boolean;
 }
 
