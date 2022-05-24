aefa9ab5531b60a993dbf5c5b362f4891c6ac7b9
bfresh13 <98920571+bhavikccits@users.noreply.github.com>
2022-05-24 13:43:49 -0400

    Added institutions PATCH test case.

--- testing/functional/tests/cypress/page-objects/Institution-objects/ManageDesignationsObject.ts
+++
@@ -1,9 +0,0 @@
-export default class ManageDesignationsObject {
-  manageDesignationButton() {
-    return cy.contains("Manage Designation");
-  }
-
-  designationAgreementsText() {
-    return cy.contains("Designation agreements");
-  }
-}
