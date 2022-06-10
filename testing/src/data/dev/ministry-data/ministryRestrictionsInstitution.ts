70f53114f05912afa696c184094aa88c2a38e725
bfresh13 <98920571+bhavikccits@users.noreply.github.com>
2022-06-10 14:09:11 -0400

    Updated code & moved testing folder in src.

---
+++ testing/src/data/dev/ministry-data/ministryRestrictionsInstitution.ts
@@ -0,0 +1,6 @@
+export enum restrictionsInstitution {
+  category = "Designation",
+  reason = "D - Institution restriction (missing from list) similar to V restriction",
+  notes = "Adding notes here about restrictions",
+  resolutionNotes = "Resolved",
+}
