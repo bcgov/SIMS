216dfdce9a73bfd72b6d4cd0fb3af0cf4d5b271a
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-07-28 15:05:23 -0700

    add roll back script (wip)

--- sources/packages/api/src-sql/DeleteCascades/Add-cascade-rules.sql
+++ sources/packages/api/src-sql/DeleteCascades/Add-cascade-rules.sql
@@ -15,3 +15,45 @@ ALTER TABLE
     sims.student_assessments
 ADD
     CONSTRAINT student_assessments_applications_id_fkey FOREIGN KEY(application_id) REFERENCES sims.applications(id);
+
+-- Create reference between application_student_files and applications --
+ALTER TABLE
+    sims.application_student_files
+ADD
+    CONSTRAINT application_student_files_student_file_id_fkey FOREIGN KEY(application_id) REFERENCES sims.applications(id);
+
+-- Create reference between cra_income_verifications and supporting_users --
+ALTER TABLE
+    sims.cra_income_verifications
+ADD
+    CONSTRAINT cra_income_verifications_supporting_user_id_fkey FOREIGN KEY(supporting_user_id) REFERENCES sims.supporting_users(id);
+
+-- Create reference between cra_income_verifications and applications --
+ALTER TABLE
+    sims.cra_income_verifications
+ADD
+    CONSTRAINT cra_income_verifications_application_id_fkey FOREIGN KEY(application_id) REFERENCES sims.applications(id);
+
+-- Create reference between designation_agreements and institutions --
+ALTER TABLE
+    sims.designation_agreements
+ADD
+    CONSTRAINT designation_agreements_institution_id_fkey FOREIGN KEY(institution_id) REFERENCES sims.institutions(id);
+
+-- Create reference between designation_agreement_locations and designation_agreements --
+ALTER TABLE
+    sims.designation_agreement_locations
+ADD
+    CONSTRAINT designation_agreement_locations_designation_agreement_id_fkey FOREIGN KEY(designation_agreement_id) REFERENCES sims.designation_agreements(id);
+
+-- Create reference between designation_agreement_locations and institution_locations --
+ALTER TABLE
+    sims.designation_agreement_locations
+ADD
+    CONSTRAINT designation_agreement_locations_location_id_fkey FOREIGN KEY(location_id) REFERENCES sims.institution_locations(id);
+
+-- Create reference between disbursement_feedback_errors and disbursement_schedules --
+ALTER TABLE
+    sims.disbursement_feedback_errors
+ADD
+    CONSTRAINT disbursement_feedback_errors_disbursement_schedule_id_fkey FOREIGN KEY(disbursement_schedule_id) REFERENCES sims.disbursement_schedules(id);
