397552eb13d1bbc9b26a01795c8d7e1685690680
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-07-28 07:31:30 -0700

    wip

--- sources/packages/api/src-sql/DeleteCascades/Drop-cascade-rules.sql
+++ sources/packages/api/src-sql/DeleteCascades/Drop-cascade-rules.sql
@@ -2,9 +2,27 @@
 ALTER TABLE
     sims.application_exception_requests DROP CONSTRAINT IF EXISTS application_exception_requests_application_exception_id_fkey;
 
+-- Create reference between application_exception_requests and application_exceptions --
+ALTER TABLE
+    sims.application_exception_requests
+ADD
+    CONSTRAINT application_exception_requests_application_exceptions_id_fkey FOREIGN KEY(application_exception_id) REFERENCES sims.application_exceptions(id);
+
 -- Delete reference between student_assessments and applications --
 ALTER TABLE
     sims.student_assessments DROP CONSTRAINT IF EXISTS student_assessments_application_id_fkey;
+
+-- Create reference between applications and student_assessments --
+ALTER TABLE
+    sims.applications
+ADD
+    CONSTRAINT applications_student_assessments_id_fkey FOREIGN KEY(current_assessment_id) REFERENCES sims.student_assessments(id);
+
+-- Create reference between student_assessments and applications --
+ALTER TABLE
+    sims.student_assessments
+ADD
+    CONSTRAINT student_assessments_applications_id_fkey FOREIGN KEY(application_id) REFERENCES sims.applications(id);
 
 -- Delete reference between applications and students --
 ALTER TABLE
