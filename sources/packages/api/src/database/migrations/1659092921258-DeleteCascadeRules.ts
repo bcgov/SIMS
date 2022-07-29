9c20cf879df6ba21b78043bc9a138982cc91c6f5
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-07-29 04:10:51 -0700

    add typeorm script

---
+++ sources/packages/api/src/database/migrations/1659092921258-DeleteCascadeRules.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { getSQLFileData } from "../../utilities";
+
+export class DeleteCascadeRules1659092921258 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(getSQLFileData("Drop-cascade.sql"));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(getSQLFileData("Add-cascade.sql"));
+  }
+}
