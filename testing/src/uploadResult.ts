c5f72cfa3a3bcd6d87e692cb6ef8892e3c25f138
Velang RN <yagnavelang@Velangneeswaras-MacBook-Pro-2.local>
2022-08-22 11:22:37 -0700

    Addressing the code review comments WRT axios

--- testing/src/uploadResult.ts
+++ testing/src/uploadResult.ts
@@ -6,16 +6,17 @@ const TQ_USERNAME = process.env.TQ_USER_NAME;
 const TQ_PASSWORD = process.env.TQ_PASSWORD;
 const TQ_SECRET = process.env.TQ_SECRET;
 const TQ_RUN_ID = process.env.TQ_RUN_ID; //( default id is 26606. Will need to change once we have integrated TQ account for the team)
+const TQ_API_URL = 'https://api.testquality.com/api'
 
-const getAuthToken = async (): Promise<AxiosResponse<any>> => {
-  const url = "https://api.testquality.com/api/oauth/access_token";
-  const body = JSON.stringify({
+async function getAuthToken (): Promise<AxiosResponse<String>> {
+  const auth_url = `${TQ_API_URL}/oauth/access_token`;
+  const body = {
     grant_type: "password",
     client_id: "2",
     client_secret: TQ_SECRET,
     username: TQ_USERNAME,
     password: TQ_PASSWORD,
-  });
+  };
   const settings = {
     headers: {
       "Content-Type": "application/json",
@@ -23,7 +24,7 @@ const getAuthToken = async (): Promise<AxiosResponse<any>> => {
   };
   try {
     console.log("Fetching the token from TestQuality");
-    const response = await axios.post(url, body, settings);
+    const response = await axios.post(auth_url, body, settings);
     console.log("Token Fetched");
     return response.data["access_token"];
   } catch (error) {
@@ -34,26 +35,23 @@ const getAuthToken = async (): Promise<AxiosResponse<any>> => {
 
 async function uploadResultFile() {
   const auth = await getAuthToken();
-  const url = `https://api.testquality.com/api/plan/${TQ_RUN_ID}/junit_xml`;
+  const url = `${TQ_API_URL}/plan/${TQ_RUN_ID}/junit_xml`;
   var formData = new FormData();
   var resultFile = fs.createReadStream("test-results.xml");
   formData.append("file", resultFile, "test-results.xml");
   try {
-    if (String(auth).length > 1) {
-      const settings = {
-        headers: {
-          ...formData.getHeaders(),
-          Authorization: `Bearer ${auth}`,
-        },
-      };
-      const response = await axios.post(url, formData, settings);
-      console.log("Successfully uploaded the test results file to TestQuality");
-      return true;
-    }
+    const settings = {
+      headers: {
+        ...formData.getHeaders(),
+        Authorization: `Bearer ${auth}`,
+      },
+    };
+    await axios.post(url, formData, settings);
+    console.log("Successfully uploaded the test results file to TestQuality");
+    return true;
   } catch (error) {
     console.error(error);
     throw error;
   }
 }
-
 uploadResultFile();
