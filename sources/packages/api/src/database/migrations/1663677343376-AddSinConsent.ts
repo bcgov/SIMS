f8472620e2c6dfd389898d41e8716c38d8b37931
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-09-20 11:03:42 -0600

    add sinconsent.

--- sources/packages/api/src/database/migrations/1663677343376-AddSinConsent.ts
+++ sources/packages/api/src/database/migrations/1663677343376-AddSinConsent.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 import { getSQLFileData } from "../../utilities";
 
-export class AddSINConsent1663354672299 implements MigrationInterface {
+export class AddSinConsent1663677343376 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       getSQLFileData("Add-sin-consent-col.sql", "Student"),
