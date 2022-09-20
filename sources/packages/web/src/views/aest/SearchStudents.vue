bcee4e05a74261b502fe21dfd8fb56f1ea18df85
guru-aot <gurumoorthy.mohan@aot-technologies.com>
2022-09-20 17:27:50 -0600

    [Web][SIMS-#1115] UI updates ministry Part 2 (#1411)
    UI Part 2 Ministry

--- sources/packages/web/src/views/aest/SearchStudents.vue
+++ sources/packages/web/src/views/aest/SearchStudents.vue
@@ -1,104 +1,113 @@
 <template>
   <full-page-container>
-    <body-header title="Search Students"> </body-header>
-    <content-group>
-      <v-row
-        ><v-col>
-          <v-text-field
-            label="Application number"
-            density="compact"
-            data-cy="appNumber"
-            variant="outlined"
-            v-model="appNumber"
-            @keyup.enter="searchStudents"
-            hide-details
-          />
-        </v-col>
-        <v-col>
-          <v-text-field
-            label="SIN"
-            density="compact"
-            data-cy="sin"
-            variant="outlined"
-            v-model="sin"
-            @keyup.enter="searchStudents"
-            hide-details
-          />
-        </v-col>
-        <v-col>
-          <v-text-field
-            label="Given names"
-            density="compact"
-            data-cy="firstName"
-            variant="outlined"
-            v-model="firstName"
-            @keyup.enter="searchStudents"
-            hide-details
-          /> </v-col
-        ><v-col>
-          <v-text-field
-            label="Last name"
-            density="compact"
-            data-cy="lastName"
-            variant="outlined"
-            v-model="lastName"
-            @keyup.enter="searchStudents"
-            hide-details
-          /> </v-col
-        ><v-col
-          ><v-btn
-            :disabled="
-              !appNumber && !firstName && !lastName && !isSINValid(sin)
-            "
-            color="primary"
-            class="p-button-raised"
-            data-cy="searchStudents"
-            @click="searchStudents()"
+    <body-header
+      title="Search Students"
+      subTitle="Lookup students by entering their information below."
+    >
+    </body-header>
+    <v-form ref="searchStudentsForm">
+      <content-group class="mb-8">
+        <v-row
+          ><v-col>
+            <v-text-field
+              label="Application number"
+              density="compact"
+              data-cy="appNumber"
+              variant="outlined"
+              v-model="appNumber"
+              @keyup.enter="searchStudents"
+              hide-details
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="SIN"
+              density="compact"
+              data-cy="sin"
+              variant="outlined"
+              v-model="sin"
+              @keyup.enter="searchStudents"
+              hide-details
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Given names"
+              density="compact"
+              data-cy="firstName"
+              variant="outlined"
+              v-model="firstName"
+              @keyup.enter="searchStudents"
+              hide-details
+            /> </v-col
+          ><v-col>
+            <v-text-field
+              label="Last name"
+              density="compact"
+              data-cy="lastName"
+              variant="outlined"
+              v-model="lastName"
+              @keyup.enter="searchStudents"
+              hide-details
+            /> </v-col
+          ><v-col
+            ><v-btn
+              color="primary"
+              class="p-button-raised"
+              data-cy="searchStudents"
+              @click="searchStudents()"
+            >
+              Search
+            </v-btn></v-col
           >
-            Search
-          </v-btn></v-col
-        >
-      </v-row>
-    </content-group>
-
-    <content-group v-if="studentsFound" class="mt-8">
-      <toggle-content :toggled="!students?.length">
-        <DataTable :value="students">
-          <Column field="firstName" header="First Name" :sortable="true">
-            <template #body="slotProps">
-              <div class="p-text-capitalize">
-                {{ slotProps.data.firstName }}
-              </div>
-            </template>
-          </Column>
-          <Column field="lastName" header="Last Name" :sortable="true">
-            <template #body="slotProps">
-              <div class="p-text-capitalize">
-                {{ slotProps.data.lastName }}
-              </div>
-            </template>
-          </Column>
-          <Column field="birthDate" header="Date of Birth">
-            <template #body="slotProps">
-              <div class="p-text-capitalize">
-                {{ dateOnlyLongString(slotProps.data.birthDate) }}
-              </div>
-            </template>
-          </Column>
-          <Column header="Action">
-            <template #body="slotProps">
-              <v-btn
-                color="primary"
-                class="p-button-raised"
-                data-cy="viewStudent"
-                @click="goToViewStudent(slotProps.data.id)"
-                >View</v-btn
-              >
-            </template>
-          </Column>
-        </DataTable>
-      </toggle-content>
-    </content-group>
+        </v-row>
+        <v-input
+          :rules="[isValidSearch()]"
+          hide-details="auto"
+          error
+        /> </content-group
+    ></v-form>
+    <template v-if="studentsFound">
+      <body-header title="Results" />
+      <content-group>
+        <toggle-content :toggled="!students?.length">
+          <DataTable :value="students">
+            <Column field="firstName" header="First Name" :sortable="true">
+              <template #body="slotProps">
+                <div class="p-text-capitalize">
+                  {{ slotProps.data.firstName }}
+                </div>
+              </template>
+            </Column>
+            <Column field="lastName" header="Last Name" :sortable="true">
+              <template #body="slotProps">
+                <div class="p-text-capitalize">
+                  {{ slotProps.data.lastName }}
+                </div>
+              </template>
+            </Column>
+            <Column field="birthDate" header="Date of Birth">
+              <template #body="slotProps">
+                <div class="p-text-capitalize">
+                  {{ dateOnlyLongString(slotProps.data.birthDate) }}
+                </div>
+              </template>
+            </Column>
+            <Column header="Action">
+              <template #body="slotProps">
+                <v-btn
+                  color="primary"
+                  class="p-button-raised"
+                  data-cy="viewStudent"
+                  @click="goToViewStudent(slotProps.data.id)"
+                  >View</v-btn
+                >
+              </template>
+            </Column>
+          </DataTable>
+        </toggle-content>
+      </content-group>
+    </template>
   </full-page-container>
 </template>
 <script lang="ts">
@@ -111,9 +120,11 @@ import {
   SearchStudentAPIOutDTO,
 } from "@/services/http/dto";
 import { useFormatters, useSnackBar, useValidators } from "@/composables";
+import { VForm } from "@/types";
 
 export default {
   setup() {
+    const searchStudentsForm = ref({} as VForm);
     const snackBar = useSnackBar();
     const { dateOnlyLongString } = useFormatters();
     const { isSINValid } = useValidators();
@@ -129,7 +140,22 @@ export default {
         params: { studentId: studentId },
       });
     };
+    const isValidSearch = () => {
+      const hasNoInput =
+        !appNumber.value && !firstName.value && !sin.value && !lastName.value;
+      if (hasNoInput) {
+        return "Please provide at least one search parameter.";
+      }
+      if (sin.value) {
+        return isSINValid(sin.value) || "Please provide a proper SIN.";
+      }
+      return true;
+    };
     const searchStudents = async () => {
+      const validationResult = await searchStudentsForm.value.validate();
+      if (!validationResult.valid) {
+        return;
+      }
       const payload: SearchStudentAPIInDTO = {
         appNumber: appNumber.value,
         firstName: firstName.value,
@@ -156,6 +182,8 @@ export default {
       goToViewStudent,
       dateOnlyLongString,
       isSINValid,
+      searchStudentsForm,
+      isValidSearch,
     };
   },
 };
