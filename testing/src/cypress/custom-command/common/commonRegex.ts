87bfc301c226799842e5b71baecfb2d5e0722f78
Velang RN <yagnavelang@Velangneeswaras-MacBook-Pro-2.local>
2022-09-26 08:17:06 -0700

    Fixing SQ checks

--- testing/src/cypress/custom-command/common/commonRegex.ts
+++ testing/src/cypress/custom-command/common/commonRegex.ts
@@ -1,8 +1,6 @@
 export default class CommonRegex {
-  dateTimeRegex = new RegExp("\\d{4}-\\d{2}-\\d{2}");
-  timeStampRegex = new RegExp(
-    "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z"
-  );
-  phoneNumberRegex = new RegExp("\\d{10,20}");
-  emailRegex = new RegExp("\\S*@[a-zA-Z0-9]{2,}.[a-zA-Z]{2,}");
+  dateTimeRegex = /\d{4}-\d{2}-\d{2}/;
+  timeStampRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/;
+  phoneNumberRegex = /\d{10,20}/;
+  emailRegex = /\S*@[a-zA-Z0-9]{2,}.[a-zA-Z]{2,}/;
 }
