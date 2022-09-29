c0a7383dfb1178a0223ad91928fd531e386a354c
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2022-09-29 09:28:46 -0700

    Migrations adjusted

--- sources/packages/backend/apps/db-migrations/src/main.ts
+++ sources/packages/backend/apps/db-migrations/src/main.ts
@@ -1,17 +1,5 @@
 import { DataSource } from "typeorm";
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
-
-const ormConfig: PostgresConnectionOptions = {
-  type: "postgres",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-  database: process.env.POSTGRES_DB || "aest",
-  username: process.env.POSTGRES_USER || "admin",
-  password: process.env.POSTGRES_PASSWORD,
-  schema: process.env.DB_SCHEMA || "sims",
-  synchronize: false,
-  migrations: ["apps/db-migrations/src/migrations/*{.ts,.js}"],
-};
+import { ormConfig } from "./data-source";
 
 async function bootstrap() {
   const migrationDataSource = new DataSource({
