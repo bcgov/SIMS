44eaeb508cd47669e350417270e4812ddfa8ee42
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2022-10-17 17:04:14 -0600

    removed console

--- sources/packages/web/src/components/common/notes/CreateNoteModal.vue
+++ sources/packages/web/src/components/common/notes/CreateNoteModal.vue
@@ -101,7 +101,6 @@ export default defineComponent({
           title: selectItem,
           value: type[selectItem],
         });
-        console.log(selectItem);
       }
       return selectItemsArray;
     };
