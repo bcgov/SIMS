c0cbeec719045b2b592f99320cc113406a5e536f
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2022-10-21 10:23:51 -0700

    ConfigService comments.

--- sources/packages/backend/apps/workers/src/services/config/config.service.ts
+++ sources/packages/backend/apps/workers/src/services/config/config.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { IConfig } from "..";
 
+/**
+ * Workers configurations.
+ */
 @Injectable()
 export class ConfigService {
+  /**
+   * Environment configurations.
+   */
   readonly env: IConfig;
 
   constructor() {
     this.env = this.getConfig();
   }
 
+  /**
+   * Loads all environment variables and parsed them.
+   * @returns parsed environment variables.
+   */
   private getConfig(): IConfig {
     return {
       bypassCRAIncomeVerification:
