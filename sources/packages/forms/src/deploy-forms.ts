b3c2ae968dfedbe90da92e3acb9a430fcca33eb9
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2022-10-25 15:56:39 -0700

    [CI/CD][SIMS #1442]Call reusable workflows (throw errors) (#1487)

--- sources/packages/forms/src/deploy-forms.ts
+++ sources/packages/forms/src/deploy-forms.ts
@@ -19,50 +19,48 @@ interface FormDeployInfo {
  * Script main execution method.
  */
 (async () => {
-  try {
-    console.info(
-      `**** Deploying Form IO Definitions to ${process.env.FORMS_URL} ****`
-    );
-    const directory = path.resolve(__dirname, `../src/form-definitions`);
-    console.info(`Getting form definitions from ${directory}`);
-    const files: string[] = fs.readdirSync(directory);
-    if (files.length === 0) {
-      console.info("No files found to be deployed!");
-      return;
-    }
-    console.info("Acquiring access token...");
-    const authHeader = await createAuthHeader();
-    const formDeployStatuses: FormDeployInfo[] = [];
-    for (const file of files) {
-      console.info(`Deploying ${file}`);
-      const filePath = path.join(directory, file);
-      const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
-      const jsonContent = JSON.parse(fileContent);
-      const formAlias = file.replace(path.extname(file), "");
-      const isDeployed = await isFormDeployed(formAlias, authHeader);
-      const formDeployStatus = { formAlias } as FormDeployInfo;
-      try {
-        if (isDeployed) {
-          await updateForm(formAlias, jsonContent, authHeader);
-          formDeployStatus.result = "updated";
-        } else {
-          await createForm(jsonContent, authHeader);
-          formDeployStatus.result = "created";
-        }
-      } catch (error) {
-        formDeployStatus.result = "error";
-        // Case an error happen, log and try to continue.
-        console.error(
-          `Error while ${
-            isDeployed ? "updating" : "creating"
-          } form ${formAlias}`
-        );
+  console.info(
+    `**** Deploying Form IO Definitions to ${process.env.FORMS_URL} ****`
+  );
+  const directory = path.resolve(__dirname, `../src/form-definitions`);
+  console.info(`Getting form definitions from ${directory}`);
+  const files: string[] = fs.readdirSync(directory);
+  if (files.length === 0) {
+    console.info("No files found to be deployed!");
+    return;
+  }
+  console.info("Acquiring access token...");
+  const authHeader = await createAuthHeader();
+  const formDeployStatuses: FormDeployInfo[] = [];
+  for (const file of files) {
+    console.info(`Deploying ${file}`);
+    const filePath = path.join(directory, file);
+    const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
+    const jsonContent = JSON.parse(fileContent);
+    const formAlias = file.replace(path.extname(file), "");
+    const isDeployed = await isFormDeployed(formAlias, authHeader);
+    const formDeployStatus = { formAlias } as FormDeployInfo;
+    try {
+      if (isDeployed) {
+        await updateForm(formAlias, jsonContent, authHeader);
+        formDeployStatus.result = "updated";
+      } else {
+        await createForm(jsonContent, authHeader);
+        formDeployStatus.result = "created";
       }
-      formDeployStatuses.push(formDeployStatus);
+    } catch (error) {
+      formDeployStatus.result = "error";
+      // Case an error happen, log and try to continue.
+      console.error(
+        `Error while ${isDeployed ? "updating" : "creating"} form ${formAlias}`
+      );
     }
-    console.table(formDeployStatuses);
-  } catch (error: unknown) {
-    console.error(`Exception occurs during Form IO deployment: ${error}`);
-    throw error;
+    formDeployStatuses.push(formDeployStatus);
+  }
+  console.table(formDeployStatuses);
+  if (formDeployStatuses.some((status) => status.result === "error")) {
+    throw new Error(
+      "At least one error happened during the deployment process. Please check the logs for further information."
+    );
   }
 })();
--- sources/packages/workflow/src/deploy.ts
+++ sources/packages/workflow/src/deploy.ts
@@ -9,36 +9,32 @@ dotenv.config({ path: path.join(__dirname, "../../../../.env") });
  * Script main execution method.
  */
 (async () => {
-  try {
-    console.info(`**** Deploying to Camunda ****\n`);
-    console.info(`Deploying to Zeebe address ${process.env.ZEEBE_ADDRESS}`);
+  console.info(`**** Deploying to Camunda ****\n`);
+  console.info(`Deploying to Zeebe address ${process.env.ZEEBE_ADDRESS}`);
 
-    const directory = path.resolve(__dirname, `../BPMN/camunda-8`);
-    console.info(`Getting resources from ${directory}`);
-    const fileNames: string[] = fs.readdirSync(directory);
-    if (fileNames.length === 0) {
-      console.info("No files found to be deployed!");
-      return;
-    }
-    const filesPaths = fileNames.map((fileName) =>
-      path.join(directory, fileName)
+  const directory = path.resolve(__dirname, `../BPMN/camunda-8`);
+  console.info(`Getting resources from ${directory}`);
+  const fileNames: string[] = fs.readdirSync(directory);
+  if (fileNames.length === 0) {
+    console.info("No files found to be deployed!");
+    return;
+  }
+  const filesPaths = fileNames.map((fileName) =>
+    path.join(directory, fileName)
+  );
+  console.info(`Files found: ${fileNames.join(", ")}`);
+  const zeebeClient = new ZBClient();
+  try {
+    const deploymentResults = await zeebeClient.deployProcess(filesPaths);
+    console.info(`\nDeployment successful!\n`);
+    console.info(
+      "Please see below the definitions deployed (non-modified definitions will not generate a new version)."
     );
-    console.info(`Files found: ${fileNames.join(", ")}`);
-    const zeebeClient = new ZBClient();
-    try {
-      const deploymentResults = await zeebeClient.deployProcess(filesPaths);
-      console.info(`\nDeployment successful!\n`);
-      console.info(
-        "Please see below the definitions deployed (non-modified definitions will not generate a new version)."
-      );
-      console.table(deploymentResults.processes);
-      console.info(`Deployment key: ${deploymentResults.key}`);
-    } catch (error: unknown) {
-      console.error("Error while executing the deployment.");
-      console.error(error);
-    }
-  } catch (error) {
-    console.error(`Exception occurs during Form IO deployment: ${error}`);
+    console.table(deploymentResults.processes);
+    console.info(`Deployment key: ${deploymentResults.key}`);
+  } catch (error: unknown) {
+    console.error("Error while executing the deployment.");
+    console.error(error);
     throw error;
   }
 })();
