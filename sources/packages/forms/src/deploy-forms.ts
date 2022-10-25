caf67e10e956aadac9985de7dad5787ebeb56466
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2022-10-25 13:59:05 -0700

    [CI/CD][SIMS #1442]Added Camunda and forms deploy to TEST (#1484)
    Included the calls to deploy Camunda definitions and Form.io definitions in the TEST workflow.

--- sources/packages/forms/src/deploy-forms.ts
+++ sources/packages/forms/src/deploy-forms.ts
@@ -26,33 +26,27 @@ interface FormDeployInfo {
     const directory = path.resolve(__dirname, `../src/form-definitions`);
     console.info(`Getting form definitions from ${directory}`);
     const files: string[] = fs.readdirSync(directory);
-
     if (files.length === 0) {
       console.info("No files found to be deployed!");
       return;
     }
-
     console.info("Acquiring access token...");
     const authHeader = await createAuthHeader();
-
     const formDeployStatuses: FormDeployInfo[] = [];
     for (const file of files) {
-      console.info(`Deploying form definition ${file}`);
+      console.info(`Deploying ${file}`);
       const filePath = path.join(directory, file);
       const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
       const jsonContent = JSON.parse(fileContent);
       const formAlias = file.replace(path.extname(file), "");
       const isDeployed = await isFormDeployed(formAlias, authHeader);
-
       const formDeployStatus = { formAlias } as FormDeployInfo;
       try {
         if (isDeployed) {
-          // TODO: Enable it back, temporarily disabled for testing.
-          // await updateForm(formAlias, jsonContent, authHeader);
+          await updateForm(formAlias, jsonContent, authHeader);
           formDeployStatus.result = "updated";
         } else {
-          // TODO: Enable it back, temporarily disabled for testing.
-          // await createForm(jsonContent, authHeader);
+          await createForm(jsonContent, authHeader);
           formDeployStatus.result = "created";
         }
       } catch (error) {
