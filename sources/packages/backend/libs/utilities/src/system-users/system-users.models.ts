a9c0af286ce9a6e33cf82604ef0917973ac767df
Ann <ann.jacob@aot-technologies.com>
2022-11-25 15:06:58 -0500

    system audit user

---
+++ sources/packages/backend/libs/utilities/src/system-users/system-users.models.ts
@@ -0,0 +1,15 @@
+/**
+ * System user enum.
+ */
+export enum SystemUser {
+  LastName = "system-user",
+  Email = "dev_sabc@gov.bc.ca",
+  UserName = "8fb44f70-6ce6-11ed-b307-8743a2da47ef@system",
+}
+
+/**
+ *  System user interface,
+ */
+export interface SystemUserDetails {
+  id: number;
+}
---
+++ sources/packages/backend/libs/utilities/src/system-users/system-users.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "@sims/sims-db";
+import { Repository } from "typeorm";
+import { SystemUser, SystemUserDetails } from "./system-users.models";
+
+@Injectable()
+export class SystemUsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  /**
+   * Get system user.If not found will create a new
+   * system user and return the user details.
+   * @return system user id.
+   */
+  private async getSystemUser(): Promise<User> {
+    const existingUser = await this.userRepo.findOne({
+      select: {
+        id: true,
+      },
+      where: {
+        userName: SystemUser.UserName,
+      },
+    });
+    if (existingUser) {
+      return existingUser;
+    }
+    // Create new system user if not exists.
+    const user = new User();
+    user.userName = SystemUser.UserName;
+    user.firstName = null;
+    user.lastName = SystemUser.LastName;
+    user.email = SystemUser.Email;
+    return this.userRepo.save(user);
+  }
+
+  /**
+   * Get system user details from the cache.
+   * If not found will create a new system user
+   * and return the user details.
+   * @return system user details.
+   */
+  async queueSystemUser(): Promise<SystemUserDetails> {
+    const systemUserKey = "systemUserDetails";
+    if (this[systemUserKey]) {
+      return this[systemUserKey] as SystemUserDetails;
+    }
+    this[systemUserKey] = {
+      id: (await this.getSystemUser()).id,
+    };
+    return this[systemUserKey];
+  }
+}
