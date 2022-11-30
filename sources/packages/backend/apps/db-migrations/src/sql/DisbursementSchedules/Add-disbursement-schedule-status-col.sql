cc0927745a5d895e9e681fd7ddb4667b85113612
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2022-11-29 18:12:11 -0800

    DB migration

--- sources/packages/backend/apps/db-migrations/src/sql/DisbursementSchedules/Add-disbursement-schedule-status-col.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/DisbursementSchedules/Add-disbursement-schedule-status-col.sql
@@ -3,4 +3,4 @@ ALTER TABLE
 ADD
     COLUMN IF NOT EXISTS disbursement_schedule_status sims.disbursement_schedule_status NOT NULL DEFAULT 'Pending';
 
-COMMENT ON COLUMN sims.disbursement_schedules.coe_status IS 'Indicates if the money amount information was already sent to be paid to the student.';
+COMMENT ON COLUMN sims.disbursement_schedules.disbursement_schedule_status IS 'Indicates if the money amount information was already sent to be paid to the student.';
