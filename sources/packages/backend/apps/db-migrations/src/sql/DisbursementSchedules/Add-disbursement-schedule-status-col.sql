76117c78b125ee79c9f0f83452decb4a9ebef5f8
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2022-11-29 19:38:52 -0800

    DB migration one more time

--- sources/packages/backend/apps/db-migrations/src/sql/DisbursementSchedules/Add-disbursement-schedule-status-col.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/DisbursementSchedules/Add-disbursement-schedule-status-col.sql
@@ -1,4 +1,4 @@
 ALTER TABLE
     sims.disbursement_schedules
 ADD
-    COLUMN IF NOT EXISTS test_2 INT;
+    COLUMN IF NOT EXISTS disbursement_schedule_status VARCHAR(50) NOT NULL DEFAULT 'Pending';
