e9368d5a09fd7390dad06602ee433395781d053f
Ann <ann.jacob@aot-technologies.com>
2022-12-07 19:50:23 -0500

    inital commit

--- sources/packages/backend/libs/services/src/constants/system-configurations-constants.ts
+++ sources/packages/backend/libs/services/src/constants/system-configurations-constants.ts
@@ -9,17 +9,20 @@ export const ZEEBE_PUBLISH_MESSAGE_DEFAULT_TIME_TO_LEAVE =
 
 /**
  * Number of attempts that a queue will retry a failed job.
+ *  todo: ann remove
  */
 const QUEUE_RETRY_ATTEMPTS = 3;
 
 /**
  * Delay in milliseconds at which every retry operation is performed on a failed job.
+ * todo: ann remove
  */
 const QUEUE_RETRY_DELAY = 180000;
 
 /**
  * Default retry config to be used for a queue which does not
  * use a queue specific retry configuration.
+ * todo:ann  remove
  */
 export const QUEUE_RETRY_DEFAULT_CONFIG = {
   /**
--- sources/packages/backend/libs/services/src/queue/queue.service.ts
+++ sources/packages/backend/libs/services/src/queue/queue.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { QueueConfiguration } from "@sims/sims-db";
 import { QueueConfigurationDetails } from "@sims/sims-db/entities/queue-configuration.type";
+import { QueueNames } from "@sims/utilities";
+import Bull from "bull";
 import { FindOptionsSelect, Repository } from "typeorm";
 import { QueueModel } from "./model/queue.model";
 
 @Injectable()
 export class QueueService {
+  private queueConfiguration: QueueConfiguration[] = undefined;
   constructor(
     @InjectRepository(QueueConfiguration)
     private queueConfigurationRepo: Repository<QueueConfiguration>,
@@ -17,13 +20,17 @@ export class QueueService {
    * @returns queue configuration.
    */
   async getAllQueueConfigurations(): Promise<QueueConfiguration[]> {
-    return this.queueConfigurationRepo.find({
+    if (this.queueConfiguration) {
+      return this.queueConfiguration;
+    }
+    this.queueConfiguration = await this.queueConfigurationRepo.find({
       select: {
         queueName: true,
         queueConfiguration:
           true as FindOptionsSelect<QueueConfigurationDetails>,
       },
     });
+    return this.queueConfiguration;
   }
 
   /**
@@ -36,5 +43,23 @@ export class QueueService {
       name: queue.queueName,
       dashboardReadonly: queue.queueConfiguration.dashboardReadonly,
     }));
+  }
+
+  /**
+   * Get queue configuration
+   * @param name queue name
+   * @returns queue configuration.
+   * todo: ann promise any
+   */
+  async getQueueConfiguration(name: QueueNames): Promise<Bull.JobOptions> {
+    const queues = await this.getAllQueueConfigurations();
+    const [queueConfig] = queues.filter((queue) => queue.queueName === name);
+    return {
+      attempts: queueConfig.queueConfiguration.retry,
+      backoff: queueConfig.queueConfiguration.retryInterval,
+      repeat: {
+        cron: queueConfig.queueConfiguration.cron,
+      },
+    };
   }
 }
