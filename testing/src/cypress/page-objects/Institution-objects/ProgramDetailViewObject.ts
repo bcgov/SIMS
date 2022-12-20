e7e87058d5f001d49224d2163acd3039ea6c884c
velang01 <110494396+velang01@users.noreply.github.com>
2022-12-19 20:33:27 -0800

    [cypress][SIMS #1549] Institution Programs (#1622)
    - Updating/Adding institution program tests
    - Adding respective cy-ids to support the above tests

---
+++ testing/src/cypress/page-objects/Institution-objects/ProgramDetailViewObject.ts
@@ -0,0 +1,47 @@
+import BaseMethods from "./BaseMethods";
+
+export default class ProgramDetailViewObject extends BaseMethods {
+  programName() {
+    return this.getElementByCyId("programName");
+  }
+
+  programStatus() {
+    return this.getElementByCyId("programStatus");
+  }
+
+  editButton() {
+    return this.getElementByCyId("programButtonAction");
+  }
+
+  programDescription() {
+    return this.getElementByCyId("programDescription");
+  }
+
+  programCredentialType() {
+    return this.getElementByCyId("programCredential");
+  }
+
+  programCIP() {
+    return this.getElementByCyId("programCIP");
+  }
+
+  programNOCCode() {
+    return this.getElementByCyId("programNOCCode");
+  }
+
+  programCode() {
+    return this.getElementByCyId("programCode");
+  }
+
+  programDetailHeader() {
+    return this.getElementByCyId("programDetailHeader");
+  }
+
+  offeringSearchBox() {
+    return this.getElementByCyId("searchBox");
+  }
+
+  addOfferingButton() {
+    return this.getElementByCyId("addNewOfferingButton");
+  }
+}
