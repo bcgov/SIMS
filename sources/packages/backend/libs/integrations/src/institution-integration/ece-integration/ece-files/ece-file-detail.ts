887115c7cb25aa4d97e551d0836f70fe7da7ba9e
guru-aot <gurumoorthy.mohan@aot-technologies.com>
2023-01-10 11:27:13 -0700

    #486 - COE send file for institutions (#1641)
    COE send file for institutions

---
+++ sources/packages/backend/libs/integrations/src/institution-integration/ece-integration/ece-files/ece-file-detail.ts
@@ -0,0 +1,65 @@
+import { DisbursementValue } from "@sims/sims-db";
+import { END_OF_LINE, round, StringBuilder } from "@sims/utilities";
+import {
+  DATE_FORMAT,
+  NUMBER_FILLER,
+  RecordTypeCodes,
+  SPACE_FILLER,
+} from "../models/ece-integration.model";
+import { ECERequestFileLine } from "./ece-file-line";
+
+/**
+ * Record of a ECE request file.
+ * The documentation about it is available on the document
+ * 'SIMSSFAS - Institution File layouts In Analysis Folder'.
+ */
+export class ECERequestFileDetail implements ECERequestFileLine {
+  recordTypeCode: RecordTypeCodes;
+  institutionCode: string;
+  disbursementValues: DisbursementValue[];
+  sin: string;
+  studentLastName: string;
+  studentGivenName: string;
+  birthDate: string;
+  applicationNumber: string;
+  institutionStudentNumber: string;
+  courseLoad: string;
+  studyStartDate: string;
+  studyEndDate: string;
+  disbursementDate: string;
+
+  getFixedFormat(): string {
+    const records = this.disbursementValues.map((disbursementValue) => {
+      const record = new StringBuilder();
+      record.append(this.recordTypeCode, 1);
+      record.append(this.institutionCode, 4);
+      record.appendWithStartFiller(
+        disbursementValue.id.toString(),
+        10,
+        NUMBER_FILLER,
+      );
+      record.append(disbursementValue.valueCode, 4);
+      record.appendWithStartFiller(
+        round(disbursementValue.valueAmount),
+        9,
+        NUMBER_FILLER,
+      );
+      record.append(this.sin, 9);
+      record.appendWithEndFiller(this.studentLastName, 25, SPACE_FILLER);
+      record.appendWithEndFiller(this.studentGivenName ?? "", 15, SPACE_FILLER);
+      record.appendDate(this.birthDate, DATE_FORMAT);
+      record.append(this.applicationNumber, 10);
+      record.appendWithEndFiller(
+        this.institutionStudentNumber,
+        12,
+        SPACE_FILLER,
+      );
+      record.append("100"); //Course load hardcoded to 100 as this file is only for FT.
+      record.appendDate(this.studyStartDate, DATE_FORMAT);
+      record.appendDate(this.studyEndDate, DATE_FORMAT);
+      record.appendDate(this.disbursementDate, DATE_FORMAT);
+      return record.toString();
+    });
+    return records.join(END_OF_LINE);
+  }
+}
---
+++ sources/packages/backend/libs/integrations/src/institution-integration/ece-integration/ece-integration.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from "@nestjs/common";
+import {
+  StudentRestrictionSharedService,
+  WorkflowClientService,
+} from "@sims/services";
+import { ConfigModule } from "@sims/utilities/config";
+import {
+  DisbursementScheduleService,
+  SshService,
+} from "@sims/integrations/services";
+import { ECEFileService } from "./ece-file.service";
+import { ECEIntegrationService } from "./ece-integration.service";
+
+@Global()
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    SshService,
+    DisbursementScheduleService,
+    ECEFileService,
+    WorkflowClientService,
+    ECEIntegrationService,
+    StudentRestrictionSharedService,
+  ],
+  exports: [ECEFileService],
+})
+export class ECEIntegrationModule {}
