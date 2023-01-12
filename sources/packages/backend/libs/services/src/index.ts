0ebbb562fc23e30c41b752dabf3418a955ef3381
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2023-01-12 12:37:42 -0800

    Change already disbursed money calculation.

--- sources/packages/backend/libs/services/src/index.ts
+++ sources/packages/backend/libs/services/src/index.ts
@@ -2,7 +2,7 @@ export * from "./zeebe/zeebe.module";
 export * from "./workflow/workflow-client.service";
 export * from "./sequence-control/sequence-control.service";
 export * from "./disbursement-schedule/disbursement-schedule.models";
-export * from "./disbursement-schedule/disbursement-schedule-service";
+export * from "./disbursement-schedule/disbursement-schedule.service";
 export * from "./disbursement-overaward/disbursement-overaward-service";
 export * from "./restriction/student-restriction-shared.service";
 export * from "./notifications/notification/gc-notify.model";
