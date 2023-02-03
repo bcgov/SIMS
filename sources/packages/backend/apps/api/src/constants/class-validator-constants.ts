d0128c6c2f9920668dfbe9dca0b2abd138f89982
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-02-02 16:25:39 -0800

    fixed constant value

--- sources/packages/backend/apps/api/src/constants/class-validator-constants.ts
+++ sources/packages/backend/apps/api/src/constants/class-validator-constants.ts
@@ -1,5 +1,5 @@
 export const JSON_10KB = 10240;
-export const JSON_20KB = 2048;
+export const JSON_20KB = 20480;
 export const FORM_NAME_MAX_LENGTH = 200;
 export const REPORT_NAME_MAX_LENGTH = 100;
 
