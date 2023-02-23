cd709feeb7ebc6a044c0bb193f17edc69b937593
dheepak-aot <dheepak.rr@aot-technologies.com>
2023-02-23 11:00:58 -0700

    modify sorting

--- sources/packages/backend/libs/services/src/disbursement-overaward/disbursement-overaward.service.ts
+++ sources/packages/backend/libs/services/src/disbursement-overaward/disbursement-overaward.service.ts
@@ -105,7 +105,7 @@ export class DisbursementOverawardService {
         student: { id: studentId },
       },
       order: {
-        createdAt: "ASC",
+        createdAt: "DESC",
       },
     });
   }
