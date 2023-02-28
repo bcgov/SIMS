28f91a3602bd07c0a5e87406132bc0d7bb4cf6ee
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-02-28 15:18:15 -0800

    review issues

--- sources/packages/backend/apps/db-migrations/src/sql/SFASIndividuals/Add-student-id-sfas-individuals.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/SFASIndividuals/Add-student-id-sfas-individuals.sql
@@ -3,6 +3,6 @@ ALTER TABLE
 ADD
     COLUMN IF NOT EXISTS student_id INTEGER REFERENCES sims.students(id);
 
-COMMENT ON COLUMN sims.sfas_individuals.student_id IS 'Student id used to import data to sims.disbursement_overawards table.';
+COMMENT ON COLUMN sims.sfas_individuals.student_id IS 'Existing student id identified by the combination of SIN, last name, and date of birth.';
 
 CREATE INDEX student_id ON sims.sfas_individuals(student_id);
