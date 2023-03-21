6c183738bb35dd9c72fb27bdf601aca9bba6c555
dheepak-aot <dheepak.rr@aot-technologies.com>
2023-03-21 13:59:24 -0600

    added comments

--- sources/packages/backend/workflow/test/test-utils/constants/system-configurations-constants.ts
+++ sources/packages/backend/workflow/test/test-utils/constants/system-configurations-constants.ts
@@ -1,1 +1,5 @@
+/**
+ * Timeout in milliseconds for the zeebe client to create process instance
+ * and wait for response.
+ */
 export const PROCESS_INSTANCE_CREATE_TIMEOUT = 90000;
--- sources/packages/backend/workflow/test/test-utils/constants/workflow-variables-constants.ts
+++ sources/packages/backend/workflow/test/test-utils/constants/workflow-variables-constants.ts
@@ -1,6 +1,18 @@
+/**
+ * E2E test variable used by load consolidated data worker to set the values for
+ * student dependent status and relationship status.
+ */
 export const E2E_STUDENT_STATUS = "e2eTestStudentStatus";
 
+/**
+ * E2E test variable used by load consolidated data worker to set the value of
+ * application exception status.
+ */
 export const E2E_APPLICATION_EXCEPTION_STATUS =
   "e2eTestApplicationExceptionStatus";
 
+/**
+ * E2E test variable used by load consolidated data worker to set the value of
+ * application PIR status.
+ */
 export const E2E_PIR_STATUS = "e2ePIRStatus";
