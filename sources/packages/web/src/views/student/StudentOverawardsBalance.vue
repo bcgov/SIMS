4f2aca4b12d2677a78042e83902b18f9953cb4fa
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2023-03-27 11:30:18 -0600

    updated

--- sources/packages/web/src/views/student/StudentOverawardsBalance.vue
+++ sources/packages/web/src/views/student/StudentOverawardsBalance.vue
@@ -7,37 +7,54 @@
         :routeLocation="{ name: StudentRoutesConst.STUDENT_DASHBOARD }"
       />
     </template>
-    <formio-container
-      formName="studentOverawardsBalance"
-      :formData="initialData"
-    />
+    <body-header-container :enableCardView="true">
+      <v-row
+        ><v-col
+          ><body-header
+            title="Understanding your overawards"
+            subTitle="It’s important to understand how overawards can impact your awards. Learn more about overawards."
+          />
+          <div class="pb-2">
+            <span class="label-bold"> If you have an overaward</span>
+            <span class="label-value">
+              Pay back your overawards in your National Student Loan Service
+              Centre (NSLSC) account Contact StudentAid BC after you pay your
+              debts, so we know to deduct it from your overaward balances If you
+              still have remaining overaward balances or a new overaward
+              occurred during a reassessment, your future awards will
+              automatically be deducted in one or more of your applications to
+              reduce your overaward balance.</span
+            >
+          </div></v-col
+        >
+        <v-col md="3">
+          <v-img
+            height="260"
+            class="ml-2"
+            alt="An illustration of a women putting a coin in a glass jar."
+            src="@/assets/images/student-glass-jar.svg" /></v-col
+      ></v-row>
+    </body-header-container>
+    <student-overaward-details :studentId="studentId" />
   </student-page-container>
 </template>
 <script lang="ts">
-import { computed, onMounted, defineComponent } from "vue";
+import { defineComponent } from "vue";
+import StudentOverawardDetails from "@/components/common/StudentOverawardDetails.vue";
 import { StudentRoutesConst } from "@/constants/routes/RouteConstants";
-import { StudentRestriction } from "@/store/modules/student/student";
-import { useStudentStore } from "@/composables";
-
-interface StudentAccountActivityFormModel {
-  restrictions: StudentRestriction[];
-}
+import BodyHeaderContainer from "@/components/layouts/BodyHeaderContainer.vue";
 
 export default defineComponent({
+  components: { StudentOverawardDetails, BodyHeaderContainer },
+  props: {
+    studentId: {
+      type: Number,
+      required: true,
+    },
+  },
   setup() {
-    const { activeRestrictions, updateRestrictions } = useStudentStore();
-
-    const initialData = computed<StudentAccountActivityFormModel>(() => {
-      return {
-        restrictions: activeRestrictions.value,
-      };
-    });
-    onMounted(async () => {
-      await updateRestrictions();
-    });
     return {
       StudentRoutesConst,
-      initialData,
     };
   },
 });
