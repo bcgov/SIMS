737dfc8a8f278429d8864365e38a27a10a7fa5ea
Ann <ann.jacob@aot-technologies.com>
2023-03-28 21:21:32 -0400

    refactor

--- sources/packages/backend/apps/api/src/services/student-assessment/student-assessment.service.ts
+++ sources/packages/backend/apps/api/src/services/student-assessment/student-assessment.service.ts
@@ -161,6 +161,7 @@ export class StudentAssessmentService extends RecordDataModelService<StudentAsse
         ASSESSMENT_INVALID_OPERATION_IN_THE_CURRENT_STATE,
       );
     }
+
     await this.startAssessmentQueue.add({
       workflowName: assessment.application.data.workflowName,
       assessmentId: assessment.id,
