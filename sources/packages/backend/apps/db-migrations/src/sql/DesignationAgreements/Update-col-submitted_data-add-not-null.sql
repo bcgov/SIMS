118aa41b4690db63fda6010a0c35909ff47833aa
Shashank Shekhar <sh16011993@gmail.com>
2023-03-29 14:53:53 -0700

    Updated code with review comments

--- sources/packages/backend/apps/db-migrations/src/sql/DesignationAgreements/Update-col-submitted_data-add-not-null.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/DesignationAgreements/Update-col-submitted_data-add-not-null.sql
@@ -1,2 +1,7 @@
 -- Updated the submitted_data column from NULL to NOT NULL
-ALTER TABLE sims.designation_agreements ALTER COLUMN submitted_data SET NOT NULL;
+ALTER TABLE
+  sims.designation_agreements
+ALTER COLUMN
+  submitted_data
+SET
+  NOT NULL;
--- sources/packages/backend/apps/db-migrations/src/sql/DesignationAgreements/Update-col-submitted_data-drop-not-null.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/DesignationAgreements/Update-col-submitted_data-drop-not-null.sql
@@ -1,2 +1,5 @@
 -- Updated the submitted_data column from NOT NULL to NULL
-ALTER TABLE sims.designation_agreements ALTER COLUMN submitted_data DROP NOT NULL;
+ALTER TABLE
+  sims.designation_agreements
+ALTER COLUMN
+  submitted_data DROP NOT NULL;
