302f76290e9c19ddd49e0a01467d8bfa45cbce20
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-04-06 13:44:45 -0700

    review issues

--- sources/packages/backend/apps/api/src/route-controllers/institution/models/institution.dto.ts
+++ sources/packages/backend/apps/api/src/route-controllers/institution/models/institution.dto.ts
@@ -131,8 +131,8 @@ export class InstitutionProfileAPIOutDTO extends InstitutionContactAPIOutDTO {
 export class InstitutionDetailAPIOutDTO extends InstitutionProfileAPIOutDTO {
   legalOperatingName: string;
   institutionTypeName?: string;
-  isBCPrivate?: boolean;
-  isBCPublic?: boolean;
+  isBCPrivate: boolean;
+  isBCPublic: boolean;
   /**
    * Indicates if the institution has a BCeID business guid
    * associated with, if not it is a basic BCeID institution.
