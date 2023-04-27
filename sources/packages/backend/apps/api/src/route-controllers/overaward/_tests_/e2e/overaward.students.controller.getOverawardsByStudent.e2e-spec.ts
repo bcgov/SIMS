4fdfa5b2aeb9bdbf096615958d8238855fad7652
Andre Pestana <78114138+andrepestana-aot@users.noreply.github.com>
2023-04-27 14:14:52 -0700

    #1835 - E2E Student Authentication (#1902)
    * initial commit
    
    * changed to use discovery service
    
    * test to fix heap out of memory
    
    * test to fix heap out of memory
    
    * test to fix heap out of memory
    
    * test to fix heap out of memory
    
    * test to fix heap out of memory
    
    * removed unnecessary unknown type
    
    * review issues
    
    * review issues
    
    * review issues
    
    * review issues

--- sources/packages/backend/apps/api/src/route-controllers/overaward/_tests_/e2e/overaward.students.controller.getOverawardsByStudent.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/overaward/_tests_/e2e/overaward.students.controller.getOverawardsByStudent.e2e-spec.ts
@@ -4,45 +4,55 @@ import {
   createFakeDisbursementOveraward,
   createFakeStudentAssessment,
   createFakeApplication,
+  saveFakeStudent,
 } from "@sims/test-utils";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import {
   Application,
   DisbursementOveraward,
   DisbursementOverawardOriginType,
-  Student,
   StudentAssessment,
 } from "@sims/sims-db";
 import {
   BEARER_AUTH_TYPE,
   createTestingAppModule,
-  getStudentToken,
   FakeStudentUsersTypes,
-  getStudentByFakeStudentUserType,
+  getStudentToken,
+  mockUserLoginInfo,
 } from "../../../../testHelpers";
-import { StudentsOverawardAPIOutDTO } from "../../models/overaward.dto";
+import { TestingModule } from "@nestjs/testing";
 
 describe("OverawardStudentsController(e2e)-getOverawardsByStudent", () => {
   let app: INestApplication;
+  let appDataSource: DataSource;
+  let appModule: TestingModule;
   let assessmentRepo: Repository<StudentAssessment>;
   let applicationRepo: Repository<Application>;
   let disbursementOverawardRepo: Repository<DisbursementOveraward>;
-  let student: Student;
 
   beforeAll(async () => {
-    const { nestApplication, dataSource } = await createTestingAppModule();
+    const { nestApplication, module, dataSource } =
+      await createTestingAppModule();
     app = nestApplication;
     assessmentRepo = dataSource.getRepository(StudentAssessment);
     applicationRepo = dataSource.getRepository(Application);
     disbursementOverawardRepo = dataSource.getRepository(DisbursementOveraward);
-    student = await getStudentByFakeStudentUserType(
-      FakeStudentUsersTypes.FakeStudentUserType1,
-      dataSource,
-    );
+    appDataSource = dataSource;
+    appModule = module;
   });
 
   it("Should return student overawards when available.", async () => {
     // Arrange
+    const student = await saveFakeStudent(appDataSource);
+
+    // Mock user service to return the saved student.
+    await mockUserLoginInfo(appModule, student);
+
+    // Get any student user token.
+    const studentToken = await getStudentToken(
+      FakeStudentUsersTypes.FakeStudentUserType1,
+    );
+
     // Prepare the student assessment to create overaward.
     const application = await applicationRepo.save(
       createFakeApplication({
@@ -62,33 +72,44 @@ describe("OverawardStudentsController(e2e)-getOverawardsByStudent", () => {
     reassessmentOveraward.originType =
       DisbursementOverawardOriginType.ReassessmentOveraward;
     reassessmentOveraward.addedDate = new Date();
-    await disbursementOverawardRepo.save(reassessmentOveraward);
-    const endpoint = "/students/overaward";
-    const studentToken = await getStudentToken(
-      FakeStudentUsersTypes.FakeStudentUserType1,
+    const savedReassessmentOveraward = await disbursementOverawardRepo.save(
+      reassessmentOveraward,
     );
+    // Create a manual overaward deduction.
+    const manualOveraward = createFakeDisbursementOveraward({ student });
+    manualOveraward.disbursementValueCode = "CSLP";
+    manualOveraward.overawardValue = -123;
+    manualOveraward.originType = DisbursementOverawardOriginType.ManualRecord;
+    manualOveraward.addedDate = new Date();
+    const savedManualOveraward = await disbursementOverawardRepo.save(
+      manualOveraward,
+    );
+
+    const endpoint = "/students/overaward";
 
     // Act/Assert
     await request(app.getHttpServer())
       .get(endpoint)
       .auth(studentToken, BEARER_AUTH_TYPE)
       .expect(HttpStatus.OK)
-      .then((response) => {
-        expect(response.body.length).toBeGreaterThan(0);
-        const overawards = response.body as StudentsOverawardAPIOutDTO[];
-        // TODO change the expect.arrayContaining, when we create the mock student token.
-        expect(overawards).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({
-              applicationNumber:
-                reassessmentOveraward.studentAssessment.application
-                  .applicationNumber,
-              awardValueCode: reassessmentOveraward.disbursementValueCode,
-              overawardValue: reassessmentOveraward.overawardValue,
-            }),
-          ]),
-        );
-      });
+      .expect([
+        {
+          dateAdded: savedManualOveraward.addedDate.toISOString(),
+          createdAt: savedManualOveraward.createdAt.toISOString(),
+          overawardOrigin: savedManualOveraward.originType,
+          awardValueCode: savedManualOveraward.disbursementValueCode,
+          overawardValue: savedManualOveraward.overawardValue,
+        },
+        {
+          dateAdded: savedReassessmentOveraward.addedDate.toISOString(),
+          createdAt: savedReassessmentOveraward.createdAt.toISOString(),
+          overawardOrigin: savedReassessmentOveraward.originType,
+          awardValueCode: savedReassessmentOveraward.disbursementValueCode,
+          overawardValue: savedReassessmentOveraward.overawardValue,
+          applicationNumber: application.applicationNumber,
+          assessmentTriggerType: studentAssessment.triggerType,
+        },
+      ]);
   });
 
   afterAll(async () => {
