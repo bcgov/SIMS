4f8a9821febe48b4ac12b17971b2225a4fbf5e10
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-05-02 16:25:03 -0700

    test

--- sources/packages/web/src/components/common/IdleTimeChecker.vue
+++ sources/packages/web/src/components/common/IdleTimeChecker.vue
@@ -123,14 +123,14 @@ export default defineComponent({
       if (await extendTimeModal.value.showModal()) {
         extendUserSessionTime();
       } else {
-        void logoff();
+        logoff();
       }
     };
 
     const checkIdle = () => {
       // Check if it was logged out from another tab/window.
       if (isLoggedOut()) {
-        void logoff();
+        logoff();
         return;
       }
 
@@ -144,7 +144,7 @@ export default defineComponent({
       // Exceeded user session time.
       if (idleTimeInSeconds > maximumIdleTime.value) {
         // Logoff immediately in case session is expired.
-        void logoff();
+        logoff();
       } else if (
         idleTimeInSeconds >=
         maximumIdleTime.value - COUNT_DOWN_TIMER_FOR_LOGOUT
