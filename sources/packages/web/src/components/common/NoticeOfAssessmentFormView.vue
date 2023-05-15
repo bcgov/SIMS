6e3c85f97bcdd84284813b1fb1cc4c44201e8192
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2023-05-15 13:49:56 -0700

    #1759 - Reissue MSFAA - Part 2 - NOA UI Changes (#1936)
    - Enabled the visualization of the MSFAAs in the NOA with additional signed/cancellation messages and the "Reissue MSFAA" button for the Ministry in case there is a canceled MSFAA associated with pending disbursement.
    - Added MSFAACancelledDate and MSFAADateSigned to the API return following the approach in place. Renamed the COE status previously named Status to COEStatus since the object itself is related to the disbursement and now we have a status for it.

--- sources/packages/web/src/components/common/NoticeOfAssessmentFormView.vue
+++ sources/packages/web/src/components/common/NoticeOfAssessmentFormView.vue
@@ -1,52 +1,216 @@
 <template>
-  <formio-container formName="noticeOfAssessment" :formData="initialData" />
+  <formio-container
+    formName="noticeOfAssessment"
+    @customEvent="customEvent"
+    :formData="initialData"
+  />
+  <confirm-modal
+    title="Reissue MSFAA number"
+    ref="confirmReissueMSFAA"
+    okLabel="Reissue MSFAA number now"
+    cancelLabel="Cancel"
+    :loading="msfaaReissueProcessing"
+    ><template #content>
+      <p>
+        <strong>Are you sure you want to proceed?</strong> Reissuing an MSFAA
+        number will impact the student's pending disbursements meaning a new
+        number will be provided for the pending disbursements. The student will
+        still be able to see their cancelled MSFAA number on their Notice of
+        Assessment.
+      </p>
+    </template></confirm-modal
+  >
 </template>
 
 <script lang="ts">
-import { watch, defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 import { StudentAssessmentsService } from "@/services/StudentAssessmentsService";
 import { AssessmentNOAAPIOutDTO } from "@/services/http/dto";
+import { ModalDialog, useFormatters } from "@/composables";
+import ConfirmModal from "@/components/common/modals/ConfirmModal.vue";
+import {
+  ApplicationStatus,
+  AssessmentStatus,
+  DisbursementScheduleStatus,
+  FormIOCustomEvent,
+  FormIOCustomEventTypes,
+  FormIOForm,
+} from "@/types";
 
 interface NoticeOfAssessment extends AssessmentNOAAPIOutDTO {
-  viewOnly?: boolean;
+  canReissueMSFAA: boolean;
 }
 
+enum MSFAAStatus {
+  Pending = "pending",
+  Signed = "signed",
+  Cancelled = "cancelled",
+}
+
+/**
+ * Dynamic data that must be inspected to render the
+ * individual MSFAA statuses and appended properties
+ * to be consumed by the form.io definition.
+ */
+export interface NOADisbursementSchedule {
+  disbursement1Status: DisbursementScheduleStatus;
+  disbursement1MSFAADateSigned: Date;
+  disbursement1MSFAACancelledDate: Date;
+  disbursement1MSFAACancelledDateFormatted: string;
+  disbursement1MSFAAStatusClass: string;
+  disbursement1MSFAAStatus: MSFAAStatus;
+  disbursement2Status: DisbursementScheduleStatus;
+  disbursement2MSFAADateSigned: Date;
+  disbursement2MSFAACancelledDate: Date;
+  disbursement2MSFAACancelledDateFormatted: string;
+  disbursement2MSFAAStatusClass: string;
+  disbursement2MSFAAStatus: MSFAAStatus;
+}
+
+/**
+ * MSFAA success icon displayed when a MSFAA is not cancelled.
+ */
+const MSFAA_SUCCESS_CLASS = "fa-regular fa-check-circle text-success";
+/**
+ * MSFAA error icon displayed when the MSFAA is cancelled.
+ */
+const MSFAA_CANCELED_CLASS = "fa-regular fa-times-circle text-danger";
+
 export default defineComponent({
-  emits: ["assessmentDataLoaded"],
+  emits: {
+    assessmentDataLoaded: (
+      applicationStatus: ApplicationStatus,
+      noaApprovalStatus: AssessmentStatus,
+    ) => {
+      return !!applicationStatus && !!noaApprovalStatus;
+    },
+    reissueMSFAA: (
+      applicationId: number,
+      loadNOA: () => Promise<void>,
+      processing: (processing: boolean) => void,
+    ) => {
+      return !!applicationId && !!loadNOA && !!processing;
+    },
+  },
+  components: { ConfirmModal },
   props: {
     assessmentId: {
       type: Number,
       required: true,
     },
-    viewOnly: {
+    canReissueMSFAA: {
       type: Boolean,
-      required: false,
+      default: false,
     },
   },
   setup(props, { emit }) {
+    const { dateOnlyLongString } = useFormatters();
+    const confirmReissueMSFAA = ref({} as ModalDialog<boolean>);
     const initialData = ref({} as NoticeOfAssessment);
+    const msfaaReissueProcessing = ref(false);
 
-    onMounted(async () => {
+    /**
+     * Defines the MSFAA status based on the signed date and cancelled dates.
+     * @param signedDate signed date.
+     * @param cancelledDate cancelled date.
+     * @returns MSFAA status.
+     */
+    const getMSFAAStatus = (
+      signedDate?: Date,
+      cancelledDate?: Date,
+    ): MSFAAStatus => {
+      if (cancelledDate) {
+        return MSFAAStatus.Cancelled;
+      }
+      return signedDate ? MSFAAStatus.Signed : MSFAAStatus.Pending;
+    };
+
+    /**
+     * Defines the icon class to displayed depending on the MSFAA status.
+     * @param msfaaStatus MSFAA status.
+     * @returns MSFAA css classes to set the MSFAA status icon.
+     */
+    const getMSFAAStatusClass = (msfaaStatus: MSFAAStatus): string => {
+      return msfaaStatus === MSFAAStatus.Cancelled
+        ? MSFAA_CANCELED_CLASS
+        : MSFAA_SUCCESS_CLASS;
+    };
+
+    const loadNOA = async () => {
       const assessment =
         await StudentAssessmentsService.shared.getAssessmentNOA(
           props.assessmentId,
         );
-      initialData.value = { ...assessment, viewOnly: props.viewOnly };
+      const noaDisbursementSchedule =
+        assessment.disbursement as NOADisbursementSchedule;
+      // Adjust disbursement schedules.
+      // First disbursement.
+      const firstMSFAAStatus = getMSFAAStatus(
+        noaDisbursementSchedule.disbursement1MSFAADateSigned,
+        noaDisbursementSchedule.disbursement1MSFAACancelledDate,
+      );
+      noaDisbursementSchedule.disbursement1MSFAAStatus = firstMSFAAStatus;
+      noaDisbursementSchedule.disbursement1MSFAACancelledDateFormatted =
+        dateOnlyLongString(
+          noaDisbursementSchedule.disbursement1MSFAACancelledDate,
+        );
+      noaDisbursementSchedule.disbursement1MSFAAStatusClass =
+        getMSFAAStatusClass(firstMSFAAStatus);
+      // Second disbursement.
+      const secondMSFAAStatus = getMSFAAStatus(
+        noaDisbursementSchedule.disbursement2MSFAADateSigned,
+        noaDisbursementSchedule.disbursement2MSFAACancelledDate,
+      );
+      noaDisbursementSchedule.disbursement2MSFAAStatus = secondMSFAAStatus;
+      noaDisbursementSchedule.disbursement2MSFAACancelledDateFormatted =
+        dateOnlyLongString(
+          noaDisbursementSchedule.disbursement2MSFAACancelledDate,
+        );
+      noaDisbursementSchedule.disbursement2MSFAAStatusClass =
+        getMSFAAStatusClass(secondMSFAAStatus);
+      // Checks if the component allows the MSFAA reissue and if there is
+      // one pending disbursement with a cancelled MSFAA.
+      const canReissueMSFAA =
+        props.canReissueMSFAA &&
+        ((noaDisbursementSchedule.disbursement1Status ===
+          DisbursementScheduleStatus.Pending &&
+          !!noaDisbursementSchedule.disbursement1MSFAACancelledDate) ||
+          (noaDisbursementSchedule.disbursement2Status ===
+            DisbursementScheduleStatus.Pending &&
+            !!noaDisbursementSchedule.disbursement2MSFAACancelledDate));
+      initialData.value = {
+        ...assessment,
+        canReissueMSFAA,
+      };
       emit(
         "assessmentDataLoaded",
-        initialData.value?.applicationStatus,
+        initialData.value.applicationStatus,
         initialData.value.noaApprovalStatus,
       );
-    });
+    };
 
-    watch(
-      () => props.viewOnly,
-      () => {
-        initialData.value.viewOnly = props.viewOnly;
-      },
-    );
+    onMounted(loadNOA);
+
+    const setMSFAAReissueProcessing = (processing: boolean) => {
+      msfaaReissueProcessing.value = processing;
+    };
+
+    const customEvent = async (_form: FormIOForm, event: FormIOCustomEvent) => {
+      if (event.type === FormIOCustomEventTypes.ReissueMSFAA)
+        if (await confirmReissueMSFAA.value.showModal()) {
+          emit(
+            "reissueMSFAA",
+            initialData.value.applicationId,
+            () => loadNOA(),
+            (processing) => setMSFAAReissueProcessing(processing),
+          );
+        }
+    };
 
     return {
+      confirmReissueMSFAA,
+      msfaaReissueProcessing,
+      customEvent,
       initialData,
     };
   },
--- sources/packages/web/src/views/student/NoticeOfAssessment.vue
+++ sources/packages/web/src/views/student/NoticeOfAssessment.vue
@@ -32,7 +32,6 @@
     </template>
     <notice-of-assessment-form-view
       :assessmentId="assessmentId"
-      :view-only="viewOnly"
       @assessmentDataLoaded="assessmentDataLoaded"
     />
 
@@ -105,6 +104,7 @@ export default defineComponent({
         });
       }
     };
+
     return {
       confirmAssessment,
       StudentRoutesConst,
