9e7ab308fa254c9bd731333f371e7254ca94755c
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-06-01 15:59:41 -0700

    added role to enum and added a e2e test

---
+++ sources/packages/backend/apps/api/test/auth.aest.e2e-spec.ts
@@ -0,0 +1,18 @@
+import { JwtService } from "@nestjs/jwt";
+import { Role } from "../src/auth";
+import { AESTGroups, getAESTToken } from "../src/testHelpers";
+
+const jwtService = new JwtService();
+
+describe("Auth Ministry", () => {
+  it("Should have specific roles when ministry user is a business administrator.", async () => {
+    //Act
+    const token = await getAESTToken(AESTGroups.BusinessAdministrators);
+
+    //Assert
+    const decodedToken = jwtService.decode(token);
+    expect(decodedToken["resource_access"].aest.roles.sort()).toEqual(
+      Object.values(Role).sort(),
+    );
+  });
+});
