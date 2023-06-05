4fad6876f7fcb99fa5cc0a09d1648e37e589c70b
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-06-05 15:07:44 -0700

    code smell

--- sources/packages/backend/apps/api/test/auth.aest.e2e-spec.ts
+++ sources/packages/backend/apps/api/test/auth.aest.e2e-spec.ts
@@ -9,12 +9,10 @@ describe("Auth Ministry", () => {
     //Act
     const token = await getAESTToken(AESTGroups.BusinessAdministrators);
     const decodedToken = jwtService.decode(token) as IUserToken;
-    const expectedRoles = decodedToken.resource_access.aest.roles.sort((a, b) =>
-      a.localeCompare(b),
-    );
+    decodedToken.resource_access.aest.roles.sort((a, b) => a.localeCompare(b));
     const allAESTRoles = Object.values(Role).sort((a, b) => a.localeCompare(b));
 
     //Assert
-    expect(expectedRoles).toEqual(allAESTRoles);
+    expect(decodedToken.resource_access.aest.roles).toEqual(allAESTRoles);
   });
 });
