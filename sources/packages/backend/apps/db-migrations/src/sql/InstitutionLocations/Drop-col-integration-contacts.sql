e0aec8ce42f5985e6381dabb0bf53d05ad925e44
dheepak-aot <dheepak.rr@aot-technologies.com>
2023-06-06 21:35:37 -0600

    initial commit

---
+++ sources/packages/backend/apps/db-migrations/src/sql/InstitutionLocations/Drop-col-integration-contacts.sql
@@ -0,0 +1,2 @@
+ALTER TABLE
+    sims.institution_locations DROP COLUMN IF EXISTS integration_contacts;
---
+++ sources/packages/backend/apps/db-migrations/src/sql/NotificationMessages/Insert-ece-response-file-processing-message.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    sims.notification_messages(id, description, template_id)
+VALUES
+    (
+        11,
+        'ECE response file processing details.',
+        'a662979f-07d4-44c0-a38f-ab9fda5671fe'
+    );
