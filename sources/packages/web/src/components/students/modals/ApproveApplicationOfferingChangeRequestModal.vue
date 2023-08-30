44c8612c291d58e35faf1e6503ca53ab32c6bc23
Shashank Shekhar <sh16011993@gmail.com>
2023-08-29 18:55:33 -0700

    Code Update

--- sources/packages/web/src/components/students/modals/ApproveApplicationOfferingChangeRequestModal.vue
+++ sources/packages/web/src/components/students/modals/ApproveApplicationOfferingChangeRequestModal.vue
@@ -14,44 +14,39 @@
           behalf).
         </p>
         <content-group-info class="div-scroll"
-          ><h4 class="label-bold color-blue">StudentAid BC Declaration</h4>
-          <v-row
-            ><v-col
-              ><span class="label-bold-large-height"
-                >I. I. I/we understand that:</span
-              >
-            </v-col></v-row
-          ><v-list class="list-color mb-5">
-            <v-list-item>
-              1. The student will have access to information provided in this
-              appendix;
-            </v-list-item>
-            <v-list-item>
-              2. The student's school will have access to information provided
-              in this appendix;
-            </v-list-item>
-            <v-list-item>
-              3. The information in this appendix is subject to audit,
-              investigation and verification as defined in the current program
-              year's StudentAid BC Policy Manual
-            </v-list-item>
-          </v-list>
-          <v-row
-            ><v-col
-              ><span class="label-bold">Collection and use of information</span>
-            </v-col></v-row
+          ><h4 class="label-bold color-blue mb-5">StudentAid BC Declaration</h4>
+          <title-value propertyTitle="I. I. I/we understand that:"
+            ><template #value
+              ><ol class="list-color ml-5 mb-5">
+                <li>
+                  The student will have access to information provided in this
+                  appendix;
+                </li>
+                <li>
+                  The student's school will have access to information provided
+                  in this appendix;
+                </li>
+                <li>
+                  The information in this appendix is subject to audit,
+                  investigation and verification as defined in the current
+                  program year's StudentAid BC Policy Manual
+                </li>
+              </ol></template
+            ></title-value
           >
-          <v-row
-            ><v-col
-              >The information included in this form and authorized above is
-              collected and managed in accordance with sections 26(c) and 26(e)
-              of the Freedom of Information and Protection of Privacy Act,
-              R.S.B.C. 1996, c. 165, and under the authority of the Canada
-              Student Financial Assistance Act, R.S.C. 1994, Chapter C-28 and
-              StudentAid BC. The information provided will be used to determine
-              eligibility for a benefit through</v-col
-            ></v-row
-          > </content-group-info
+          <title-value propertyTitle="Collection and use of information">
+            <template #value
+              ><span
+                >The information included in this form and authorized above is
+                collected and managed in accordance with sections 26(c) and
+                26(e) of the Freedom of Information and Protection of Privacy
+                Act, R.S.B.C. 1996, c. 165, and under the authority of the
+                Canada Student Financial Assistance Act, R.S.C. 1994, Chapter
+                C-28 and StudentAid BC. The information provided will be used to
+                determine eligibility for a benefit through</span
+              ></template
+            >
+          </title-value></content-group-info
         ><v-checkbox
           v-model="approveApplicationOfferingChangeRequestModal.studentConsent"
           label="I agree to the terms and conditions of the StudentAid BC Declaration
