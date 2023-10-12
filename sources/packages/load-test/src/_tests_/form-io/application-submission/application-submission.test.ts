dc00ac528251baaaa37bd7966ad1a8afb2641eeb
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2023-10-12 11:31:41 -0700

    Added some comments.

--- sources/packages/load-test/src/_tests_/form-io/application-submission/application-submission.test.ts
+++ sources/packages/load-test/src/_tests_/form-io/application-submission/application-submission.test.ts
@@ -1,3 +1,9 @@
+/**
+ * Methods like setup and default are part of the K6 life cycle.
+ * @see https://k6.io/docs/using-k6/test-lifecycle
+ * The exported options are part of the K6 configuration.
+ * @see https://k6.io/docs/using-k6/test-lifecycle
+ */
 import { check, sleep } from "k6";
 import { APPLICATION_SUBMISSION_PAYLOAD } from "./models";
 import {
@@ -9,16 +15,29 @@ interface SetupData {
   formIOHeader: Record<string, string>;
 }
 
+/**
+ * Define test-run behavior.
+ */
 export const options = {
   vus: 100,
   duration: "120s",
 };
 
+/**
+ * Set up data for processing, share data among VUs.
+ * Part of the K6 life cycle.
+ * @returns data to be consumed
+ */
 export function setup(): SetupData {
   const formIOHeader = createFormAuthHeader();
   return { formIOHeader };
 }
 
+/**
+ * Test scenario to executed as many times as defined by the test options.
+ * Part of the K6 life cycle.
+ * @param setupData setup data returned by setup method.
+ */
 export default function (setupData: SetupData) {
   const submitResponse = formSubmission(
     "sfaa2023-24",
--- sources/packages/load-test/src/_tests_/form-io/form-io-get-application/form-io-get-application.test.ts
+++ sources/packages/load-test/src/_tests_/form-io/form-io-get-application/form-io-get-application.test.ts
@@ -1,3 +1,9 @@
+/**
+ * Methods like setup and default are part of the K6 life cycle.
+ * @see https://k6.io/docs/using-k6/test-lifecycle
+ * The exported options are part of the K6 configuration.
+ * @see https://k6.io/docs/using-k6/test-lifecycle
+ */
 import { check, sleep } from "k6";
 import {
   createFormAuthHeader,
@@ -8,23 +14,37 @@ interface SetupData {
   formIOHeader: Record<string, string>;
 }
 
+/**
+ * Define test-run behavior.
+ * Part of the K6 life cycle.
+ */
 export const options = {
   vus: 30,
   duration: "60s",
 };
 
+/**
+ * Set up data for processing, share data among VUs.
+ * Part of the K6 life cycle.
+ * @returns data to be consumed
+ */
 export function setup(): SetupData {
   const formIOHeader = createFormAuthHeader();
   return { formIOHeader };
 }
 
+/**
+ * Test scenario to executed as many times as defined by the test options.
+ * Part of the K6 life cycle.
+ * @param setupData setup data returned by setup method.
+ */
 export default function (setupData: SetupData) {
   const response = getFormByAlias("sfaa2023-24", setupData.formIOHeader);
   check(response, {
     "Created with success": (r) => r.status === 200,
   });
   if (response.status !== 200) {
-    console.log(response.status_text);
+    console.warn(response.status_text);
   }
   sleep(1);
 }
