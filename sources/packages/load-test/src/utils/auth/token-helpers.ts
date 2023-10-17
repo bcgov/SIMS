55087f0784e9939c01e7a8fd962fbc575338411b
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2023-10-17 12:37:47 -0700

    Comments adjustment.

--- sources/packages/load-test/src/utils/auth/token-helpers.ts
+++ sources/packages/load-test/src/utils/auth/token-helpers.ts
@@ -20,7 +20,7 @@ const tokenCache: Record<string, TokenCacheResponse> = {};
  * @param jwtExp exp token property that defines the expiration.
  * @param maxSecondsToExpired Amount of seconds before the token expires that should be considered to renew it.
  * For instance, if a token has 10min expiration and the parameter is defined as 30, the token will
- * be renewed the result will be true if the token was acquired 9min30s ago or if it is already expired.
+ * be renewed if the token was acquired 9min30s ago or if it is already expired.
  * @returns true if the token is expired or about to expire based on the property maxSecondsToExpired.
  */
 function needRenewJwtToken(jwtExp: number, maxSecondsToExpired = 0): boolean {
