f6199f6719920dc850c1e132fcce32e752116c7e
Dheepak Ramanathan <54600590+dheepak-aot@users.noreply.github.com>
2023-10-20 10:38:14 -0600

    #2431 - Ministry API - Institution search and Institution details E2E tests (#2447)
    Ministry API - Institution search and Institution details E2E tests

---
+++ sources/packages/backend/apps/api/src/route-controllers/institution/_tests_/e2e/institution.aest.controller.searchInstitutions.e2e-spec.ts
@@ -0,0 +1,141 @@
+import { HttpStatus, INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import * as faker from "faker";
+import {
+  AESTGroups,
+  BEARER_AUTH_TYPE,
+  createTestingAppModule,
+  getAESTToken,
+} from "../../../../testHelpers";
+import {
+  E2EDataSources,
+  createE2EDataSources,
+  createFakeInstitution,
+} from "@sims/test-utils";
+import { SearchInstitutionAPIOutDTO } from "../../models/institution.dto";
+
+describe("InstitutionAESTController(e2e)-searchInstitutions", () => {
+  let app: INestApplication;
+  let db: E2EDataSources;
+  const FAKE_INSTITUTION_NAME = "Search E2E Test institution name";
+
+  beforeAll(async () => {
+    const { nestApplication, dataSource } = await createTestingAppModule();
+    app = nestApplication;
+    db = createE2EDataSources(dataSource);
+  });
+
+  it("Should return institution(s) when searched by legal name.", async () => {
+    // Arrange
+    const institution = createFakeInstitution();
+    institution.operatingName = FAKE_INSTITUTION_NAME;
+    // Using uuid to keep the legal operating name unique for institution.
+    institution.legalOperatingName = faker.random.uuid();
+    await db.institution.save(institution);
+    // Modifying the text to uppercase to validate non case sensitive search.
+    const legalNameSearchText = institution.legalOperatingName.toUpperCase();
+    const mailingAddress = institution.institutionAddress.mailingAddress;
+    const expectedSearchResult: SearchInstitutionAPIOutDTO[] = [
+      {
+        id: institution.id,
+        legalName: institution.legalOperatingName,
+        operatingName: institution.operatingName,
+        address: {
+          addressLine1: mailingAddress.addressLine1,
+          addressLine2: mailingAddress.addressLine2,
+          city: mailingAddress.city,
+          provinceState: mailingAddress.provinceState,
+          country: mailingAddress.country,
+          postalCode: mailingAddress.postalCode,
+        },
+      },
+    ];
+
+    const endpoint = `/aest/institution/search?legalName=${legalNameSearchText}`;
+    const token = await getAESTToken(AESTGroups.BusinessAdministrators);
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect(expectedSearchResult);
+  });
+
+  it("Should return institutions when searched by legal name and operating name.", async () => {
+    // Arrange
+    const institution = createFakeInstitution();
+    institution.operatingName = FAKE_INSTITUTION_NAME;
+    // Using uuid to keep the legal operating name unique for institution.
+    institution.legalOperatingName = faker.random.uuid();
+    await db.institution.save(institution);
+    const operatingNameSearchText = "Search E2E Test institution";
+    const legalNameSearchText = institution.legalOperatingName;
+    const mailingAddress = institution.institutionAddress.mailingAddress;
+    const expectedSearchResult: SearchInstitutionAPIOutDTO[] = [
+      {
+        id: institution.id,
+        legalName: institution.legalOperatingName,
+        operatingName: institution.operatingName,
+        address: {
+          addressLine1: mailingAddress.addressLine1,
+          addressLine2: mailingAddress.addressLine2,
+          city: mailingAddress.city,
+          provinceState: mailingAddress.provinceState,
+          country: mailingAddress.country,
+          postalCode: mailingAddress.postalCode,
+        },
+      },
+    ];
+
+    const endpoint = `/aest/institution/search?legalName=${legalNameSearchText}&operatingName=${operatingNameSearchText}`;
+    const token = await getAESTToken(AESTGroups.Operations);
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect(expectedSearchResult);
+  });
+
+  it("Should return empty result when search criteria does not return any institution.", async () => {
+    // Arrange
+    const searchText = "Institution that does not exist";
+    const endpoint = `/aest/institution/search?operatingName=${searchText}`;
+    const token = await getAESTToken(AESTGroups.OperationsAdministrators);
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect([]);
+  });
+
+  it("Should throw bad request error when no search criteria exist.", async () => {
+    // Arrange
+    const endpoint = "/aest/institution/search";
+    const token = await getAESTToken(AESTGroups.MOFOperations);
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.BAD_REQUEST)
+      .expect({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: [
+          "legalName must be shorter than or equal to 250 characters",
+          "legalName should not be empty",
+          "operatingName must be shorter than or equal to 250 characters",
+          "operatingName should not be empty",
+        ],
+        error: "Bad Request",
+      });
+  });
+
+  afterAll(async () => {
+    await app?.close();
+  });
+});
