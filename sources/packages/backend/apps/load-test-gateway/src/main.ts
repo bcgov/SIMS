c3cf866a2c40d49ecd99cfe0ba9c866cc84948d2
Dheepak Ramaswamy <dheepak.rr@aot-technologies.com>
2023-10-31 16:48:31 -0600

    adding submit end point

--- sources/packages/backend/apps/load-test-gateway/src/main.ts
+++ sources/packages/backend/apps/load-test-gateway/src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from "@sims/utilities/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(LoadTestModule);
+  app.setGlobalPrefix("load-test");
   const config = app.get<ConfigService>(ConfigService);
   await app.listen(config.loadTestAPIPort);
 }
--- sources/packages/backend/apps/load-test-gateway/src/route-controllers/workflow/workflow.controller.ts
+++ sources/packages/backend/apps/load-test-gateway/src/route-controllers/workflow/workflow.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Patch, Post } from "@nestjs/common";
 
 @Controller("workflow")
 export class WorkflowController {
   @Post("prepare-assessment-data")
   async prepareAssessmentData(): Promise<number[]> {
     return [100];
+  }
+
+  @Patch("submit-assessment")
+  async submitAssessment(): Promise<void> {
+    return;
   }
 }
