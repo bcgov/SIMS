e214e19c6d14ff0484147f8fb82a359fb6e42b93
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2023-10-31 16:13:46 -0700

    Allow createIER12SchedulerJobMock receive the date optionally.

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/institution-integration/ier12-integration/_tests_/e2e/utils/mock-utils.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/institution-integration/ier12-integration/_tests_/e2e/utils/mock-utils.ts
@@ -5,16 +5,17 @@ import { GeneratedDateQueueInDTO } from "../../../models/ier.model";
 
 /**
  * Creates the mocked IER12 job payload for the scheduler.
- * @param date date of processing.
+ * @param date optional date of processing.
  * @returns mocked IER12 payload.
  */
 export function createIER12SchedulerJobMock(
-  date: string | Date,
+  date?: string | Date,
 ): DeepMocked<Job<GeneratedDateQueueInDTO>> {
   // Queued job payload.
-  const data = {
-    generatedDate: getISODateOnlyString(date),
-  };
+  const data = {} as GeneratedDateQueueInDTO;
+  if (date) {
+    data.generatedDate = getISODateOnlyString(date);
+  }
   // Queued job.
   return createMock<Job<GeneratedDateQueueInDTO>>({ data });
 }
