c22306f09f61f992528a313a40d8e96562bcdac1
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2023-11-03 19:09:01 -0700

    Added multiple files scenarios

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/institution-integration/ier12-integration/_tests_/e2e/ier12-integration.scheduler.e2e-spec.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/institution-integration/ier12-integration/_tests_/e2e/ier12-integration.scheduler.e2e-spec.ts
@@ -14,6 +14,7 @@ import {
   saveFakeStudentRestriction,
   createFakeInstitution,
   createFakeInstitutionLocation,
+  getUploadedFiles,
 } from "@sims/test-utils";
 import * as Client from "ssh2-sftp-client";
 import { ArrayContains, Not } from "typeorm";
@@ -54,6 +55,7 @@ import {
 import { createIER12SchedulerJobMock } from "./utils";
 import { isValidFileTimestamp } from "@sims/test-utils/utils";
 import { FULL_TIME_DISBURSEMENT_FEEDBACK_ERRORS } from "@sims/integrations/services/disbursement-schedule/disbursement-schedule.models";
+import * as dayjs from "dayjs";
 
 describe(describeProcessorRootTest(QueueNames.IER12Integration), () => {
   let app: INestApplication;
@@ -611,6 +613,139 @@ describe(describeProcessorRootTest(QueueNames.IER12Integration), () => {
     const firstDisbursementId = numberToText(firstDisbursement.id);
     expect(line1).toBe(
       `${assessmentId}${firstDisbursementId}${defaultApplicationNumber}            242963189Doe                      John           19980113B   MA       Address Line 1           Address Line 2           Victoria                 BC  Z1Z1Z1          Program                  Program description                               undergraduateCertificate 0001    8   0512123401234ADR1                         62000081620001010000033330000004444000000555500000066660019100F2000060120002001COMP20000601000010000000006598000000032400N NNN            20000602        000966867900000000000009668679003002001003000003005NNY000000000000000000000000000000000000000000000000000000000000NY0000000000000144430000000000000000000000000000000000007777000000150056000000000000000000000000000000000000000000003000000000050000000065430000009876120000792200 0000000000000000000000000000000000000000000000000000000000000000000000DISW2000081520000815Completed Sent      20000816                        CSLF0000100000BCSL0000659800CSGP0000000000CSGD0000000000CSGF0000000000CSGT0000000000BCAG0000032400SBSD0000000000BGPD0000000000    0000000000`,
+    );
+  });
+
+  it("Should generate 2 IER12 files for locations from different institutions using distinct institution codes.", async () => {
+    // Arrange
+    const testInputDataLocationA = {
+      student: JOHN_DOE_FROM_CANADA,
+      application: {
+        applicationNumber: defaultApplicationNumber,
+        studentNumber: undefined,
+        relationshipStatus: RelationshipStatus.Married,
+        applicationStatus: ApplicationStatus.Completed,
+        applicationStatusUpdatedOn: undefined,
+      },
+      assessment: {
+        triggerType: AssessmentTriggerType.OriginalAssessment,
+        assessmentDate: referenceSubmissionDate,
+        workflowData: WORKFLOW_DATA_MARRIED_WITH_DEPENDENTS,
+        assessmentData: ASSESSMENT_DATA_MARRIED,
+        disbursementSchedules: [
+          {
+            coeStatus: COEStatus.completed,
+            disbursementScheduleStatus: DisbursementScheduleStatus.Sent,
+            disbursementDate: undefined,
+            updatedAt: undefined,
+            dateSent: undefined,
+            disbursementValues:
+              AWARDS_SINGLE_DISBURSEMENT_RESTRICTION_WITHHELD_FUNDS,
+          },
+        ],
+      },
+      educationProgram:
+        PROGRAM_UNDERGRADUATE_CERTIFICATE_WITHOUT_INSTITUTION_PROGRAM_CODE,
+      offering: OFFERING_FULL_TIME,
+    };
+    const applicationA = await saveIER12TestInputData(
+      db,
+      testInputDataLocationA,
+      { institutionLocation: locationA },
+      {
+        programYearPrefix: sharedProgramYearPrefix,
+        submittedDate: referenceSubmissionDate,
+      },
+    );
+
+    const testInputDataLocationB = {
+      student: JOHN_DOE_FROM_CANADA,
+      application: {
+        applicationNumber: defaultApplicationNumber,
+        studentNumber: undefined,
+        relationshipStatus: RelationshipStatus.Married,
+        applicationStatus: ApplicationStatus.Completed,
+        applicationStatusUpdatedOn: undefined,
+      },
+      assessment: {
+        triggerType: AssessmentTriggerType.OriginalAssessment,
+        // Add o one hour to ensure the proper file upload order.
+        assessmentDate: dayjs(referenceSubmissionDate).add(1, "h").toDate(),
+        workflowData: WORKFLOW_DATA_MARRIED_WITH_DEPENDENTS,
+        assessmentData: ASSESSMENT_DATA_MARRIED,
+        disbursementSchedules: [
+          {
+            coeStatus: COEStatus.completed,
+            disbursementScheduleStatus: DisbursementScheduleStatus.Sent,
+            disbursementDate: undefined,
+            updatedAt: undefined,
+            dateSent: undefined,
+            disbursementValues:
+              AWARDS_SINGLE_DISBURSEMENT_RESTRICTION_WITHHELD_FUNDS,
+          },
+        ],
+      },
+      educationProgram:
+        PROGRAM_UNDERGRADUATE_CERTIFICATE_WITHOUT_INSTITUTION_PROGRAM_CODE,
+      offering: OFFERING_FULL_TIME,
+    };
+    const applicationB = await saveIER12TestInputData(
+      db,
+      testInputDataLocationB,
+      { institutionLocation: locationB },
+      {
+        programYearPrefix: sharedProgramYearPrefix,
+        submittedDate: referenceSubmissionDate,
+      },
+    );
+
+    // Queued job.
+    const job = createIER12SchedulerJobMock(referenceSubmissionDate);
+
+    // Act
+    const ier12Results = await processor.processIER12File(job);
+
+    // Assert
+    // Assert process result.
+    expect(ier12Results).toBeDefined();
+    // File timestamp.
+    const [firstTimestampResult, secondTimestampResult] =
+      getFileNameAsCurrentTimestampMock.mock.results;
+    expect(isValidFileTimestamp(firstTimestampResult.value)).toBe(true);
+    expect(isValidFileTimestamp(secondTimestampResult.value)).toBe(true);
+    expect(ier12Results).toStrictEqual([
+      getSuccessSummaryMessages(firstTimestampResult.value, {
+        institutionCode: locationA.institutionCode,
+      }),
+      getSuccessSummaryMessages(secondTimestampResult.value, {
+        institutionCode: locationB.institutionCode,
+      }),
+    ]);
+    // Assert file output.
+    const [fileA, fileB] = getUploadedFiles(sftpClientMock);
+    // Location A file validation.
+    expect(fileA).toBeDefined();
+    expect(fileA.fileLines?.length).toBe(1);
+    const [fileALine1] = fileA.fileLines;
+    const [firstDisbursementA] =
+      applicationA.currentAssessment.disbursementSchedules;
+    const assessmentAId = numberToText(applicationA.currentAssessment.id);
+    // Line 1 validations.
+    const firstDisbursementAId = numberToText(firstDisbursementA.id);
+    expect(fileALine1).toBe(
+      `${assessmentAId}${firstDisbursementAId}${defaultApplicationNumber}            242963189Doe                      John           19980113B   MA       Address Line 1           Address Line 2           Victoria                 BC  Z1Z1Z1          Program                  Program description                               undergraduateCertificate 0001    8   0512123401234ADR1                         62000081620001010000033330000004444000000555500000066660019100F2000060120002001COMP20000601000000000000000000000000000000N NNN            20000601        000966867900000000000009668679003002001003000003005NNY000000000000000000000000000000000000000000000000000000000000NY0000000000000144430000000000000000000000000000000000007777000000150056000000000000000000000000000000000000000000003000000000050000000065430000009876120000000000 0000000000000000000000000000000000000000000000000000000000000000000000DISS2000081520000815Completed Sent      20000816                        CSLF0000000000BCSL0000000000CSGP0000000000CSGD0000000000CSGF0000000000CSGT0000000000BCAG0000000000SBSD0000000000BGPD0000000000    0000000000`,
+    );
+    // Location B file validation.
+    expect(fileB).toBeDefined();
+    expect(fileB.fileLines?.length).toBe(1);
+    const [fileBLine1] = fileB.fileLines;
+    const [firstDisbursementB] =
+      applicationB.currentAssessment.disbursementSchedules;
+    const assessmentBId = numberToText(applicationB.currentAssessment.id);
+    // Line 1 validations.
+    const firstDisbursementBId = numberToText(firstDisbursementB.id);
+    expect(fileBLine1).toBe(
+      `${assessmentBId}${firstDisbursementBId}${defaultApplicationNumber}            242963189Doe                      John           19980113B   MA       Address Line 1           Address Line 2           Victoria                 BC  Z1Z1Z1          Program                  Program description                               undergraduateCertificate 0001    8   0512123401234ADR1                         62000081620001010000033330000004444000000555500000066660019100F2000060120002001COMP20000601000010000000006598000000032400N NNN            20000601        000966867900000000000009668679003002001003000003005NNY000000000000000000000000000000000000000000000000000000000000NY0000000000000144430000000000000000000000000000000000007777000000150056000000000000000000000000000000000000000000003000000000050000000065430000009876120000792200 0000000000000000000000000000000000000000000000000000000000000000000000DISW2000081520000815Completed Sent      20000816                        CSLF0000100000BCSL0000659800CSGP0000000000CSGD0000000000CSGF0000000000CSGT0000000000BCAG0000032400SBSD0000000000BGPD0000000000    0000000000`,
     );
   });
 });
--- sources/packages/backend/libs/test-utils/src/mocks/ssh-service-mock.ts
+++ sources/packages/backend/libs/test-utils/src/mocks/ssh-service-mock.ts
@@ -41,28 +41,43 @@ export function createSSHServiceMock(
 }
 
 /**
- * Get the parameters provided to the put method of the SSH client that represents the
- * remote file path and the data that would be uploaded to the SFTP in a real scenario.
+ * Get the parameters provided to the put method of the SSH client that
+ * represents the data that would be uploaded to the SFTP in a real scenario.
  * @param sshClientMock SSH mocked client.
  * @returns file name and file content of the supposed-to-be uploaded file.
  */
 export function getUploadedFile(
   sshClientMock: DeepMocked<Client>,
 ): UploadedFile {
-  const uploadedFile = {} as UploadedFile;
+  const [uploadedFile] = getUploadedFiles(sshClientMock);
+  return uploadedFile;
+}
+
+/**
+ * Get the parameters provided to the put method of the SSH client that represents the
+ * remote file(s) path and the data that would be uploaded to the SFTP in a real scenario.
+ * @param sshClientMock SSH mocked client.
+ * @returns file(s) name and file(s) content of the supposed-to-be uploaded file(s).
+ */
+export function getUploadedFiles(
+  sshClientMock: DeepMocked<Client>,
+): UploadedFile[] {
   if (!sshClientMock.put.mock.calls.length) {
     throw new Error(
       "SSH client mock was not invoked which means that there was no attempt to upload a file.",
     );
   }
-  const [[input, remoteFilePath]] = sshClientMock.put.mock.calls;
-  if (input) {
-    uploadedFile.fileLines = input.toString().split(END_OF_LINE);
-  }
-  if (remoteFilePath) {
-    uploadedFile.remoteFilePath = remoteFilePath.toString();
-  }
-  return uploadedFile;
+  return sshClientMock.put.mock.calls.map((call) => {
+    const uploadedFile = {} as UploadedFile;
+    const [input, remoteFilePath] = call;
+    if (input) {
+      uploadedFile.fileLines = input.toString().split(END_OF_LINE);
+    }
+    if (remoteFilePath) {
+      uploadedFile.remoteFilePath = remoteFilePath.toString();
+    }
+    return uploadedFile;
+  });
 }
 
 /**
