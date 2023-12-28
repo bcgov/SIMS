364646de4827a54dd82c630477f352222df75200
andrepestana-aot <andre.pestana@aot-technologies.com>
2023-12-28 15:29:14 -0800

    changed logic; added tests for other program years

---
+++ sources/packages/backend/workflow/test/2023-2024/fulltime-assessment/appeals/fulltime-assessment-2023-2024-PDPPD-status.e2e-spec.ts
@@ -0,0 +1,102 @@
+import { PROGRAM_YEAR } from "../../constants/program-year.constants";
+import {
+  createFakeConsolidatedFulltimeData,
+  executeFullTimeAssessmentForProgramYear,
+} from "../../../test-utils";
+
+describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-PDPPD-status.`, () => {
+  it("Should be true when student selected PD/PPD status through the application without any appeal.", async () => {
+    // Arrange
+    const assessmentConsolidatedData =
+      createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+    assessmentConsolidatedData.studentDataApplicationPDPPDStatus = "yes";
+
+    // Act
+    const calculatedAssessment = await executeFullTimeAssessmentForProgramYear(
+      PROGRAM_YEAR,
+      assessmentConsolidatedData,
+    );
+
+    // Assert
+    expect(calculatedAssessment.variables.calculatedDataPDPPDStatus).toBe(true);
+  });
+
+  it(
+    "Should be true when student did not select PD/PPD status from the application but requested a change " +
+      "selecting 'Yes, I have disability and want this application to be assessed for disability funding.'",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataApplicationPDPPDStatus =
+        "noIDoNotHaveADisability";
+      assessmentConsolidatedData.appealsStudentDisabilityAppealData = {
+        disabilityInformation: "yes",
+      };
+
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+
+      // Assert
+      expect(calculatedAssessment.variables.calculatedDataPDPPDStatus).toBe(
+        true,
+      );
+    },
+  );
+
+  it(
+    "Should be false when student selected PD/PPD status from the application but requested a change " +
+      "selecting 'No, I want to access other funding types while I wait for a verification decision.'",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataApplicationPDPPDStatus = "yes";
+      assessmentConsolidatedData.appealsStudentDisabilityAppealData = {
+        disabilityInformation: "noIWantToAccessOtherFundingTypes",
+      };
+
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+
+      // Assert
+      expect(calculatedAssessment.variables.calculatedDataPDPPDStatus).toBe(
+        false,
+      );
+    },
+  );
+
+  it(
+    "Should be false when student selected PD/PPD status from the application but requested a change " +
+      "selecting 'No, I do not have a disability.'",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataApplicationPDPPDStatus = "yes";
+      assessmentConsolidatedData.appealsStudentDisabilityAppealData = {
+        disabilityInformation: "noIDoNotHaveADisability",
+      };
+
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+
+      // Assert
+      expect(calculatedAssessment.variables.calculatedDataPDPPDStatus).toBe(
+        false,
+      );
+    },
+  );
+});
