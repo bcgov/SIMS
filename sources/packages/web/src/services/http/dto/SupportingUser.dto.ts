81b649ba567879fa09e63ec7eae2e637fcde71d9
Lewis Chen <148148914+lewischen-aot@users.noreply.github.com>
2024-01-04 10:28:38 -0800

    #2482 - Update PT Declarations Wording (#2631)
    - Update (COPY PASTE ) PT student declaration to reflect the wording
    provided in the Word document above
    - Update (Copy Paste) PT partner declaration to reflect the wording
    provided in the Word document above
    
    PT Student Declarations Wording Screenshot
    
    ![pt-student-declarations-wording](https://github.com/bcgov/SIMS/assets/148148914/76eb40c6-ab61-490a-b3ea-918541c2cdc1)
    
    PT Partner Declarations Wording Screenshot
    
    ![pt-partner-declarations-wording](https://github.com/bcgov/SIMS/assets/148148914/dc11caf3-d5cc-43b7-9404-0a7ec4bd13b9)

--- sources/packages/web/src/services/http/dto/SupportingUser.dto.ts
+++ sources/packages/web/src/services/http/dto/SupportingUser.dto.ts
@@ -1,4 +1,4 @@
-import { SupportingUserType } from "@/types";
+import { OfferingIntensity, SupportingUserType } from "@/types";
 import { ContactInformationAPIOutDTO } from "./Address.dto";
 import { Expose } from "class-transformer";
 
@@ -43,6 +43,7 @@ export class UpdateSupportingUserAPIInDTO extends ApplicationIdentifierAPIInDTO 
 export interface ApplicationAPIOutDTO {
   programYearStartDate: string;
   formName: string;
+  offeringIntensity: OfferingIntensity;
 }
 
 export interface ApplicationSupportingUsersAPIOutDTO {
