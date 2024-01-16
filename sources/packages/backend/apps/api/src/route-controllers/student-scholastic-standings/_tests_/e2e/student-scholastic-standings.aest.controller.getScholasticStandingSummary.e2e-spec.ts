7e93b9408656db75b7b5b172574ebcbd4cec4267
Shashank Shekhar <sh16011993@gmail.com>
2024-01-16 12:05:43 -0800

    #2594 - Display Lifetime Tally Of Unsuccessful Weeks [e2e tests] (#2681)
    ## As a part of this PR, the following e2e tests were written:
    
    - ###
    StudentScholasticStandingsInstitutionsController(e2e)-getScholasticStandingSummary.
    √ Should throw status code 403 Forbidden when a BC Private Institution
    requests student scholastic summary.
    √ Should throw status code 403 Forbidden when an institution accesses
    the scholastic summary for an unauthorized student.
    √ Should get the scholastic standing summary for the provided student
    including the data retrieved from the SFAS system when a BC Public
    Institution requests it.
    √ Should return zero lifetime unsuccessful completion weeks for the
    provided student as a part of the student scholastic summary when the
    student has no unsuccessful completion weeks in SIMS or in the SFAS
    system when a BC Public Institution requests it.
    
    - ###
    StudentScholasticStandingsAESTController(e2e)-getScholasticStandingSummary.
    √ Should get the scholastic standing summary for the provided student
    including the data retrieved from the SFAS system when a ministry user
    requests it.
    √ Should not retrieve the SFAS system data as a part of the scholastic
    standing summary for the provided student when the combination of birth
    date, last name and sin provided does not exist in the SFAS relations.
    
    - ###
    StudentScholasticStandingsStudentsController(e2e)-getScholasticStandingSummary.
    √ Should get the scholastic standing summary for the provided student
    including the data retrieved from the sfas system when a student user
    requests it.

---
+++ sources/packages/backend/apps/api/src/route-controllers/student-scholastic-standings/_tests_/e2e/student-scholastic-standings.aest.controller.getScholasticStandingSummary.e2e-spec.ts
@@ -0,0 +1,89 @@
+import { HttpStatus, INestApplication } from "@nestjs/common";
+import {
+  E2EDataSources,
+  createE2EDataSources,
+  createFakeStudentScholasticStanding,
+  saveFakeApplication,
+  saveFakeStudent,
+  saveFakeSFASIndividual,
+} from "@sims/test-utils";
+import {
+  AESTGroups,
+  BEARER_AUTH_TYPE,
+  createTestingAppModule,
+  getAESTToken,
+} from "../../../../testHelpers";
+import * as request from "supertest";
+
+describe("StudentScholasticStandingsAESTController(e2e)-getScholasticStandingSummary.", () => {
+  let app: INestApplication;
+  let db: E2EDataSources;
+
+  beforeAll(async () => {
+    const { nestApplication, dataSource } = await createTestingAppModule();
+    app = nestApplication;
+    db = createE2EDataSources(dataSource);
+  });
+
+  it("Should get the scholastic standing summary for the provided student including the data retrieved from the sfas system when a ministry user requests it.", async () => {
+    // Arrange
+    const student = await saveFakeStudent(db.dataSource);
+    const application = await saveFakeApplication(db.dataSource, {
+      student,
+    });
+    const scholasticStanding = createFakeStudentScholasticStanding(
+      { submittedBy: student.user, application },
+      {
+        initialValues: { unsuccessfulWeeks: 15 },
+      },
+    );
+    await db.studentScholasticStanding.save(scholasticStanding);
+    await saveFakeSFASIndividual(db.dataSource, {
+      initialValues: {
+        lastName: student.user.lastName,
+        birthDate: student.birthDate,
+        sin: student.sinValidation.sin,
+        unsuccessfulCompletion: 12,
+      },
+    });
+    const endpoint = `/aest/scholastic-standing/summary/student/${student.id}`;
+    const token = await getAESTToken(AESTGroups.BusinessAdministrators);
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect({ lifetimeUnsuccessfulCompletionWeeks: 27 });
+  });
+
+  it("Should not retrieve the sfas system data as a part of the scholastic standing summary for the provided student when the combination of birth date, last name and sin provided does not exist in the SFAS relations.", async () => {
+    // Arrange
+    const student = await saveFakeStudent(db.dataSource);
+    const application = await saveFakeApplication(db.dataSource, {
+      student,
+    });
+    const scholasticStanding = createFakeStudentScholasticStanding(
+      { submittedBy: student.user, application },
+      {
+        initialValues: { unsuccessfulWeeks: 15 },
+      },
+    );
+    await db.studentScholasticStanding.save(scholasticStanding);
+    await saveFakeSFASIndividual(db.dataSource, {
+      initialValues: {
+        lastName: student.user.lastName,
+        birthDate: new Date().toISOString(),
+        sin: student.sinValidation.sin,
+        unsuccessfulCompletion: 12,
+      },
+    });
+    const endpoint = `/aest/scholastic-standing/summary/student/${student.id}`;
+    const token = await getAESTToken(AESTGroups.BusinessAdministrators);
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect({ lifetimeUnsuccessfulCompletionWeeks: 15 });
+  });
+});
---
+++ sources/packages/backend/apps/api/src/route-controllers/student-scholastic-standings/_tests_/e2e/student-scholastic-standings.students.controller.getScholasticStandingSummary.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { HttpStatus, INestApplication } from "@nestjs/common";
+import {
+  E2EDataSources,
+  createE2EDataSources,
+  createFakeStudentScholasticStanding,
+  saveFakeApplication,
+  saveFakeStudent,
+} from "@sims/test-utils";
+import {
+  BEARER_AUTH_TYPE,
+  FakeStudentUsersTypes,
+  createTestingAppModule,
+  getStudentToken,
+  mockUserLoginInfo,
+} from "../../../../testHelpers";
+import * as request from "supertest";
+import { saveFakeSFASIndividual } from "@sims/test-utils/factories/sfas-individuals";
+import { TestingModule } from "@nestjs/testing";
+
+describe("StudentScholasticStandingsStudentsController(e2e)-getScholasticStandingSummary.", () => {
+  let app: INestApplication;
+  let db: E2EDataSources;
+  let appModule: TestingModule;
+
+  beforeAll(async () => {
+    const { nestApplication, module, dataSource } =
+      await createTestingAppModule();
+    app = nestApplication;
+    db = createE2EDataSources(dataSource);
+    appModule = module;
+  });
+
+  it("Should get the scholastic standing summary for the provided student including the data retrieved from the sfas system when a student user requests it.", async () => {
+    // Arrange
+    const student = await saveFakeStudent(db.dataSource);
+    // Mock user service to return the saved student.
+    await mockUserLoginInfo(appModule, student);
+    const application = await saveFakeApplication(db.dataSource, {
+      student,
+    });
+    const scholasticStanding = createFakeStudentScholasticStanding(
+      { submittedBy: student.user, application },
+      {
+        initialValues: { unsuccessfulWeeks: 5 },
+      },
+    );
+    await db.studentScholasticStanding.save(scholasticStanding);
+    await saveFakeSFASIndividual(db.dataSource, {
+      initialValues: {
+        lastName: student.user.lastName,
+        birthDate: student.birthDate,
+        sin: student.sinValidation.sin,
+        unsuccessfulCompletion: 12,
+      },
+    });
+    const endpoint = "/students/scholastic-standing/summary";
+    const token = await getStudentToken(
+      FakeStudentUsersTypes.FakeStudentUserType1,
+    );
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect({ lifetimeUnsuccessfulCompletionWeeks: 17 });
+  });
+});
