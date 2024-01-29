da9a92d40ac718105658f738d31d091eb099ac5a
Andre Pestana <78114138+andrepestana-aot@users.noreply.github.com>
2024-01-29 12:12:33 -0800

    #2522 - COE Disability Application Status description (#2747)
    COE Disability Application Status description: 
    - "Assessment Includes disability funding types." (corresponds to option
    1 in the student form)
    - "No disability funding types included on assessment." (corresponds to
    option 2 or 3 in the student form)
    
    
    ![image](https://github.com/bcgov/SIMS/assets/78114138/d6411907-1463-4675-b396-f9e4e9012a51)

--- sources/packages/web/src/composables/useFormatters.ts
+++ sources/packages/web/src/composables/useFormatters.ts
@@ -373,16 +373,10 @@ export function useFormatters() {
   const applicationDisabilityStatusToDisplay = (
     applicationDisabilityStatus: ApplicationDisabilityStatus,
   ): string => {
-    switch (applicationDisabilityStatus) {
-      case ApplicationDisabilityStatus.yes:
-        return "Yes, I identify as a student with a disability and have verified my Disability Status with StudentAid BC before. I want this application to be assessed for all funding types (including disability type funding).";
-      case ApplicationDisabilityStatus.noIWantToAccessOtherFundingTypes:
-        return "No, I have not verified my Disability Status with StudentAid BC and do not identify as a student with a disability at this time. I want this application to be assessed for other funding types while my Appendix 8 form is being processed to verify my Disability Status.";
-      case ApplicationDisabilityStatus.noIDoNotHaveADisability:
-        return "No, I will not verify my Disability Status with StudentAid BC. I want this application to be assessed for other funding types only (no disability type funding).";
-      default:
-        return "";
+    if (applicationDisabilityStatus === ApplicationDisabilityStatus.yes) {
+      return "Assessment includes disability funding types.";
     }
+    return "No disability funding types included on assessment.";
   };
 
   return {
