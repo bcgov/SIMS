e964e3c3d1e07795b7e84e0244c45eb8c43bc840
Andre Pestana <78114138+andrepestana-aot@users.noreply.github.com>
2024-02-06 14:13:01 -0800

    #2713 - Revise Disbursement Schedule for PT offerings (#2788)
    - Changed the parttime-configure-disbursement workflow:
    
    ![image](https://github.com/bcgov/SIMS/assets/78114138/9b17f6d4-361d-4ca1-b7d7-0cb72fd9f71d)
    
    ```
    [
      {
        disbursementDate: disbursmenentScheduleDate1,
        negotiatedExpiryDate: disbursmenentScheduleDate1,
        disbursements: [
          {
            valueCode: "CSLP",
            valueType: "Canada Loan",
            valueAmount: ceiling(finalFederalAwardNetCSLPAmount / 2),
            awardEligibility: true
          },
          {
            valueCode: "CSGP",
            valueType: "Canada Grant",
            valueAmount: ceiling(finalFederalAwardNetCSGPAmount / 2),
            awardEligibility: awardEligibilityCSGP
          },
          {
            valueCode: "CSGD",
            valueType: "Canada Grant",
            valueAmount: ceiling(finalFederalAwardNetCSGDAmount /2),
            awardEligibility: awardEligibilityCSGD
          },
          {
            valueCode: "CSPT",
            valueType: "Canada Grant",
            valueAmount: ceiling(finalFederalAwardNetCSPTAmount / 2),
            awardEligibility: awardEligibilityCSPT
          },
          {
            valueCode: "BCAG",
            valueType: "BC Grant",
            valueAmount: ceiling(finalProvincialAwardNetBCAGAmount / 2),
            awardEligibility: awardEligibilityBCAG
          },
          {
            valueCode: "SBSD",
            valueType: "BC Grant",
            valueAmount: ceiling(finalProvincialAwardNetSBSDAmount / 2),
            awardEligibility: awardEligibilitySBSD
          }
        ]
        [item.awardEligibility != false]
      },
        {
        disbursementDate: disbursmenentScheduleDate2,
        negotiatedExpiryDate: disbursmenentScheduleDate2,
        disbursements: [
             {
                valueCode: "CSLP",
                valueType: "Canada Loan",
                valueAmount: ceiling(finalFederalAwardNetCSLPAmount / 2),
                awardEligibility: true
              },
              {
                valueCode: "CSGP",
                valueType: "Canada Grant",
                valueAmount: ceiling(finalFederalAwardNetCSGPAmount / 2),
                awardEligibility: awardEligibilityCSGP
              },
              {
                valueCode: "CSGD",
                valueType: "Canada Grant",
                valueAmount: ceiling(finalFederalAwardNetCSGDAmount /2),
                awardEligibility: awardEligibilityCSGD
              },
              {
                valueCode: "CSPT",
                valueType: "Canada Grant",
                valueAmount: ceiling(finalFederalAwardNetCSPTAmount / 2),
                awardEligibility: awardEligibilityCSPT
              },
              {
                valueCode: "BCAG",
                valueType: "BC Grant",
                valueAmount: ceiling(finalProvincialAwardNetBCAGAmount / 2),
                awardEligibility: awardEligibilityBCAG
              },
              {
                valueCode: "SBSD",
                valueType: "BC Grant",
                valueAmount: ceiling(finalProvincialAwardNetSBSDAmount / 2),
                awardEligibility: awardEligibilitySBSD
              }
        ]
        [item.awardEligibility != false]
      }
    ][count(item.disbursements) > 0]
    
    ```
    
    
    Offering with less than 17 weeks:
    
    ![image](https://github.com/bcgov/SIMS/assets/78114138/b04bba5a-b959-4047-9067-e74fb8b49d1b)
    
    Offering with more than 17 weeks:
    
    ![image](https://github.com/bcgov/SIMS/assets/78114138/ce53e6f1-3b18-4f8c-9f35-a51b41d63b21)
    
    - Added E2E tests;
    - Replaced `decimal` used in disbursement for 17 weeks or less by
    `ceiling`.

---
+++ sources/packages/backend/workflow/test/test-utils/factories/configure-disbursement-data.ts
@@ -0,0 +1,28 @@
+import { ConfigureDisbursementData } from "workflow/test/models";
+
+/**
+ * Create fake data for configure disbursement part time workflow.
+ * @param programYear program year.
+ * @returns configure disbursement data.
+ */
+export function createFakeConfigureDisbursementPartTimeData(
+  programYear: string,
+): ConfigureDisbursementData {
+  const [, programEndYear] = programYear.split("-");
+  return {
+    offeringStudyStartDate: `${programEndYear}-02-01`,
+    offeringStudyEndDate: `${programEndYear}-05-24`,
+    offeringWeeks: 17,
+    awardEligibilityCSGP: true,
+    awardEligibilityCSGD: true,
+    awardEligibilityCSPT: true,
+    awardEligibilityBCAG: true,
+    awardEligibilitySBSD: true,
+    finalFederalAwardNetCSLPAmount: 1000,
+    finalFederalAwardNetCSGPAmount: 2000,
+    finalFederalAwardNetCSGDAmount: 3000,
+    finalFederalAwardNetCSPTAmount: 4000,
+    finalProvincialAwardNetBCAGAmount: 5000,
+    finalProvincialAwardNetSBSDAmount: 6000,
+  };
+}
