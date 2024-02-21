febbfc751fdba683af91c6fdd0254ae60ebc0743
Shashank Shekhar <sh16011993@gmail.com>
2024-02-20 19:15:11 -0800

    Code Update

--- sources/packages/backend/libs/src-sql/sfas-restrictions/Bulk-insert-restrictions.sql
+++ sources/packages/backend/libs/src-sql/sfas-restrictions/Bulk-insert-restrictions.sql
@@ -8,7 +8,7 @@
 INSERT INTO
   sims.student_restrictions (student_id, restriction_id)
 SELECT
-  DISTINCT(sfas_individuals.student_id),
+  students.student_id,
   (
     SELECT
       restrictions.id
@@ -19,27 +19,52 @@ SELECT
   ) AS restriction_id
 FROM
   (
-    SELECT
-      sfas_restrictions.code,
-      CASE
-        WHEN sfas_restrictions.code = 'A12' THEN '12'
-        WHEN sfas_restrictions.code = 'AF1' THEN 'AF'
-        WHEN sfas_restrictions.code = 'B2D' THEN 'B2'
-        ELSE sfas_restrictions.code
-      END AS mapped_code,
-      sfas_restrictions.individual_id,
-      sfas_restrictions.is_included
-    FROM
-      sims.sfas_restrictions sfas_restrictions
-    WHERE
-      sfas_restrictions.removal_date IS NULL
-      AND sfas_restrictions.is_included = false
-  ) mapped_restrictions
-  INNER JOIN sims.sfas_individuals sfas_individuals ON mapped_restrictions.individual_id = sfas_individuals.id
-  LEFT JOIN sims.restrictions restrictions ON mapped_restrictions.mapped_code = restrictions.restriction_code
+    (
+      SELECT
+        student_restrictions.student_id AS student_id
+      FROM
+        sims.student_restrictions student_restrictions
+      WHERE
+        student_restrictions.restriction_id = (
+          SELECT
+            id
+          FROM
+            sims.restrictions
+          WHERE
+            restriction_code = 'LGCY'
+        )
+        AND student_restrictions.is_active = TRUE
+    ) filtered_student_restrictions
+    RIGHT JOIN (
+      SELECT
+        DISTINCT(sfas_individuals.student_id) AS student_id
+      FROM
+        (
+          SELECT
+            sfas_restrictions.code,
+            CASE
+              WHEN sfas_restrictions.code = 'A12' THEN '12'
+              WHEN sfas_restrictions.code = 'AF1' THEN 'AF'
+              WHEN sfas_restrictions.code = 'B2D' THEN 'B2'
+              ELSE sfas_restrictions.code
+            END AS mapped_code,
+            sfas_restrictions.individual_id,
+            sfas_restrictions.is_included
+          FROM
+            sims.sfas_restrictions sfas_restrictions
+          WHERE
+            sfas_restrictions.removal_date IS NULL
+            AND sfas_restrictions.is_included = FALSE
+        ) mapped_restrictions
+        INNER JOIN sims.sfas_individuals sfas_individuals ON mapped_restrictions.individual_id = sfas_individuals.id
+        LEFT JOIN sims.restrictions restrictions ON mapped_restrictions.mapped_code = restrictions.restriction_code
+      WHERE
+        sfas_individuals.student_id IS NOT NULL
+        AND restrictions.restriction_code IS NULL
+    ) students ON filtered_student_restrictions.student_id = students.student_id
+  )
 WHERE
-  sfas_individuals.student_id IS NOT NULL
-  AND restrictions.restriction_code IS NULL;
+  filtered_student_restrictions.student_id IS NULL;
 
 /* 
  * After all the SFAS restrictions are imported, 
