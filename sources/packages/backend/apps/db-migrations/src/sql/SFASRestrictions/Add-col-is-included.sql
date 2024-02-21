4a605953f5c5f59825605439b2121e51443394ce
Shashank Shekhar <sh16011993@gmail.com>
2024-02-21 14:15:42 -0800

    Code Update

--- sources/packages/backend/apps/db-migrations/src/sql/SFASRestrictions/Add-col-is-included.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE
-  sims.sfas_restrictions
-ADD
-  COLUMN processed BOOLEAN NOT NULL DEFAULT FALSE;
-
-COMMENT ON COLUMN sfas_restrictions.processed IS 'This field indicates whether the SFAS restriction has been added to the student restrictions table or not.';
