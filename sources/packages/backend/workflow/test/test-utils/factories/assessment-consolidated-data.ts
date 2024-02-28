b45172eeb0026f403d4e91dad4d6c2f3073939a6
Guru-AOT <gurumoorthy.mohan@aot-technologies.com>
2024-02-28 10:47:19 -0700

    updated

--- sources/packages/backend/workflow/test/test-utils/factories/assessment-consolidated-data.ts
+++ sources/packages/backend/workflow/test/test-utils/factories/assessment-consolidated-data.ts
@@ -88,6 +88,7 @@ export function createFakePartTimeAssessmentConsolidatedData(
     offeringCourseLoad: 40,
     studentDataCRAReportedIncome: 10001,
     partner1CRAReportedIncome: 15001,
+    programYearTotalPartTimeCSGD: 200,
   };
 }
 /**
