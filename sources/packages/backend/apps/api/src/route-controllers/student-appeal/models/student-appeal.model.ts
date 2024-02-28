df73ea786a1504bab5a37c2d09197e0ac54e4186
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2024-02-27 17:04:32 -0700

    Student financial information request a change

--- sources/packages/backend/apps/api/src/route-controllers/student-appeal/models/student-appeal.model.ts
+++ sources/packages/backend/apps/api/src/route-controllers/student-appeal/models/student-appeal.model.ts
@@ -1,6 +1,6 @@
 /**
  * Student appeal form data which is not persisted
- * and used for logic or validation purpose.
+ * but used for logic or validation purpose.
  */
 export class StudentAppealNonSubmissionData {
   programYear: string;
