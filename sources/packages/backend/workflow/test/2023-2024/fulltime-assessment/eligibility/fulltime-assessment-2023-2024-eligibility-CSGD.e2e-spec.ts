cf34e4138703bcf46098e15a7d7245f728aa554c
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2024-03-12 16:17:01 -0700

    Missing createFakeStudentDependentEligible refactor

--- sources/packages/backend/workflow/test/2023-2024/fulltime-assessment/eligibility/fulltime-assessment-2023-2024-eligibility-CSGD.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2023-2024/fulltime-assessment/eligibility/fulltime-assessment-2023-2024-eligibility-CSGD.e2e-spec.ts
@@ -72,7 +72,6 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-eligibility-CSGD
     assessmentConsolidatedData.studentDataDependants = [
       createFakeStudentDependentEligible(
         DependentEligibility.EligibleOver22YearsOld,
-        assessmentConsolidatedData.offeringStudyStartDate,
       ),
     ];
     // Act
