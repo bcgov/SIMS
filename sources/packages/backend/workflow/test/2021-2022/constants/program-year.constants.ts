925eb20f413dadb1029098aeeaa2f483e642fc9e
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2024-03-14 16:28:06 -0700

    #2705 - Removed 2021-2022 E2E Tests and E2E Files Renamed (#2995)
    Removed 2021-2022 E2E tests, and removed the program year from E2E files
    already in program year folders.
    
    #### Deleted 2021-2022 E2E Tests as requested in the ticket.
    
    ![image](https://github.com/bcgov/SIMS/assets/61259237/ddb42564-01c6-44e5-a92f-522a20cad993)
    
    #### Remove the program year from the file names.
    
    ![image](https://github.com/bcgov/SIMS/assets/61259237/c131b5f6-376d-495c-a88a-44301db9202a)

--- sources/packages/backend/workflow/test/2021-2022/constants/program-year.constants.ts
+++
@@ -1,6 +0,0 @@
-export const PROGRAM_YEAR = "2021-2022";
-/**
- * Additional transportation onsite distance limit for setting the
- * boundary in E2E tests
- */
-export const ADDITIONAL_TRANSPORTATION_ONSITE_DISTANCE_LIMIT = 280;
