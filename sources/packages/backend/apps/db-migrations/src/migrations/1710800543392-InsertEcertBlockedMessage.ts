90f43444448d78986c69ec34b1c465a7ae809d83
Shashank Shekhar <sh16011993@gmail.com>
2024-03-20 09:58:13 -0700

    Code Update

--- sources/packages/backend/apps/db-migrations/src/migrations/1710800543392-InsertEcertBlockedMessage.ts
+++ sources/packages/backend/apps/db-migrations/src/migrations/1710800543392-InsertEcertBlockedMessage.ts
@@ -6,13 +6,19 @@ export class InsertEcertBlockedMessage1710800543392
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      getSQLFileData("Insert-ecert-blocked-message.sql", "Ecert"),
+      getSQLFileData(
+        "Insert-ecert-blocked-message.sql",
+        "NotificationMessages",
+      ),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      getSQLFileData("Rollback-insert-ecert-blocked-message.sql", "Ecert"),
+      getSQLFileData(
+        "Rollback-insert-ecert-blocked-message.sql",
+        "NotificationMessages",
+      ),
     );
   }
 }
