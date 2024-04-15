a42f8af3e341c66df38d9305a034cee913b3c17f
katrina-aot-git <katrina.strickland_git_hub_only@aot-technologies.com>
2024-04-15 15:26:13 -0700

    update warning conditional and expand variable name

--- sources/packages/web/src/views/student/financial-aid-application/FullTimeApplication.vue
+++ sources/packages/web/src/views/student/financial-aid-application/FullTimeApplication.vue
@@ -159,7 +159,7 @@ export default defineComponent({
         disabilityStatus: disabilityStatusToDisplay(
           studentInfo.disabilityStatus,
         ),
-        disabilityStatusValue: studentInfo.disabilityStatus,
+        studentProfileDisabilityStatusValue: studentInfo.disabilityStatus,
       };
       const programYear = {
         programYearStartDate: applicationData.programYearStartDate,
