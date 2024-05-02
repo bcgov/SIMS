f9a71916827a3feb637e2802f1cc03dd5b3f0c45
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2024-05-02 11:00:07 -0600

    fixing the SQL

--- sources/packages/backend/apps/db-migrations/src/migrations/1714624663811-CreateECertFeedbackErrors.ts
+++ sources/packages/backend/apps/db-migrations/src/migrations/1714624663811-CreateECertFeedbackErrors.ts
@@ -31,7 +31,7 @@ export class CreateECertFeedbackErrors1714624663811
     await queryRunner.query(
       getSQLFileData(
         "Rollback-create-ecert-feedback-errors-table.sql",
-        "EertFeedbackErrors",
+        "ECertFeedbackErrors",
       ),
     );
   }
