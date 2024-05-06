87a8ace13768d2193964778318fd2cda6b65bbc3
Shashank Shekhar <sh16011993@gmail.com>
2024-05-06 16:20:40 -0700

    Code Update

--- sources/packages/backend/libs/services/src/report/report.service.ts
+++ sources/packages/backend/libs/services/src/report/report.service.ts
@@ -59,28 +59,6 @@ export class ReportService extends RecordDataModelService<ReportConfig> {
   }
 
   /**
-   * Stream file as downloadable response.
-   * @param response http response as file.
-   * @param reportName report name.
-   * @param fileContent content of the file.
-   */
-  streamFile(response: Response, reportName: string, fileContent: string) {
-    const timestamp = getFileNameAsCurrentTimestamp();
-    const filename = `${reportName}_${timestamp}.csv`;
-    response.setHeader(
-      "Content-Disposition",
-      `attachment; filename=${filename}`,
-    );
-    response.setHeader("Content-Type", "text/csv");
-    response.setHeader("Content-Length", fileContent.toString().length);
-
-    const stream = new Readable();
-    stream.push(fileContent.toString());
-    stream.push(null);
-    stream.pipe(response);
-  }
-
-  /**
    * When a filter param value is of type object
    * then convert the object to string array to be able to pass as query argument.
    * Otherwise return the value.
