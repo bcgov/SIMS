a7bd2b84839b7966add4807404f60bc1816faf04
Lewis Chen <lewis.chen@aot-technologies.com>
2024-07-05 17:39:45 +0000

    removal of unused class and methods

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/esdc-integration/ecert-integration/disbursement-receipts-integration.scheduler.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/esdc-integration/ecert-integration/disbursement-receipts-integration.scheduler.ts
@@ -4,7 +4,7 @@ import { QueueService } from "@sims/services/queue";
 import { SystemUsersService } from "@sims/services/system-users";
 import { QueueNames } from "@sims/utilities";
 import { Job, Queue } from "bull";
-import { QueueProcessSummary } from "../../../models/processors.models";
+import { ProcessSummary } from "@sims/utilities/logger";
 import { BaseScheduler } from "../../base-scheduler";
 import { ESDCFileResponse } from "../models/esdc.models";
 
@@ -29,18 +29,15 @@ export class DisbursementReceiptsFileIntegrationScheduler extends BaseScheduler<
   async processDisbursementReceipts(
     job: Job<void>,
   ): Promise<ESDCFileResponse[]> {
-    const summary = new QueueProcessSummary({
-      appLogger: this.logger,
-      jobLogger: job,
-    });
-    await summary.info(
+    const processSummary = new ProcessSummary();
+    processSummary.info(
       `Processing full time disbursement receipts integration job ${job.id} of type ${job.name}.`,
     );
     const auditUser = this.systemUsersService.systemUser;
     const processResponse =
       await this.disbursementReceiptProcessingService.process(auditUser.id);
     await this.cleanSchedulerQueueHistory();
-    await summary.info(
+    processSummary.info(
       `Completed full time disbursement receipts integration job ${job.id} of type ${job.name}.`,
     );
     return processResponse.map((response) => ({
