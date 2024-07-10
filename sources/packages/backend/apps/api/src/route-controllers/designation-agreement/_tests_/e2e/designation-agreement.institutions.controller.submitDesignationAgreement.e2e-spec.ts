ee25845df9a9a4ef0e3d715124af5660f4f9693b
katrina-aot-git <katrina.strickland_git_hub_only@aot-technologies.com>
2024-07-10 16:18:34 -0700

    change error for multiple designation requests and add e2e test

--- sources/packages/backend/apps/api/src/route-controllers/designation-agreement/_tests_/e2e/designation-agreement.institutions.controller.submitDesignationAgreement.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/designation-agreement/_tests_/e2e/designation-agreement.institutions.controller.submitDesignationAgreement.e2e-spec.ts
@@ -27,7 +27,9 @@ describe("DesignationAgreementInstitutionsController(e2e)-submitDesignationAgree
   let app: INestApplication;
   let db: E2EDataSources;
   let collegeF: Institution;
+  let collegeC: Institution;
   let collegeFLocation: InstitutionLocation;
+  let collegeCLocation: InstitutionLocation;
   let testingModule: TestingModule;
 
   beforeAll(async () => {
@@ -35,16 +37,30 @@ describe("DesignationAgreementInstitutionsController(e2e)-submitDesignationAgree
       await createTestingAppModule();
     app = nestApplication;
     db = createE2EDataSources(dataSource);
-    const { institution } = await getAuthRelatedEntities(
+    let { institution } = await getAuthRelatedEntities(
       db.dataSource,
       InstitutionTokenTypes.CollegeFAdminLegalSigningUser,
     );
     collegeF = institution;
+
+    ({ institution } = await getAuthRelatedEntities(
+      db.dataSource,
+      InstitutionTokenTypes.CollegeCAdminLegalSigningUser,
+    ));
+    collegeC = institution;
+
     collegeFLocation = createFakeInstitutionLocation({ institution: collegeF });
     await authorizeUserTokenForLocation(
       db.dataSource,
       InstitutionTokenTypes.CollegeFAdminLegalSigningUser,
       collegeFLocation,
+    );
+
+    collegeCLocation = createFakeInstitutionLocation({ institution: collegeC });
+    await authorizeUserTokenForLocation(
+      db.dataSource,
+      InstitutionTokenTypes.CollegeCAdminLegalSigningUser,
+      collegeCLocation,
     );
     testingModule = module;
   });
@@ -99,6 +115,58 @@ describe("DesignationAgreementInstitutionsController(e2e)-submitDesignationAgree
     expect(designationAgreement.designationStatus).toBe(
       DesignationAgreementStatus.Pending,
     );
+  });
+
+  it("Should return an unprocessable entity when a new designation is requested and there is already a pending designation agreement.", async () => {
+    // Arrange
+    const payload = {
+      dynamicData: {
+        eligibilityOfficers: [],
+        enrolmentOfficers: [],
+        scheduleA: false,
+        scheduleB: false,
+        scheduleD: false,
+        legalAuthorityName: "SIMS COLLC",
+        legalAuthorityEmailAddress: "test@gov.bc.ca",
+        agreementAccepted: false,
+      },
+      locations: [
+        {
+          locationId: collegeCLocation.id,
+          requestForDesignation: true,
+        },
+      ],
+    };
+    const formService = await getProviderInstanceForModule(
+      testingModule,
+      AppInstitutionsModule,
+      FormService,
+    );
+    formService.dryRunSubmission = jest
+      .fn()
+      .mockResolvedValue({ valid: true, data: { data: payload } });
+    const institutionUserToken = await getInstitutionToken(
+      InstitutionTokenTypes.CollegeCAdminLegalSigningUser,
+    );
+    const endpoint = "/institutions/designation-agreement";
+
+    // Act/Assert
+    let designationAgreementId: number;
+    await request(app.getHttpServer())
+      .post(endpoint)
+      .send(payload)
+      .auth(institutionUserToken, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.CREATED)
+      .then((response) => {
+        expect(response.body.id).toBeGreaterThan(0);
+        designationAgreementId = response.body.id;
+      });
+
+    await request(app.getHttpServer())
+      .post(endpoint)
+      .send(payload)
+      .auth(institutionUserToken, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.UNPROCESSABLE_ENTITY)
   });
 
   afterAll(async () => {
