8bc0f4535c7d3f8cc764eac006ac05bce0b325f8
Andre Pestana <andre.pestana@aot-technologies.com>
2024-07-15 17:36:21 -0700

    review issues

--- sources/packages/backend/apps/queue-consumers/src/services/index.ts
+++ sources/packages/backend/apps/queue-consumers/src/services/index.ts
@@ -2,5 +2,4 @@ export * from "./student-assessment/student-assessment.service";
 export * from "./application/application.service";
 export * from "./workflow/workflow-enqueuer.service";
 export * from "./cas-supplier/cas-supplier.service";
-export * from "./cas-supplier/cas.service";
 export * from "./student-file/student-file.service";
