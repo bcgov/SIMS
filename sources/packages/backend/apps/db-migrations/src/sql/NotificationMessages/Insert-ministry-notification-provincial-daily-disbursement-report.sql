013c3c48e54ac619cbb3815ab1df46378d64d0aa
Lewis Chen <lewis.chen@aot-technologies.com>
2024-07-16 10:04:25 -0700

    fix - changed notification id to 28

--- sources/packages/backend/apps/db-migrations/src/sql/NotificationMessages/Insert-ministry-notification-provincial-daily-disbursement-report.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/NotificationMessages/Insert-ministry-notification-provincial-daily-disbursement-report.sql
@@ -2,7 +2,7 @@ INSERT INTO
   sims.notification_messages(id, description, template_id)
 VALUES
   (
-    27,
+    28,
     'Ministry notification for provincial daily disbursement report.',
     '730db0dc-967b-4adb-afa2-38235ad9f051'
   );
