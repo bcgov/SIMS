51379bf30e51c9ab7d209de3171c87d5849e538a
Shashank Shekhar <sh16011993@gmail.com>
2024-07-23 14:54:08 -0700

    Migration revert bug fix

--- sources/packages/backend/apps/db-migrations/src/sql/Queue/Rollback-drop-fin-process-provincial-daily-disbursements-integration.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Queue/Rollback-drop-fin-process-provincial-daily-disbursements-integration.sql
@@ -1,13 +1,14 @@
 INSERT INTO
-    queue_configurations(queue_name, queue_configuration)
+  sims.queue_configurations(queue_name, queue_configuration, queue_settings)
 VALUES
-    (
-        'fin-process-provincial-daily-disbursements-integration',
-        '{
-        "retry": 3,
-        "retryInterval": 180000,
-        "dashboardReadonly": false,
-        "cron": "0 18 * * *",
-        "cleanUpPeriod": 2592000000
-      }' :: json
-    );
+  (
+    'fin-process-provincial-daily-disbursements-integration',
+    '{ 
+        "retry": 3, 
+        "retryInterval": 180000, 
+        "dashboardReadonly": false, 
+        "cron": "0 18 * * *", 
+        "cleanUpPeriod": 2592000000 
+      }' :: json,
+    '{ "maxStalledCount": 0, "lockDuration": 60000, "lockRenewTime": 5000 }' :: json
+  );
