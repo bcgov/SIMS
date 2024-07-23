07f7f9896312a6be9d85d85e33a1116974b44cfa
Shashank Shekhar <sh16011993@gmail.com>
2024-07-23 11:32:56 -0700

    #2973 - File Uploader Bug

--- sources/packages/web/src/services/FormUploadService.ts
+++ sources/packages/web/src/services/FormUploadService.ts
@@ -1,6 +1,7 @@
-import { FormUploadFileInfo } from "@/types";
+import { FileOriginType, FormUploadFileInfo } from "@/types";
 import { AxiosRequestConfig } from "axios";
 import ApiClient from "../services/http/ApiClient";
+import { useFileUtils } from "@/composables";
 
 /**
  * Implements the methods and signatures that are necessaries
@@ -46,18 +47,13 @@ export default class FormUploadService {
 
   public async downloadFile(fileInfo: FormUploadFileInfo) {
     try {
-      const fileContent = await ApiClient.FileUpload.download(fileInfo.url);
-      // Change the storage type to base64 to allow the file to be "downloaded"
-      // using the bytes retrieved instead of just opening an url.
-      // if we use directly the url we will not have the oportunity to authorize
-      // the file download.
-      return {
-        storage: "base64",
-        url: fileContent,
-        originalName: fileInfo.originalName,
-        name: fileInfo.name,
-        type: fileInfo.type,
+      const fileUtils = useFileUtils();
+      const studentDocumentDetails = {
+        fileName: fileInfo.originalName,
+        uniqueFileName: fileInfo.name,
+        fileOrigin: FileOriginType.Temporary,
       };
+      await fileUtils.downloadStudentDocument(studentDocumentDetails);
     } catch {
       throw new Error(
         "There was an unexpected error while downloading the file.",
