bcb4b41d1217f7632b7415a82e37ae77e97c14dd
katrina-aot-git <katrina.strickland_git_hub_only@aot-technologies.com>
2024-08-09 15:32:48 -0700

    add function and tests for reducing content to ascii

--- sources/packages/backend/libs/integrations/src/services/ssh/sftp-integration-base.ts
+++ sources/packages/backend/libs/integrations/src/services/ssh/sftp-integration-base.ts
@@ -56,7 +56,7 @@ export abstract class SFTPIntegrationBase<DownloadType> {
     try {
       this.logger.log(`Uploading ${remoteFilePath}`);
       return await client.put(
-        Buffer.from(rawContent, FILE_DEFAULT_ENCODING),
+        this.convertRawContent(rawContent),
         remoteFilePath,
       );
     } finally {
@@ -64,6 +64,112 @@ export abstract class SFTPIntegrationBase<DownloadType> {
       await SshService.closeQuietly(client);
       this.logger.log("SFTP client finalized.");
     }
+  }
+
+  /**
+   * Converts input into an ASCII 7 bit buffer.
+   * @param rawContent Raw content in string format.
+   * @returns a buffer of the input with extended ASCII accented characters converted to unaccented characters.
+   */
+  convertRawContent(rawContent: string) {
+    const content = Buffer.from(rawContent, FILE_DEFAULT_ENCODING);
+    for (const [index, b] of content.entries()) {
+      if (b > 127) {
+        // if extended ascii
+        switch (b) {
+          case 192:
+          case 193:
+          case 194:
+          case 195:
+          case 196:
+          case 197: // ÀÁÂÃÄÅ
+            content[index] = 65; // replace with A
+            break;
+          case 199: // Ç
+            content[index] = 67; // replace with C
+            break;
+          case 200:
+          case 201:
+          case 202:
+          case 203: // ÈÉÊË
+            content[index] = 69; // replace with E
+            break;
+          case 204:
+          case 205:
+          case 206:
+          case 207: // ÌÍÎÏ
+            content[index] = 73; // replace with I
+            break;
+          case 209: // Ñ
+            content[index] = 78; // replace with N
+            break;
+          case 210:
+          case 211:
+          case 212:
+          case 213:
+          case 214: // ÒÓÔÕÖ
+            content[index] = 79; // replace with O
+            break;
+          case 217:
+          case 218:
+          case 219:
+          case 220: // ÙÚÛÜ
+            content[index] = 85; // replace with U
+            break;
+          case 223: // ß
+            content[index] = 115; // replace with s
+            break;
+          case 224:
+          case 225:
+          case 226:
+          case 227:
+          case 228:
+          case 229: // àáâãäå
+            content[index] = 97; // replace with a
+            break;
+          case 231: // ç
+            content[index] = 99; // replace with c
+            break;
+          case 232:
+          case 233:
+          case 234:
+          case 235: // èéêë
+            content[index] = 101; // replace with e
+            break;
+          case 236:
+          case 237:
+          case 238:
+          case 239: // ìíîï
+            content[index] = 105; // replace with i
+            break;
+          case 241: // ñ
+            content[index] = 110; // replace with n
+            break;
+          case 242:
+          case 243:
+          case 244:
+          case 245:
+          case 246: // óòôõö
+            content[index] = 111; // replace with o
+            break;
+          case 249:
+          case 250:
+          case 251:
+          case 252: // ùúûü
+            content[index] = 117; // replace with u
+            break;
+          case 253:
+          case 255: // ýÿ
+            content[index] = 121; // replace with y
+            break;
+          default:
+            content[index] = 63; // replace with ? by default
+            break;
+        }
+      }
+      // else keep the same character
+    }
+    return content;
   }
 
   /**
