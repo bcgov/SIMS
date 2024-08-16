03f709502bd810143f0f130d8446d66013105c46
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2024-08-16 15:38:47 -0700

    Reverting file

--- sources/packages/web/src/services/http/common/HttpClient.ts
+++ sources/packages/web/src/services/http/common/HttpClient.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const apiUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://dev-aest-sims.apps.silver.devops.gov.bc.ca/api"
-    : "http://localhost:3000/api";
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3000/api";
 
 const axiosOptions = { baseURL: apiUrl };
 const axiosInstance = axios.create(axiosOptions);
