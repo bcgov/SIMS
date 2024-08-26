4a1b9551aa3d7a58fe29a046ac6a0e06337e8af2
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2024-08-26 14:18:10 -0600

    initial-commit

---
+++ sources/packages/backend/apps/db-migrations/src/sql/Queue/Update-configuration-scheduler-cron.sql
@@ -0,0 +1,167 @@
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
+
+update
+    sims.queue_configurations
+set
+    queue_configuration = jsonb_set(
+        queue_configuration,
+        '{cron}',
+        '"0 15 * * *"',
+        false
+    )
+where
+    queue_name = 'ier12-integration';
