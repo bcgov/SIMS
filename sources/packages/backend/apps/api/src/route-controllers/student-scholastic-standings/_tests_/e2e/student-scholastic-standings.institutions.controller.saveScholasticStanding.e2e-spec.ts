4f1c3742c19f09674d90c5f1ce53ab1bff7f4e7f
guru-aot <gurumoorthy.mohan@gov.bc.ca>
2024-09-18 12:36:16 -0600

    removed only

--- sources/packages/backend/apps/api/src/route-controllers/student-scholastic-standings/_tests_/e2e/student-scholastic-standings.institutions.controller.saveScholasticStanding.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/student-scholastic-standings/_tests_/e2e/student-scholastic-standings.institutions.controller.saveScholasticStanding.e2e-spec.ts
@@ -185,7 +185,7 @@ describe("StudentScholasticStandingsInstitutionsController(e2e)-saveScholasticSt
       });
   });
 
-  it.only("Should create a new scholastic standing when the institution user requests it and keep the existent student appeal.", async () => {
+  it("Should create a new scholastic standing when the institution user requests it and keep the existent student appeal.", async () => {
     // Arrange
     const application = await saveFakeApplication(
       db.dataSource,
