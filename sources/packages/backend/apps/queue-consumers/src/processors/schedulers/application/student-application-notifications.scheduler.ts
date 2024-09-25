1c324525866cd9c170678b3b3584a1a6d0d3b519
Ashish <hi@bidyashish.com>
2024-09-25 16:45:32 -0700

    wip

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/application/student-application-notifications.scheduler.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/application/student-application-notifications.scheduler.ts
@@ -1,51 +1,52 @@
 import { InjectQueue, Process, Processor } from "@nestjs/bull";
+import { QueueService } from "@sims/services/queue";
 import { Job, Queue } from "bull";
 import { BaseScheduler } from "../base-scheduler";
-import { QueueNames } from "@sims/utilities";
-import { QueueService } from "@sims/services/queue";
-import { ApplicationService } from "../../../services";
 import {
-  QueueProcessSummary,
-  QueueProcessSummaryResult,
-} from "../../models/processors.models";
-import { SystemUsersService } from "@sims/services/system-users";
+  InjectLogger,
+  LoggerService,
+  ProcessSummary,
+} from "@sims/utilities/logger";
+import {
+  getSuccessMessageWithAttentionCheck,
+  logProcessSummaryToJobLogger,
+} from "../../../utilities";
+import { QueueNames } from "@sims/utilities";
 
-/**
- * Process applications archiving.
- */
 @Processor(QueueNames.StudentApplicationNotifications)
 export class StudentApplicationNotificationsScheduler extends BaseScheduler<void> {
   constructor(
     @InjectQueue(QueueNames.StudentApplicationNotifications)
     schedulerQueue: Queue<void>,
     queueService: QueueService,
-    private readonly applicationService: ApplicationService,
-    private readonly systemUsersService: SystemUsersService,
   ) {
     super(schedulerQueue, queueService);
   }
 
-  /**
-   * Process all the applications pending to be archived.
-   * @param job applications archiving job.
-   * @returns processing result.
-   */
   @Process()
-  async StudentApplicationNotifications(
-    job: Job<void>,
-  ): Promise<QueueProcessSummaryResult> {
-    const summary = new QueueProcessSummary({
-      appLogger: this.logger,
-      jobLogger: job,
-    });
+  async StudentApplicationNotifications(job: Job<void>): Promise<string[]> {
+    const processSummary = new ProcessSummary();
 
-    // await summary.info("Processing pending applications to be archived.");
-    // const auditUser = this.systemUsersService.systemUser;
-    // const archivedApplicationsCount =
-    //   await this.applicationService.archiveApplications(auditUser.id);
-    await summary.info(`Total of applications archived: dfd AShish`);
-    await summary.info("Completed applications archiving process.");
-    // await this.cleanSchedulerQueueHistory();
-    return summary.getSummary();
+    try {
+      this.logger.log(
+        `Processing application changes report integration job. Job id: ${job.id} and Job name: ${job.name}.`,
+      );
+      // TODO: Processing implementation of application changes report.
+      return getSuccessMessageWithAttentionCheck(
+        ["Process finalized with success."],
+        processSummary,
+      );
+    } catch (error: unknown) {
+      const errorMessage = "Unexpected error while executing the job.";
+      processSummary.error(errorMessage, error);
+      return [errorMessage];
+    } finally {
+      this.logger.logProcessSummary(processSummary);
+      await logProcessSummaryToJobLogger(processSummary, job);
+      await this.cleanSchedulerQueueHistory();
+    }
   }
+
+  @InjectLogger()
+  logger: LoggerService;
 }
