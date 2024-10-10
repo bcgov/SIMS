ebe090977aa164bcbe25857b4f56ef730f9ddf86
Shashank Shekhar <sh16011993@gmail.com>
2024-10-10 14:20:54 -0700

    Code Update: Review Comments

--- sources/packages/web/src/components/common/students/StudentProfile.vue
+++ sources/packages/web/src/components/common/students/StudentProfile.vue
@@ -195,6 +195,7 @@ export default defineComponent({
         snackBar.success("Profile Information updated successfully.");
         return true;
       } catch (error) {
+        studentEditProfile.value.loading = false;
         snackBar.error(
           "An error happened while updating the profile information. Your profile information could not be updated.",
         );
