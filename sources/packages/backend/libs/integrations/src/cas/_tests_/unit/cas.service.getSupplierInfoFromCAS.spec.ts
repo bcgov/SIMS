3b8c575190dc61d9ae776e02a3997533bd5ca5a9
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2024-10-10 17:16:02 -0700

    Unit test change

--- sources/packages/backend/libs/integrations/src/cas/_tests_/unit/cas.service.getSupplierInfoFromCAS.spec.ts
+++ sources/packages/backend/libs/integrations/src/cas/_tests_/unit/cas.service.getSupplierInfoFromCAS.spec.ts
@@ -1,20 +1,24 @@
 import { CASService } from "@sims/integrations/cas/cas.service";
 import { TestBed } from "@automock/jest";
 import { HttpService } from "@nestjs/axios";
+import { ConfigService } from "@sims/utilities/config";
 
 describe("CASService-getSupplierInfoFromCAS", () => {
   let casService: CASService;
   let httpService: HttpService;
+  let configService: ConfigService;
 
   beforeAll(() => {
     const { unit, unitRef } = TestBed.create(CASService).compile();
     casService = unit;
+    configService = unitRef.get(ConfigService);
     httpService = unitRef.get(HttpService);
   });
 
   it("Should invoke CAS API with last name upper case and without special characters when last name has special characters and is not complexly upper case.", () => {
     // Arrange
     httpService.axiosRef.get = jest.fn();
+    configService.casIntegration.baseUrl = "cas-url";
 
     // Act
     casService.getSupplierInfoFromCAS(
@@ -25,7 +29,7 @@ describe("CASService-getSupplierInfoFromCAS", () => {
 
     // Assert
     expect(httpService.axiosRef.get).toHaveBeenCalledWith(
-      "undefined/cfs/supplier/LAST NAME WITH SPECIAL CHARACTERS: AEIOU-AEIOU/lastname/sin/sin",
+      "cas-url/cfs/supplier/LAST NAME WITH SPECIAL CHARACTERS: AEIOU-AEIOU/lastname/sin/sin",
       { headers: { Authorization: "Bearer token" } },
     );
   });
