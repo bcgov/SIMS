bb28292fe92ca739e3b373b5f4edbb8da7bae8f1
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2024-10-18 17:50:24 -0600

    Service to get all student updates

--- sources/packages/backend/libs/integrations/src/services/student/student.service.ts
+++ sources/packages/backend/libs/integrations/src/services/student/student.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { DisabilityStatus, SINValidation, Student, User } from "@sims/sims-db";
+import {
+  Application,
+  ApplicationStatus,
+  DisabilityStatus,
+  SINValidation,
+  Student,
+  User,
+} from "@sims/sims-db";
 import { InjectLogger, LoggerService } from "@sims/utilities/logger";
-import { EntityManager, Raw, Repository, UpdateResult } from "typeorm";
+import {
+  Brackets,
+  EntityManager,
+  Raw,
+  Repository,
+  UpdateResult,
+} from "typeorm";
 
 @Injectable()
 export class StudentService {
   constructor(
     @InjectRepository(Student)
     private readonly studentRepo: Repository<Student>,
+    @InjectRepository(Application)
+    private readonly applicationRepo: Repository<Application>,
   ) {}
 
   /**
@@ -147,6 +162,47 @@ export class StudentService {
       { id: studentId },
       { disabilityStatus, studentPDUpdateAt: disabilityStatusUpdatedDate },
     );
+  }
+  /**
+   * Get all student ids of students who have one or more updates
+   * since the date provided.
+   * The updates can be one or more of the following:
+   * - Student or User data
+   * - Sin validation data
+   * - Cas supplier data
+   * - Overawards data
+   * @param modifiedSince the date after which the student data was updated.
+   */
+  async getAllStudentsWithUpdates(modifiedSince: Date): Promise<number[]> {
+    const applications = await this.applicationRepo
+      .createQueryBuilder("application")
+      .select(["application.id", "student.id"])
+      .distinctOn(["student.id"])
+      .innerJoin("application.student", "student")
+      .innerJoin("student.user", "user")
+      .innerJoin("student.sinValidation", "sinValidation")
+      .innerJoin("student.casSupplier", "casSupplier")
+      .leftJoin("student.overawards", "overaward")
+      .where("application.applicationStatus != :overwritten", {
+        overwritten: ApplicationStatus.Overwritten,
+      })
+      .andWhere("application.currentAssessment is not null")
+      .andWhere(
+        new Brackets((qb) => {
+          qb.where("student.updatedAt > :modifiedSince")
+            .orWhere("user.updatedAt > :modifiedSince")
+            .orWhere("sinValidation.updatedAt > :modifiedSince")
+            .orWhere("casSupplier.updatedAt > :modifiedSince")
+            .orWhere("overaward.updatedAt > :modifiedSince");
+        }),
+      )
+      .setParameter("modifiedSince", modifiedSince)
+      .getMany();
+    // Extract the student ids from the applications.
+    const modifiedStudentIds = applications.map(
+      (application) => application.student.id,
+    );
+    return modifiedStudentIds;
   }
 
   @InjectLogger()
