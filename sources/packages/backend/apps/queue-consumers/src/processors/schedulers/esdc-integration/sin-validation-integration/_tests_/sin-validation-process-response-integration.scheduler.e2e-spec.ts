d008bc875f35e75dc329e9c02d6e473b9cbd6afe
Ashish <hi@bidyashish.com>
2024-10-25 12:54:42 -0700

    update

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/esdc-integration/sin-validation-integration/_tests_/sin-validation-process-response-integration.scheduler.e2e-spec.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/esdc-integration/sin-validation-integration/_tests_/sin-validation-process-response-integration.scheduler.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { DeepMocked } from "@golevelup/ts-jest";
+import { createMock, DeepMocked } from "@golevelup/ts-jest";
 import { INestApplication } from "@nestjs/common";
 import { QueueNames } from "@sims/utilities";
 import {
@@ -9,6 +9,8 @@ import { E2EDataSources, createE2EDataSources } from "@sims/test-utils";
 import * as Client from "ssh2-sftp-client";
 import * as path from "path";
 import { SINValidationResponseIntegrationScheduler } from "../sin-validation-process-response-integration.scheduler";
+import { mockDownloadFiles } from "@sims/test-utils/mocks";
+import { Job } from "bull";
 
 const SIN_VALIDATION_FILENAME = "PCSLP.PBC.BC0000.ISR";
 
@@ -22,10 +24,7 @@ describe(
     let sinValidationResponseFolder: string;
 
     beforeAll(async () => {
-      sinValidationResponseFolder = path.join(
-        __dirname,
-        "sin-validation-response-files",
-      );
+      sinValidationResponseFolder = path.join(__dirname, "sin-receive-files");
       process.env.ESDC_RESPONSE_FOLDER = sinValidationResponseFolder;
       const { nestApplication, dataSource, sshClientMock } =
         await createTestingAppModule();
@@ -42,7 +41,38 @@ describe(
     // Add your test cases here
 
     // Dummy TODO test
-    it("TODO: should process SIN validation response file", () => {
+    it("TODO: should process SIN validation response file", async () => {
+      // Queued job.
+      const job = createMock<Job<void>>();
+
+      mockDownloadFiles(sftpClientMock, [SIN_VALIDATION_FILENAME]);
+
+      // Act
+      const processResult = await processor.processSINValidationResponse(job);
+
+      console.log(processResult);
+      // Assert
+      const downloadedFile = path.join(
+        process.env.ESDC_RESPONSE_FOLDER,
+        SIN_VALIDATION_FILENAME,
+      );
+      console.log(downloadedFile);
+      debugger;
+      // Assert
+      expect(processResult).toStrictEqual([
+        {
+          processSummary: [
+            `Processing file ${SIN_VALIDATION_FILENAME}.`,
+            "File contains:",
+            "Confirmed SIN validation records (type R): 2.",
+            "Cancelled SIN validation records (type C): 1.",
+            "Record from line 2, updated as confirmed.",
+            "Record from line 4, updated as confirmed.",
+            "Record from line 3, updated as cancelled.",
+          ],
+          errorsSummary: [],
+        },
+      ]);
       // This is a placeholder test
       expect(true).toBe(true);
     });
