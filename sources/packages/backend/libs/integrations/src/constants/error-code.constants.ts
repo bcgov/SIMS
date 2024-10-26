069a7cf598e8eb48ce1f140228a534baa0ff6796
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2024-10-25 17:07:03 -0700

    First name fix and error log

--- sources/packages/backend/libs/integrations/src/constants/error-code.constants.ts
+++ sources/packages/backend/libs/integrations/src/constants/error-code.constants.ts
@@ -1,4 +1,10 @@
 export const DOCUMENT_NUMBER_NOT_FOUND = "DOCUMENT_NUMBER_NOT_FOUND";
 
-// Error code used when there is an CAS authentication error.
+/**
+ * Error code used when there is an CAS authentication error.
+ */
 export const CAS_AUTH_ERROR = "CAS_AUTH_ERROR";
+/**
+ * CAS bad request error.
+ */
+export const CAS_BAD_REQUEST = "CAS_BAD_REQUEST";
