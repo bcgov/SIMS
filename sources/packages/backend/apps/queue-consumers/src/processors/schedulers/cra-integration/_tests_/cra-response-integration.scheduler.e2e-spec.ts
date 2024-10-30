8faa4e583c4d0ce8a657be52e9965bd2ed8c3caa
Bidyashish <hi@bidyashish.com>
2024-10-30 12:00:13 -0700

    #3745 - Modify process that reads SIN & CRA verification response files (#3835)
    ***Acceptance Criteria***
    - [x] Jobs must continue processing remaining records in a file after it
    fails to process a record.
    - [x] SIN and CRA response files will need to be archived after
    processing even where there are failed records. (IMB staff will need to
    monitor the jobs to confirm if errors occur processing SIMS response
    files and address them accordingly)
    - [x] Disable error reporting when SIMS Reference_IDX fails to match.
    - [x] If records are identified as SIMS records (based on the project
    area information being present), errors will be generated as usual.
    - [x] If records are identified as SFAS records (based on the project
    area information NOT being present), records should be skipped.
    - [x] For the SIN validation ([source
    reference](https://github.com/bcgov/SIMS/blob/8864eaa8a5f62360f5e3c91c6de06f319e008250/sources/packages/backend/libs/integrations/src/services/sin-validation/sin-validation.service.ts#L119)),
    if the `referenceIndex` is not found on SIMS, assume the record is from
    SFAS and do not log as an error.
    - [x] For income verifications, the records should already be skipped.
    Execute a manual test to ensure that a file with records that do not
    contain the `VERIFICATION_ID` in the free project area is skipped.
    Consider creating an E2E to execute the test.
    - [x] Demo for both cases with a sample file.

---
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/cra-integration/_tests_/cra-response-integration.scheduler.e2e-spec.ts
@@ -0,0 +1,98 @@
+import { createMock, DeepMocked } from "@golevelup/ts-jest";
+import { INestApplication } from "@nestjs/common";
+import { QueueNames } from "@sims/utilities";
+import {
+  createTestingAppModule,
+  describeProcessorRootTest,
+} from "../../../../../test/helpers";
+import {
+  E2EDataSources,
+  createE2EDataSources,
+  createFakeCRAIncomeVerification,
+  saveFakeApplication,
+  saveFakeStudent,
+} from "@sims/test-utils";
+import * as Client from "ssh2-sftp-client";
+import * as path from "path";
+import { CRAResponseIntegrationScheduler } from "../cra-response-integration.scheduler";
+import {
+  createFileFromStructuredRecords,
+  getStructuredRecords,
+  mockDownloadFiles,
+} from "@sims/test-utils/mocks";
+import { Job } from "bull";
+import { ApplicationStatus } from "@sims/sims-db";
+
+const CRA_FILENAME = "CRA_200_PBCSA00000.TXT";
+
+describe(describeProcessorRootTest(QueueNames.CRAResponseIntegration), () => {
+  let app: INestApplication;
+  let processor: CRAResponseIntegrationScheduler;
+  let db: E2EDataSources;
+  let sftpClientMock: DeepMocked<Client>;
+  let craResponseFolder: string;
+
+  beforeAll(async () => {
+    craResponseFolder = path.join(__dirname, "cra-receive-files");
+    process.env.CRA_RESPONSE_FOLDER = craResponseFolder;
+    const { nestApplication, dataSource, sshClientMock } =
+      await createTestingAppModule();
+    app = nestApplication;
+    db = createE2EDataSources(dataSource);
+    sftpClientMock = sshClientMock;
+    processor = app.get(CRAResponseIntegrationScheduler);
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+  });
+
+  it("Should process SIN response file ignoring non-SIMS records when the file contains responses from requests that were not created by SIMS.", async () => {
+    // Arrange.
+    const student = await saveFakeStudent(db.dataSource);
+
+    const application = await saveFakeApplication(
+      db.dataSource,
+      { student },
+      { applicationStatus: ApplicationStatus.InProgress },
+    );
+
+    // Create CRA income verifications for student.
+    const studentCRAIncomeVerification = createFakeCRAIncomeVerification({
+      application,
+    });
+    await db.craIncomeVerification.save([studentCRAIncomeVerification]);
+    // Queued job.
+    const job = createMock<Job<void>>();
+    mockDownloadFiles(sftpClientMock, [CRA_FILENAME]);
+
+    mockDownloadFiles(sftpClientMock, [CRA_FILENAME], (fileContent: string) => {
+      const file = getStructuredRecords(fileContent);
+      file.records[2] = file.records[2].replace(
+        "CRA_INCOME_VERIFICATION",
+        studentCRAIncomeVerification.id.toString().padStart(9, "0"),
+      );
+      return createFileFromStructuredRecords(file);
+    });
+
+    // Act
+    const processResult = await processor.processResponses(job);
+    // Assert
+    const downloadedFile = path.join(
+      process.env.CRA_RESPONSE_FOLDER,
+      CRA_FILENAME,
+    );
+
+    // Assert
+    expect(processResult).toStrictEqual([
+      {
+        processSummary: [
+          `Processing file ${downloadedFile}.`,
+          "File contains 2 verifications.",
+          "Processed income verification. Total income record line 5. Status record from line 4.",
+        ],
+        errorsSummary: [],
+      },
+    ]);
+  });
+});
