3bc808202fcbcda91fef2d282e7f57e21a881292
Andre Pestana <andre.pestana@aot-technologies.com>
2024-11-26 15:27:44 -0800

    review issues

--- sources/packages/backend/apps/db-migrations/src/sql/Reports/Create-disbursements-without-valid-supplier-report.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Reports/Create-disbursements-without-valid-supplier-report.sql
@@ -39,7 +39,7 @@ VALUES
       INNER JOIN sims.cas_suppliers cas_suppliers ON cas_suppliers.student_id = students.id
       INNER JOIN sims.applications applications ON applications.student_id = students.id
       INNER JOIN sims.student_assessments student_assessments ON student_assessments.application_id = applications.id
-      LEFT JOIN sims.disbursement_schedules disbursement_schedules ON disbursement_schedules.student_assessment_id = student_assessments.id
+      INNER JOIN sims.disbursement_schedules disbursement_schedules ON disbursement_schedules.student_assessment_id = student_assessments.id
       INNER JOIN sims.disbursement_values disbursement_values ON disbursement_values.disbursement_schedule_id = disbursement_schedules.id
     WHERE
       disbursement_schedules.disbursement_schedule_status = ''Sent''
