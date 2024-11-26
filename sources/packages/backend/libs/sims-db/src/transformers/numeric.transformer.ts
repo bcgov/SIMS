250dcbb8a27b43ee93b1599f56aa42e3eaaceae0
Shashank Shekhar <sh16011993@gmail.com>
2024-11-26 12:31:24 -0800

    #3693 - Institution: 0 is removed from the Exceptional costs field bug

--- sources/packages/backend/libs/sims-db/src/transformers/numeric.transformer.ts
+++ sources/packages/backend/libs/sims-db/src/transformers/numeric.transformer.ts
@@ -14,7 +14,7 @@ export const numericTransformer: ValueTransformer = {
    * @returns numeric/decimal string converted to a number.
    */
   from: (value: string | null): number | null => {
-    if (value) {
+    if (value !== null) {
       return parseFloat(value);
     }
     return null;
