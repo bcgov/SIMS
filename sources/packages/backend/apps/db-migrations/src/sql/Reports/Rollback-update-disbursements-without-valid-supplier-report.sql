99df14424617abec4c6fc24a80e9e4a6e7587f7b
Andre Pestana <78114138+andrepestana-aot@users.noreply.github.com>
2024-11-27 09:18:41 -0800

    #3763 - Ministry Reports - Disbursements Without Supplier (CAS) - Query fix (#4004)
    Fixed joins to sin_validations and cas_suppliers:
    
    From: 
    ```
          INNER JOIN sims.sin_validations sin_validations ON students.id = sin_validations.student_id
          INNER JOIN sims.cas_suppliers cas_suppliers ON cas_suppliers.student_id = students.id
    ```
    To:
    ```
          INNER JOIN sims.sin_validations sin_validations ON students.sin_validation_id = sin_validations.id
          INNER JOIN sims.cas_suppliers cas_suppliers ON students.cas_supplier_id = cas_suppliers.id
    ```
    
    -Migration revert:
    
    ![image](https://github.com/user-attachments/assets/f84e3ebb-a724-4080-8295-f9b7b17412e4)

---
+++ sources/packages/backend/apps/db-migrations/src/sql/Reports/Rollback-update-disbursements-without-valid-supplier-report.sql
@@ -0,0 +1,55 @@
+UPDATE
+  sims.report_configs
+SET
+  report_sql = (
+    'SELECT
+      users.first_name AS "Given Name",
+      users.last_name AS "Last Name",
+      sin_validations.sin AS "SIN",
+      users.identity_provider_type AS "Profile Type",
+      students.contact_info -> ''address'' ->> ''addressLine1'' AS "Address Line 1",
+      students.contact_info -> ''address'' ->> ''city'' AS "City",
+      students.contact_info -> ''address'' ->> ''provinceState'' AS "Province",
+      students.contact_info -> ''address'' ->> ''country'' AS "Country",
+      students.contact_info -> ''address'' ->> ''postalCode'' AS "Postal Code",
+      students.disability_status AS "Disability Status",
+      SUM(
+        CASE
+          WHEN disbursement_values.value_code = ''BCAG'' THEN disbursement_values.effective_amount
+          ELSE 0
+        END
+      ) AS "BCAG",
+      SUM(
+        CASE
+          WHEN disbursement_values.value_code = ''SBSD'' THEN disbursement_values.effective_amount
+          ELSE 0
+        END
+      ) AS "SBSD",
+      SUM(
+        CASE
+          WHEN disbursement_values.value_code = ''BGPD'' THEN disbursement_values.effective_amount
+          ELSE 0
+        END
+      ) AS "BGPD"
+    FROM
+      sims.students students
+      INNER JOIN sims.users users ON students.user_id = users.id
+      INNER JOIN sims.sin_validations sin_validations ON students.id = sin_validations.student_id
+      INNER JOIN sims.cas_suppliers cas_suppliers ON cas_suppliers.student_id = students.id
+      INNER JOIN sims.applications applications ON applications.student_id = students.id
+      INNER JOIN sims.student_assessments student_assessments ON student_assessments.application_id = applications.id
+      INNER JOIN sims.disbursement_schedules disbursement_schedules ON disbursement_schedules.student_assessment_id = student_assessments.id
+      INNER JOIN sims.disbursement_values disbursement_values ON disbursement_values.disbursement_schedule_id = disbursement_schedules.id
+    WHERE
+      disbursement_schedules.disbursement_schedule_status = ''Sent''
+      AND disbursement_schedules.date_sent BETWEEN :startDate
+      AND :endDate
+      AND cas_suppliers.is_valid = false
+    GROUP BY
+      users.id,
+      sin_validations.sin,
+      students.contact_info,
+      students.disability_status;'
+  )
+WHERE
+  report_name = 'Disbursements_Without_Valid_Supplier_Report';
