649a0a55ca4da7534e2a656a70f1120db3f2d13c
Shashank Shekhar <sh16011993@gmail.com>
2024-12-02 13:55:12 -0800

    Code Update: Replaced unused parameter with underscore _

--- sources/packages/web/src/views/student/financial-aid-application/FullTimeApplication.vue
+++ sources/packages/web/src/views/student/financial-aid-application/FullTimeApplication.vue
@@ -300,7 +300,7 @@ export default defineComponent({
 
     const pageChanged = async (
       isInFirstPage: boolean,
-      currentPage: number,
+      _: number,
       isInLastPage: boolean,
     ) => {
       isFirstPage.value = isInFirstPage;
