29bd8fa63795807f7f8796c9d0cf41da4d8da818
guru-aot <gurumoorthy.mohan@gov.bc.ca>
2024-12-10 16:40:08 -0700

    updated

--- sources/packages/web/src/views/aest/student/CASSupplierInformation.vue
+++ sources/packages/web/src/views/aest/student/CASSupplierInformation.vue
@@ -25,7 +25,7 @@
                   :disabled="notAllowed || retryButtonDisabled"
                   @click="retryCASSupplier"
                   prepend-icon="fa:fa fa-repeat"
-                  :loading="processing"
+                  :loading="retryLoading"
                   >Retry</v-btn
                 >
               </template>
@@ -148,7 +148,7 @@ export default defineComponent({
     const { dateOnlyLongString, emptyStringFiller, booleanToYesNo } =
       useFormatters();
     const showModal = ref(false);
-    const processing = ref(false);
+    const retryLoading = ref(false);
     const casSupplierInfo = ref({} as CASSupplierInfoAPIOutDTO);
     const addCASSupplierModal = ref(
       {} as ModalDialog<AddCASSupplierAPIInDTO | boolean>,
@@ -173,7 +173,7 @@ export default defineComponent({
 
     const retryCASSupplier = async () => {
       try {
-        processing.value = true;
+        retryLoading = true;
         await CASSupplierService.shared.retryCASSupplier(props.studentId);
         snackBar.success(
           "A new CAS pending verification record was created. A new attempt will be made to get information from CAS.",
@@ -184,7 +184,7 @@ export default defineComponent({
           "Unexpected error while retrying CAS supplier information.",
         );
       } finally {
-        processing.value = false;
+        retryLoading = false;
       }
     };
 
@@ -221,7 +221,7 @@ export default defineComponent({
       emptyStringFiller,
       retryCASSupplier,
       retryButtonDisabled,
-      processing,
+      retryLoading,
     };
   },
 });
