8818a4a40e4e8a0b0e2471c173b268718bd20727
Andre Pestana <andre.pestana@aot-technologies.com>
2024-12-23 13:55:05 -0800

    fixed comment

--- sources/packages/backend/apps/db-migrations/src/sql/SFASPartTimeApplications/Add-application-cancel-date.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/SFASPartTimeApplications/Add-application-cancel-date.sql
@@ -3,4 +3,4 @@ ALTER TABLE
 ADD
   COLUMN application_cancel_date DATE;
 
-COMMENT ON COLUMN sims.sfas_part_time_applications.application_cancel_date IS 'Date that this application was canceled (sail_application_events.event_date).';
+COMMENT ON COLUMN sims.sfas_part_time_applications.application_cancel_date IS 'Date that this application was cancelled (sail_application_events.event_date).';
