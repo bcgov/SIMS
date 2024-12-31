64ea48f75aac07da42e4ec82cec862c1df6db2b9
Gurumoorthy Mohan <gurumoorthy.mohan@aot-technologies.com>
2024-12-31 10:23:46 -0700

    #3946-Update BC Grants displayed (#4190)
    Report changed to show the values of BC Grant.
    
    ![image](https://github.com/user-attachments/assets/0c2e7f64-b878-4d21-b5a9-d50f5962b9c6)
    
    
    Migration revert Screenshot
    
    
    ![image](https://github.com/user-attachments/assets/f22e5eba-1ae2-43f7-8193-9a12cea4ee4a)
    
    Sample query to run and check in the DB
    
    > (select
    to_char(dr.disburse_date, 'YYYY-MM-DD') as "Date of Disbursement",
            dr.student_sin as "SIN",
            app.application_number as "Application Number",
            ds.document_number as "Certificate Number",
            drv.grant_type as "Funding Code",
            drv.grant_amount as "Disbursement Amount"
          from
            sims.disbursement_receipts dr
    inner join sims.disbursement_receipt_values drv on
    drv.disbursement_receipt_id = dr.id
    inner join sims.disbursement_schedules ds on ds.id =
    dr.disbursement_schedule_id
    inner join sims.student_assessments sa on sa.id =
    ds.student_assessment_id
            inner join sims.applications app on app.id = sa.application_id
    inner join sims.education_programs_offerings epo on epo.id =
    sa.offering_id
          where
            epo.offering_intensity = 'Full Time'
            and dr.disburse_date between '2024-01-01'
            and '2025-01-01'
          union
          all
          select
    to_char(dr.disburse_date, 'YYYY-MM-DD') as "Date of Disbursement",
            dr.student_sin as "SIN",
            app.application_number as "Application Number",
            ds.document_number as "Certificate Number",
            case
              when dr.funding_type = 'BC' then 'BCSL'
              when dr.funding_type = 'FE' then 'CSL'
            end as "Funding Code",
            dr.total_disbursed_amount as "Disbursement Amount"
          from
            sims.disbursement_receipts dr
    inner join sims.disbursement_schedules ds on ds.id =
    dr.disbursement_schedule_id
    inner join sims.student_assessments sa on sa.id =
    ds.student_assessment_id
            inner join sims.applications app on app.id = sa.application_id
    inner join sims.education_programs_offerings epo on epo.id =
    sa.offering_id
          where
            epo.offering_intensity = 'Full Time'
            and dr.disburse_date between '2024-01-01'
            and '2025-01-01'
          union
          all
          select distinct
    to_char(dr.disburse_date, 'YYYY-MM-DD') as "Date of Disbursement",
            dr.student_sin as "SIN",
            app.application_number as "Application Number",
            ds.document_number as "Certificate Number",
    		dv.value_code as "Funding Code",
    		dv.value_amount as "Disbursement Amount"
          from
            sims.disbursement_receipts dr
    inner join sims.disbursement_schedules ds on ds.id =
    dr.disbursement_schedule_id
    inner join sims.disbursement_values dv on dv.disbursement_schedule_id =
    ds.id
    inner join sims.student_assessments sa on sa.id =
    ds.student_assessment_id
            inner join sims.applications app on app.id = sa.application_id
    inner join sims.education_programs_offerings epo on epo.id =
    sa.offering_id
          where
            epo.offering_intensity = 'Full Time'
    		and dv.value_type = 'BC Grant'
            and dr.disburse_date between '2024-01-01'
            and '2025-01-01'
        )
        order by
          "Date of Disbursement",
          "Certificate Number"
    
    Execution plan:
    
    ![image](https://github.com/user-attachments/assets/ea7901d5-4221-4877-82d6-e68890490dd8)
    
    ---------
    
    Co-authored-by: guru-aot <gurumoorthy.mohan@gov.bc.ca>

---
+++ sources/packages/backend/apps/db-migrations/src/sql/Reports/Rollback-update-disbursements-report-bc-grants.sql
@@ -0,0 +1,52 @@
+UPDATE
+    sims.report_configs
+SET
+    report_sql = (
+    '(
+      select
+        to_char(dr.disburse_date, ''YYYY-MM-DD'') as "Date of Disbursement",
+        dr.student_sin as "SIN",
+        app.application_number as "Application Number",
+        ds.document_number as "Certificate Number",
+        drv.grant_type as "Funding Code",
+        drv.grant_amount as "Disbursement Amount"
+      from
+        sims.disbursement_receipts dr
+        inner join sims.disbursement_receipt_values drv on drv.disbursement_receipt_id = dr.id
+        inner join sims.disbursement_schedules ds on ds.id = dr.disbursement_schedule_id
+        inner join sims.student_assessments sa on sa.id = ds.student_assessment_id
+        inner join sims.applications app on app.id = sa.application_id
+        inner join sims.education_programs_offerings epo on epo.id = sa.offering_id
+      where
+        epo.offering_intensity = any(:offeringIntensity)
+        and dr.disburse_date between :startDate
+        and :endDate
+      union
+      all
+      select
+        to_char(dr.disburse_date, ''YYYY-MM-DD'') as "Date of Disbursement",
+        dr.student_sin as "SIN",
+        app.application_number as "Application Number",
+        ds.document_number as "Certificate Number",
+        case
+          when dr.funding_type = ''BC'' then ''BCSL''
+          when dr.funding_type = ''FE'' then ''CSL''
+        end as "Funding Code",
+        dr.total_disbursed_amount as "Disbursement Amount"
+      from
+        sims.disbursement_receipts dr
+        inner join sims.disbursement_schedules ds on ds.id = dr.disbursement_schedule_id
+        inner join sims.student_assessments sa on sa.id = ds.student_assessment_id
+        inner join sims.applications app on app.id = sa.application_id
+        inner join sims.education_programs_offerings epo on epo.id = sa.offering_id
+      where
+        epo.offering_intensity = any(:offeringIntensity)
+        and dr.disburse_date between :startDate
+        and :endDate
+    )
+    order by
+      "Date of Disbursement",
+      "Certificate Number"'
+    )
+WHERE
+    report_name = 'Disbursement_Report';
