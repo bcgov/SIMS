f88441c75ded081767e71513b1a09ebc4332a257
Andre Pestana <andre.pestana@aot-technologies.com>
2025-01-08 15:30:25 -0800

    checking GHA tests failing

--- sources/packages/backend/apps/api/src/auth/_tests_/e2e/auth.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/auth/_tests_/e2e/auth.e2e-spec.ts
@@ -329,15 +329,19 @@ describe("Authentication (e2e)", () => {
         .expect(HttpStatus.OK);
     });
 
-    it("Should return a HttpStatus FORBIDDEN(403) when a read-only institution user tries to access a non-reading-only route to their institution.", async () => {
+    it.only("Should return a HttpStatus FORBIDDEN(403) when a read-only institution user tries to access a non-reading-only route to their institution.", async () => {
       // Arrange
+
+      console.log("Before getAuthRelatedEntities !#@$!%$&");
       const { institution: collegeE } = await getAuthRelatedEntities(
         db.dataSource,
         InstitutionTokenTypes.CollegeEReadOnlyUser,
       );
+      console.log("Before createFakeInstitutionLocation !#$%%^%*&(*");
       const collegeELocation = createFakeInstitutionLocation({
         institution: collegeE,
       });
+      console.log("Before authorizeUserTokenForLocation !#$%%^%*&(*");
       await authorizeUserTokenForLocation(
         db.dataSource,
         InstitutionTokenTypes.CollegeEReadOnlyUser,
@@ -345,10 +349,11 @@ describe("Authentication (e2e)", () => {
         InstitutionUserTypes.readOnlyUser,
       );
       const endpoint = `/auth-test/institution-location-modifying-route/${collegeELocation.id}`;
+      console.log("Before getInstitutionToken !#$%%^%*&(*");
       const collegEInstitutionUserToken = await getInstitutionToken(
         InstitutionTokenTypes.CollegeEReadOnlyUser,
       );
-
+      console.log("Before request !#$%%^%*&(*");
       // Act/Assert
       await request(app.getHttpServer())
         .get(endpoint)
