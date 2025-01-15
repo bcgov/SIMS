af7f672f734cfefc083296abef7d4f7fcf987214
Lewis Chen <lewis.chen@aot-technologies.com>
2025-01-15 11:50:25 -0800

    fix comments in code and db

--- sources/packages/backend/apps/db-migrations/src/sql/SFASIndividuals/Rollback-update-swap-csl-bcsl-columns.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/SFASIndividuals/Rollback-update-swap-csl-bcsl-columns.sql
@@ -3,3 +3,7 @@ UPDATE
 SET
     bcsl_overaward = csl_overaward,
     csl_overaward = bcsl_overaward;
+
+COMMENT ON COLUMN sims.sfas_individuals.csl_overaward IS 'Canada Student Loan total overaward balance (overaward_balance.overaward_balance_amt, overaward_balance_cde = "BCSL").';
+
+COMMENT ON COLUMN sims.sfas_individuals.bcsl_overaward IS 'BC Student Loan total overaward balance (overaward_balance.overaward_balance_amt, overaward_balance_cde = "CSL").';
--- sources/packages/backend/apps/db-migrations/src/sql/SFASIndividuals/Update-swap-csl-bcsl-columns.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/SFASIndividuals/Update-swap-csl-bcsl-columns.sql
@@ -3,3 +3,7 @@ UPDATE
 SET
     csl_overaward = bcsl_overaward,
     bcsl_overaward = csl_overaward;
+
+COMMENT ON COLUMN sims.sfas_individuals.csl_overaward IS 'Canada Student Loan total overaward balance (overaward_balance.overaward_balance_amt, overaward_balance_cde = "CSL").';
+
+COMMENT ON COLUMN sims.sfas_individuals.bcsl_overaward IS 'BC Student Loan total overaward balance (overaward_balance.overaward_balance_amt, overaward_balance_cde = "BCSL").';
