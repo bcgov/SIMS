e8ae0ea8c6971662215b8231eae7e2c80cf25bf6
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-01-20 15:39:47 -0700

    initial commit

--- sources/packages/backend/workflow/test/2023-2024/parttime-assessment/eligibility/parttime-assessment-eligibility-CSPT.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2023-2024/parttime-assessment/eligibility/parttime-assessment-eligibility-CSPT.e2e-spec.ts
@@ -4,6 +4,12 @@ import {
   createFakeConsolidatedPartTimeData,
   executePartTimeAssessmentForProgramYear,
 } from "../../../test-utils";
+import { YesNoOptions } from "@sims/test-utils";
+import { StudentDependent } from "workflow/test/models";
+import {
+  createFakeStudentDependentEligible,
+  DependentEligibility,
+} from "../../../test-utils/factories";
 
 describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSPT.`, () => {
   it(
@@ -26,6 +32,58 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSPT
       expect(
         calculatedAssessment.variables.finalFederalAwardNetCSPTAmount,
       ).toBeGreaterThan(0);
+    },
+  );
+
+  describe(
+    "Should determine CSPT as eligible when total assessed need is greater than or equal to 1 " +
+      "and total family income is less than the threshold and ",
+    () => {
+      const familySizeInputs = [2, 3, 4, 5, 6, 7, 8];
+      for (const familySize of familySizeInputs) {
+        it(`family size is ${familySize}`, async () => {
+          // Arrange
+          const assessmentConsolidatedData =
+            createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
+          assessmentConsolidatedData.studentDataCRAReportedIncome = 1000;
+          assessmentConsolidatedData.studentDataHasDependents =
+            YesNoOptions.Yes;
+          const dependents: StudentDependent[] = [];
+          // Considering that the student is single, add dependents to make the family size to expected number.
+          for (let i = 1; i < familySize; i++) {
+            dependents.push(
+              createFakeStudentDependentEligible(
+                DependentEligibility.Eligible0To18YearsOld,
+                {
+                  referenceDate:
+                    assessmentConsolidatedData.offeringStudyStartDate,
+                },
+              ),
+            );
+          }
+          assessmentConsolidatedData.studentDataDependants = dependents;
+
+          // Act
+          const calculatedAssessment =
+            await executePartTimeAssessmentForProgramYear(
+              PROGRAM_YEAR,
+              assessmentConsolidatedData,
+            );
+
+          // Assert
+          // Validate family size.
+          expect(calculatedAssessment.variables.calculatedDataFamilySize).toBe(
+            familySize,
+          );
+          // Validate award eligibility for family size.
+          expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(
+            true,
+          );
+          expect(
+            calculatedAssessment.variables.finalFederalAwardNetCSPTAmount,
+          ).toBeGreaterThan(0);
+        });
+      }
     },
   );
 
