e234f0b202b73f89207568678746e7efe75a3f29
Dheepak Ramanathan <54600590+dheepak-aot@users.noreply.github.com>
2025-01-21 10:04:50 -0700

    #4262 - Fix part-time family size variables DMN (#4267)
    # Fix part-time family size variables DMN
    
    - [x] Updated the PT DMN for family size to match family size of 7.
    Before the update the match was missing for family size = 7.
    - [x] Updated the modeler platform version and exporter version for DMN.
    - [x] Added E2E tests(Picked one of the award eligibility to satisfy all
    possible conditions based on family size).

--- sources/packages/backend/workflow/test/2024-2025/parttime-assessment/eligibility/parttime-assessment-eligibility-CSPT.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2024-2025/parttime-assessment/eligibility/parttime-assessment-eligibility-CSPT.e2e-spec.ts
@@ -4,6 +4,11 @@ import {
   createFakeConsolidatedPartTimeData,
   executePartTimeAssessmentForProgramYear,
 } from "../../../test-utils";
+import { YesNoOptions } from "@sims/test-utils";
+import {
+  createFakeStudentDependentEligible,
+  DependentEligibility,
+} from "../../../test-utils/factories";
 
 describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSPT.`, () => {
   it(
@@ -28,6 +33,57 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSPT
       ).toBeGreaterThan(0);
     },
   );
+
+  describe("Should determine CSPT as eligible when total assessed need is greater than or equal to 1 and ", () => {
+    const familySizeInputs: { familySize: number; familyIncome: number }[] = [
+      { familySize: 2, familyIncome: 40000 },
+      { familySize: 3, familyIncome: 50000 },
+      { familySize: 4, familyIncome: 60000 },
+      { familySize: 5, familyIncome: 70000 },
+      { familySize: 6, familyIncome: 75000 },
+      { familySize: 7, familyIncome: 80000 },
+      { familySize: 8, familyIncome: 80000 },
+    ];
+    for (const { familySize, familyIncome } of familySizeInputs) {
+      it(`family size is ${familySize} and family income is ${familyIncome}`, async () => {
+        // Arrange
+        const assessmentConsolidatedData =
+          createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
+        assessmentConsolidatedData.studentDataCRAReportedIncome = familyIncome;
+        assessmentConsolidatedData.studentDataHasDependents = YesNoOptions.Yes;
+        // Considering that the student is single, add dependents to make the family size to expected number.
+        assessmentConsolidatedData.studentDataDependants = Array.from(
+          { length: familySize - 1 },
+          () =>
+            createFakeStudentDependentEligible(
+              DependentEligibility.Eligible0To18YearsOld,
+              {
+                referenceDate:
+                  assessmentConsolidatedData.offeringStudyStartDate,
+              },
+            ),
+        );
+
+        // Act
+        const calculatedAssessment =
+          await executePartTimeAssessmentForProgramYear(
+            PROGRAM_YEAR,
+            assessmentConsolidatedData,
+          );
+
+        // Assert
+        // Validate family size.
+        expect(calculatedAssessment.variables.calculatedDataFamilySize).toBe(
+          familySize,
+        );
+        // Validate award eligibility for family size.
+        expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(true);
+        expect(
+          calculatedAssessment.variables.finalFederalAwardNetCSPTAmount,
+        ).toBeGreaterThan(0);
+      });
+    }
+  });
 
   it("Should determine CSPT as not eligible when total family income is greater than the threshold.", async () => {
     // Arrange
