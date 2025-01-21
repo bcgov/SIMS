c6b53254e85b1b656cfa5e48a8dfabd66c2acd83
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-01-21 09:13:54 -0700

    E2E Tests Update

--- sources/packages/backend/workflow/test/2023-2024/parttime-assessment/eligibility/parttime-assessment-eligibility-CSPT.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2023-2024/parttime-assessment/eligibility/parttime-assessment-eligibility-CSPT.e2e-spec.ts
@@ -5,7 +5,6 @@ import {
   executePartTimeAssessmentForProgramYear,
 } from "../../../test-utils";
 import { YesNoOptions } from "@sims/test-utils";
-import { StudentDependent } from "workflow/test/models";
 import {
   createFakeStudentDependentEligible,
   DependentEligibility,
@@ -35,57 +34,56 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSPT
     },
   );
 
-  describe(
-    "Should determine CSPT as eligible when total assessed need is greater than or equal to 1 " +
-      "and total family income is less than the threshold and ",
-    () => {
-      const familySizeInputs = [2, 3, 4, 5, 6, 7, 8];
-      for (const familySize of familySizeInputs) {
-        it(`family size is ${familySize}`, async () => {
-          // Arrange
-          const assessmentConsolidatedData =
-            createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-          assessmentConsolidatedData.studentDataCRAReportedIncome = 1000;
-          assessmentConsolidatedData.studentDataHasDependents =
-            YesNoOptions.Yes;
-          const dependents: StudentDependent[] = [];
-          // Considering that the student is single, add dependents to make the family size to expected number.
-          for (let i = 1; i < familySize; i++) {
-            dependents.push(
-              createFakeStudentDependentEligible(
-                DependentEligibility.Eligible0To18YearsOld,
-                {
-                  referenceDate:
-                    assessmentConsolidatedData.offeringStudyStartDate,
-                },
-              ),
-            );
-          }
-          assessmentConsolidatedData.studentDataDependants = dependents;
+  describe("Should determine CSPT as eligible when total assessed need is greater than or equal to 1 and ", () => {
+    const familySizeInputs: { familySize: number; familyIncome: number }[] = [
+      { familySize: 2, familyIncome: 40000 },
+      { familySize: 3, familyIncome: 50000 },
+      { familySize: 4, familyIncome: 60000 },
+      { familySize: 5, familyIncome: 70000 },
+      { familySize: 6, familyIncome: 75000 },
+      { familySize: 7, familyIncome: 80000 },
+      { familySize: 8, familyIncome: 80000 },
+    ];
+    for (const { familySize, familyIncome } of familySizeInputs) {
+      it(`family size is ${familySize} and family income is ${familyIncome}`, async () => {
+        // Arrange
+        const assessmentConsolidatedData =
+          createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
+        assessmentConsolidatedData.studentDataCRAReportedIncome = familyIncome;
+        assessmentConsolidatedData.studentDataHasDependents = YesNoOptions.Yes;
+        // Considering that the student is single, add dependents to make the family size to expected number.
+        assessmentConsolidatedData.studentDataDependants = Array.from(
+          { length: familySize - 1 },
+          () =>
+            createFakeStudentDependentEligible(
+              DependentEligibility.Eligible0To18YearsOld,
+              {
+                referenceDate:
+                  assessmentConsolidatedData.offeringStudyStartDate,
+              },
+            ),
+        );
 
-          // Act
-          const calculatedAssessment =
-            await executePartTimeAssessmentForProgramYear(
-              PROGRAM_YEAR,
-              assessmentConsolidatedData,
-            );
+        // Act
+        const calculatedAssessment =
+          await executePartTimeAssessmentForProgramYear(
+            PROGRAM_YEAR,
+            assessmentConsolidatedData,
+          );
 
-          // Assert
-          // Validate family size.
-          expect(calculatedAssessment.variables.calculatedDataFamilySize).toBe(
-            familySize,
-          );
-          // Validate award eligibility for family size.
-          expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(
-            true,
-          );
-          expect(
-            calculatedAssessment.variables.finalFederalAwardNetCSPTAmount,
-          ).toBeGreaterThan(0);
-        });
-      }
-    },
-  );
+        // Assert
+        // Validate family size.
+        expect(calculatedAssessment.variables.calculatedDataFamilySize).toBe(
+          familySize,
+        );
+        // Validate award eligibility for family size.
+        expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(true);
+        expect(
+          calculatedAssessment.variables.finalFederalAwardNetCSPTAmount,
+        ).toBeGreaterThan(0);
+      });
+    }
+  });
 
   it("Should determine CSPT as not eligible when total family income is greater than the threshold.", async () => {
     // Arrange
