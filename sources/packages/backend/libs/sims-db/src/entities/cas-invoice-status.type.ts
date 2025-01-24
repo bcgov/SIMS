9f4375ac34e4dc82cf763ae1c8b68efe96a2b71f
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-01-24 13:05:03 -0800

    Comments.

--- sources/packages/backend/libs/sims-db/src/entities/cas-invoice-status.type.ts
+++ sources/packages/backend/libs/sims-db/src/entities/cas-invoice-status.type.ts
@@ -1,5 +1,18 @@
+/**
+ * Indicates the status of the invoice in relation to its submission to CAS.
+ */
 export enum CASInvoiceStatus {
+  /**
+   * Record was created and has not been sent to CAS.
+   */
   Pending = "Pending",
+  /**
+   * Record was sent to CAS.
+   */
   Approved = "Sent",
+  /**
+   * Some error happened while trying to send the invoice to CAS
+   * and a manual intervention is required.
+   */
   Rejected = "Manual intervention",
 }
