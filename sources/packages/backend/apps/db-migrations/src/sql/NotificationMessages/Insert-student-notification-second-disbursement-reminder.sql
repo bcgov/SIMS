f4b2e8b3cff4dbdf184f60ec11a3681d1442b804
Lewis Chen <lewis.chen@aot-technologies.com>
2025-01-28 13:42:55 -0800

    minor fix

--- sources/packages/backend/apps/db-migrations/src/sql/NotificationMessages/Insert-student-notification-second-disbursement-reminder.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/NotificationMessages/Insert-student-notification-second-disbursement-reminder.sql
@@ -3,6 +3,6 @@ INSERT INTO
 VALUES
   (
     31,
-    'Student application notification for overdue second disbursement still pending.',
+    'Student application notification for second disbursement still pending.',
     '55fcf228-b899-49a7-ab80-9b854c0bd884'
   );
