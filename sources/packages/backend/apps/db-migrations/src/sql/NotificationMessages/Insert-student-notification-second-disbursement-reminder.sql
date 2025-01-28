0ce37c6fb3ebfdb9288b98891a4a9f3dc390adb5
Lewis Chen <lewis.chen@aot-technologies.com>
2025-01-28 09:46:46 -0800

    initial commit

---
+++ sources/packages/backend/apps/db-migrations/src/sql/NotificationMessages/Insert-student-notification-second-disbursement-reminder.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+  sims.notification_messages(id, description, template_id)
+VALUES
+  (
+    31,
+    'Student application notification for overdue second disbursement still pending.',
+    '55fcf228-b899-49a7-ab80-9b854c0bd884'
+  );
