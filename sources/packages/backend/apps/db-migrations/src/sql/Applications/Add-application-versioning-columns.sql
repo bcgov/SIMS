c2b6c2fbf6dd0244854ec53f9c8fcaf1d7816808
Shashank Shekhar <sh16011993@gmail.com>
2025-02-03 16:57:42 -0800

    Code Update: Review Comments
    - Added the DB migration to have the new columns populated.

--- sources/packages/backend/apps/db-migrations/src/sql/Applications/Add-application-versioning-columns.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Applications/Add-application-versioning-columns.sql
@@ -11,3 +11,65 @@ ADD
   COLUMN preceding_application_id INT REFERENCES sims.applications(id);
 
 COMMENT ON COLUMN sims.applications.preceding_application_id IS 'The immediate previous application from which the current application was created.';
+
+-- Set parent-application-id and preceding-application-id for existing draft applications.
+UPDATE
+  sims.applications
+SET
+  parent_application_id = id,
+  preceding_application_id = id
+WHERE
+  application_number IS NULL;
+
+-- Set parent-application-id for non-draft applications.
+UPDATE
+  sims.applications
+SET
+  parent_application_id = (
+    SELECT
+      id
+    FROM
+      sims.applications applications
+    WHERE
+      applications.application_number = sims.applications.application_number
+    ORDER BY
+      applications.submitted_date
+    LIMIT
+      1
+  )
+WHERE
+  application_number IS NOT NULL;
+
+-- Set preceding-application-id for non-draft applications.
+UPDATE
+  sims.applications
+SET
+  preceding_application_id = COALESCE(
+    (
+      SELECT
+        id
+      FROM
+        sims.applications applications
+      WHERE
+        applications.application_number = sims.applications.application_number
+        AND applications.submitted_date < sims.applications.submitted_date
+      ORDER BY
+        applications.submitted_date DESC
+      LIMIT
+        1
+    ), sims.applications.id
+  )
+WHERE
+  application_number IS NOT NULL;
+
+-- Update parent-application-id and preceding-application-id columns to not null.
+ALTER TABLE
+  sims.applications
+ALTER COLUMN
+  parent_application_id
+SET
+  NOT NULL,
+ALTER COLUMN
+  preceding_application_id
+SET
+  NOT NULL;
