c32f30118bcd877884c93dbeba06742b3905617c
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-02-07 12:30:09 -0700

    Renaming the file

--- sources/packages/backend/apps/db-migrations/src/migrations/1738900082886-AlterSFASApplicationExpansionColumnType.ts
+++ sources/packages/backend/apps/db-migrations/src/migrations/1738900082886-AlterSFASApplicationExpansionColumnType.ts
@@ -6,14 +6,14 @@ export class AlterSFASApplicationExpansionColumnType1738900082886
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      getSQLFileData("Alter-expansion-column-type.sql", "SFASApplications"),
+      getSQLFileData("Alter-expansion-columns-type.sql", "SFASApplications"),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       getSQLFileData(
-        "Rollback-alter-expansion-column-type.sql",
+        "Rollback-alter-expansion-columns-type.sql",
         "SFASApplications",
       ),
     );
