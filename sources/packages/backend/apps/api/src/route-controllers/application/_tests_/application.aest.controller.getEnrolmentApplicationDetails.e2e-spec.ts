4d218db1a17e4ac535a7dcd386c6ead8c4b48080
Shashank Shekhar <sh16011993@gmail.com>
2025-02-18 09:21:16 -0800

    Code Update: e2e tests

--- sources/packages/backend/apps/api/src/route-controllers/application/_tests_/application.aest.controller.getEnrolmentApplicationDetails.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/application/_tests_/application.aest.controller.getEnrolmentApplicationDetails.e2e-spec.ts
@@ -74,7 +74,8 @@ describe("ApplicationAESTController(e2e)-getEnrolmentApplicationDetails", () => 
       .expect(HttpStatus.NOT_FOUND)
       .expect({
         statusCode: HttpStatus.NOT_FOUND,
-        message: "Current application for application 99999999 was not found.",
+        message:
+          "Application id 99999999 not found or not in relevant status to get enrolment details.",
         error: "Not Found",
       });
   });
