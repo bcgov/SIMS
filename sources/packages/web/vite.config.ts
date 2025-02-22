e4267a9affd62b139e1439b588b886e1f2ba54ea
Bidyashish <hi@bidyashish.com>
2025-02-21 17:48:40 -0800

    update

--- sources/packages/web/vite.config.ts
+++ sources/packages/web/vite.config.ts
@@ -6,7 +6,16 @@ import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // ...
+        },
+        transformAssetUrls: {
+          // ...
+        },
+      },
+    }),
     vuetify({
       autoImport: true,
     }),
