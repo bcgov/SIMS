85e996903a12548db4486e5b045124a439aedce2
Bidyashish <hi@bidyashish.com>
2025-02-21 17:32:30 -0800

    udpate

--- sources/packages/web/src/plugins/vuetify.ts
+++ sources/packages/web/src/plugins/vuetify.ts
@@ -1,12 +1,12 @@
 import "@mdi/font/css/materialdesignicons.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "vuetify/styles";
+import "vuetify/lib/styles/main.sass";
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import * as components from "vuetify/lib/components";
+import * as directives from "vuetify/lib/directives";
 import { aliases, fa } from "vuetify/iconsets/fa";
 import { mdi } from "vuetify/iconsets/mdi";
-import { VDivider, VDataTableServer, VDataTable } from "vuetify/components";
+import { VDivider, VDataTableServer, VDataTable } from "vuetify/lib/components";
 
 export default createVuetify({
   components: {
--- sources/packages/web/src/vite-env.d.ts
+++ sources/packages/web/src/vite-env.d.ts
@@ -4,13 +4,3 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
-// Form.io style declarations
-declare module "@formio/js/dist/formio.full.css" {
-  const content: any;
-  export default content;
-}
-
-declare module "@formio/js" {
-  const Formio: any;
-  export default Formio;
-}
