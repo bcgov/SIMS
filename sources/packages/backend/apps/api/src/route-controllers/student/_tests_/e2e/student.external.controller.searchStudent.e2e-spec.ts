212fde64663e58f3f7efbe75eb8b4055bf170552
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-02-22 20:04:38 -0700

    added more E2E Tests.

--- sources/packages/backend/apps/api/src/route-controllers/student/_tests_/e2e/student.external.controller.searchStudent.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/student/_tests_/e2e/student.external.controller.searchStudent.e2e-spec.ts
@@ -3,6 +3,7 @@ import * as request from "supertest";
 import {
   createE2EDataSources,
   E2EDataSources,
+  saveFakeSFASIndividual,
   saveFakeStudent,
 } from "@sims/test-utils";
 import { OfferingIntensity } from "@sims/sims-db";
@@ -21,6 +22,9 @@ describe("StudentExternalController(e2e)-searchStudents", () => {
   let db: E2EDataSources;
   const endpoint = "/external/student";
   const VALID_SIN = "656173713";
+  const searchPayload: StudentSearchAPIInDTO = {
+    sin: VALID_SIN,
+  };
 
   beforeAll(async () => {
     const { nestApplication, dataSource } = await createTestingAppModule();
@@ -36,22 +40,21 @@ describe("StudentExternalController(e2e)-searchStudents", () => {
       },
       { isValidSIN: false },
     );
+    // Remove the student from SFAS to not interfere with other tests.
+    await db.sfasIndividual.delete({ sin: VALID_SIN });
   });
 
   it(
     "Should return student information from SIMS when the student with provided SIN exist in SIMS" +
-      ` but not in SFAS and the student does not have any ${OfferingIntensity.fullTime} applications`,
+      ` but not in SFAS and the student does not have any ${OfferingIntensity.fullTime} applications.`,
     async () => {
       // Arrange
       const student = await saveFakeStudent(db.dataSource, undefined, {
         sinValidationInitialValue: { sin: VALID_SIN },
         includeAddressLine2: true,
       });
-      const studentSIN = student.sinValidation.sin;
       const address = student.contactInfo.address;
-      const searchPayload: StudentSearchAPIInDTO = {
-        sin: studentSIN,
-      };
+
       const token = await getExternalUserToken();
       const expectedStudentServiceResult: StudentSearchResultAPIOutDTO = {
         isLegacy: false,
@@ -77,6 +80,89 @@ describe("StudentExternalController(e2e)-searchStudents", () => {
         .auth(token, BEARER_AUTH_TYPE)
         .expect(HttpStatus.OK)
         .expect(expectedStudentServiceResult);
+    },
+  );
+
+  it(
+    "Should return student information from SIMS when the student with provided SIN exist in SFAS" +
+      ` but not in SIMS and the student does not have any ${OfferingIntensity.fullTime} applications.`,
+    async () => {
+      // Arrange
+      const legacyStudent = await saveFakeSFASIndividual(db.dataSource, {
+        initialValues: { sin: VALID_SIN },
+        includeAddressLine2: true,
+      });
+      const token = await getExternalUserToken();
+      const expectedStudentServiceResult: StudentSearchResultAPIOutDTO = {
+        isLegacy: true,
+        givenNames: legacyStudent.firstName,
+        lastName: legacyStudent.lastName,
+        sin: legacyStudent.sin,
+        birthDate: legacyStudent.birthDate,
+        phoneNumber: legacyStudent.phoneNumber?.toString(),
+        address: {
+          addressLine1: legacyStudent.addressLine1,
+          addressLine2: legacyStudent.addressLine2,
+          city: legacyStudent.city,
+          provinceState: legacyStudent.provinceState,
+          country: legacyStudent.country,
+          postalCode: legacyStudent.postalZipCode,
+        },
+        applications: [],
+      };
+
+      // Act/Assert
+      await request(app.getHttpServer())
+        .post(endpoint)
+        .send(searchPayload)
+        .auth(token, BEARER_AUTH_TYPE)
+        .expect(HttpStatus.OK)
+        .expect(expectedStudentServiceResult);
+    },
+  );
+
+  it(
+    "Should return student information from SIMS when the student with provided SIN exist in both SFAS and SIMS" +
+      ` and the student does not have any ${OfferingIntensity.fullTime} applications in both the systems.`,
+    async () => {
+      // Arrange
+      // Create fake SIMS student.
+      const student = await saveFakeStudent(db.dataSource, undefined, {
+        sinValidationInitialValue: { sin: VALID_SIN },
+        includeAddressLine2: true,
+      });
+      // Create Fake SFAS Individual with same SIN as SIMS.
+      await saveFakeSFASIndividual(db.dataSource, {
+        initialValues: { sin: VALID_SIN },
+        includeAddressLine2: true,
+      });
+      const address = student.contactInfo.address;
+      const token = await getExternalUserToken();
+      const expectedSIMSStudentSearchResult: StudentSearchResultAPIOutDTO = {
+        isLegacy: false,
+        givenNames: student.user.firstName,
+        lastName: student.user.lastName,
+        sin: student.sinValidation.sin,
+        birthDate: student.birthDate,
+        phoneNumber: student.contactInfo.phone,
+        address: {
+          addressLine1: address.addressLine1,
+          addressLine2: address.addressLine2,
+          city: address.city,
+          provinceState: address.provinceState,
+          country: address.country,
+          postalCode: address.postalCode,
+        },
+        applications: [],
+      };
+
+      // Act/Assert
+      await request(app.getHttpServer())
+        .post(endpoint)
+        .send(searchPayload)
+        .auth(token, BEARER_AUTH_TYPE)
+        .expect(HttpStatus.OK)
+        .expect(expectedSIMSStudentSearchResult);
     },
   );
 
