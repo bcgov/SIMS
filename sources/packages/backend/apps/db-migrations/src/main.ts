2676ecc87a525a25f7a99e07464e6903bdd6d350
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-03-02 08:42:38 -0800

    Testing local migration execution

--- sources/packages/backend/apps/db-migrations/src/main.ts
+++ sources/packages/backend/apps/db-migrations/src/main.ts
@@ -1,7 +1,9 @@
+import { Logger } from "@nestjs/common";
 import { DBMigrationsService } from "./repl/db-migrations.service";
 
 (async () => {
-  console.info("Starting migration process.");
+  const logger = new Logger();
+  logger.log("Starting migration process.");
   const migrationsService = new DBMigrationsService();
   await migrationsService.run();
 })();
--- sources/packages/backend/apps/db-migrations/src/repl/repl.ts
+++ sources/packages/backend/apps/db-migrations/src/repl/repl.ts
@@ -1,22 +1,19 @@
 import "../../../../env-setup";
 import { repl } from "@nestjs/core";
 import { REPLModule } from "./repl.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
-  console.info("DB Migrations REPL(Read-Eval-Print-Loop) started.");
-  console.info("Please use the associated terminal to interact with the REPL.");
-  console.info("Once on terminal type: npm run migration:repl");
-  console.info("Some available commands:");
-  console.info("await $(DBMigrationsService).list()");
-  console.info("await $(DBMigrationsService).run()");
-  console.info("await $(DBMigrationsService).rollback()");
-  console.info("Use the command debug() for a list of providers.");
-  console.info(
-    "Use the command methods(provider name) for a list of available methods.",
-  );
-  console.info(
-    "To see more about Nestjs REPL visit https://docs.nestjs.com/recipes/repl",
-  );
+  const logger = new Logger();
+  logger.log("DB Migrations REPL(Read-Eval-Print-Loop) started.");
+  logger.warn(`Current version: ${process.env.VERSION}`);
+  logger.log("Please use the associated terminal to interact with the REPL.");
+  logger.log("Once on terminal type: npm run migration:repl");
+  logger.log("Available commands:");
+  logger.log("await $(DBMigrationsService).list()");
+  logger.log("await $(DBMigrationsService).run()");
+  logger.log("await $(DBMigrationsService).rollback()");
+  logger.log("Use '.exit' to leave the REPL.");
   await repl(REPLModule);
 }
 bootstrap();
