2d4cb3ac81ce603fe7debb439f2d19e1b6acfe09
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-03-03 15:59:34 -0800

    Allow log all for troubleshooting.

--- sources/packages/backend/apps/db-migrations/src/data-source.ts
+++ sources/packages/backend/apps/db-migrations/src/data-source.ts
@@ -1,5 +1,12 @@
+import { LogLevel } from "typeorm";
 import "../../../env-setup";
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+/**
+ * Allow enable the log for all operations for troubleshooting.
+ */
+const logging =
+  process.env.LOG_ALL === "true" ? "all" : (["error", "warn"] as LogLevel[]);
 
 export const ormConfig: PostgresConnectionOptions = {
   type: "postgres",
@@ -11,5 +18,5 @@ export const ormConfig: PostgresConnectionOptions = {
   schema: process.env.DB_SCHEMA || "sims",
   synchronize: false,
   migrations: ["apps/db-migrations/src/migrations/*{.ts,.js}"],
-  logging: ["error", "warn"],
+  logging,
 };
