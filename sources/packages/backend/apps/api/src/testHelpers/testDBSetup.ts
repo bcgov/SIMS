491935f9db83a29b8f221d509dcd864e5d155c25
guru-aot <gurumoorthy.mohan@aot-technologies.com>
2025-03-26 12:51:48 -0600

    updated

--- sources/packages/backend/apps/api/src/testHelpers/testDBSetup.ts
+++ sources/packages/backend/apps/api/src/testHelpers/testDBSetup.ts
@@ -5,7 +5,6 @@ export async function setupDB() {
     return simsDataSource.initialize();
   } catch (excp) {
     console.error(`Exception to connected db: ${excp}`);
-    //console.dir(ormConfig);
   }
 }
 
