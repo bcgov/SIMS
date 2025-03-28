0c7b753bec98d0a8debebc5dd671f7b205ea7d9d
guru-aot <gurumoorthy.mohan@aot-technologies.com>
2025-03-28 16:27:20 -0600

    updated

--- sources/packages/backend/apps/api/src/route-controllers/health-check/health.controller.spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/health-check/health.controller.spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { HealthController } from "./health.controller";
 import { DatabaseModule } from "@sims/sims-db";
+import { TerminusModule } from "@nestjs/terminus";
 
 // TODO: must mock DB dependencies.
-describe.skip("HealthController", () => {
+describe("HealthController", () => {
   let healthController: HealthController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
+      imports: [DatabaseModule, TerminusModule],
       controllers: [HealthController],
     }).compile();
 
@@ -17,10 +18,8 @@ describe.skip("HealthController", () => {
 
   describe("root", () => {
     it("should return Hello world string with db connection status and version", () => {
-      const expected = `Hello World! The database dataSource is true and version: ${
-        process.env.VERSION ?? "-1"
-      }`;
-      expect(healthController.getHello()).toBe(expected);
+      console.log(healthController.check());
+      expect(healthController.check());
     });
   });
 });
