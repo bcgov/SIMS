e8b4086deee3560330bac08594a50d347420086f
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-04-04 16:28:49 -0600

    Add columns to history table.

--- sources/packages/backend/apps/db-migrations/src/sql/EducationProgramsOfferings/Add-online-instruction-columns.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/EducationProgramsOfferings/Add-online-instruction-columns.sql
@@ -20,3 +20,27 @@ COMMENT ON COLUMN sims.education_programs_offerings.total_online_duration IS 'Pe
 COMMENT ON COLUMN sims.education_programs_offerings.minimum_online_duration IS 'Percentage of minimum duration that will be provided by online delivery in the blended offering.';
 
 COMMENT ON COLUMN sims.education_programs_offerings.maximum_online_duration IS 'Percentage of maximum duration that will be provided by online delivery in the blended offering.';
+
+-- Add new columns to the history table. 
+ALTER TABLE
+    sims.education_programs_offerings_history
+ADD
+    COLUMN online_instruction_mode VARCHAR(50),
+ADD
+    COLUMN is_online_duration_same_always VARCHAR(3),
+ADD
+    COLUMN total_online_duration SMALLINT,
+ADD
+    COLUMN minimum_online_duration SMALLINT,
+ADD
+    COLUMN maximum_online_duration SMALLINT;
+
+COMMENT ON COLUMN sims.education_programs_offerings_history.online_instruction_mode IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.education_programs_offerings_history.is_online_duration_same_always IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.education_programs_offerings_history.total_online_duration IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.education_programs_offerings_history.minimum_online_duration IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.education_programs_offerings_history.maximum_online_duration IS 'Historical data from the original table. See original table comments for details.';
--- sources/packages/backend/apps/db-migrations/src/sql/EducationProgramsOfferings/Rollback-add-online-instruction-columns.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/EducationProgramsOfferings/Rollback-add-online-instruction-columns.sql
@@ -4,3 +4,11 @@ ALTER TABLE
     DROP COLUMN total_online_duration,
     DROP COLUMN minimum_online_duration,
     DROP COLUMN maximum_online_duration;
+
+-- Drop new columns from the history table.
+ALTER TABLE
+    sims.education_programs_offerings_history DROP COLUMN online_instruction_mode,
+    DROP COLUMN is_online_duration_same_always,
+    DROP COLUMN total_online_duration,
+    DROP COLUMN minimum_online_duration,
+    DROP COLUMN maximum_online_duration;
