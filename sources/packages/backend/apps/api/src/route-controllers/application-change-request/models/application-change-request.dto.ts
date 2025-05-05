1f40d90966360c91ddeda9df5c028dfedc52d723
Bidyashish <hi@bidyashish.com>
2025-05-05 15:00:15 -0700

    template

---
+++ sources/packages/backend/apps/api/src/route-controllers/application-change-request/models/application-change-request.dto.ts
@@ -0,0 +1,12 @@
+/**
+ * DTO for pending application change request summary.
+ */
+export class ApplicationChangeRequestPendingSummaryAPIOutDTO {
+  requestId: number;
+  applicationId: number;
+  studentId: number;
+  submittedDate: Date;
+  firstName?: string;
+  lastName: string;
+  applicationNumber: string;
+} 
