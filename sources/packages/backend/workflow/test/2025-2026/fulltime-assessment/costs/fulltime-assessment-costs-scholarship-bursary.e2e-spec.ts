aa6040bf2691e3657562691ee158e370ef04f6cb
Carly Cotton <carly.cotton@gov.bc.ca>
2025-06-03 12:36:39 -0700

    Fixed the test case variable names and added extra.

--- sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/costs/fulltime-assessment-costs-scholarship-bursary.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/costs/fulltime-assessment-costs-scholarship-bursary.e2e-spec.ts
@@ -41,7 +41,7 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
       assessmentConsolidatedData.offeringWeeks = 18;
       assessmentConsolidatedData.studentDataRelationshipStatus = "single";
       assessmentConsolidatedData.studentDataScholarshipAmount = 1700;
-      assessmentConsolidatedData.programYearTotalScholarshipsBursaries = 0;
+      assessmentConsolidatedData.programYearTotalFullTimeScholarshipsBursaries = 0;
       // Act
       const calculatedAssessment =
         await executeFullTimeAssessmentForProgramYear(
@@ -57,11 +57,13 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
       ).toBe(1800);
       // The amount declared by the student is used in future applications in the same program year.
       // This should be the lower of the amount declared by the student, or the difference between the PY limit and the remaining limit.
+      // ($1700 < $1800, so $1700 is used)
       expect(
         calculatedAssessment.variables
           .calculatedDataExemptScholarshipsBursaries,
       ).toBe(1700);
       // If the amount declared by the student is less then the PY limit less the remaining limit, then the total scholarships and bursaries is $0.
+      // ($1700 < $1800, so $0 is used)
       expect(
         calculatedAssessment.variables.calculatedDataTotalScholarshipsBursaries,
       ).toBe(0);
@@ -79,7 +81,7 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
       assessmentConsolidatedData.offeringWeeks = 18;
       assessmentConsolidatedData.studentDataRelationshipStatus = "single";
       assessmentConsolidatedData.studentDataScholarshipAmount = 1900;
-      assessmentConsolidatedData.programYearTotalScholarshipsBursaries = 0;
+      assessmentConsolidatedData.programYearTotalFullTimeScholarshipsBursaries = 0;
       // Act
       const calculatedAssessment =
         await executeFullTimeAssessmentForProgramYear(
@@ -95,11 +97,13 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
       ).toBe(1800);
       // The amount declared by the student is used in future applications in the same program year.
       // This should be the lower of the amount declared by the student or the remaining limit.
+      // ($1900 > $1800, so $1800 is used)
       expect(
         calculatedAssessment.variables
           .calculatedDataExemptScholarshipsBursaries,
       ).toBe(1800);
       // If the amount declared by the student is greater than the PY limit for exemption, the difference is considered towards the contribution.
+      // ($1900 - $1800 = $100)
       expect(
         calculatedAssessment.variables.calculatedDataTotalScholarshipsBursaries,
       ).toBe(100);
@@ -107,8 +111,8 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
   );
 
   it(
-    "Should calculate $100 for total scholarship and bursaries when " +
-      "student declares that they received scholarship and bursaries " +
+    "Should calculate $1200 for total scholarship and bursaries when " +
+      "student declares that they received scholarship and bursaries over the program year limit " +
       "and student has declared scholarship or bursaries in the current program year.",
     async () => {
       // Arrange
@@ -117,7 +121,7 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
       assessmentConsolidatedData.offeringWeeks = 18;
       assessmentConsolidatedData.studentDataRelationshipStatus = "single";
       assessmentConsolidatedData.studentDataScholarshipAmount = 2000;
-      assessmentConsolidatedData.programYearTotalScholarshipsBursaries = 1000;
+      assessmentConsolidatedData.programYearTotalFullTimeScholarshipsBursaries = 1000;
       // Act
       const calculatedAssessment =
         await executeFullTimeAssessmentForProgramYear(
@@ -127,21 +131,62 @@ describe(`E2E Test Workflow part-time-assessment-${PROGRAM_YEAR}-costs-scholarsh
       // Assert
 
       // PY limit is $1800 before any declared scholarship or bursary amounts are considered towards contribution.
-      // Student has already had an exemption of $1000 in the current program year.
+      // Student has already had an exemption of $1000 in the current program year. ($1800 - $1000 = $800 remaining limit)
       expect(
         calculatedAssessment.variables
           .calculatedDataRemainingScholarshipsBursariesLimit,
       ).toBe(800);
       // The amount declared by the student is used in future applications towards the same PY limit.
-      // If the remaining limit is greater than the declared amount, this should be the student declared value.
+      // If the remaining limit is greater than the declared amount, this should be the student declared value. ($2000 > $800 remaining limit)
       expect(
         calculatedAssessment.variables
           .calculatedDataExemptScholarshipsBursaries,
       ).toBe(800);
       // If the amount declared by the student is greater than the PY limit for exemption, the difference is considered towards the contribution.
+      // ($2000 - $800 = $1200)
       expect(
         calculatedAssessment.variables.calculatedDataTotalScholarshipsBursaries,
       ).toBe(1200);
+    },
+  );
+
+  it(
+    "Should calculate $0 for total scholarship and bursaries when " +
+      "student declares that they received scholarship and bursaries under the program year limit " +
+      "and student has declared scholarship or bursaries in the current program year.",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.offeringWeeks = 18;
+      assessmentConsolidatedData.studentDataRelationshipStatus = "single";
+      assessmentConsolidatedData.studentDataScholarshipAmount = 600;
+      assessmentConsolidatedData.programYearTotalFullTimeScholarshipsBursaries = 1000;
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+      // Assert
+
+      // PY limit is $1800 before any declared scholarship or bursary amounts are considered towards contribution.
+      // Student has already had an exemption of $1000 in the current program year. ($1800 - $1000 = $800 remaining limit)
+      expect(
+        calculatedAssessment.variables
+          .calculatedDataRemainingScholarshipsBursariesLimit,
+      ).toBe(800);
+      // The amount declared by the student is used in future applications towards the same PY limit.
+      // If the remaining limit is greater than the declared amount, this should be the student declared value. ($600 < $800 remaining limit)
+      expect(
+        calculatedAssessment.variables
+          .calculatedDataExemptScholarshipsBursaries,
+      ).toBe(600);
+      // If the amount declared by the student is greater than the PY limit for exemption, the difference is considered towards the contribution.
+      // ($600 - $600 = $0)
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalScholarshipsBursaries,
+      ).toBe(0);
     },
   );
 
