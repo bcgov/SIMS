750421694b79fe98e00bb07b7d08c070127d0c15
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-06-17 16:03:59 -0700

    E2E tests adjustments

--- sources/packages/backend/apps/api/src/route-controllers/student/_tests_/e2e/student.students.controller.getStudentProfile.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/student/_tests_/e2e/student.students.controller.getStudentProfile.e2e-spec.ts
@@ -15,11 +15,13 @@ import {
 import { getUserFullName } from "../../../../utilities";
 import { TestingModule } from "@nestjs/testing";
 import { addDays } from "@sims/utilities";
+import { ConfigService } from "@sims/utilities/config/config.service";
 
 describe("StudentInstitutionsController(e2e)-getStudentProfile", () => {
   let app: INestApplication;
   let appModule: TestingModule;
   let db: E2EDataSources;
+  let configService: ConfigService;
   const endpoint = "/students/student";
 
   beforeAll(async () => {
@@ -28,6 +30,11 @@ describe("StudentInstitutionsController(e2e)-getStudentProfile", () => {
     app = nestApplication;
     appModule = module;
     db = createE2EDataSources(dataSource);
+    configService = appModule.get(ConfigService);
+  });
+
+  beforeEach(() => {
+    allowBetaUsersOnly(false);
   });
 
   it("Should get the student profile when a student account exists.", async () => {
@@ -68,12 +75,13 @@ describe("StudentInstitutionsController(e2e)-getStudentProfile", () => {
         },
         disabilityStatus: student.disabilityStatus,
         validSin: student.sinValidation.isValidSIN,
-        hasFulltimeAccess: false,
+        hasFulltimeAccess: true,
       });
   });
 
-  it("Should get the student profile with the hasFulltimeAccess as true user when the student was added to the beta users table.", async () => {
+  it("Should get the student profile with the hasFulltimeAccess as true user when the student was added to the beta users table and allowBetaUsersOnly is true.", async () => {
     // Arrange
+    allowBetaUsersOnly(true);
     const student = await saveFakeStudent(db.dataSource);
     // Register the student as a beta user for full-time.
     await db.betaUsersAuthorizations.save({
@@ -98,8 +106,9 @@ describe("StudentInstitutionsController(e2e)-getStudentProfile", () => {
       .expect((response) => expect(response.body.hasFulltimeAccess).toBe(true));
   });
 
-  it("Should get the student profile with hasFulltimeAccess as false when the student was added to the beta users table with a future date.", async () => {
+  it("Should get the student profile with hasFulltimeAccess as false when the student was added to the beta users table with a future date and allowBetaUsersOnly is true.", async () => {
     // Arrange
+    allowBetaUsersOnly(true);
     const student = await saveFakeStudent(db.dataSource);
     // Register the student as a beta user for full-time.
     await db.betaUsersAuthorizations.save({
@@ -125,6 +134,38 @@ describe("StudentInstitutionsController(e2e)-getStudentProfile", () => {
         expect(response.body.hasFulltimeAccess).toBe(false),
       );
   });
+
+  it("Should get the student profile with hasFulltimeAccess as true when the student was not added to the beta users table but allowBetaUsersOnly is false.", async () => {
+    // Arrange
+    allowBetaUsersOnly(false);
+    const student = await saveFakeStudent(db.dataSource);
+
+    // Mock user service to return the saved student.
+    await mockUserLoginInfo(appModule, student);
+
+    // Get any student user token.
+    const studentToken = await getStudentToken(
+      FakeStudentUsersTypes.FakeStudentUserType1,
+    );
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(studentToken, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect((response) => expect(response.body.hasFulltimeAccess).toBe(true));
+  });
+
+  /**
+   * Mock the allowBetaUsersOnly config value to allow changing the behavior
+   * of the beta users authorization between tests.
+   * @param allow true to allow beta users only, false to allow all users.
+   */
+  function allowBetaUsersOnly(allow: boolean) {
+    jest
+      .spyOn(configService, "allowBetaUsersOnly", "get")
+      .mockReturnValue(allow);
+  }
 
   afterAll(async () => {
     await app?.close();
