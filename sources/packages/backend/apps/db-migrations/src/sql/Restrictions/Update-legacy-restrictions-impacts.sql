653b1dbb58c7f56d6e275ac69a44c43a3ff94e2f
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2025-06-20 09:06:54 -0700

    #4777 - Revise impact of LGCY_SSRN,SSD,Z1 - DB migration (#4840)
    Please see below the actions taken for each restriction.
    
    ### Rename LGCY_SSRN into SSRN
    
    - Deleted restriction map for `LGCY_SSRN`;
    - Renamed `LGCY_SSRN` to `SSRN`.
    - Updated `SSRN` columns `is_legacy`, `description`, and `action_type`.
    _Note:_ `SSRN` will now work as a direct SFAS to SIMS map since they
    have the same code.
    
    ### Remap LGCY_SSD into SSR
    
    - Ensure the new map will be present, deleting the existing record (if
    present), and creating the expected new map from SFAS `SSD` to SIMS
    `SSR`.
    
    ### Z1 converted from federal to provincial
    
    - Removed the map from SFAS `Z1` to SIMS `LGCY_Z1`;
    - Removed the `Z1` federal restriction. _Note:_ The `restriction_type`
    was added in the `where` to ensure the SQL can be executed twice, not
    causing any issue;
    - Converted the provincial `LGCY_Z1` to a non-legacy restriction;
    - Updated `Z1` columns `is_legacy`, `description`, and `action_type`.
    
    ## Rollback (no DB effect)
    
    
    ![image](https://github.com/user-attachments/assets/8477b54a-bf3a-4617-a58c-70f92d6644ce)

---
+++ sources/packages/backend/apps/db-migrations/src/sql/Restrictions/Update-legacy-restrictions-impacts.sql
@@ -0,0 +1,77 @@
+-- Remove the SSRN legacy map since it will become a direct map to the SIMS restriction.
+DELETE FROM
+    sims.sfas_restriction_maps
+WHERE
+    code = 'LGCY_SSRN';
+
+-- Rename the existing legacy SSRN converting it to a non-legacy restriction.
+UPDATE
+    sims.restrictions
+SET
+    restriction_code = 'SSRN',
+    description = 'Poor Scholastic Standing.',
+    restriction_category = 'Academic',
+    notification_type = 'Error',
+    is_legacy = false,
+    action_type = ARRAY ['Stop full time disbursement', 'Stop full time apply'] :: sims.restriction_action_types [],
+    updated_at = NOW(),
+    modifier = (
+        SELECT
+            id
+        FROM
+            sims.users
+        WHERE
+            -- System user.
+            user_name = '8fb44f70-6ce6-11ed-b307-8743a2da47ef@system'
+    )
+WHERE
+    restriction_code = 'LGCY_SSRN';
+
+-- Remap LGCY_SSD to SSR.
+-- Delete the legacy LGCY_SSD map, if exists to ensure the insert can be executed.
+DELETE FROM
+    sims.sfas_restriction_maps
+WHERE
+    code = 'LGCY_SSD';
+
+-- Insert the new SSR map.
+INSERT INTO
+    sims.sfas_restriction_maps (legacy_code, code, is_legacy_only)
+VALUES
+    ('SSD', 'SSR', FALSE);
+
+-- Delete legacy Z1 map.
+DELETE FROM
+    sims.sfas_restriction_maps
+WHERE
+    code = 'LGCY_Z1';
+
+-- Delete Z1 federal restriction.
+DELETE FROM
+    sims.restrictions
+WHERE
+    restriction_code = 'Z1'
+    AND restriction_type = 'Federal';
+
+-- Convert existing LGCY_Z1 to a non-legacy restriction.
+UPDATE
+    sims.restrictions
+SET
+    restriction_code = 'Z1',
+    is_legacy = false,
+    description = 'Grant overaward. Not eligible for BCSL. Eligible for CSL.',
+    restriction_category = 'BCSL Delinquency',
+    notification_type = 'Error',
+    action_type = ARRAY ['Stop full time BC funding'] :: sims.restriction_action_types [],
+    updated_at = NOW(),
+    modifier = (
+        SELECT
+            id
+        FROM
+            sims.users
+        WHERE
+            -- System user.
+            user_name = '8fb44f70-6ce6-11ed-b307-8743a2da47ef@system'
+    )
+WHERE
+    restriction_code = 'LGCY_Z1'
