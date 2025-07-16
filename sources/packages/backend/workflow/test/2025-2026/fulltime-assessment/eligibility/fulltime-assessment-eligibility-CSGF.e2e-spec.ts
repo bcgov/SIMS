5ca369c2ae6e953b8104a85f0a7777c8c7d3ec84
Carly Cotton <128620426+CarlyCotton@users.noreply.github.com>
2025-07-15 18:06:37 -0700

    #4768 - FT Assessment - BCAG Income Threshold in Eligibility (#4925)
    - Updated values in PY 25/26 to the correct ones
    - Included income thresholds
    - Updated test cases for the two BCAGs:
        - added a requirement for income in the positive test
        - added a fail on income test

--- sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/eligibility/fulltime-assessment-eligibility-CSGF.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/eligibility/fulltime-assessment-eligibility-CSGF.e2e-spec.ts
@@ -121,8 +121,8 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-eligibility-CSGF
       CredentialType.UnderGraduateCertificate;
     assessmentConsolidatedData.programLength =
       ProgramLengthOptions.TwoToThreeYears;
-    // 114436 is the threshold for a family of 3 (2 dependents + the student).
-    assessmentConsolidatedData.studentDataTaxReturnIncome = 114437;
+    // 115978 is the threshold for a family of 3 (2 dependents + the student).
+    assessmentConsolidatedData.studentDataTaxReturnIncome = 115979;
     assessmentConsolidatedData.studentDataDependants = [
       createFakeStudentDependentEligible(
         DependentEligibility.Eligible0To18YearsOld,
