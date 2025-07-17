d8151c6bd2c77718adeb6418fca2a2e928c87869
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-07-17 12:07:50 -0600

    Appeal components

---
+++ sources/packages/web/src/components/students/StudentAppealInstructions.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="font-bold">Instructions:</p>
+  <ul>
+    <li>You must complete all fields of the change request form.</li>
+    <li>
+      All information that has not changed should match what was entered on your
+      application.
+    </li>
+    <li>
+      Information from previously approved Change Requests attached to this
+      application must be re-entered here.
+    </li>
+  </ul>
+</template>
