6f9434392cfa9f68e300b8a1cfa7345a2f0ba755
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-07-21 12:05:11 -0700

    Adjusted NODE_ENV to use vite meta

--- sources/packages/web/vite.config.ts
+++ sources/packages/web/vite.config.ts
@@ -4,44 +4,39 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      vue({
-        template: { transformAssetUrls },
-      }),
-      vuetify({
-        autoImport: true,
-      }),
-    ],
-    resolve: {
-      extensions: [".mjs", ".js", ".ts", ".vue", ".scss"],
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".vue", ".scss"],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        outputStyle: "compressed",
       },
     },
-    define: {
-      "process.env.NODE_ENV": JSON.stringify(mode),
+    devSourcemap: true,
+  },
+  server: {
+    port: 8080,
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          quietDeps: true,
-          outputStyle: "compressed",
-        },
-      },
-      devSourcemap: true,
-    },
-    server: {
-      port: 8080,
-    },
-    build: {
-      commonjsOptions: {
-        transformMixedEsModules: true,
-      },
-    },
-    optimizeDeps: {
-      exclude: ["vuetify"],
-    },
-  };
+  },
+  optimizeDeps: {
+    exclude: ["vuetify"],
+  },
 });
