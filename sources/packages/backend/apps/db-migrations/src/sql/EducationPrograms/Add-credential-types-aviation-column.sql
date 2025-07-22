e52f5e611d3dd60cdbbf78808cd031186598b693
Shashank Shekhar <sh16011993@gmail.com>
2025-07-22 16:10:10 -0700

    Code Update: Review Comments

--- sources/packages/backend/apps/db-migrations/src/sql/EducationPrograms/Add-credential-types-aviation-column.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/EducationPrograms/Add-credential-types-aviation-column.sql
@@ -10,4 +10,4 @@ ALTER TABLE
 ADD
   COLUMN credential_types_aviation jsonb;
 
-COMMENT ON COLUMN sims.education_programs_history.credential_types_aviation IS 'Aviation program credential types.';
+COMMENT ON COLUMN sims.education_programs_history.credential_types_aviation IS 'Historical data from the original table. See the original table comments for details.';
