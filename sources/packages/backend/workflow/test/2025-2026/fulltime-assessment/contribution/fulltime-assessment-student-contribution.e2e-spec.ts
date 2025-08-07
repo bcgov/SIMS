773926b175de872dffab4e69d85a06e7d26b8066
Carly Cotton <carly.cotton@gov.bc.ca>
2025-08-07 16:51:52 -0700

    More tests

--- sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/contribution/fulltime-assessment-student-contribution.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/contribution/fulltime-assessment-student-contribution.e2e-spec.ts
@@ -4,6 +4,7 @@ import {
   createFakeConsolidatedFulltimeData,
   executeFullTimeAssessmentForProgramYear,
 } from "../../../test-utils";
+import { YesNoOptions } from "@sims/test-utils";
 
 describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-student-contribution.`, () => {
   it(
@@ -30,11 +31,11 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-student-contribu
       ).toBe(false);
       // The federal and provincial fixed student contributions are based on family income and size when the student is not exempt.
       expect(calculatedAssessment.variables.calculatedDataTotalFederalFSC).toBe(
-        851,
+        851.4692307692308,
       );
       expect(
         calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
-      ).toBe(851);
+      ).toBe(851.4692307692308);
       // The total spouse contribution is not calculated for single students.
       expect(
         calculatedAssessment.variables.calculatedDataTotalSpouseContribution,
@@ -50,15 +51,171 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-student-contribu
       // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
       expect(
         calculatedAssessment.variables.calculatedDataTotalFederalContribution,
-      ).toBe(851);
+      ).toBe(851.4692307692308);
       // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
       expect(
         calculatedAssessment.variables
           .calculatedDataTotalProvincialContribution,
-      ).toBe(851);
+      ).toBe(851.4692307692308);
     },
   );
 
+  it(
+    "Should calculate total fixed student contribution for a single independent student that is not exempt from contribution " +
+      " and has no dependants and $1000 of targeted resources.",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataScholarshipAmount = 2800;
+
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+      // Assert
+      // If the student is indigenous, a former youth in care, or has a disability, they are exempt from the federal and provincial fixed student contributions.
+      expect(
+        calculatedAssessment.variables.calculatedDataFederalFSCExempt,
+      ).toBe(false);
+      expect(
+        calculatedAssessment.variables.calculatedDataProvincialFSCExempt,
+      ).toBe(false);
+      // The federal and provincial fixed student contributions are based on family income and size when the student is not exempt.
+      expect(calculatedAssessment.variables.calculatedDataTotalFederalFSC).toBe(
+        851.4692307692308,
+      );
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
+      ).toBe(851.4692307692308);
+      // The total spouse contribution is not calculated for single students.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalSpouseContribution,
+      ).toBe(undefined);
+      // The total parental contribution is not calculated for independent students.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalParentalContribution,
+      ).toBe(undefined);
+      // Targeted resources are scholarships and bursaries, government funding, non-government funding and voluntary parental contributions.
+      // The first $1800 of scholarships and bursaries are exempt from contribution in a program year.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalTargetedResources,
+      ).toBe(1000);
+      // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalFederalContribution,
+      ).toBe(1851.4692307692308);
+      // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+      expect(
+        calculatedAssessment.variables
+          .calculatedDataTotalProvincialContribution,
+      ).toBe(1851.4692307692308);
+    },
+  );
+
+  it(
+    "Should calculate total fixed student contribution (long offering) for a single independent student that is not exempt from contribution " +
+      " and has no dependants and no targeted resources.",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.offeringWeeks = 52;
+
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+      // Assert
+      // If the student is indigenous, a former youth in care, or has a disability, they are exempt from the federal and provincial fixed student contributions.
+      expect(
+        calculatedAssessment.variables.calculatedDataFederalFSCExempt,
+      ).toBe(false);
+      expect(
+        calculatedAssessment.variables.calculatedDataProvincialFSCExempt,
+      ).toBe(false);
+      // The federal and provincial fixed student contributions are based on family income and size when the student is not exempt.
+      expect(calculatedAssessment.variables.calculatedDataTotalFederalFSC).toBe(
+        2660.841346153845,
+      );
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
+      ).toBe(2660.841346153845);
+      // The total spouse contribution is not calculated for single students.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalSpouseContribution,
+      ).toBe(undefined);
+      // The total parental contribution is not calculated for independent students.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalParentalContribution,
+      ).toBe(undefined);
+      // Targeted resources are scholarships and bursaries, government funding, non-government funding and voluntary parental contributions.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalTargetedResources,
+      ).toBe(0);
+      // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalFederalContribution,
+      ).toBe(2660.841346153845);
+      // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+      expect(
+        calculatedAssessment.variables
+          .calculatedDataTotalProvincialContribution,
+      ).toBe(2660.841346153845);
+    },
+  );
+
+  it("Should calculate $0 for total fixed student contribution for a single independent student that is exempt from contribution (indigenous).", async () => {
+    // Arrange
+    const assessmentConsolidatedData =
+      createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+    assessmentConsolidatedData.studentDataIndigenousStatus = YesNoOptions.Yes;
+
+    // Act
+    const calculatedAssessment = await executeFullTimeAssessmentForProgramYear(
+      PROGRAM_YEAR,
+      assessmentConsolidatedData,
+    );
+    // Assert
+    // If the student is indigenous, a former youth in care, or has a disability, they are exempt from the federal and provincial fixed student contributions.
+    expect(calculatedAssessment.variables.calculatedDataFederalFSCExempt).toBe(
+      true,
+    );
+    expect(
+      calculatedAssessment.variables.calculatedDataProvincialFSCExempt,
+    ).toBe(true);
+    // The federal and provincial fixed student contributions are based on family income and size when the student is not exempt.
+    expect(calculatedAssessment.variables.calculatedDataTotalFederalFSC).toBe(
+      0,
+    );
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
+    ).toBe(0);
+    // The total spouse contribution is not calculated for single students.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalSpouseContribution,
+    ).toBe(undefined);
+    // The total parental contribution is not calculated for independent students.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalParentalContribution,
+    ).toBe(undefined);
+    // Targeted resources are scholarships and bursaries, government funding, non-government funding and voluntary parental contributions.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalTargetedResources,
+    ).toBe(0);
+    // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalFederalContribution,
+    ).toBe(0);
+    // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalProvincialContribution,
+    ).toBe(0);
+  });
   afterAll(async () => {
     // Closes the singleton instance created during test executions.
     await ZeebeMockedClient.getMockedZeebeInstance().close();
