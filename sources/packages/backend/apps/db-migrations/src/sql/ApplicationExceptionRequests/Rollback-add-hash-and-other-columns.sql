6418a8121fded24b48a9607304e6814b9224ec4a
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2025-08-08 13:27:48 -0700

    #4941 - Approved exception does not repeat - DB Migrations Fixes (#5040)
    - Adjusting the previously DB migration to remove the `exception_index`
    column.
    - Removed the already existing unique constraint that prevents an
    exception name from being repeated, which must happen now that multiple
    exceptions with the same name will be saved for dependents and parents.
    - Removed the recently added constraint, as discussed with @dheepak-aot,
    there is not a great benefit and it may cause an issue depending on how
    the descriptions are defined.
    
    ## Migration Rollback
    
    <img width="813" height="160" alt="image"
    src="https://github.com/user-attachments/assets/c29d03a4-e1eb-4aa2-b8f6-41903e00c560"
    />

--- sources/packages/backend/apps/db-migrations/src/sql/ApplicationExceptionRequests/Rollback-add-hash-and-other-columns.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/ApplicationExceptionRequests/Rollback-add-hash-and-other-columns.sql
@@ -1,5 +1,10 @@
 ALTER TABLE
-    sims.application_exception_requests DROP COLUMN exception_index,
-    DROP COLUMN exception_description,
+    sims.application_exception_requests DROP COLUMN exception_description,
     DROP COLUMN approval_exception_request_id,
     DROP COLUMN exception_hash;
+
+-- Recreate the original unique constraint on application_exception_id and exception_name.
+ALTER TABLE
+    sims.application_exception_requests
+ADD
+    CONSTRAINT application_exception_request_application_exception_id_exce_key UNIQUE (application_exception_id, exception_name);
