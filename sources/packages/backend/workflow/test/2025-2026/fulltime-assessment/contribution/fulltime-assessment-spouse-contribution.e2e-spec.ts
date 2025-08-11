11c965fa617d6257269f8a447808cc691e3b5352
Carly Cotton <carly.cotton@gov.bc.ca>
2025-08-11 07:14:36 -0700

    Variable fix and more tests

--- sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/contribution/fulltime-assessment-spouse-contribution.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/contribution/fulltime-assessment-spouse-contribution.e2e-spec.ts
@@ -141,7 +141,7 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-spouse-contribut
         YesNoOptions.No;
       assessmentConsolidatedData.studentDataEstimatedSpouseIncome = 30000;
       assessmentConsolidatedData.studentDataYouthInCare = YesNoOptions.Yes;
-      assessmentConsolidatedData.programYearSpouseContributionWeeks = 12; // Spouse contribution for 12 weeks have been calculated in the program year.
+      assessmentConsolidatedData.programYearTotalFullTimeSpouseContributionWeeks = 12; // Spouse contribution for 12 weeks have been calculated in the program year.
 
       // Act
       const calculatedAssessment =
@@ -193,7 +193,7 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-spouse-contribut
     },
   );
 
-  it("Should calculate $0 fixed student contribution for a married independent student when both are exempt ().", async () => {
+  it("Should calculate $0 fixed student contribution for a married independent student when both are exempt (EI Benefits).", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
@@ -206,6 +206,68 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-spouse-contribut
     assessmentConsolidatedData.studentDataPartnerHasFedralProvincialPDReceipt =
       YesNoOptions.No;
     assessmentConsolidatedData.studentDataEstimatedSpouseIncome = 30000;
+    assessmentConsolidatedData.studentDataYouthInCare = YesNoOptions.Yes;
+
+    // Act
+    const calculatedAssessment = await executeFullTimeAssessmentForProgramYear(
+      PROGRAM_YEAR,
+      assessmentConsolidatedData,
+    );
+    // Assert
+    // If the student is indigenous, a former youth in care, or has a disability, they are exempt from the federal and provincial fixed student contributions.
+    expect(calculatedAssessment.variables.calculatedDataFederalFSCExempt).toBe(
+      true,
+    );
+    expect(
+      calculatedAssessment.variables.calculatedDataProvincialFSCExempt,
+    ).toBe(true);
+    // The spouse can be exempt from contribution if they are a full-time student at the same time.
+    // Or when they have a disability, or are receiving certain types of income assistance.
+    expect(
+      calculatedAssessment.variables.calculatedDataSpousalContributionExempt,
+    ).toBe(true);
+    // The federal and provincial fixed student contributions are based on family income and size when the student is not exempt.
+    expect(calculatedAssessment.variables.calculatedDataTotalFederalFSC).toBe(
+      0,
+    );
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
+    ).toBe(0);
+    // The total spouse contribution is not calculated for single students.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalSpouseContribution,
+    ).toBe(0);
+    // The total parental contribution is not calculated for independent students.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalParentalContribution,
+    ).toBe(undefined);
+    // Targeted resources are scholarships and bursaries, government funding, non-government funding and voluntary parental contributions.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalTargetedResources,
+    ).toBe(0);
+    // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalFederalContribution,
+    ).toBe(0);
+    // Combination of provincial fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+    expect(
+      calculatedAssessment.variables.calculatedDataTotalProvincialContribution,
+    ).toBe(0);
+  });
+
+  it("Should calculate $0 fixed student contribution for a married independent student when both are exempt (income).", async () => {
+    // Arrange
+    const assessmentConsolidatedData =
+      createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+    assessmentConsolidatedData.studentDataHasDependents = YesNoOptions.No;
+    assessmentConsolidatedData.studentDataRelationshipStatus = "married";
+    assessmentConsolidatedData.studentDataPartnerHasEmploymentInsuranceBenefits =
+      YesNoOptions.No;
+    assessmentConsolidatedData.studentDataPartnerHasTotalIncomeAssistance =
+      YesNoOptions.No;
+    assessmentConsolidatedData.studentDataPartnerHasFedralProvincialPDReceipt =
+      YesNoOptions.No;
+    assessmentConsolidatedData.studentDataEstimatedSpouseIncome = 10000;
     assessmentConsolidatedData.studentDataYouthInCare = YesNoOptions.Yes;
 
     // Act
