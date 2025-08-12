cc5f6a8cb1f58b7f0a81b87997838b3145b6ff69
Carly Cotton <carly.cotton@gov.bc.ca>
2025-08-11 17:03:34 -0700

    More test cases

--- sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/contribution/fulltime-assessment-spouse-contribution.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2025-2026/fulltime-assessment/contribution/fulltime-assessment-spouse-contribution.e2e-spec.ts
@@ -190,6 +190,70 @@ describe(`E2E Test Workflow fulltime-assessment-${PROGRAM_YEAR}-spouse-contribut
 
   it(
     "Should calculate total fixed student contribution for a married independent student when the student is exempt " +
+      "and the partner is not exempt, is a full-time student (study weeks greater than offering), and has not already contributed.",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedFulltimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataHasDependents = YesNoOptions.No;
+      assessmentConsolidatedData.studentDataRelationshipStatus = "married";
+      assessmentConsolidatedData.studentDataPartnerHasEmploymentInsuranceBenefits =
+        YesNoOptions.No;
+      assessmentConsolidatedData.studentDataPartnerHasTotalIncomeAssistance =
+        YesNoOptions.No;
+      assessmentConsolidatedData.studentDataPartnerHasFedralProvincialPDReceipt =
+        YesNoOptions.No;
+      assessmentConsolidatedData.studentDataEstimatedSpouseIncome = 30000;
+      assessmentConsolidatedData.studentDataYouthInCare = YesNoOptions.Yes;
+      assessmentConsolidatedData.studentDataPartnerStudyWeeks = 20;
+
+      // Act
+      const calculatedAssessment =
+        await executeFullTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+      // Assert
+      // The spouse can be exempt from contribution if they are a full-time student at the same time.
+      // Or when they have a disability, or are receiving certain types of income assistance.
+      expect(
+        calculatedAssessment.variables.calculatedDataSpousalContributionExempt,
+      ).toBe(false);
+      // The federal and provincial fixed student contributions are based on family income and size when the student is not exempt.
+      expect(calculatedAssessment.variables.calculatedDataTotalFederalFSC).toBe(
+        0,
+      );
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
+      ).toBe(0);
+      // The spouse contribution weeks is the lower of remaining spouse contribution weeks and offering weeks (less any study overlap weeks).
+      expect(
+        calculatedAssessment.variables.calculatedDataSpouseContributionWeeks,
+      ).toBe(16);
+      // The total spouse contribution is calculated based on the spouse contribution weeks, spouse contribution rate, and family income.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalSpouseContribution,
+      ).toBe(769.9384615384616);
+      // Combination of federal fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalFederalContribution,
+      ).toBe(
+        calculatedAssessment.variables.calculatedDataTotalSpouseContribution +
+          calculatedAssessment.variables.calculatedDataTotalFederalFSC,
+      );
+      // Combination of provincial fixed student contribution, spouse contribution, parental contribution, and targeted resources.
+      expect(
+        calculatedAssessment.variables
+          .calculatedDataTotalProvincialContribution,
+      ).toBe(
+        calculatedAssessment.variables.calculatedDataTotalSpouseContribution +
+          calculatedAssessment.variables.calculatedDataTotalProvincialFSC,
+      );
+    },
+  );
+
+  it(
+    "Should calculate total fixed student contribution for a married independent student when the student is exempt " +
       "and the partner is not exempt, is not a full-time student, and has already contributed in the program year (20 weeks).",
     async () => {
       // Arrange
