8839b0ea03451fce9f07cbdb399ffe2fa5ddb732
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-08-14 17:45:27 -0700

    Added scenario to get previouslyApprovedOn

--- sources/packages/backend/apps/api/src/route-controllers/application-exception/_tests_/e2e/application-exception.aest.getExceptionById.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/application-exception/_tests_/e2e/application-exception.aest.getExceptionById.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { HttpStatus, INestApplication } from "@nestjs/common";
 import * as request from "supertest";
-import { DataSource } from "typeorm";
 import {
   AESTGroups,
   BEARER_AUTH_TYPE,
@@ -10,21 +9,27 @@ import {
 import { ApplicationExceptionStatus } from "@sims/sims-db";
 import { getUserFullName } from "../../../../utilities";
 import { saveFakeApplicationWithApplicationException } from "../application-exception-helper";
+import {
+  createE2EDataSources,
+  createFakeApplicationException,
+  createFakeApplicationExceptionRequest,
+  E2EDataSources,
+} from "@sims/test-utils";
 
 describe("ApplicationExceptionAESTController(e2e)-getExceptionById", () => {
   let app: INestApplication;
-  let appDataSource: DataSource;
+  let db: E2EDataSources;
 
   beforeAll(async () => {
     const { nestApplication, dataSource } = await createTestingAppModule();
     app = nestApplication;
-    appDataSource = dataSource;
+    db = createE2EDataSources(dataSource);
   });
 
   it("Should get an application exception by id when available.", async () => {
     // Arrange
     const application = await saveFakeApplicationWithApplicationException(
-      appDataSource,
+      db.dataSource,
       undefined,
       { applicationExceptionStatus: ApplicationExceptionStatus.Approved },
     );
@@ -52,6 +57,59 @@ describe("ApplicationExceptionAESTController(e2e)-getExceptionById", () => {
           {
             exceptionName: firstExceptionRequest.exceptionName,
             exceptionDescription: firstExceptionRequest.exceptionDescription,
+          },
+        ],
+      });
+  });
+
+  it("Should get a previously approved application exception by id when available.", async () => {
+    // Arrange
+    const application = await saveFakeApplicationWithApplicationException(
+      db.dataSource,
+      undefined,
+      { applicationExceptionStatus: ApplicationExceptionStatus.Approved },
+    );
+    const [firstExceptionRequest] =
+      application.applicationException.exceptionRequests;
+
+    const now = new Date();
+    const approvedException = createFakeApplicationException();
+    approvedException.exceptionStatus = ApplicationExceptionStatus.Approved;
+    approvedException.application = application;
+    approvedException.assessedDate = now;
+    approvedException.exceptionRequests = [
+      createFakeApplicationExceptionRequest({
+        applicationException: approvedException,
+      }),
+    ];
+    await db.applicationException.save(approvedException);
+    firstExceptionRequest.approvalExceptionRequest =
+      approvedException.exceptionRequests[0];
+    await db.applicationExceptionRequest.save(firstExceptionRequest);
+
+    const endpoint = `/aest/application-exception/${application.applicationException.id}`;
+    const token = await getAESTToken(AESTGroups.BusinessAdministrators);
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(token, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect({
+        exceptionStatus: application.applicationException.exceptionStatus,
+        submittedDate: application.applicationException.createdAt.toISOString(),
+        noteDescription:
+          application.applicationException.exceptionNote.description,
+        assessedByUserName: getUserFullName(
+          application.applicationException.assessedBy,
+        ),
+        assessedDate:
+          application.applicationException.assessedDate.toISOString(),
+        exceptionRequests: [
+          {
+            exceptionName: firstExceptionRequest.exceptionName,
+            exceptionDescription: firstExceptionRequest.exceptionDescription,
+            previouslyApprovedOn: now.toISOString(),
           },
         ],
       });
