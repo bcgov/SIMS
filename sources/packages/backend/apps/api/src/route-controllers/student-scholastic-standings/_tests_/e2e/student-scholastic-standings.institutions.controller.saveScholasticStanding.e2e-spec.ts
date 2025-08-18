3267174d4c614372027fd7ab5b9828f6d1e2eff8
Shashank Shekhar <sh16011993@gmail.com>
2025-08-18 16:29:04 -0700

    Code Update: Review Comment

--- sources/packages/backend/apps/api/src/route-controllers/student-scholastic-standings/_tests_/e2e/student-scholastic-standings.institutions.controller.saveScholasticStanding.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/student-scholastic-standings/_tests_/e2e/student-scholastic-standings.institutions.controller.saveScholasticStanding.e2e-spec.ts
@@ -1036,60 +1036,64 @@ describe("StudentScholasticStandingsInstitutionsController(e2e)-saveScholasticSt
       ]);
     });
 
-    it(`Should create an ${RestrictionCode.SSR} restriction when the student equals or exceeds ${SCHOLASTIC_STANDING_MINIMUM_UNSUCCESSFUL_WEEKS} weeks.`, async () => {
-      // Arrange
-      const unsuccessfulWeeks =
-        SCHOLASTIC_STANDING_MINIMUM_UNSUCCESSFUL_WEEKS - 1;
-      const student = await saveFakeStudent(db.dataSource);
-      const legacyProfile = createFakeSFASIndividual({
-        initialValues: {
-          student,
-          unsuccessfulCompletion: unsuccessfulWeeks,
-        },
-      });
-      await db.sfasIndividual.save(legacyProfile);
-      mockFormioDryRun({
-        studentScholasticStandingChangeType:
-          StudentScholasticStandingChangeType.StudentDidNotCompleteProgram,
-        numberOfUnsuccessfulWeeks: 1,
-      });
-      // Create a fake application with the same student.
-      const application = await saveFakeApplication(
-        db.dataSource,
-        {
-          institutionLocation: collegeFLocation,
-          student,
-        },
-        {
-          offeringIntensity: OfferingIntensity.fullTime,
-          applicationStatus: ApplicationStatus.Completed,
-        },
-      );
-      // Institution token.
-      const institutionUserToken = await getInstitutionToken(
-        InstitutionTokenTypes.CollegeFUser,
-      );
-      const endpoint = `/institutions/scholastic-standing/location/${collegeFLocation.id}/application/${application.id}`;
-
-      // Act/Assert
-      await request(app.getHttpServer())
-        .post(endpoint)
-        .send(payload)
-        .auth(institutionUserToken, BEARER_AUTH_TYPE)
-        .expect(HttpStatus.CREATED);
-      const studentRestrictions = await getActiveStudentRestrictions(
-        application.student.id,
-      );
-      expect(studentRestrictions).toEqual([
-        {
-          id: expect.any(Number),
-          restriction: {
-            id: ssrRestriction.id,
-            restrictionCode: RestrictionCode.SSR,
+    it(
+      `Should create an ${RestrictionCode.SSR} restriction when the student equals or exceeds ${SCHOLASTIC_STANDING_MINIMUM_UNSUCCESSFUL_WEEKS} weeks ` +
+        " as a combination of unsuccessful weeks from legacy and SIMS.",
+      async () => {
+        // Arrange
+        const legacyUnsuccessfulWeeks =
+          SCHOLASTIC_STANDING_MINIMUM_UNSUCCESSFUL_WEEKS - 1;
+        const student = await saveFakeStudent(db.dataSource);
+        const legacyProfile = createFakeSFASIndividual({
+          initialValues: {
+            student,
+            unsuccessfulCompletion: legacyUnsuccessfulWeeks,
           },
-        },
-      ]);
-    });
+        });
+        await db.sfasIndividual.save(legacyProfile);
+        mockFormioDryRun({
+          studentScholasticStandingChangeType:
+            StudentScholasticStandingChangeType.StudentDidNotCompleteProgram,
+          numberOfUnsuccessfulWeeks: 1,
+        });
+        // Create a fake application with the same student.
+        const application = await saveFakeApplication(
+          db.dataSource,
+          {
+            institutionLocation: collegeFLocation,
+            student,
+          },
+          {
+            offeringIntensity: OfferingIntensity.fullTime,
+            applicationStatus: ApplicationStatus.Completed,
+          },
+        );
+        // Institution token.
+        const institutionUserToken = await getInstitutionToken(
+          InstitutionTokenTypes.CollegeFUser,
+        );
+        const endpoint = `/institutions/scholastic-standing/location/${collegeFLocation.id}/application/${application.id}`;
+
+        // Act/Assert
+        await request(app.getHttpServer())
+          .post(endpoint)
+          .send(payload)
+          .auth(institutionUserToken, BEARER_AUTH_TYPE)
+          .expect(HttpStatus.CREATED);
+        const studentRestrictions = await getActiveStudentRestrictions(
+          application.student.id,
+        );
+        expect(studentRestrictions).toEqual([
+          {
+            id: expect.any(Number),
+            restriction: {
+              id: ssrRestriction.id,
+              restrictionCode: RestrictionCode.SSR,
+            },
+          },
+        ]);
+      },
+    );
 
     it(`Should create an ${RestrictionCode.SSRN} restriction when the student already has an ${RestrictionCode.SSR}.`, async () => {
       // Arrange
