1d41fabf16341b73f58a67cd21d573323c31e4a0
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-08-20 07:06:05 -0600

    update E2E tests

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/institution-integration/ier12-integration/_tests_/e2e/models/data-inputs/programs-inputs.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/institution-integration/ier12-integration/_tests_/e2e/models/data-inputs/programs-inputs.ts
@@ -2,8 +2,7 @@ import { IER12Program } from "./data-inputs.models";
 
 export const PROGRAM_UNDERGRADUATE_CERTIFICATE_WITHOUT_INSTITUTION_PROGRAM_CODE: IER12Program =
   {
-    name: "Program",
-    description: "Program description",
+    name: "Program name",
     credentialType: "undergraduateCertificate",
     fieldOfStudyCode: 1,
     cipCode: "12.1234",
@@ -15,10 +14,7 @@ export const PROGRAM_UNDERGRADUATE_CERTIFICATE_WITHOUT_INSTITUTION_PROGRAM_CODE:
 
 export const PROGRAM_GRADUATE_DIPLOMA_WITH_INSTITUTION_PROGRAM_CODE: IER12Program =
   {
-    name: "Some Program With Description Too Long",
-    // Added new line character expecting to be replaced during
-    // file processing.
-    description: "Some program with\n description too long",
+    name: "Program with long name to ensure the program name space of 75 characters is used",
     credentialType: "graduateDiploma",
     fieldOfStudyCode: 1,
     cipCode: "12.1234",
