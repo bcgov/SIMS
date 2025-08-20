069243c71c82f3b37dfcf63ad2ae193f8483e11b
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-08-20 10:35:15 -0600

    remove replace line breaks

--- sources/packages/backend/libs/utilities/src/string-utils.ts
+++ sources/packages/backend/libs/utilities/src/string-utils.ts
@@ -1,24 +1,5 @@
 import { FILE_DEFAULT_ENCODING, UTF8_BYTE_ORDER_MARK } from "@sims/utilities";
 
-const REPLACE_LINE_BREAK_REGEX = /\r?\n|\r/g;
-
-/**
- * Replace the line breaks in the given text.
- * @param data data to replace the line break.
- * @param options replace options:
- * - `replaceText`: text to replace the line break.
- * @returns line break replaced string.
- */
-export function replaceLineBreaks(
-  data?: string,
-  options?: { replaceText?: string },
-): string {
-  if (!data) {
-    return data;
-  }
-  return data.replace(REPLACE_LINE_BREAK_REGEX, options?.replaceText ?? "");
-}
-
 /**
  * List of characters to be translated before the content is converted to an
  * ASCII buffer. These characters do not have an equivalent character in ISO8859-1
