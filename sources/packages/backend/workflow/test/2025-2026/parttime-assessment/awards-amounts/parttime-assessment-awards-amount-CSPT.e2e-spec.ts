9c09abffca9d85a29c3f6bab6995f304eedeee1c
Carly Cotton <128620426+CarlyCotton@users.noreply.github.com>
2025-09-15 14:57:29 -0700

    #5162 - BCAG and CSPT PY Limit (PT 25/26) (#5171)
    ### Change Summary
    Previously the program year limit was based on the maximum program year
    award amount ($1000 or $2520 respectively). Now the maximum program year
    limit for each award is based on their income-calculated amount
    (`provincialAwardBCAGAmount` or `federalAwardCSPTAmount`.)
    
    The dmn program year limit value for both CSPT and BCAG was replaced in
    the `limitAward` values by the income-based `federalAwardCSPT` /
    `federalAwardBCAG` amounts.
    <img width="818" height="231" alt="image"
    src="https://github.com/user-attachments/assets/aa5cb1e3-e2d0-446a-8308-6e894eb39783"
    />
    
    ### Test Cases
    Modified a few tests just to include formatting (like a period at the
    end of the description)
    
    **BCAG Tests**
    ✓ Should determine Net BCAG amount as remaining need when
    awardEligibilityBCAG is true and remaining need is less than remaining
    BCAG but higher than the minimum award amount ($100).
    ✓ Should determine Net BCAG amount as remaining need when
    awardEligibilityBCAG is true and remaining BCAG is less than remaining
    need but higher than the minimum award amount ($100).
    _Updated: ._
    
    **CSPT Tests**
    ✓ Should determine Net CSPT amount as the lower of remaining CSPT and
    remaining need when awardEligibilityCSPT is true, both are over the
    minimum award amount ($100) and student has max award amount based on
    income.
    ✓ Should determine Net CSPT amount as remaining need when
    awardEligibilityCSPT is true and remaining need is less than remaining
    CSPT but higher than the minimum award amount ($100).
    _Updated: Should determine federalAwardNetCSPTAmount as zero when
    awardEligibilityCSPT is true and difference between the
    programYearLimits and CSPT awarded in the program year previously is
    less than 100._

--- sources/packages/backend/workflow/test/2025-2026/parttime-assessment/awards-amounts/parttime-assessment-awards-amount-CSPT.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2025-2026/parttime-assessment/awards-amounts/parttime-assessment-awards-amount-CSPT.e2e-spec.ts
@@ -6,7 +6,7 @@ import {
 } from "../../../test-utils";
 
 describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CSPT.`, () => {
-  it("Should determine federalAwardCSPTAmount when awardEligibilityCSPT is true and calculatedDataTotalFamilyIncome <= limitAwardCSPTIncomeCap", async () => {
+  it("Should determine federalAwardCSPTAmount when awardEligibilityCSPT is true and calculatedDataTotalFamilyIncome <= limitAwardCSPTIncomeCap.", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -36,7 +36,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CS
     expect(calculatedAssessment.variables.federalAwardCSPTAmount).toBe(2520);
   });
 
-  it("Should determine federalAwardCSPTAmount when awardEligibilityCSPT is true and calculatedDataTotalFamilyIncome > limitAwardCSPTIncomeCap", async () => {
+  it("Should determine federalAwardCSPTAmount when awardEligibilityCSPT is true and calculatedDataTotalFamilyIncome > limitAwardCSPTIncomeCap.", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -64,7 +64,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CS
     );
   });
 
-  it("Should determine federalAwardNetCSPTAmount when awardEligibilityCSPT is true", async () => {
+  it("Should determine federalAwardNetCSPTAmount when awardEligibilityCSPT is true.", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -101,7 +101,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CS
     expect(calculatedAssessment.variables.federalAwardNetCSPTAmount).toBe(0);
   });
 
-  it("Should determine federalAwardNetCSPTAmount when awardEligibilityCSPT is true and no CSPT awarded in the program year previously", async () => {
+  it("Should determine federalAwardNetCSPTAmount when awardEligibilityCSPT is true and no CSPT awarded in the program year previously.", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -123,7 +123,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CS
     expect(calculatedAssessment.variables.federalAwardNetCSPTAmount).toBe(2520);
   });
 
-  it("Should determine federalAwardNetCSPTAmount as zero when awardEligibilityCSPT is true and difference between the programYearLimits and CSPT awarded in the program year previously is less than 100", async () => {
+  it("Should determine federalAwardNetCSPTAmount as zero when awardEligibilityCSPT is true and difference between the programYearLimits and CSPT awarded in the program year previously is less than 100.", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -131,7 +131,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CS
     assessmentConsolidatedData.studentDataRelationshipStatus = "married";
     assessmentConsolidatedData.studentDataIsYourPartnerAbleToReport = true;
     assessmentConsolidatedData.partner1CRAReportedIncome = 22999;
-    assessmentConsolidatedData.programYearTotalPartTimeCSPT = 2421;
+    assessmentConsolidatedData.programYearTotalPartTimeCSPT = 795;
     // Act
     const calculatedAssessment = await executePartTimeAssessmentForProgramYear(
       PROGRAM_YEAR,
@@ -139,12 +139,82 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-CS
     );
     // Assert
     expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(true);
-    expect(calculatedAssessment.variables.limitAwardCSPTRemaining).toBe(99);
-    expect(
-      calculatedAssessment.variables.federalAwardCSPTAmount,
-    ).toBeGreaterThan(100);
+    expect(calculatedAssessment.variables.federalAwardCSPTAmount).toBe(
+      804.617856,
+    );
+    // Award limit remaining is $804.617856 - $795 = $9.617856 which is less than $100.
+    expect(calculatedAssessment.variables.limitAwardCSPTRemaining).toBeLessThan(
+      100,
+    );
     expect(calculatedAssessment.variables.federalAwardNetCSPTAmount).toBe(0);
   });
+
+  it(
+    "Should determine Net CSPT amount as the lower of remaining CSPT and remaining need when awardEligibilityCSPT is true, " +
+      "both are over the minimum award amount ($100) and student has max award amount based on income.",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataCRAReportedIncome = 37700;
+      assessmentConsolidatedData.programYearTotalPartTimeCSPT = 1000;
+      // Act
+      const calculatedAssessment =
+        await executePartTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+      // Assert
+      expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(true);
+      expect(calculatedAssessment.variables.federalAwardCSPTAmount).toBe(2520);
+      // Award limit remaining is $2520 - $1,000 = $1,520
+      expect(calculatedAssessment.variables.limitAwardCSPTRemaining).toBe(1520);
+      // When remaining award is less than remaining need, remaining award is used as net.
+      expect(
+        calculatedAssessment.variables.limitAwardCSPTRemaining,
+      ).toBeLessThan(
+        calculatedAssessment.variables.calculatedDataTotalRemainingNeed1,
+      );
+      expect(calculatedAssessment.variables.federalAwardNetCSPTAmount).toBe(
+        calculatedAssessment.variables.limitAwardCSPTRemaining,
+      );
+    },
+  );
+
+  it(
+    "Should determine Net CSPT amount as remaining need when awardEligibilityCSPT is true " +
+      "and remaining need is less than remaining CSPT but higher than the minimum award amount ($100).",
+    async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
+      assessmentConsolidatedData.offeringActualTuitionCosts = 500;
+      assessmentConsolidatedData.offeringMandatoryFees = 100;
+      assessmentConsolidatedData.offeringProgramRelatedCosts = 200;
+      assessmentConsolidatedData.studentDataCRAReportedIncome = 36810;
+      assessmentConsolidatedData.programYearTotalPartTimeCSPT = 150;
+      assessmentConsolidatedData.offeringCourseLoad = 30;
+      // Act
+      const calculatedAssessment =
+        await executePartTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
+      // Assert
+      expect(calculatedAssessment.variables.awardEligibilityCSPT).toBe(true);
+      expect(calculatedAssessment.variables.federalAwardCSPTAmount).toBe(2520);
+      // Award limit remaining is $2520 - $150 = $2370.
+      expect(calculatedAssessment.variables.limitAwardCSPTRemaining).toBe(2370);
+      // When remaining need is less than remaining award, remaining need is used as net.
+      expect(
+        calculatedAssessment.variables.calculatedDataTotalRemainingNeed1,
+      ).toBeLessThan(calculatedAssessment.variables.limitAwardCSPTRemaining);
+      // Need should be tuition ($500) + fees ($100) + program related costs ($200) + transportation ($13 x 15 weeks) + miscellaneous ($10 x 15 weeks)
+      expect(calculatedAssessment.variables.federalAwardNetCSPTAmount).toBe(
+        1145,
+      );
+    },
+  );
 
   afterAll(async () => {
     // Closes the singleton instance created during test executions.
