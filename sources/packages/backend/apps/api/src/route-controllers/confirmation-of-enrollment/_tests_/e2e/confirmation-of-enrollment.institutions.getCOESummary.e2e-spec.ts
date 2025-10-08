3dbbea7ae5a925b9643fda8f3997e57c282e4a1e
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-10-08 10:50:09 -0700

    Adjusted E2E description

--- sources/packages/backend/apps/api/src/route-controllers/confirmation-of-enrollment/_tests_/e2e/confirmation-of-enrollment.institutions.getCOESummary.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/confirmation-of-enrollment/_tests_/e2e/confirmation-of-enrollment.institutions.getCOESummary.e2e-spec.ts
@@ -573,7 +573,9 @@ describe("ConfirmationOfEnrollmentInstitutionsController(e2e)-getCOESummary", ()
 
   it(
     "Should get the COE current summary only for applications with offering end date till today" +
-      " when there is one COE for an offering with end date as today, and one COE with the offering end date in the past.",
+      " when there is one COE for an offering with end date as today, " +
+      " one COE with the offering end date as tomorrow, " +
+      " and one COE with the offering end date in the past.",
     async () => {
       // Arrange
       const today = getISODateOnlyString(new Date());
