4549f6b36a4ba1a12e37ee9a99c99476727e3fd6
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-10-16 14:27:53 -0700

    Fixes for history table

--- sources/packages/backend/apps/db-migrations/src/sql/Student/Add-cols-modified-independent.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Student/Add-cols-modified-independent.sql
@@ -1,10 +1,22 @@
 ALTER TABLE
   sims.students
 ADD
-  COLUMN modified_independent_status sims.modified_independent_status,
+  COLUMN modified_independent_status sims.modified_independent_status DEFAULT NULL,
 ADD
-  COLUMN modified_independent_appeal_request_id INT REFERENCES sims.student_appeal_requests(id);
+  COLUMN modified_independent_appeal_request_id INT REFERENCES sims.student_appeal_requests(id) DEFAULT NULL;
 
 COMMENT ON COLUMN sims.students.modified_independent_status IS 'Status of the modified independent associated to the student.';
 
 COMMENT ON COLUMN sims.students.modified_independent_appeal_request_id IS 'Reference to the appeal request ID if the student has requested an appeal for the modified independent status.';
+
+-- History table new columns.
+ALTER TABLE
+  sims.students_history
+ADD
+  COLUMN modified_independent_status sims.modified_independent_status,
+ADD
+  COLUMN modified_independent_appeal_request_id INT;
+
+COMMENT ON COLUMN sims.students_history.modified_independent_status IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.students_history.modified_independent_appeal_request_id IS 'Historical data from the original table. See original table comments for details.';
--- sources/packages/backend/apps/db-migrations/src/sql/Student/Rollback-add-cols-modified-independent.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Student/Rollback-add-cols-modified-independent.sql
@@ -1,3 +1,7 @@
 ALTER TABLE
+  sims.students_history DROP COLUMN modified_independent_status,
+  DROP COLUMN modified_independent_appeal_request_id;
+
+ALTER TABLE
   sims.students DROP COLUMN modified_independent_status,
   DROP COLUMN modified_independent_appeal_request_id;
