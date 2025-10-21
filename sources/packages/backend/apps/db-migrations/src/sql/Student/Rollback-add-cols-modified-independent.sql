466429a6d51e9c32b55cc1de7b8aa8f6ba3fdd1a
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2025-10-21 12:52:31 -0600

    updating migrations

--- sources/packages/backend/apps/db-migrations/src/sql/Student/Rollback-add-cols-modified-independent.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Student/Rollback-add-cols-modified-independent.sql
@@ -1,7 +1,11 @@
 ALTER TABLE
   sims.students_history DROP COLUMN modified_independent_status,
-  DROP COLUMN modified_independent_appeal_request_id;
+  DROP COLUMN modified_independent_appeal_request_id,
+  DROP COLUMN modified_independent_status_updated_by,
+  DROP COLUMN modified_independent_status_updated_on;
 
 ALTER TABLE
   sims.students DROP COLUMN modified_independent_status,
-  DROP COLUMN modified_independent_appeal_request_id;
+  DROP COLUMN modified_independent_appeal_request_id,
+  DROP COLUMN modified_independent_status_updated_by,
+  DROP COLUMN modified_independent_status_updated_on;
