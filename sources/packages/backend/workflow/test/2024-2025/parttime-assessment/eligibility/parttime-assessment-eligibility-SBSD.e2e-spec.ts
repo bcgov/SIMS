3c199639484fa504cdc5c724e9ddf4240318a733
ninosamson <33761012+ninosamson@users.noreply.github.com>
2025-11-18 12:21:33 -0800

    Branch Sync-up: Cherry Pick Hotfix 2.16.1 to Main  (#5382)
    Cherry pick to bring hotfix from 2.16.1 to main
    
    Co-authored-by: Dheepak Ramanathan <54600590+dheepak-aot@users.noreply.github.com>

--- sources/packages/backend/workflow/test/2024-2025/parttime-assessment/eligibility/parttime-assessment-eligibility-SBSD.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2024-2025/parttime-assessment/eligibility/parttime-assessment-eligibility-SBSD.e2e-spec.ts
@@ -4,25 +4,14 @@ import {
   createFakeConsolidatedPartTimeData,
   executePartTimeAssessmentForProgramYear,
 } from "../../../test-utils";
-import { InstitutionTypes } from "../../../models";
 
 describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-SBSD.`, () => {
-  // Expected and not expected institution types.
-  const EXPECTED_INSTITUTION_TYPES = [
-    InstitutionTypes.BCPublic,
-    InstitutionTypes.BCPrivate,
-  ];
-  const NOT_EXPECTED_INSTITUTION_TYPES = Object.values(InstitutionTypes).filter(
-    (type) => !EXPECTED_INSTITUTION_TYPES.includes(type),
-  );
 
-  describe("Should determine SBSD as eligible when total assessment need is greater than or equal to 1, application PD/PPD status is 'yes' and", () => {
-    for (const institutionType of EXPECTED_INSTITUTION_TYPES) {
-      it(`institutionType is ${institutionType}`, async () => {
+  it("Should determine SBSD as eligible when total assessment need is greater than or equal to 1 and application PD/PPD status is 'yes'" 
+    + " and total family income is within the threshold.", async () => {
         // Arrange
         const assessmentConsolidatedData =
           createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-        assessmentConsolidatedData.institutionType = institutionType;
         assessmentConsolidatedData.studentDataApplicationPDPPDStatus = "yes";
 
         // Act
@@ -38,16 +27,11 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-SBSD
           calculatedAssessment.variables.finalProvincialAwardNetSBSDAmount,
         ).toBeGreaterThan(0);
       });
-    }
-  });
 
-  describe("Should determine SBSD as not eligible when total family income is greater than the threshold and", () => {
-    for (const institutionType of EXPECTED_INSTITUTION_TYPES) {
-      it(`institutionType is ${institutionType}`, async () => {
+  it("Should determine SBSD as not eligible when total family income is greater than the threshold.", async () => {
         // Arrange
         const assessmentConsolidatedData =
           createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-        assessmentConsolidatedData.institutionType = institutionType;
         assessmentConsolidatedData.studentDataApplicationPDPPDStatus = "yes";
         assessmentConsolidatedData.studentDataCRAReportedIncome = 75479;
 
@@ -64,32 +48,6 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-SBSD
           calculatedAssessment.variables.finalProvincialAwardNetSBSDAmount,
         ).toBe(0);
       });
-    }
-  });
-
-  describe("Should determine SBSD as not eligible when", () => {
-    for (const institutionType of NOT_EXPECTED_INSTITUTION_TYPES) {
-      it(`institutionType is ${institutionType}`, async () => {
-        // Arrange
-        const assessmentConsolidatedData =
-          createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-        assessmentConsolidatedData.institutionType = institutionType;
-
-        // Act
-        const calculatedAssessment =
-          await executePartTimeAssessmentForProgramYear(
-            PROGRAM_YEAR,
-            assessmentConsolidatedData,
-          );
-
-        // Assert
-        expect(calculatedAssessment.variables.awardEligibilitySBSD).toBe(false);
-        expect(
-          calculatedAssessment.variables.finalProvincialAwardNetSBSDAmount,
-        ).toBe(0);
-      });
-    }
-  });
 
   it("Should determine SBSD as not eligible when application PD/PPD status is 'no'.", async () => {
     // Arrange
