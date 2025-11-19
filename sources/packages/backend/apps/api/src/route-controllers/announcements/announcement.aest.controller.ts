e6a1a7bc974c2e0e17b2d72769bf6bfb0fc89df5
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-11-19 12:57:46 -0800

    Testing fake controller with wrong patterns

---
+++ sources/packages/backend/apps/api/src/route-controllers/announcements/announcement.aest.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Query } from "@nestjs/common";
+import BaseController from "../BaseController";
+import { AnnouncementService } from "../../services";
+import {
+  AnnouncementsAPIOutDTO,
+  StudentAnnouncementsAPIInDTO,
+} from "./models/announcement.dto";
+import { AllowAuthorizedParty } from "../../auth/decorators";
+import { AuthorizedParties } from "../../auth";
+
+@AllowAuthorizedParty(AuthorizedParties.aest)
+@Controller("announcements")
+export class AnnouncementController extends BaseController {
+  constructor(private readonly announcementService: AnnouncementService) {
+    super();
+  }
+
+  /**
+   * Get system announcements
+   * @param systemAnnouncementOptions target for the announcement
+   * @returns sistem announcement list
+   */
+  @Get()
+  async getAnnouncements(
+    @Query() systemAnnouncementOptions: StudentAnnouncementsAPIInDTO,
+  ): Promise<AnnouncementsAPIOutDTO> {
+    const announcements = await this.announcementService.getAnnouncements(
+      systemAnnouncementOptions.target,
+    );
+    const announcementsResponse = announcements.map((announcement) => ({
+      messageTitle: announcement.messageTitle,
+      message: announcement.message,
+      startDate: announcement.startDate,
+      endDate: announcement.endDate,
+      target: announcement.target,
+    }));
+
+    return { announcements: announcementsResponse };
+  }
+}
