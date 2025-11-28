da0cae621c73d79b7d7ffe15e2877ab43d276b93
Tiago Graf <tiago.graf@quartech.com>
2025-11-28 14:28:56 -0800

    Update allow-during-maintenance-mode.decorator.ts

--- sources/packages/backend/apps/api/src/auth/decorators/allow-during-maintenance-mode.decorator.ts
+++ sources/packages/backend/apps/api/src/auth/decorators/allow-during-maintenance-mode.decorator.ts
@@ -1,5 +1,5 @@
-import { SetMetadata } from "@nestjs/common";
+import { CustomDecorator, SetMetadata } from "@nestjs/common";
 
 export const ALLOW_DURING_MAINTENANCE_MODE_KEY = "allowDuringMaintenanceMode";
-export const AllowDuringMaintenanceMode = (): MethodDecorator =>
+export const AllowDuringMaintenanceMode = (): CustomDecorator =>
   SetMetadata(ALLOW_DURING_MAINTENANCE_MODE_KEY, true);
