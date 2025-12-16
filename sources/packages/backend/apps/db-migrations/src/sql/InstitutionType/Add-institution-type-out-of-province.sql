12dd38f4d7aff57a5776b3f787b9733afddfb1d5
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-12-16 09:25:47 -0800

    Added schema

--- sources/packages/backend/apps/db-migrations/src/sql/InstitutionType/Add-institution-type-out-of-province.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/InstitutionType/Add-institution-type-out-of-province.sql
@@ -1,12 +1,12 @@
 -- Add "Out of Province Public" institution type
 INSERT INTO
-    institution_type(id, name)
+    sims.institution_type(id, name)
 VALUES
     (7, 'Out of Province Private');
 
 -- Update existing "Out of Province" institution type to "Out of Province Public"
 UPDATE
-    institution_type
+    sims.institution_type
 SET
     NAME = 'Out of Province Public'
 WHERE
