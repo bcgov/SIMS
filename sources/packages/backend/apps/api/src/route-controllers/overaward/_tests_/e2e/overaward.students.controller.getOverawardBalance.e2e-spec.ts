aeb9e0d6963b695413e9e78e24ff4bec84fd546f
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-12-22 15:35:14 -0800

    Add one E2E

--- sources/packages/backend/apps/api/src/route-controllers/overaward/_tests_/e2e/overaward.students.controller.getOverawardBalance.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/overaward/_tests_/e2e/overaward.students.controller.getOverawardBalance.e2e-spec.ts
@@ -14,7 +14,8 @@ import {
   createTestingAppModule,
   FakeStudentUsersTypes,
   getStudentToken,
-  mockUserLoginInfo,
+  mockJWTUserInfo,
+  resetMockJWTUserInfo,
 } from "../../../../testHelpers";
 import { TestingModule } from "@nestjs/testing";
 
@@ -33,12 +34,16 @@ describe("OverawardStudentsController(e2e)-getOverawardBalance", () => {
     disbursementOverawardRepo = dataSource.getRepository(DisbursementOveraward);
   });
 
+  beforeEach(() => {
+    resetMockJWTUserInfo(appModule);
+  });
+
   it("Should return correct value for overaward balance when available.", async () => {
     // Arrange
     const student = await saveFakeStudent(appDataSource);
 
-    // Mock user service to return the saved student.
-    await mockUserLoginInfo(appModule, student);
+    // Mock student user token.
+    await mockJWTUserInfo(appModule, student.user);
 
     // Get any student user token.
     const studentToken = await getStudentToken(
@@ -80,6 +85,47 @@ describe("OverawardStudentsController(e2e)-getOverawardBalance", () => {
       .auth(studentToken, BEARER_AUTH_TYPE)
       .expect(HttpStatus.OK)
       .expect({ overawardBalanceValues: { CSLF: 400, CSLP: 99 } });
+  });
+
+  it("Should not return a balance when student has a $0 dollar balance.", async () => {
+    // Arrange
+    const student = await saveFakeStudent(appDataSource);
+
+    // Mock student user token.
+    await mockJWTUserInfo(appModule, student.user);
+
+    // Get any student user token.
+    const studentToken = await getStudentToken(
+      FakeStudentUsersTypes.FakeStudentUserType1,
+    );
+
+    // Create an overaward.
+    const legacyOverawardCSLF = createFakeDisbursementOveraward({ student });
+    legacyOverawardCSLF.disbursementValueCode = "CSLF";
+    legacyOverawardCSLF.overawardValue = 100;
+    legacyOverawardCSLF.originType =
+      DisbursementOverawardOriginType.LegacyOveraward;
+
+    // Create a manual overaward deduction.
+    const manualOverawardCSLF = createFakeDisbursementOveraward({ student });
+    manualOverawardCSLF.disbursementValueCode = "CSLF";
+    manualOverawardCSLF.overawardValue = -100;
+    manualOverawardCSLF.originType =
+      DisbursementOverawardOriginType.ManualRecord;
+
+    // Persist the overawards.
+    await disbursementOverawardRepo.save([
+      legacyOverawardCSLF,
+      manualOverawardCSLF,
+    ]);
+    const endpoint = "/students/overaward/balance";
+
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(studentToken, BEARER_AUTH_TYPE)
+      .expect(HttpStatus.OK)
+      .expect({});
   });
 
   afterAll(async () => {
