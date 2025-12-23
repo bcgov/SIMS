851bc9e86580891e5891f54279aa7b30be30c8b5
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2025-12-23 13:49:51 -0800

    E2E tests

--- sources/packages/backend/apps/api/src/route-controllers/confirmation-of-enrollment/_tests_/e2e/confirmation-of-enrollment.institutions.getCOEDeniedReason.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/confirmation-of-enrollment/_tests_/e2e/confirmation-of-enrollment.institutions.getCOEDeniedReason.e2e-spec.ts
@@ -7,6 +7,35 @@ import {
   InstitutionTokenTypes,
 } from "../../../../testHelpers";
 
+const COE_COMMON_DENIAL_REASONS = [
+  {
+    value: 6,
+    label:
+      "Our records indicate you are not registered, please re-submit the application after registration",
+  },
+  {
+    value: 5,
+    label:
+      "Programs you are registered in is not eligible for Student Aid BC funding, please contact Financial Aid Office for more information",
+  },
+  {
+    value: 4,
+    label:
+      "Study period dates are incorrect, please contact Financial Aid Office for more information",
+  },
+  {
+    value: 3,
+    label:
+      "Study period dates selected are incorrect, please submit an application for each semester individually",
+  },
+  {
+    value: 2,
+    label:
+      "School is unable to confirm student identity, please confirm your student number or contact Financial Student Aid Office for more information",
+  },
+  { value: 1, label: "Other" },
+];
+
 describe("ConfirmationOfEnrollmentInstitutionsController(e2e)-getCOEDeniedReason", () => {
   let app: INestApplication;
 
@@ -15,10 +44,10 @@ describe("ConfirmationOfEnrollmentInstitutionsController(e2e)-getCOEDeniedReason
     app = nestApplication;
   });
 
-  it("Should get COE denial reasons static list.", async () => {
+  it("Should get COE denial reasons list when part-time intensity is provided.", async () => {
     // Arrange
     const endpoint =
-      "/institutions/location/confirmation-of-enrollment/denial-reasons";
+      "/institutions/location/confirmation-of-enrollment/denial-reasons?offeringIntensity=Part Time";
     // Act/Assert
     await request(app.getHttpServer())
       .get(endpoint)
@@ -30,31 +59,35 @@ describe("ConfirmationOfEnrollmentInstitutionsController(e2e)-getCOEDeniedReason
       .expect((response) => {
         expect(response.body).toStrictEqual([
           {
-            value: 6,
+            value: 7,
             label:
-              "Our records indicate you are not registered, please re-submit the application after registration",
+              "You are currently enrolled as a full-time student. Please cancel your part-time application and reapply using the full-time application process",
           },
+          ...COE_COMMON_DENIAL_REASONS,
+        ]);
+      });
+  });
+
+  it("Should get COE denial reasons list when full-time intensity is provided.", async () => {
+    // Arrange
+    const endpoint =
+      "/institutions/location/confirmation-of-enrollment/denial-reasons?offeringIntensity=Full Time";
+    // Act/Assert
+    await request(app.getHttpServer())
+      .get(endpoint)
+      .auth(
+        await getInstitutionToken(InstitutionTokenTypes.CollegeCUser),
+        BEARER_AUTH_TYPE,
+      )
+      .expect(HttpStatus.OK)
+      .expect((response) => {
+        expect(response.body).toStrictEqual([
           {
-            value: 5,
+            value: 8,
             label:
-              "Programs you are registered in is not eligible for Student Aid BC funding, please contact Financial Aid Office for more information",
+              "You are currently enrolled as a part-time student. Please cancel your full-time application and reapply using the part-time application process",
           },
-          {
-            value: 4,
-            label:
-              "Study period dates are incorrect, please contact Financial Aid Office for more information",
-          },
-          {
-            value: 3,
-            label:
-              "Study period dates selected are incorrect, please submit an application for each semester individually",
-          },
-          {
-            value: 2,
-            label:
-              "School is unable to confirm student identity, please confirm your student number or contact Financial Student Aid Office for more information",
-          },
-          { value: 1, label: "Other" },
+          ...COE_COMMON_DENIAL_REASONS,
         ]);
       });
   });
