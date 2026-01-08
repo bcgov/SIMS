0ef9ed0f902c74461278de12117aa34568c92768
Dheepak Ramanathan <54600590+dheepak-aot@users.noreply.github.com>
2026-01-08 16:30:12 -0700

    #5586 - Update COE auto approval to assign the document number (#5593)
    Update COE auto approval to assign the document number

--- sources/packages/backend/libs/integrations/src/esdc-integration/e-cert-cancellation-response/e-cert-cancellation-response-integration.module.ts
+++ sources/packages/backend/libs/integrations/src/esdc-integration/e-cert-cancellation-response/e-cert-cancellation-response-integration.module.ts
@@ -3,12 +3,20 @@ import { ConfigModule } from "@sims/utilities/config";
 import { SshService } from "@sims/integrations/services";
 import { ECertCancellationResponseIntegrationService } from "./e-cert-cancellation-response.integration.service";
 import { ECertCancellationResponseProcessingService } from "./e-cert-cancellation-response.processing.service";
-import { DisbursementScheduleSharedService } from "@sims/services";
+import {
+  AssessmentSequentialProcessingService,
+  ConfirmationOfEnrollmentService,
+  DisbursementScheduleSharedService,
+  SequenceControlService,
+} from "@sims/services";
 
 @Module({
   imports: [ConfigModule],
   providers: [
     SshService,
+    AssessmentSequentialProcessingService,
+    SequenceControlService,
+    ConfirmationOfEnrollmentService,
     DisbursementScheduleSharedService,
     ECertCancellationResponseIntegrationService,
     ECertCancellationResponseProcessingService,
