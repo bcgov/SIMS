86a143bc7a8146cb254cc798ef3222521a2dfc05
Tiago Graf <tiago.graf@quartech.com>
2026-01-22 16:29:06 -0800

    Fix tests

--- sources/packages/backend/apps/api/src/route-controllers/assessment/assessment.controller.service.ts
+++ sources/packages/backend/apps/api/src/route-controllers/assessment/assessment.controller.service.ts
@@ -143,12 +143,13 @@ export class AssessmentControllerService {
     assessmentDTO.studentDataGovernmentFundingCosts =
       assessment.workflowData.studentData.governmentFundingCosts;
     const interfaceTotalAssessedCost =
-      assessment.workflowData.calculatedData.interfaceEducationCosts +
-      assessment.workflowData.calculatedData.interfaceChildCareCosts +
-      assessment.workflowData.calculatedData.interfaceTransportationAmount +
-      assessment.workflowData.calculatedData
-        .interfaceAdditionalTransportationAmount;
-    if (interfaceTotalAssessedCost != null) {
+      (assessment.workflowData.calculatedData.interfaceEducationCosts ?? 0) +
+      (assessment.workflowData.calculatedData.interfaceChildCareCosts ?? 0) +
+      (assessment.workflowData.calculatedData.interfaceTransportationAmount ??
+        0) +
+      (assessment.workflowData.calculatedData
+        .interfaceAdditionalTransportationAmount ?? 0);
+    if (interfaceTotalAssessedCost > 0) {
       assessmentDTO.calculatedDataInterfaceTotalAssessedCost =
         interfaceTotalAssessedCost;
     }
