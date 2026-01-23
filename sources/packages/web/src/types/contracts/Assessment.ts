850fa8e2f0fd5e26272643be6874e094cdba0bb9
weskubo-cgi <80127167+weskubo-cgi@users.noreply.github.com>
2026-01-23 11:04:42 -0800

    #5567 - Fixing missed files (#5664)
    ### Overview
    Somehow missed two file updates during a final refactor in the PR,
    causing this issue:
    
    <img width="1752" height="384" alt="image"
    src="https://github.com/user-attachments/assets/d37541e4-4198-456f-bcde-674ec486a214"
    />
    
    Web is building and #5567 functionality is working as expected:
    
    <img width="1072" height="662" alt="image"
    src="https://github.com/user-attachments/assets/4662e67f-a06a-402f-8756-41d74984ecdb"
    />
    
    Co-authored-by: weskubo-cgi <Wesley.Kubo@gov.bc.ca>

--- sources/packages/web/src/types/contracts/Assessment.ts
+++ sources/packages/web/src/types/contracts/Assessment.ts
@@ -7,3 +7,30 @@ export interface AssessmentDetailHeader {
   offeringStudyStartDate: string;
   offeringStudyEndDate: string;
 }
+
+/**
+ * Type of award to be displayed in the awards table.
+ */
+export enum AwardTableType {
+  Estimated = "Estimated",
+  Final = "Final",
+}
+
+/**
+ * Adjustments that were made to an award.
+ */
+export interface AwardAdjustmentAmounts {
+  /**
+   * Value amount already disbursed for the same application and
+   * the same award that was subtracted from the calculated award.
+   */
+  disbursedAmountSubtracted: number;
+  /**
+   * Overaward amount value subtracted from the award calculated.
+   */
+  overawardAmountSubtracted: number;
+  /**
+   * Restriction amount value subtracted from the award calculated.
+   */
+  restrictionAmountSubtracted: number;
+}
