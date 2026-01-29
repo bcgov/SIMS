c161de9532298c5728924717e9d24f584717ed26
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2026-01-28 18:12:09 -0700

    DB Migrations

---
+++ sources/packages/backend/apps/db-migrations/src/sql/Institution/Add-additional-information-cols.sql
@@ -0,0 +1,77 @@
+ALTER TABLE
+    sims.institutions
+ADD
+    COLUMN country CHAR(2),
+ADD
+    COLUMN province CHAR(2),
+ADD
+    COLUMN classification VARCHAR(50),
+ADD
+    COLUMN organization_status VARCHAR(50),
+ADD
+    COLUMN medical_school_status VARCHAR(50);
+
+COMMENT ON COLUMN sims.institutions.country IS 'Country code of the institution country.';
+
+COMMENT ON COLUMN sims.institutions.province IS 'Province code of the institution province if the country is Canada.';
+
+COMMENT ON COLUMN sims.institutions.classification IS 'Classification of the institution (e.g. public or private).';
+
+COMMENT ON COLUMN sims.institutions.organization_status IS 'Organization status of the institution (e.g. profit or non-profit).';
+
+COMMENT ON COLUMN sims.institutions.medical_school_status IS 'Indicates the status of the institution being considered as a medical school.';
+
+-- Add columns to history table.
+ALTER TABLE
+    sims.institutions_history
+ADD
+    COLUMN country CHAR(2),
+ADD
+    COLUMN province CHAR(2),
+ADD
+    COLUMN classification VARCHAR(50),
+ADD
+    COLUMN organization_status VARCHAR(50),
+ADD
+    COLUMN medical_school_status VARCHAR(50);
+
+COMMENT ON COLUMN sims.institutions_history.country IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.institutions_history.province IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.institutions_history.classification IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.institutions_history.organization_status IS 'Historical data from the original table. See original table comments for details.';
+
+COMMENT ON COLUMN sims.institutions_history.medical_school_status IS 'Historical data from the original table. See original table comments for details.';
+
+-- Update the applicable new columns based on the institution type.
+UPDATE
+    sims.institutions
+SET
+    country = CASE
+        -- Update for Canadian institution types(BC Public, BC Private, Out of Province Public, Out of Province Private) to CA.
+        -- Update for United States institution type to US.
+        WHEN institution_type_id IN (1, 2, 3, 7) THEN 'CA'
+        WHEN institution_type_id = 4 THEN 'US'
+        ELSE NULL
+    END,
+    province = CASE
+        -- Update for Canadian BC institution types(BC Public, BC Private) to BC.
+        WHEN institution_type_id IN (1, 2) THEN 'BC'
+        ELSE NULL
+    END,
+    classification = CASE
+        -- Update for Canadian public institution types(BC Public, Out of Province Public) to Public.
+        -- Update for Canadian private institution types(BC Private, Out of Province Private) to Private.
+        WHEN institution_type_id IN (1, 3) THEN 'Public'
+        WHEN institution_type_id IN (2, 7) THEN 'Private'
+        ELSE NULL
+    END,
+    medical_school_status = CASE
+        -- Update for International Medical institution type to Yes.
+        -- Update for International institution type to No.
+        WHEN institution_type_id = 6 THEN 'Yes'
+        WHEN institution_type_id = 5 THEN 'No'
+        ELSE NULL
+    END;
