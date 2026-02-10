37b3c62969af5be03d2c4b6c0adeb9c1969d00d6
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2026-02-10 16:44:00 -0700

    review comment adjustments.

--- sources/packages/backend/apps/api/src/route-controllers/institution/_tests_/e2e/institution.aest.controller.updateInstitution.e2e-spec.ts
+++ sources/packages/backend/apps/api/src/route-controllers/institution/_tests_/e2e/institution.aest.controller.updateInstitution.e2e-spec.ts
@@ -195,41 +195,41 @@ describe("InstitutionAESTController(e2e)-updateInstitution", () => {
       });
   });
 
-  /**
-   * Get the institution update payload with valid data.
-   * @returns institution update payload.
-   */
-  function getUpdatePayload(): InstitutionProfileAPIInDTO {
-    return {
-      operatingName: "Updated Institution operating name",
-      regulatingBody: "icbc",
-      establishedDate: "2023-06-01",
-      primaryEmail: "test@test.ca",
-      primaryPhone: "7785367878",
-      website: "https://www.test.ca",
-      country: CANADA_COUNTRY_CODE,
-      province: BC_PROVINCE_CODE,
-      classification: InstitutionClassification.Public,
-      organizationStatus: InstitutionOrganizationStatus.Profit,
-      medicalSchoolStatus: InstitutionMedicalSchoolStatus.No,
-      primaryContactFirstName: "Primary",
-      primaryContactLastName: "Contact",
-      primaryContactEmail: "test@test.ca",
-      primaryContactPhone: "7785367878",
-      mailingAddress: {
-        addressLine1: "123 Gorge Rd E",
-        addressLine2: "",
-        selectedCountry: "Canada",
-        country: "Canada",
-        city: "Victoria",
-        postalCode: "V1V1V1",
-        provinceState: "BC",
-        canadaPostalCode: "V1V1V1",
-      },
-    };
-  }
-
   afterAll(async () => {
     await app?.close();
   });
 });
+
+/**
+ * Get the institution update payload with valid data.
+ * @returns institution update payload.
+ */
+function getUpdatePayload(): InstitutionProfileAPIInDTO {
+  return {
+    operatingName: "Updated Institution operating name",
+    regulatingBody: "icbc",
+    establishedDate: "2023-06-01",
+    primaryEmail: "test@test.ca",
+    primaryPhone: "7785367878",
+    website: "https://www.test.ca",
+    country: CANADA_COUNTRY_CODE,
+    province: BC_PROVINCE_CODE,
+    classification: InstitutionClassification.Public,
+    organizationStatus: InstitutionOrganizationStatus.Profit,
+    medicalSchoolStatus: InstitutionMedicalSchoolStatus.No,
+    primaryContactFirstName: "Primary",
+    primaryContactLastName: "Contact",
+    primaryContactEmail: "test@test.ca",
+    primaryContactPhone: "7785367878",
+    mailingAddress: {
+      addressLine1: "123 Gorge Rd E",
+      addressLine2: "",
+      selectedCountry: "Canada",
+      country: "Canada",
+      city: "Victoria",
+      postalCode: "V1V1V1",
+      provinceState: "BC",
+      canadaPostalCode: "V1V1V1",
+    },
+  };
+}
