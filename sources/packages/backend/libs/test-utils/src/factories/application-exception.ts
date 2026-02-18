a6b0be8836e0e2c9730a4dca02445b6f89902071
Tiago Graf <tiago.graf@quartech.com>
2026-02-18 10:33:48 -0800

    Update application-exception.ts

--- sources/packages/backend/libs/test-utils/src/factories/application-exception.ts
+++ sources/packages/backend/libs/test-utils/src/factories/application-exception.ts
@@ -5,14 +5,14 @@ import {
   User,
 } from "@sims/sims-db";
 import { createFakeNote } from "./note";
-import { createFakeUser } from "./user";
 
 /**
  * Creates a fake application exception with the passed status.
  * @param relations dependencies:
- * - `creator` student user that created the application,
+ * - `creator` student user that created the application (required if exceptionNote needs a creator).
  * - `assessedBy` ministry user that approved or denied the application exception.
  * @returns a fake application exception.
+ * Note: exceptionNote will only be created if a creator is provided in relations.
  */
 export function createFakeApplicationException(relations?: {
   creator?: User;
@@ -22,11 +22,15 @@ export function createFakeApplicationException(relations?: {
   applicationException.exceptionStatus = ApplicationExceptionStatus.Pending;
   applicationException.assessedDate = new Date();
   applicationException.assessedBy = relations?.assessedBy;
-  const creator = relations?.creator ?? createFakeUser();
-  applicationException.creator = creator;
-  const applicationExceptionNote = createFakeNote(NoteType.Application, {
-    creator,
-  });
-  applicationException.exceptionNote = applicationExceptionNote;
+  applicationException.creator = relations?.creator;
+
+  // Only create exception note if creator is provided (since notes require a creator)
+  if (relations?.creator) {
+    const applicationExceptionNote = createFakeNote(NoteType.Application, {
+      creator: relations.creator,
+    });
+    applicationException.exceptionNote = applicationExceptionNote;
+  }
+
   return applicationException;
 }
