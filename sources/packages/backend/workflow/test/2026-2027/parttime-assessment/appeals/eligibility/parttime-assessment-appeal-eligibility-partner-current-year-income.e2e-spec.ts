f65a1bb49f6810e175f23c6d47fe5c0712d70a71
Carly Cotton <carly.cotton@gov.bc.ca>
2026-02-26 09:44:20 -0800

    Parttime test conversion.

--- sources/packages/backend/workflow/test/2026-2027/parttime-assessment/appeals/eligibility/parttime-assessment-appeal-eligibility-partner-current-year-income.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2026-2027/parttime-assessment/appeals/eligibility/parttime-assessment-appeal-eligibility-partner-current-year-income.e2e-spec.ts
@@ -1,84 +1,66 @@
 import { PROGRAM_YEAR } from "../../../constants/program-year.constants";
 import {
   ZeebeMockedClient,
-  createFakeConsolidatedPartTimeData,
+  createFakePartTimeAssessmentConsolidatedData,
   executePartTimeAssessmentForProgramYear,
 } from "../../../../test-utils";
+import { YesNoOptions } from "@sims/test-utils";
+import { RelationshipStatusType } from "workflow/test/models";
 
 describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-appeal-eligibility-partner-current-year-income.`, () => {
-  it("Should evaluate the partner current year income appeal as eligible when the student is married.", async () => {
-    // Arrange
-    const assessmentConsolidatedData =
-      createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-    assessmentConsolidatedData.studentDataRelationshipStatus = "married";
-    assessmentConsolidatedData.partner1TotalIncome = 100002;
-    // Act
-    const calculatedAssessment = await executePartTimeAssessmentForProgramYear(
-      PROGRAM_YEAR,
-      assessmentConsolidatedData,
-    );
+  const appealEligibilityScenarios = [
+    {
+      studentDataRelationshipStatus: "married",
+      expectedEligibility: true,
+    },
+    {
+      studentDataRelationshipStatus: "single",
+      expectedEligibility: false,
+    },
+    {
+      studentDataRelationshipStatus: "other",
+      expectedEligibility: false,
+    },
+    {
+      studentDataRelationshipStatus: "marriedUnable",
+      expectedEligibility: false,
+    },
+  ];
+  for (const {
+    studentDataRelationshipStatus,
+    expectedEligibility,
+  } of appealEligibilityScenarios) {
+    it(`Should evaluate the partner current year income appeal as ${expectedEligibility ? "eligible" : "not eligible"} when the student is ${studentDataRelationshipStatus}.`, async () => {
+      // Arrange
+      const assessmentConsolidatedData =
+        createFakePartTimeAssessmentConsolidatedData(PROGRAM_YEAR);
+      assessmentConsolidatedData.studentDataRelationshipStatus =
+        studentDataRelationshipStatus as RelationshipStatusType;
+      // Needed for married students
+      assessmentConsolidatedData.partner1TotalIncome = 100002;
+      assessmentConsolidatedData.partner1PartnerCaringForDependant =
+        YesNoOptions.No;
+      assessmentConsolidatedData.partner1HasEmploymentInsuranceBenefits =
+        YesNoOptions.No;
+      assessmentConsolidatedData.partner1HasTotalIncomeAssistance =
+        YesNoOptions.No;
+      assessmentConsolidatedData.partner1HasFedralProvincialPDReceipt =
+        YesNoOptions.No;
 
-    // Assert
-    expect(
-      calculatedAssessment.variables
-        .isEligibleForPartnerCurrentYearIncomeAppeal,
-    ).toBe(true);
-  });
+      // Act
+      const calculatedAssessment =
+        await executePartTimeAssessmentForProgramYear(
+          PROGRAM_YEAR,
+          assessmentConsolidatedData,
+        );
 
-  it("Should evaluate the partner current year income appeal as not eligible when the student is single.", async () => {
-    // Arrange
-    const assessmentConsolidatedData =
-      createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-    assessmentConsolidatedData.studentDataRelationshipStatus = "single";
-    // Act
-    const calculatedAssessment = await executePartTimeAssessmentForProgramYear(
-      PROGRAM_YEAR,
-      assessmentConsolidatedData,
-    );
-
-    // Assert
-    expect(
-      calculatedAssessment.variables
-        .isEligibleForPartnerCurrentYearIncomeAppeal,
-    ).toBe(false);
-  });
-
-  it("Should evaluate the partner current year income appeal as not eligible when the student relationship status is other.", async () => {
-    // Arrange
-    const assessmentConsolidatedData =
-      createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-    assessmentConsolidatedData.studentDataRelationshipStatus = "other";
-    // Act
-    const calculatedAssessment = await executePartTimeAssessmentForProgramYear(
-      PROGRAM_YEAR,
-      assessmentConsolidatedData,
-    );
-
-    // Assert
-    expect(
-      calculatedAssessment.variables
-        .isEligibleForPartnerCurrentYearIncomeAppeal,
-    ).toBe(false);
-  });
-
-  it("Should evaluate the partner current year income appeal as not eligible when the student relationship status is unable to report.", async () => {
-    // Arrange
-    const assessmentConsolidatedData =
-      createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-    assessmentConsolidatedData.studentDataRelationshipStatus = "marriedUnable";
-    // Act
-    const calculatedAssessment = await executePartTimeAssessmentForProgramYear(
-      PROGRAM_YEAR,
-      assessmentConsolidatedData,
-    );
-
-    // Assert
-    expect(
-      calculatedAssessment.variables
-        .isEligibleForPartnerCurrentYearIncomeAppeal,
-    ).toBe(false);
-  });
-
+      // Assert
+      expect(
+        calculatedAssessment.variables
+          .isEligibleForPartnerCurrentYearIncomeAppeal,
+      ).toBe(expectedEligibility);
+    });
+  }
   afterAll(async () => {
     // Closes the singleton instance created during test executions.
     await ZeebeMockedClient.getMockedZeebeInstance().close();
