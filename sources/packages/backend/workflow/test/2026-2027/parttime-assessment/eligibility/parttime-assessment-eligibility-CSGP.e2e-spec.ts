fcbbbb64b597e7c96bcd2f731929339957934802
Carly Cotton <carly.cotton@gov.bc.ca>
2026-04-07 14:52:43 -0700

    Remainder of PT Award eligibility and tests

--- sources/packages/backend/workflow/test/2026-2027/parttime-assessment/eligibility/parttime-assessment-eligibility-CSGP.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2026-2027/parttime-assessment/eligibility/parttime-assessment-eligibility-CSGP.e2e-spec.ts
@@ -4,10 +4,15 @@ import {
   createFakeConsolidatedPartTimeData,
   executePartTimeAssessmentForProgramYear,
 } from "../../../test-utils";
+import {
+  InstitutionClassification,
+  InstitutionOrganizationStatus,
+} from "@sims/sims-db";
+import { Provinces } from "@sims/test-utils";
 
 describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSGP.`, () => {
   it(
-    "Should determine CSGP as eligible when total assessed need is greater than or equal to 1 " +
+    "Should determine CSGP as assessment eligible when total assessed need is greater than or equal to 1 " +
       "and application PD/PPD status is true and income is below the limitAwardCSGPThresholdIncome.",
     async () => {
       // Arrange
@@ -24,14 +29,13 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSGP
         );
 
       // Assert
-      expect(calculatedAssessment.variables.awardEligibilityCSGP).toBe(true);
-      expect(
-        calculatedAssessment.variables.finalFederalAwardNetCSGPAmount,
-      ).toBeGreaterThan(0);
+      expect(calculatedAssessment.variables.assessmentEligibilityCSGP).toBe(
+        true,
+      );
     },
   );
 
-  it("Should determine CSGP as not eligible when application PD/PPD status is not 'yes'.", async () => {
+  it("Should determine CSGP as not assessment eligible when application PD/PPD status is not 'yes'.", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -44,13 +48,15 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSGP
     );
 
     // Assert
-    expect(calculatedAssessment.variables.awardEligibilityCSGP).toBe(false);
+    expect(calculatedAssessment.variables.assessmentEligibilityCSGP).toBe(
+      false,
+    );
     expect(calculatedAssessment.variables.finalFederalAwardNetCSGPAmount).toBe(
       0,
     );
   });
 
-  it("Should determine CSGP as not eligible when income is above the limitAwardCSGPThresholdIncome", async () => {
+  it("Should determine CSGP as not assessment eligible when income is above the limitAwardCSGPThresholdIncome", async () => {
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
@@ -64,11 +70,91 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-eligibility-CSGP
     );
 
     // Assert
-    expect(calculatedAssessment.variables.awardEligibilityCSGP).toBe(false);
+    expect(calculatedAssessment.variables.assessmentEligibilityCSGP).toBe(
+      false,
+    );
     expect(calculatedAssessment.variables.finalFederalAwardNetCSGPAmount).toBe(
       0,
     );
   });
+
+  const TEST_AWARD_ELIGIBILITY = [
+    {
+      inputData: {
+        institutionProvince: Provinces.BritishColumbia,
+        institutionCountry: "CA",
+        institutionClassification: InstitutionClassification.Public,
+        institutionOrganizationStatus:
+          InstitutionOrganizationStatus.NotForProfit,
+        studentDataApplicationPDPPDStatus: "yes",
+      },
+      expectedData: {
+        expectedAssessmentEligibility: true,
+        expectedInstitutionEligibility: true,
+        expectedAwardEligibility: true,
+      },
+    },
+    {
+      inputData: {
+        institutionCountry: "AU",
+        institutionClassification: InstitutionClassification.Private,
+        institutionOrganizationStatus: InstitutionOrganizationStatus.Profit,
+        institutionProvince: undefined,
+        studentDataApplicationPDPPDStatus: "yes",
+      },
+      expectedData: {
+        expectedAssessmentEligibility: true,
+        expectedInstitutionEligibility: false,
+        expectedAwardEligibility: false,
+      },
+    },
+    {
+      inputData: {
+        institutionProvince: Provinces.BritishColumbia,
+        institutionCountry: "CA",
+        institutionClassification: InstitutionClassification.Public,
+        institutionOrganizationStatus:
+          InstitutionOrganizationStatus.NotForProfit,
+        studentDataApplicationPDPPDStatus: "no", // Ensures that the PD status is false which makes the assessment ineligible.
+      },
+      expectedData: {
+        expectedAssessmentEligibility: false,
+        expectedInstitutionEligibility: true,
+        expectedAwardEligibility: false,
+      },
+    },
+  ];
+  for (const testEligibility of TEST_AWARD_ELIGIBILITY) {
+    it(
+      `Should determine CSGP as ${testEligibility.expectedData.expectedAwardEligibility ? "eligible" : "not eligible"} when the assessment is ${testEligibility.expectedData.expectedAssessmentEligibility ? "eligible" : "not eligible"} and ` +
+        `institution is ${testEligibility.expectedData.expectedInstitutionEligibility ? "eligible" : "not eligible"}.`,
+      async () => {
+        // Arrange
+        const assessmentConsolidatedData = {
+          ...createFakeConsolidatedPartTimeData(PROGRAM_YEAR),
+          ...testEligibility.inputData,
+        };
+
+        // Act
+        const calculatedAssessment =
+          await executePartTimeAssessmentForProgramYear(
+            PROGRAM_YEAR,
+            assessmentConsolidatedData,
+          );
+        // Assert
+        expect(calculatedAssessment.variables.assessmentEligibilityCSGP).toBe(
+          testEligibility.expectedData.expectedAssessmentEligibility,
+        );
+        expect(
+          calculatedAssessment.variables.dmnPartTimeAwardInstitutionEligibility
+            .isEligibleCSGP,
+        ).toBe(testEligibility.expectedData.expectedInstitutionEligibility);
+        expect(calculatedAssessment.variables.awardEligibilityCSGP).toBe(
+          testEligibility.expectedData.expectedAwardEligibility,
+        );
+      },
+    );
+  }
 
   afterAll(async () => {
     // Closes the singleton instance created during test executions.
