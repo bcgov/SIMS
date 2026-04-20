68f2040e80193513a82407585fbb2548de8037c6
Shashank Shekhar <sh16011993@gmail.com>
2026-04-20 07:46:57 -0700

    #5887 - SFA Eligibility for International Institutions

---
+++ sources/packages/backend/apps/db-migrations/src/sql/DynamicFormConfigurations/Insert-sfa-eligibility-international-institutions.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+  sims.dynamic_form_configurations (
+    form_type,
+    form_definition_name,
+    form_category,
+    form_description,
+    has_application_scope,
+    allow_bundled_submission
+  )
+VALUES
+  (
+    'SFA eligibility international institutions',
+    'ptsfaeligibilityinternationalinstitutions',
+    'Student appeal',
+    'Request to determine eligibility for SFA for international institutions.',
+    TRUE,
+    TRUE
+  );
