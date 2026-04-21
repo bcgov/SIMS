d36ae6bbd71907b16e5d55a0e7927d53a85bac78
andrewsignori-aot <andrew.signori@aot-technologies.com>
2026-04-21 09:20:31 -0700

    Fix migration

--- sources/packages/backend/apps/db-migrations/src/sql/DynamicFormConfigurations/Add-authorization-key-column.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/DynamicFormConfigurations/Add-authorization-key-column.sql
@@ -8,6 +8,14 @@ COMMENT ON COLUMN sims.dynamic_form_configurations.authorization_key IS 'Identif
 UPDATE
     sims.dynamic_form_configurations
 SET
+    authorization_key = 'accessibility-grant-eligibility-appeal'
+WHERE
+    form_definition_name = 'ptaccessibilitygranteligibilityappeal';
+
+UPDATE
+    sims.dynamic_form_configurations
+SET
     authorization_key = REPLACE(LOWER(form_type), ' ', '-')
 WHERE
-    form_category IN ('Student appeal', 'Student form');
+    form_category IN ('Student appeal', 'Student form')
+    AND authorization_key IS NULL;
