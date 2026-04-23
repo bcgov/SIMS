ecdf30bbbfc15f9ada9c49d242ddf28ec865d5e4
andrewsignori-aot <andrew.signori@aot-technologies.com>
2026-04-23 10:47:12 -0700

    Added not null constraint

--- sources/packages/backend/apps/db-migrations/src/sql/DynamicFormConfigurations/Add-authorization-key-column.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/DynamicFormConfigurations/Add-authorization-key-column.sql
@@ -22,3 +22,17 @@ SET
 WHERE
     form_category IN ('Student appeal', 'Student form')
     AND authorization_key IS NULL;
+
+-- Enforce that authorization_key is provided for student appeals and student forms.
+ALTER TABLE
+    sims.dynamic_form_configurations
+ADD
+    CONSTRAINT dynamic_form_configurations_authorization_key_not_null_constraint CHECK (
+        form_category NOT IN ('Student appeal', 'Student form')
+        OR (
+            authorization_key IS NOT NULL
+            AND LENGTH(authorization_key) >= 1
+        )
+    );
+
+COMMENT ON CONSTRAINT dynamic_form_configurations_authorization_key_not_null_constraint ON sims.dynamic_form_configurations IS 'Ensures that authorization_key is provided and non-empty for Student appeal and Student form categories.';
