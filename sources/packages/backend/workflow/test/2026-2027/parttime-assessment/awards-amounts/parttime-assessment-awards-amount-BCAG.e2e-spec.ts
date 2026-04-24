66972fc1b7fb279fca8908c459b7b1e9c4c4519b
Shashank Shekhar <sh16011993@gmail.com>
2026-04-24 07:24:00 -0700

    Revert "Code Update"
    This reverts commit b8be3e14245c563d7f24faa8bfa4e1391b6565df.

--- sources/packages/backend/workflow/test/2026-2027/parttime-assessment/awards-amounts/parttime-assessment-awards-amount-BCAG.e2e-spec.ts
+++ sources/packages/backend/workflow/test/2026-2027/parttime-assessment/awards-amounts/parttime-assessment-awards-amount-BCAG.e2e-spec.ts
@@ -13,7 +13,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-BC
     // Arrange
     const assessmentConsolidatedData =
       createFakeConsolidatedPartTimeData(PROGRAM_YEAR);
-    assessmentConsolidatedData.studentDataCRAReportedIncome = 23318;
+    assessmentConsolidatedData.studentDataCRAReportedIncome = 24412;
     assessmentConsolidatedData.studentDataRelationshipStatus = "married";
     assessmentConsolidatedData.partner1CRAReportedIncome = 30000;
     // Act
@@ -22,7 +22,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-BC
       assessmentConsolidatedData,
     );
     // Assert
-    // For a family of 2, limitAwardBCAGIncomeCap is 53318.
+    // For a family of 2, limitAwardBCAGIncomeCap is 54412.
     expect(
       calculatedAssessment.variables.calculatedDataTotalFamilyIncome,
     ).toBeLessThanOrEqual(
@@ -30,7 +30,7 @@ describe(`E2E Test Workflow parttime-assessment-${PROGRAM_YEAR}-awards-amount-BC
         .limitAwardBCAGIncomeCap,
     );
     expect(calculatedAssessment.variables.calculatedDataTotalFamilyIncome).toBe(
-      53318,
+      54412,
     );
     expect(calculatedAssessment.variables.provincialAwardBCAGAmount).toBe(
       calculatedAssessment.variables.dmnPartTimeAwardAllowableLimits
