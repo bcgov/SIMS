dfff91a3c0a1afa8f89688f8258a48c178adc97f
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2026-04-24 14:57:38 -0700

    Migration updates for forms/appeals roles.

--- sources/packages/backend/apps/db-migrations/src/migrations/1777067691103-DynamicFormConfigInsertFTSFAInternationalEligibilityAppeal.ts
+++ sources/packages/backend/apps/db-migrations/src/migrations/1777067691103-DynamicFormConfigInsertFTSFAInternationalEligibilityAppeal.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 import { getSQLFileData } from "../utilities/sqlLoader";
 
-export class DynamicFormConfigInsertFTSFAInternationalEligibilityAppeal1776874641285 implements MigrationInterface {
+export class DynamicFormConfigInsertFTSFAInternationalEligibilityAppeal1777067691103 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       getSQLFileData(
