ff297f37d60fce9740a75d5589c783190e1897f3
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2026-05-07 15:48:17 -0700

    #5635 - Formio Spring Upgrade - Backward Compatible Changes (#6109)
    - Started using
    [API_KEYS](https://help.form.io/userguide/projects/project-settings#api-keys)
    instead of using user/pass/token authentication in the server-to-server
    communication.
      - Kept user and pass only for the server deployment.
    - Forms deployment adjusted to stop using the JWT token.
    - API forms service refactored to stop using the JWT token.
    - New GitHub env secret `FORMS_API_KEY` to be added to the release
    instructions.
    - Please note, the form.io secrets entries are prefixed with `FORMIO_*`,
    hence the secret name was defined as `FORMIO_API_KEY` while the env
    variable was defined as `FORMS_API_KEY`, following the existing ones.

--- sources/packages/backend/libs/utilities/src/config/config.service.ts
+++ sources/packages/backend/libs/utilities/src/config/config.service.ts
@@ -69,10 +69,7 @@ export class ConfigService {
   get forms(): FormsConfig {
     return this.getCachedConfig("formsConfig", {
       formsUrl: process.env.FORMS_URL,
-      serviceAccountCredential: {
-        userName: process.env.FORMS_SA_USER_NAME,
-        password: process.env.FORMS_SA_PASSWORD,
-      },
+      apiKey: process.env.FORMS_API_KEY,
     });
   }
 
