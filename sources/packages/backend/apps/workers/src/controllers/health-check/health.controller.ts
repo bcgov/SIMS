83de066f791ed3c3accce7ea7fc66dffcbb84362
Tiago Graf <tiago.graf@quartech.com>
2026-05-07 14:29:49 -0700

    #5896 - Add configuration for health check timeout (#6111)
    ### Summary
    
    Makes the health check timeout configurable instead of always default
    (1000ms)
    
    The healthcheck controller endpoint receives the timeout as a parameter
    /health/timeout/<timeoutInMS>, the docker configuration set its
    hardcoded timeouts to 1500ms for readiness (/health/timeout/1500) and
    3000ms for liveness (/health/timeout/3000)

--- sources/packages/backend/apps/workers/src/controllers/health-check/health.controller.ts
+++ sources/packages/backend/apps/workers/src/controllers/health-check/health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
 import {
   HealthCheck,
   HealthCheckService,
@@ -22,15 +22,17 @@ export class HealthController {
 
   /**
    * Check the health of the workers.
+   * @param timeout amount of milliseconds for the health checks.
    * @returns the status of the health for workers, with info or error and details.
    */
-  @Get()
+  @Get("timeout/:timeout")
   @HealthCheck()
-  async check() {
+  async check(@Param("timeout", ParseIntPipe) timeout: number) {
     return this.healthCheckService.check([
       () =>
         this.typeOrmHealthIndicator.pingCheck("sims-db", {
           connection: this.dataSource,
+          timeout,
         }),
       () => this.zeebeHealthIndicator.check("zeebe"),
     ]);
