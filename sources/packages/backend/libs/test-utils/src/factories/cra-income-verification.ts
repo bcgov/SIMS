df41ca44ab57675f1b5b06d11c900239b67c2395
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2026-05-12 15:57:51 -0700

    Missed file.

--- sources/packages/backend/libs/test-utils/src/factories/cra-income-verification.ts
+++ sources/packages/backend/libs/test-utils/src/factories/cra-income-verification.ts
@@ -31,12 +31,18 @@ export function createFakeCRAIncomeVerification(
     relations?.application ?? createFakeApplication(relations);
   craIncomeVerification.taxYear = options?.initialValues?.taxYear ?? 2022;
   craIncomeVerification.craReportedIncome =
-    options?.initialValues?.craReportedIncome ?? null;
+    options?.initialValues?.craReportedIncome ?? undefined;
   craIncomeVerification.reportedIncome =
     options?.initialValues?.reportedIncome ?? 5000;
   craIncomeVerification.application = application;
-  craIncomeVerification.supportingUser = relations?.supportingUser ?? null;
+  craIncomeVerification.supportingUser = relations?.supportingUser ?? undefined;
   craIncomeVerification.dateReceived =
-    options?.initialValues?.dateReceived ?? null;
+    options?.initialValues?.dateReceived ?? undefined;
+  craIncomeVerification.fileReceived =
+    options?.initialValues?.fileReceived ?? undefined;
+  craIncomeVerification.dateSent =
+    options?.initialValues?.dateSent ?? undefined;
+  craIncomeVerification.fileSent =
+    options?.initialValues?.fileSent ?? undefined;
   return craIncomeVerification;
 }
