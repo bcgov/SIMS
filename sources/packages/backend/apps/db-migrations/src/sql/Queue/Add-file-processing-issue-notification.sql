92e67219aca4c78f1ae6ab997ab0507834a3273b
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2026-05-13 14:15:45 -0700

    Updated cron schedule to match AC.

--- sources/packages/backend/apps/db-migrations/src/sql/Queue/Add-file-processing-issue-notification.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/Queue/Add-file-processing-issue-notification.sql
@@ -1,11 +1,11 @@
--- The scheduler is set to run at 7 a.m. UTC which is 12 a.m. PDT every day.
+-- The scheduler is set to run at 2 p.m. UTC which is 7 a.m. PDT M-F.
 INSERT INTO
   sims.queue_configurations(queue_name, queue_configuration, queue_settings)
 VALUES
   (
     'file-processing-issue-notification',
     '{
-        "cron": "0 7 * * *",
+        "cron": "0 14 * * 1-5",
         "retry": 3,
         "cleanUpPeriod": 2592000000,
         "retryInterval": 180000,
