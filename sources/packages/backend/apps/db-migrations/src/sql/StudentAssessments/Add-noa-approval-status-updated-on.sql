0c850e28a819ee530621c1c3f8ca4ac5da4b4605
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2026-05-21 14:32:52 -0700

    Added migration script.

--- sources/packages/backend/apps/db-migrations/src/sql/StudentAssessments/Add-noa-approval-status-updated-on.sql
+++ sources/packages/backend/apps/db-migrations/src/sql/StudentAssessments/Add-noa-approval-status-updated-on.sql
@@ -5,10 +5,18 @@ ADD
 
 COMMENT ON COLUMN sims.student_assessments.noa_approval_status_updated_on IS 'Date and time when the noa_approval_status column was updated.';
 
--- TODO Add migration if required
--- UPDATE
---     sims.student_assessments sa
--- SET
---     noa_approval_status_updated_on = NOW()
--- WHERE
---     noa_approval_status = 'Required';
+-- Migrate noa_approval_status_updated_on where noa_approval_status is 'Required' using application_status_updated_on since it is set right before the noa_approval_status is updated to 'Required'.
+-- This will allow existing Assessments to get notified according to schedule.
+UPDATE
+    sims.student_assessments sa
+SET
+    noa_approval_status_updated_on = (
+        SELECT
+            application_status_updated_on
+        FROM
+            sims.applications
+        WHERE
+            current_assessment_id = sa.id
+    )
+WHERE
+    sa.noa_approval_status = 'Required';
