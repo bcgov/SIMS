52c6d01e0b2d8f2f45eb769e9830731a37bb07c0
Tiago Graf <tiago.graf@quartech.com>
2026-07-03 16:36:10 -0700

    Update index.ts

--- sources/packages/backend/apps/api/src/utilities/class-validation/index.ts
+++ sources/packages/backend/apps/api/src/utilities/class-validation/index.ts
@@ -7,7 +7,6 @@ export * from "./custom-validators/period-min-length";
 export * from "./custom-validators/period-max-length";
 export * from "./validation-utils";
 export * from "./custom-validators/is-date-after";
-export * from "./custom-validators/is-date-after-today";
 export * from "./custom-validators/json-max-size";
 export * from "./custom-validators/is-max-cost-value";
 export * from "./custom-validators/allow-if";
