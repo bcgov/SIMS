7ed482d03035ae693bd033f2e19c1c209a34804b
weskubo-cgi <80127167+weskubo-cgi@users.noreply.github.com>
2026-07-02 22:24:24 -0700

    #5542 - PD Banner updates (#6321)
    ## Summary
    - Update PD banner on 2025-2026 PT and FT applications to include
    updated content and document link.
    - Update PD button on Student Profile to included updated content and
    document link.
    
    ## Screenshots
    ### Student -> Student Profile
    <img width="1001" height="610" alt="image"
    src="https://github.com/user-attachments/assets/8b1a1a8d-0881-4747-be32-39d0a11cb9c8"
    />
    
    ### Student -> PT 2026-2027 application
    <img width="1414" height="622" alt="image"
    src="https://github.com/user-attachments/assets/e7796830-5989-403a-bd2d-1e5f203b4f75"
    />
    
    ---------
    
    Co-authored-by: weskubo-cgi <Wesley.Kubo@gov.bc.ca>

--- sources/packages/web/src/components/students/modals/PDStatusApplicationModal.vue
+++ sources/packages/web/src/components/students/modals/PDStatusApplicationModal.vue
@@ -1,51 +1,58 @@
 <template>
   <modal-dialog-base
     title="Apply for disability status"
-    :showDialog="showDialog"
+    :show-dialog="showDialog"
   >
-    <template v-slot:content>
-      <p class="mt-4">
-        If you are a student with a permanent disability, or persistent or
-        prolonged disability, you may be eligible for disability grants and
-        bursaries. You will need to submit an Appendix 8 form to apply for
-        disability status. You can find more information on the Appendix 8 and
-        the application process
-        <a
-          rel="noopener"
-          target="_blank"
-          class="formio-href"
-          href="https://studentaidbc.ca/sites/all/files/form-library/appendix_8-parttime.pdf"
-          >here</a
-        >.
-      </p>
+    <template #content>
+      <p>To apply for disability status please follow these instructions:</p>
+      <ul>
+        <li>
+          Download the
+          <a
+            rel="noopener"
+            target="_blank"
+            class="formio-href"
+            href="https://studentaidbc.ca/sites/all/files/form-library/disability-verification.pdf"
+            >Disability Verification Form</a
+          >
+          and take it to a qualified medical assessor for completion.
+        </li>
+        <li>
+          Visit the Forms area near the top right of your Student Account main
+          page.
+        </li>
+        <li>
+          Select Student Forms > Standard Forms > Disability status application.
+        </li>
+        <li>
+          Upload the completed form to the Upload supporting documents area
+          within the Disability status application.
+        </li>
+        <li>
+          Complete your Declaration and submit your Disability status
+          application for review.
+        </li>
+      </ul>
     </template>
-    <template v-slot:footer>
+    <template #footer>
       <footer-buttons
-        primaryLabel="Close"
-        @primaryClick="resolvePromise(false)"
-        :showSecondaryButton="false"
+        primary-label="Close"
+        @primary-click="resolvePromise(false)"
+        :show-secondary-button="false"
       />
     </template>
   </modal-dialog-base>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import ModalDialogBase from "@/components/generic/ModalDialogBase.vue";
 import { useModalDialog } from "@/composables";
 
-export default defineComponent({
-  components: {
-    ModalDialogBase,
-  },
-  setup() {
-    const { showDialog, showModal, resolvePromise } = useModalDialog<boolean>();
+const { showDialog, showModal, resolvePromise } = useModalDialog<boolean>();
 
-    return {
-      resolvePromise,
-      showDialog,
-      showModal,
-    };
-  },
+defineExpose({
+  resolvePromise,
+  showDialog,
+  showModal,
 });
 </script>
