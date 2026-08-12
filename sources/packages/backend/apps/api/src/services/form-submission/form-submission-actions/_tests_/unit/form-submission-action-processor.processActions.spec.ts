ceac808eb0e535fd8e9e818100f7cc9aed03a549
andrewsignori-aot <andrew.signori@aot-technologies.com>
2026-08-12 09:43:28 -0700

    Reverting E2E test

--- sources/packages/backend/apps/api/src/services/form-submission/form-submission-actions/_tests_/unit/form-submission-action-processor.processActions.spec.ts
+++ sources/packages/backend/apps/api/src/services/form-submission/form-submission-actions/_tests_/unit/form-submission-action-processor.processActions.spec.ts
@@ -1,5 +1,4 @@
-import type { Mocked } from "@suites/doubles.jest";
-import { TestBed } from "@suites/unit";
+import { Mocked, TestBed } from "@suites/unit";
 import { EntityManager } from "typeorm";
 import { FormSubmissionActionProcessor } from "../../../form-submission-actions/form-submission-action-processor";
 import { FormSubmissionCreateAppealAssessmentAction } from "../../../form-submission-actions/form-submission-create-appeal-assessment-action";
@@ -30,18 +29,12 @@ describe("FormSubmissionActionProcessor-processActions", () => {
     );
     // Allow setting the action types directly to avoid spying on getters
     // that would not work as expected for protected properties.
-    (
-      formSubmissionCreateAppealAssessmentAction as unknown as Record<
-        string,
-        unknown
-      >
-    )["actionType"] = FormSubmissionActionType.CreateStudentAppealAssessment;
-    (
-      formSubmissionUpdateModifiedIndependentAction as unknown as Record<
-        string,
-        unknown
-      >
-    )["actionType"] = FormSubmissionActionType.UpdateModifiedIndependent;
+    (formSubmissionCreateAppealAssessmentAction as Record<string, unknown>)[
+      "actionType"
+    ] = FormSubmissionActionType.CreateStudentAppealAssessment;
+    (formSubmissionUpdateModifiedIndependentAction as Record<string, unknown>)[
+      "actionType"
+    ] = FormSubmissionActionType.UpdateModifiedIndependent;
   });
 
   beforeEach(() => {
