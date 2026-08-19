ecd5dae74cd8a2659d8e69ef1000886a6631139f
Tiago Graf <tiago.graf@quartech.com>
2026-08-18 18:01:55 -0700

    Update data-source.ts

--- sources/packages/backend/libs/sims-db/src/data-source.ts
+++ sources/packages/backend/libs/sims-db/src/data-source.ts
@@ -168,6 +168,7 @@ function getORMCacheConfig(): ORMCacheConfig | false {
           connectTimeout: ORM_CACHE_REDIS_CONNECT_TIMEOUT,
           maxRetriesPerRequest: ORM_CACHE_REDIS_MAX_RETRIES_PER_REQUEST,
         },
+        enableOfflineQueue: false, // Fail cache commands immediately while disconnected.
         clusterRetryStrategy: retryStrategy,
       },
     },
