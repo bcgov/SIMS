aba19010826ed12ffb34965a4027810d9a0b57bc
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2026-08-20 16:16:26 -0600

    Updated the study breaks logic to cap at 52 weeks.

---
+++ sources/packages/backend/apps/db-migrations/src/sql/EducationProgramsOfferings/Rollback-update-total-funded-weeks.sql
@@ -0,0 +1,18 @@
+-- Update the study_breaks JSONB column to restore the original totalFundedWeeks value before the previous update.
+-- Update is only applied to rows where the study_breaks_before_update column is not null
+-- indicating that the total funded weeks were updated only on these rows where the study_breaks_before_update was populated with the original value before the update.
+UPDATE
+    sims.education_programs_offerings
+SET
+    study_breaks = jsonb_set(
+        study_breaks,
+        '{totalFundedWeeks}',
+        to_jsonb(
+            (study_breaks_before_update -> 'totalFundedWeeks') :: SMALLINT
+        ),
+        false
+    )
+WHERE
+    study_breaks_before_update IS NOT NULL;
+
+-- The column study_breaks_before_update is not dropped to be available for any investigation if required.
