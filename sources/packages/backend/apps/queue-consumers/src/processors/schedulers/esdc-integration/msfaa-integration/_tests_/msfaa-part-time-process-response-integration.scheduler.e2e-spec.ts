ce8b72f0ce66fa50d1183e7c6f4a676c1f749ae7
andrewsignori-aot <andrew.signori@aot-technologies.com>
2026-09-10 16:53:33 -0700

    E2E fixes, work in progress

--- sources/packages/backend/apps/queue-consumers/src/processors/schedulers/esdc-integration/msfaa-integration/_tests_/msfaa-part-time-process-response-integration.scheduler.e2e-spec.ts
+++ sources/packages/backend/apps/queue-consumers/src/processors/schedulers/esdc-integration/msfaa-integration/_tests_/msfaa-part-time-process-response-integration.scheduler.e2e-spec.ts
@@ -179,10 +179,9 @@ describe(
           templateId: true,
           recipients: true,
           messageContent: true,
-          notificationMessage: { templateId: true },
           user: { email: true, firstName: true, lastName: true },
         },
-        relations: { notificationMessage: true, user: true },
+        relations: { user: true },
         where: {
           notificationMessage: {
             id: notificationMessageType,
@@ -192,23 +191,29 @@ describe(
           },
         },
       });
-      expect(notification.dateSent).toBe(null);
-      expect(notification.messagePayload).toStrictEqual({
-        email_address: notification.user.email,
-        template_id: notification.notificationMessage.templateId,
-        personalisation: {
-          lastName: notification.user.lastName,
-          givenNames: notification.user.firstName,
+      expect(notification).toEqual({
+        id: expect.any(Number),
+        dateSent: null,
+        messagePayload: {
+          email_address: notification.user.email,
+          template_id: "b3093a44-da3d-4ea5-af3a-1542535ae7e9",
+          personalisation: {
+            lastName: notification.user.lastName,
+            givenNames: notification.user.firstName,
+          },
         },
-      });
-      expect(notification.templateId).toBe(
-        notification.notificationMessage.templateId,
-      );
-      expect(notification.recipients).toStrictEqual([notification.user.email]);
-      expect(notification.messageContent).toStrictEqual({
-        params: {
+        templateId: "9d08fc5d-65c8-4be7-960b-a72b21298b71",
+        recipients: [notification.user.email],
+        messageContent: {
+          params: {
+            lastName: notification.user.lastName,
+            givenNames: notification.user.firstName,
+          },
+        },
+        user: {
+          email: notification.user.email,
+          firstName: notification.user.firstName,
           lastName: notification.user.lastName,
-          givenNames: notification.user.firstName,
         },
       });
     });
