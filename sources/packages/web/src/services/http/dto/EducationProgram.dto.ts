61db4158d56d113c2cb77a94e3f07a86ec9ffd84
Dheepak Ramanathan <dheepak.rr@aot-technologies.com>
2026-09-18 15:28:34 -0600

    Removing obsolete model

--- sources/packages/web/src/services/http/dto/EducationProgram.dto.ts
+++ sources/packages/web/src/services/http/dto/EducationProgram.dto.ts
@@ -87,49 +87,6 @@ export interface EducationProgramsSummaryAPIOutDTO {
   isExpired: boolean;
 }
 
-export interface ProgramFormModel {
-  name: string;
-  description?: string;
-  credentialType: string;
-  cipCode: string;
-  nocCode?: string;
-  sabcCode?: string;
-  regulatoryBody: string;
-  otherRegulatoryBody?: string;
-  programDeliveryTypes: ProgramDeliveryTypes;
-  deliveredOnlineAlsoOnsite?: string;
-  sameOnlineCreditsEarned?: string;
-  earnAcademicCreditsOtherInstitution?: string;
-  courseLoadCalculation: string;
-  completionYears: string;
-  eslEligibility: string;
-  hasJointInstitution: string;
-  hasJointDesignatedInstitution?: string;
-  programIntensity: ProgramIntensity;
-  institutionProgramCode?: string;
-  minHoursWeek?: string;
-  isAviationProgram?: string;
-  minHoursWeekAvi?: string;
-  entranceRequirements: EntranceRequirements;
-  hasWILComponent: string;
-  isWILApproved?: string;
-  wilProgramEligibility?: string;
-  hasTravel: string;
-  travelProgramEligibility?: string;
-  hasIntlExchange: string;
-  intlExchangeProgramEligibility?: string;
-  programDeclaration: boolean;
-  fieldOfStudyCode: number;
-  credentialTypesAviation?: AviationProgramCredentialTypes;
-  isBCPrivate: boolean;
-  isBCPublic: boolean;
-  convertedValues: {
-    programDeliveryTypes: ProgramDeliveryTypeValues[];
-    entranceRequirements: string[];
-    credentialTypesAviation: string[];
-  };
-}
-
 /**
  * Complete program information used to create or
  * update an education program.
